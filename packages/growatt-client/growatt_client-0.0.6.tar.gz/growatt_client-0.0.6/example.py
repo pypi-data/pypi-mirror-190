9127ceb0b2862b61498b3c1d5169ce4eb1bde200
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:14:42 +0000

    Add package growatt-client/0.0.6/growatt_client-0.0.6.tar.gz

---
+++ packages/growatt-client/growatt_client-0.0.6.tar.gz/growatt_client-0.0.6/example.py
@@ -0,0 +1,45 @@
+import asyncio
+import logging
+from sys import argv
+
+from growatt_client import GrowattClient
+
+# defaults
+# USB port of RS232/RS485 converter
+DEFAULT_PORT = "/dev/ttyUSB0"
+# Growatt modbus address
+DEFAULT_ADDRESS = 0x01
+
+logging.basicConfig(level=logging.INFO)
+
+
+async def run_async_client():
+    port = str(argv[1]) if len(argv) > 1 else DEFAULT_PORT
+    address = int(argv[2]) if len(argv) > 2 else DEFAULT_ADDRESS
+    client = GrowattClient(port, address)
+
+    # try:
+
+    data = await client.async_update()
+    ser = client.get_serial_number()
+    # data = await asyncio.wait_for(client.async_update(), timeout=10)
+
+    logging.info(
+        f" Serial number: {ser} "
+        f"Firmware: {client.get_firmware()} "
+        f"Model Number: {client.get_model_number()}"
+    )
+
+    logging.debug(f"Sensors data: {data}")
+    for key in data:
+        desc = client.get_attribute(key)
+        value = data[key]
+        d = desc["description"]
+        u = desc["unit"]
+        logging.info(f"{d} {value} {u}")
+
+    # except Exception as error:
+    #     logging.error("Error: " + repr(error))
+
+if __name__ == "__main__":
+    asyncio.run(run_async_client())
