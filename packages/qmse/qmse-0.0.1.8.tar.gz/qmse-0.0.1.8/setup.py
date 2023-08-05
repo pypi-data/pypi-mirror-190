e816a8999f0f5d9d29e9c239cf3f8d3532764d62
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:13:07 +0000

    Add package qmse/0.0.1.8/qmse-0.0.1.8.tar.gz

---
+++ packages/qmse/qmse-0.0.1.8.tar.gz/qmse-0.0.1.8/setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+setuptools.setup(
+    name="qmse",
+    version="0.0.1.8",
+    license='MIT',
+    author="qmse",
+    author_email="",
+    description="qmse",
+    long_description=open('README.md').read(),
+    url="https://github.com",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        # 패키지에 대한 태그
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"
+    ],
+)
