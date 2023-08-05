42d418314a89bd32fad16c9c9f737f2ed120ae99
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:10:47 +0000

    Add package postgres-ddl/1.3/postgres_ddl-1.3.tar.gz

---
+++ packages/postgres-ddl/postgres_ddl-1.3.tar.gz/postgres_ddl-1.3/postgres_ddl/Config.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+class Config():
+    Indent = " " * 2
+    NL = chr(10)
+    IsSeqGrantsInTable = False
+
+    @staticmethod
+    def Parse(json):
+        Config.Indent = " " * (json.get("indent") or 2)
+        Config.NL = json.get("new_line") or chr(10)
+
+        style = json.get("style") or []
+        Config.IsSeqGrantsInTable = "seq_grants_in_table" in style
