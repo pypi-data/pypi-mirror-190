ab4337d651b886fd340c10298ff117d284a23bbc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:14:43 +0000

    Add package eon-fms-client/0.2.1/eon_fms_client-0.2.1.tar.gz

---
+++ packages/eon-fms-client/eon_fms_client-0.2.1.tar.gz/eon_fms_client-0.2.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+setup(
+    name='eon_fms_client',
+    version='0.2.1',
+    license='MIT',
+    author="Ahmad Salameh",
+    author_email='a.salameh@eonaligner.com',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    url='https://bitbucket.org/eon-mes/broker_utilities/src/master',
+    keywords='eon fms client',
+    install_requires=[
+        "python-magic==0.4.27",
+        "requests==2.25.1",
+    ],
+)
