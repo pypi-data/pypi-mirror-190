6fb5dbf8d195383da1694b3ee9857631376757eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:39 +0000

    Add package this-is/1.0.4/this_is-1.0.4-py2.py3-none-any.whl

---
+++ packages/this-is/this_is-1.0.4-py2.py3-none-any.whl/this_is.py
@@ -0,0 +1,101 @@
+def en_us():
+    msg = """The Zen of Python, by Tim Peters
+
+Beautiful is better than ugly.
+Explicit is better than implicit.
+Simple is better than complex.
+Complex is better than complicated.
+Flat is better than nested.
+Sparse is better than dense.
+Readability counts.
+Special cases aren't special enough to break the rules.
+Although practicality beats purity.
+Errors should never pass silently.
+Unless explicitly silenced.
+In the face of ambiguity, refuse the temptation to guess.
+There should be one and preferably only one obvious way to do it.
+Although that way may not be obvious at first unless you're Dutch.
+Now is better than never.
+Although never is often better than *right* now.
+If the implementation is hard to explain, it's a bad idea.
+If the implementation is easy to explain, it may be a good idea.
+Namespaces are one honking great idea let's do more of those!"""
+    print(msg)
+
+
+def pt_br():
+    msg = """The Zen of Python, by Tim Peters
+
+Bonito é melhor que feio.
+Explícito é melhor que implícito.
+Simples é melhor que complexo.
+Complexo é melhor que complicado.
+Linear é melhor do que aninhado.
+Esparso é melhor que denso.
+Legibilidade conta.
+Casos especiais não são especiais o bastante para quebrar as regras.
+Ainda que praticidade vença a pureza.
+Erros nunca devem passar silenciosamente.
+A menos que sejam explicitamente silenciados.
+Diante da ambiguidade, recuse a tentação de adivinhar.
+Dever haver um e preferencialmente apenas um modo óbvio para fazer algo.
+Embora esse modo possa não ser óbvio a princípio a menos que você seja holandês.
+Agora é melhor que nunca.
+Apesar de que nunca normalmente é melhor do que *exatamente* agora
+Se a implementação é difícil de explicar, é uma má ideia
+Se a implementação é fácil de explicar, pode ser uma boa ideia
+Namespaces são uma grande ideia, vamos ter mais dessas!"""
+    print(msg)
+
+
+def es():
+    msg = """The Zen of Python, by Tim Peters
+
+Lo bello es mejor que lo feo.
+Explícito es mejor que implícito.
+Mejor es simple que complejo.
+Complejo es mejor que complicado.
+Lineal es mejor que anidado.
+Es mejor escaso que denso.
+La legibilidad cuenta.
+Los casos especiales no son lo suficientemente especiales como para romper las reglas.
+Aunque la practicidad triunfa sobre la pureza.
+Los errores nunca deben pasar silenciosamente.
+A menos que estén explícitamente silenciados.
+Ante la ambigüedad, rechace la tentación de adivinar.
+Debe haber una, y preferiblemente solo una, forma obvia de hacer algo.
+Aunque este modo puede no ser obvio al principio a menos que seas holandés.
+Ahora está mejor que nunca.
+Aunque normalmente nunca es mejor que *exactamente* ahora
+Si la implementación es difícil de explicar, es una mala idea.
+Si la implementación es fácil de explicar, podría ser una buena idea.
+Los espacios de nombres son una gran idea, ¡tengamos más de estos!"""
+    print(msg)
+
+
+def fr():
+    """Traduction de Cécile Trevian, sous licence CC BY-NC-SA dans document
+    https://perso.limsi.fr/pointal/_media/python:cours:courspython3.pdf"""
+    msg = """The Zen of Python, by Tim Peters
+
+Préfère :
+      la beauté à la laideur,
+      l'explicite à l'implicite,
+      le simple au complexe
+      et le complexe au compliqué,
+      le déroulé à l'imbriqué,
+      l'aéré au compact.
+Prends en compte la lisibilité.
+Les cas particuliers ne le sont jamais assez pour violer les règles.
+Mais, à la pureté, privilégie l'aspect pratique.
+Ne passe pas les erreurs sous silence,
+... ou bâillonne-les explicitement.
+Face à l'ambiguïté, à deviner ne te laisse pas aller.
+Sache qu'il ne devrait [y] avoir qu'une et une seule façon de procéder,
+même si, de prime abord, elle n'est pas évidente, à moins d'être Néerlandais.
+Mieux vaut maintenant que jamais.
+Cependant jamais est souvent mieux qu'immédiatement.
+Si l'implémentation s'explique difficilement, c'est une mauvaise idée.
+Si l'implémentation s'explique aisément, c'est peut-être une bonne idée.
+Les espaces de nommage ! Sacrée bonne idée ! Faisons plus de trucs comme ça. """
+    print(msg)
