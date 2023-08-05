ba9d5a62fa2e8d780b8e735a79a1ea58b17fd2f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:15:05 +0000

    Add package PyLeaves/1.0.2/PyLeaves-1.0.2.tar.gz

---
+++ packages/PyLeaves/PyLeaves-1.0.2.tar.gz/PyLeaves-1.0.2/pyleaves/leaves.py
@@ -0,0 +1,9 @@
+# Copyright (©) 2023 https://github.com/MrMKN
+# Telegram Link : https://t.me/Mr_MKN 
+# Repo Link : https://github.com/MrMKN/PyLeaves
+# License Link : https://github.com/MrMKN/PyLeaves/blob/main/LICENSE
+
+from .PyroTools import pyro_progress
+
+class Leaves:
+    progress_for_pyrogram = pyro_progress        
