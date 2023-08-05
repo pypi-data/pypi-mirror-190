2e65380ab6e4d7ae0097bf7cf9b03041c0fcfa00
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:11:09 +0000

    Add package mojoRPG/0.0.5/mojoRPG-0.0.5.tar.gz

---
+++ packages/mojoRPG/mojoRPG-0.0.5.tar.gz/mojoRPG-0.0.5/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.5'
+DESCRIPTION = 'Mojo RPG test game'
+LONG_DESCRIPTION = 'A package demonstrates mojo rpg combat'
+
+setup(
+    name="mojoRPG",
+    version=VERSION,
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    author="Alex Iannicelli",
+    author_email="atiannicelli@gmail.com",
+    license='MIT',
+    packages=find_packages(),
+    install_requires=[],
+    keywords='mojo',
+    classifiers= [
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        'License :: OSI Approved :: MIT License',
+        "Programming Language :: Python :: 3",
+    ]
+)
