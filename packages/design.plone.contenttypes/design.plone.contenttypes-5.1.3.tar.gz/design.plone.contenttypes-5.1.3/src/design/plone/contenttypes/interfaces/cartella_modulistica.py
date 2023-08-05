779a1c579567600a7b1a91ac7392ee8a95702b5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:35 +0000

    Add package design.plone.contenttypes/5.1.3/design.plone.contenttypes-5.1.3.tar.gz

---
+++ packages/design.plone.contenttypes/design.plone.contenttypes-5.1.3.tar.gz/design.plone.contenttypes-5.1.3/src/design/plone/contenttypes/interfaces/cartella_modulistica.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+from zope import schema
+from plone.supermodel import model
+
+from design.plone.contenttypes.interfaces import IDesignPloneContentType
+from design.plone.contenttypes import _
+
+
+class ICartellaModulistica(model.Schema, IDesignPloneContentType):
+    """Cartella Modulistica"""
+
+    visualize_files = schema.Bool(
+        title=_("visualize_files", default="Visualizza i file allegati"),
+        description="Visualizza i file nei tutti i contentuti della cartella al posto di scaricare immediatamente",
+        required=False,
+        default=False,
+    )
