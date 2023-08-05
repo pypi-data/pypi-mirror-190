fc87ef4c6743b1232650701758ced22eb40472ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:13:47 +0000

    Add package PyOngc/1.0.1/PyOngc-1.0.1-py3-none-any.whl

---
+++ packages/PyOngc/PyOngc-1.0.1-py3-none-any.whl/pyongc/__init__.py
@@ -0,0 +1,12 @@
+"""Python interface to OpenNGC database data.
+
+:license: MIT, see LICENSE for more details.
+"""
+
+from pkg_resources import resource_filename
+
+
+__version__ = '1.0.1'
+DBDATE = 20221023  # Version of database data
+
+DBPATH = resource_filename(__name__, 'ongc.db')
