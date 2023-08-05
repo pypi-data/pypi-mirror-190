3fec715f7247077f407c196b369b08e32160d1c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:14:05 +0000

    Add package jupyterlab-sentry/0.3.0/jupyterlab_sentry-0.3.0.tar.gz

---
+++ packages/jupyterlab-sentry/jupyterlab_sentry-0.3.0.tar.gz/jupyterlab_sentry-0.3.0/src/index.ts
@@ -0,0 +1,30 @@
+import {
+  JupyterFrontEnd,
+  JupyterFrontEndPlugin
+} from '@jupyterlab/application';
+
+import { ISettingRegistry } from '@jupyterlab/settingregistry';
+import * as Sentry from "@sentry/react";
+import { BrowserTracing } from "@sentry/tracing";
+/**
+ * Initialization data for the jupyterlab-sentry extension.
+ */
+const PLUGIN_ID = 'jupyterlab-sentry:plugin';
+const plugin: JupyterFrontEndPlugin<void> = {
+  id: PLUGIN_ID,
+  autoStart: true,
+  requires: [ISettingRegistry],
+  activate: async (
+    app: JupyterFrontEnd,
+    settingRegistry: ISettingRegistry,
+  ) => {
+    const setting = await settingRegistry.load(PLUGIN_ID)
+    Sentry.init({
+      dsn: setting.get('dsn').composite as string,
+      integrations: [new BrowserTracing()],
+      tracesSampleRate: setting.get('tracesSampleRate').composite as number,
+    });
+  }
+};
+
+export default plugin;
