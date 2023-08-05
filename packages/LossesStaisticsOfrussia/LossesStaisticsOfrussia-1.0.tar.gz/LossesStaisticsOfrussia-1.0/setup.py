d239d4a9aa2dd0419cf3dd5875c88971dd8f5fc6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:15:46 +0000

    Add package LossesStaisticsOfrussia/1.0/LossesStaisticsOfrussia-1.0.tar.gz

---
+++ packages/LossesStaisticsOfrussia/LossesStaisticsOfrussia-1.0.tar.gz/LossesStaisticsOfrussia-1.0/setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+setuptools.setup(
+	name='LossesStaisticsOfrussia',
+	version='1.0',
+	author='nazar.rudenok',
+	author_email='nazarrudenok1@gmail.com',
+	description='',
+	packages=['LossesStaisticsOfrussia'],
+	classifiers=[
+		"Programming Language :: Python :: 3",
+		"License :: OSI Approved :: MIT License",
+		"Operating System :: OS Independent",
+	],
+	python_requires='>=3.6',
+)
