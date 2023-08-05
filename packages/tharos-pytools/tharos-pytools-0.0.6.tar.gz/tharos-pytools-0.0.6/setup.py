03e165205382aef38a35c1291ee3d77b28825d94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:09:57 +0000

    Add package tharos-pytools/0.0.6/tharos-pytools-0.0.6.tar.gz

---
+++ packages/tharos-pytools/tharos-pytools-0.0.6.tar.gz/tharos-pytools-0.0.6/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+setup(
+    name='tharos-pytools',
+    version='0.0.6',
+    description='Testing installation of Package',
+    url='https://github.com/Tharos-ux/tharos-pytools',
+    author='Tharos',
+    author_email='dubois.siegfried@gmail.com',
+    license='MIT',
+    packages=['tharospytools'],
+    zip_safe=False
+)
---
+++ packages/tharos-pytools/tharos-pytools-0.0.6.tar.gz/tharos-pytools-0.0.6/tharospytools/__init__.py
@@ -0,0 +1,4 @@
+'Init paths'
+from .multithreading import futures_collector, limit_memory
+from .matplotlib_tools import get_palette, get_palette_from_list
+from .overloading import overload
