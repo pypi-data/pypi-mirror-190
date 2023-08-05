1259b247063c3a2d36f33a1c6d3aa8c16fc80b6a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:14:27 +0000

    Add package giant-events/0.2.10/giant-events-0.2.10.tar.gz

---
+++ packages/giant-events/giant-events-0.2.10.tar.gz/giant-events-0.2.10/giant_events/cms_apps.py
@@ -0,0 +1,21 @@
+from cms.app_base import CMSApp
+from cms.apphook_pool import apphook_pool
+
+
+class EventsApp(CMSApp):
+    """
+    App hook for Events app
+    """
+
+    app_name = "giant_events"
+    name = "Giant Events"
+
+    def get_urls(self, page=None, language=None, **kwargs):
+        """
+        Return the path to the apps urls module
+        """
+
+        return ["events.urls"]
+
+if settings.REGISTER_EVENTS_APP:
+    apphook_pool.register(NewsApp)
