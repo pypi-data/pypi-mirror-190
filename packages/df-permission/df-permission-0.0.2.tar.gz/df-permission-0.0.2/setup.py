32f4c18f26749ad7604c893e93ef3b54d68f1e87
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:14:08 +0000

    Add package df-permission/0.0.2/df-permission-0.0.2.tar.gz

---
+++ packages/df-permission/df-permission-0.0.2.tar.gz/df-permission-0.0.2/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.2'
+DESCRIPTION = 'Django field permission package'
+
+
+def read(f):
+    return open(f, 'r', encoding='utf-8').read()
+
+
+# Setting up
+setup(
+    name="df-permission",
+    version=VERSION,
+    author="Maxmudov Asliddin",
+    author_email="<asliddin750750@gmail.com>",
+    description=DESCRIPTION,
+    long_description=read('README.md'),
+    long_description_content_type='text/markdown',
+    packages=find_packages(include=['df_permission']),
+    install_requires=['django>=3', 'django-rest-framework'],
+    keywords=['python', 'field', 'permission', 'field permission', 'django field'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
