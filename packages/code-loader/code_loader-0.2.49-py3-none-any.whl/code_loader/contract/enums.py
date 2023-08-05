167eaab90ffe3df9c3ea3f2387752cd22286dbce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:14:45 +0000

    Add package code-loader/0.2.49/code_loader-0.2.49-py3-none-any.whl

---
+++ packages/code-loader/code_loader-0.2.49-py3-none-any.whl/code_loader/contract/enums.py
@@ -0,0 +1,55 @@
+from enum import Enum, IntEnum
+
+
+class Metric(Enum):
+    MeanSquaredError = 'MeanSquaredError'
+    MeanSquaredLogarithmicError = 'MeanSquaredLogarithmicError'
+    MeanAbsoluteError = 'MeanAbsoluteError'
+    MeanAbsolutePercentageError = 'MeanAbsolutePercentageError'
+    Accuracy = 'Accuracy'
+    BinaryAccuracy = 'BinaryAccuracy'
+    MeanIOU = 'MeanIOU'
+    ConfusionMatrixClassification = 'ConfusionMatrixClassification'
+    ConfusionMatrixBinaryClassification = 'ConfusionMatrixBinaryClassification'
+
+
+class LeapDataType(Enum):
+    Image = 'Image'
+    Text = 'Text'
+    Graph = 'Graph'
+    HorizontalBar = 'HorizontalBar'
+    ImageMask = 'ImageMask'
+    TextMask = 'TextMask'
+    ImageWithBBox = 'ImageWithBBox'
+
+
+class DatasetMetadataType(Enum):
+    float = "float"
+    string = "string"
+    int = "int"
+    boolean = "boolean"
+
+
+class DataStateType(Enum):
+    training = "training"
+    validation = "validation"
+    test = "test"
+    unlabeled = "unlabeled"
+
+
+class DataStateEnum(IntEnum):
+    training = 0
+    validation = 1
+    test = 2
+    unlabeled = 3
+
+
+class ConfusionMatrixValue(Enum):
+    Positive = "Positive"
+    Negative = "Negative"
+
+
+# todo: handle test not run due to error in pre process and Add to TestingSectionEnum Enum didn't run
+class TestingSectionEnum(Enum):
+    Warnings = "Warnings"
+    Errors = "Errors"
