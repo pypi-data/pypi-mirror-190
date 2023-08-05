67ce1d1f51e7358c39aef7a64e52484987807892
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:11:39 +0000

    Add package invz-package/0.0.6/invz_package-0.0.6.tar.gz

---
+++ packages/invz-package/invz_package-0.0.6.tar.gz/invz_package-0.0.6/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+requirements =[
+    'Pillow',
+    'numpy',
+    'tqdm',
+    'gdown',
+    'insightface',
+]
+
+pypandoc_enabled = True
+try:
+    import pypandoc
+    print('pandoc enabled')
+    long_description = pypandoc.convert_file('README.md', 'rst')
+except (IOError, ImportError, ModuleNotFoundError):
+    print('WARNING: pandoc not enabled')
+    long_description = open('README.md').read()
+    pypandoc_enabled = False
+
+setup(
+    name="invz_package",
+    version="0.0.6",
+    author="Innerverz-by.JJY",
+    author_email="pensee0.0a@innerverz.com",
+    description="innerverz package",
+    long_description=long_description,
+    python_requires=">=3.6",
+    install_requires=requirements,
+    packages=find_packages()
+    
+    
+)
