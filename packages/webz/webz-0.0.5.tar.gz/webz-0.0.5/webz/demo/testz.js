4ddd61386274a1e66321189b252f64da60d54fdb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:26 +0000

    Add package webz/0.0.5/webz-0.0.5.tar.gz

---
+++ packages/webz/webz-0.0.5.tar.gz/webz-0.0.5/webz/demo/testz.js
@@ -0,0 +1,7 @@
+[
+    //(url前缀, key, 初始化参数)
+    ("func/", test.func)
+    ("page/", webz.static, ["page", "pages"])
+    ("webz/js/", webz.static, ["webz/js", "webz.js"])
+    ("webz/css/", webz.static, ["webzcss", "webz.css"])
+]
