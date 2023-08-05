3ba87ea45d8c67159086cd293b2a126ee415b090
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:21:27 +0000

    Add package ayaka/0.0.3.3a1/ayaka-0.0.3.3a1.tar.gz

---
+++ packages/ayaka/ayaka-0.0.3.3a1.tar.gz/ayaka-0.0.3.3a1/ayaka/__init__.py
@@ -0,0 +1,17 @@
+'''
+Ayaka - 猫猫，猫猫！ 
+
+专注群聊、多人互动的插件开发
+
+文档：https://bridgel.github.io/ayaka/
+
+注意：文档版本与pypi包正式版本一致，因此其内容可能会落后于各个beta版
+'''
+from .adapters import get_adapter, AyakaEvent
+from .helpers import load_data_from_file, debug_print, simple_repr, ensure_dir_exists
+from .download import resource_download, resource_download_by_res_info, get_file_hash, ResInfo, ResItem
+from .core import AyakaDB, AyakaCat, AyakaSubscribe
+
+# 兼容性
+from .config import AyakaConfig
+from .core import UserDBBase, GroupDBBase, get_session
---
+++ packages/ayaka/ayaka-0.0.3.3a1.tar.gz/ayaka-0.0.3.3a1/ayaka/config.py
@@ -0,0 +1,79 @@
+'''管理插件配置，提供读写支持'''
+import json
+from loguru import logger
+from pydantic import ValidationError, BaseModel
+from .helpers import ensure_dir_exists, singleton
+
+AYAKA_VERSION = "0.0.3.3a1"
+
+
+@singleton
+def get_data_path():
+    return ensure_dir_exists("data/ayaka")
+
+
+class AyakaConfig(BaseModel):
+    '''继承时请填写`__config_name__`
+
+    该配置保存在data/ayaka/<__config_name__>.json
+
+    在修改不可变成员属性时，`AyakaConfig`会自动写入到本地文件，但修改可变成员属性时，需要手动执行save函数
+    '''
+    __config_name__ = ""
+    '''配置文件的名称'''
+
+    def __init__(self, **data):
+        name = self.__config_name__
+        if not name:
+            raise Exception("__config_name__不可为空")
+
+        try:
+            path = get_data_path() / f"{name}.json"
+            # 存在则读取
+            if path.exists():
+                with path.open("r", encoding="utf8") as f:
+                    data = json.load(f)
+
+            # 载入数据
+            super().__init__(**data)
+
+        except ValidationError as e:
+            logger.error(
+                f"导入配置失败，请检查{name}的配置是否正确；如果不确定出错的原因，可以尝试更新插件，删除旧配置并重启bot")
+            raise e
+
+        # 强制更新（更新默认值）
+        self.save()
+        logger.opt(colors=True).debug(f"已载入配置文件 <g>{name}</g>")
+
+    def __setattr__(self, name, value):
+        if getattr(self, name) != value:
+            super().__setattr__(name, value)
+            self.save()
+
+    def save(self):
+        '''修改可变成员变量后，需要使用该方法才能保存其值到文件'''
+        name = self.__config_name__
+        data = self.dict()
+        path = get_data_path() / f"{name}.json"
+        with path.open("w+", encoding="utf8") as f:
+            json.dump(data, f, ensure_ascii=0, indent=4)
+
+
+class RootConfig(AyakaConfig):
+    '''根配置'''
+
+    __config_name__ = "root"
+
+    version: str = AYAKA_VERSION
+    '''版本号'''
+
+    auto_ob11_qqguild_patch: bool = True
+    '''在ob11协议中自动使用qqguild_patch'''
+
+
+@singleton
+def get_root_config():
+    '''获取ayaka根配置'''
+    logger.opt(colors=True).info(f"<y>ayaka</y> 当前版本 <y>{AYAKA_VERSION}</y>")
+    return RootConfig()
