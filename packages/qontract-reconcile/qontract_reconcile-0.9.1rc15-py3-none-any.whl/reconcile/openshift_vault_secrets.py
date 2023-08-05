dc112d2b0f2cdf04f5b39dc408266a6db9565baa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:13:49 +0000

    Add package qontract-reconcile/0.9.1rc15/qontract_reconcile-0.9.1rc15-py3-none-any.whl

---
+++ packages/qontract-reconcile/qontract_reconcile-0.9.1rc15-py3-none-any.whl/reconcile/openshift_vault_secrets.py
@@ -0,0 +1,39 @@
+from typing import Any
+
+import reconcile.openshift_resources_base as orb
+from reconcile.utils.semver_helper import make_semver
+
+QONTRACT_INTEGRATION = "openshift-vault-secrets"
+QONTRACT_INTEGRATION_VERSION = make_semver(1, 9, 3)
+PROVIDERS = ["vault-secret"]
+
+
+def run(
+    dry_run,
+    thread_pool_size=10,
+    internal=None,
+    use_jump_host=True,
+    cluster_name=None,
+    namespace_name=None,
+    defer=None,
+):
+    orb.QONTRACT_INTEGRATION = QONTRACT_INTEGRATION
+    orb.QONTRACT_INTEGRATION_VERSION = QONTRACT_INTEGRATION_VERSION
+
+    orb.run(
+        dry_run=dry_run,
+        thread_pool_size=thread_pool_size,
+        internal=internal,
+        use_jump_host=use_jump_host,
+        providers=PROVIDERS,
+        cluster_name=cluster_name,
+        namespace_name=namespace_name,
+    )
+
+
+def early_exit_desired_state(*args, **kwargs) -> dict[str, Any]:
+    namespaces, _ = orb.get_namespaces(PROVIDERS)
+
+    return {
+        "namespaces": namespaces,
+    }
