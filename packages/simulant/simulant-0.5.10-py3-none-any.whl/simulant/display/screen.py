530f3729f2e8ef4dc0fa441e6cadb47bd9bbe894
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:14:02 +0000

    Add package simulant/0.5.10/simulant-0.5.10-py3-none-any.whl

---
+++ packages/simulant/simulant-0.5.10-py3-none-any.whl/simulant/display/screen.py
@@ -0,0 +1,30 @@
+from simulant.display.window import Window
+from simulant.os import Shell
+
+
+class Screen(Shell):
+    # todo singleton
+    windows = []  # todo: xdotool get all windows
+
+    def __init__(self, number):
+        self.number = number
+
+    @property
+    def focus_window(self):
+        return [window for window in self.windows if window.is_focus][0]  # todo change focus
+
+    def get_windows(self, class_name):
+        windows_ids = self.execute(f"xdotool search --classname --onlyvisible {class_name}")
+        windows_obj = [Window(window_id) for window_id in windows_ids]
+        self.windows.extend(windows_obj)
+        return windows_obj
+
+    @staticmethod
+    def is_select():  # todo check xdotool
+        return True
+
+    def __str__(self):
+        return f"<{self.__class__.__name__}: {self.number}>"
+
+    def __repr__(self):
+        return self.__str__()
