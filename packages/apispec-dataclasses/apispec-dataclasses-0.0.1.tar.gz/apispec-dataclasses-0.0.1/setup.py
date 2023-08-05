adeae53357d8b8d33f6c6b931ccbd905a22a7780
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:14:39 +0000

    Add package apispec-dataclasses/0.0.1/apispec-dataclasses-0.0.1.tar.gz

---
+++ packages/apispec-dataclasses/apispec-dataclasses-0.0.1.tar.gz/apispec-dataclasses-0.0.1/setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+from setuptools import find_packages, setup
+
+setup(
+    name="apispec-dataclasses",
+    version="0.0.1",
+    description="Plugin for apispec to add dataclasses support",
+    platforms="all",
+    classifiers=[
+        "License :: OSI Approved :: Apache Software License",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Development Status :: 1 - Planning",
+    ],
+    python_requires=">=3.7",
+    zip_safe=False,
+)
