26d4d1f9a9d82572a81fd46e5bf477c03880e141
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:16:36 +0000

    Add package spiakid-drs/0.0.6/spiakid-drs-0.0.6.tar.gz

---
+++ packages/spiakid-drs/spiakid-drs-0.0.6.tar.gz/spiakid-drs-0.0.6/src/prototype.py
@@ -0,0 +1,43 @@
+import h5py
+import sys
+import numpy as np
+import pandas as pd
+from Functions import GUI as G
+from Functions import Modelisation as mod
+import argparse
+from pathlib import Path
+
+
+def parse():
+    # read in command line arguments
+    parser = argparse.ArgumentParser(description='MKID Pipeline CLI')
+    parser.add_argument('--init', action='store_true', help='launch the interface')
+    parser.add_argument('--outp', help='output destination', dest='out_cfg', default=None)
+    parser.add_argument('--inp', help='data location', dest='in_cfg' , default=None)
+    parser.add_argument('--format', help='format of the plot', default='.jpg', dest='form')
+    parser.add_argument('--dir', help='create input and output folder', action='store_true')
+    return parser.parse_args()
+
+
+if __name__ == "__main__":
+    args = parse()
+    if args.init:
+        G.interface()
+        print('Done')
+        sys.exit(0)
+
+    if args.out_cfg!=None and args.in_cfg!=None:
+        mod.Data_read(args.in_cfg,args.out_cfg,args.form)
+        print('Done')
+
+    if args.dir:
+        output = Path('Output')
+        Input = Path('Data_location')
+        if output.exists():
+            pass
+        else:
+            output.mkdir()
+        if Input.exists():
+            pass
+        else:
+            Input.mkdir()
