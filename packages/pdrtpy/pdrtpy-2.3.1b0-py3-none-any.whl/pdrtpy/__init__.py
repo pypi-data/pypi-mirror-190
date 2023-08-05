d47f53617b3745f3ff1e926249528d4e32dff7df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:16:08 +0000

    Add package pdrtpy/2.3.1b0/pdrtpy-2.3.1b0-py3-none-any.whl

---
+++ packages/pdrtpy/pdrtpy-2.3.1b0-py3-none-any.whl/pdrtpy/__init__.py
@@ -0,0 +1,16 @@
+__all__ = [ "pdrutils", "measurement", "modelset"]
+
+VERSION = "2.3.1b"
+AUTHORS =  'Marc W. Pound, Mark G. Wolfire'
+DESCRIPTION="PhotoDissociation Region Toolbox (PDRT), astrophysics analysis tools"
+KEYWORDS = "PDR photodissociation astronomy astrophysics"
+
+# easter egg
+motto = "Reliable astrophysics at everyday low, low prices! "+ u"\u00AE"
+
+def version():
+    """Version of the PDRT code
+
+    :rtype: str
+    """
+    return VERSION
