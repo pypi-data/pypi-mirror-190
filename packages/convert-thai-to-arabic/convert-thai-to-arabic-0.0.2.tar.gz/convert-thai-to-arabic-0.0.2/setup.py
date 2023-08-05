ef386087af968b4d31b21378ea6ca32f7019e650
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:14:28 +0000

    Add package convert-thai-to-arabic/0.0.2/convert-thai-to-arabic-0.0.2.tar.gz

---
+++ packages/convert-thai-to-arabic/convert-thai-to-arabic-0.0.2.tar.gz/convert-thai-to-arabic-0.0.2/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='convert-thai-to-arabic',
+    version='0.0.2',
+    description='Convert Thai numbers in file names to Arabic numbers',
+    author='jaytrairat',
+    author_email='jaytrairat@outlook.com',
+    url='https://github.com/jaytrairat/python-connvert-thai-to-arabic',
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'convert-thai-to-arabic = convert_thai_to_arabic.convert_thai_to_arabic:main'
+        ],
+    },
+)
