c687e0ba52bce54d9af5576ce8e21924104662f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:05:53 +0000

    Add package urm-validatetoken/0.0.5/urm_validatetoken-0.0.5-py3-none-any.whl

---
+++ packages/urm-validatetoken/urm_validatetoken-0.0.5-py3-none-any.whl/urm_validatetoken/constants.py
@@ -0,0 +1,13 @@
+
+HEADER_AUTHORIZATION = "Authorization"
+HEADER_AUTHORIZATION_MISSING = "Authorization header is missing in the request."
+HEADER_CONTENT_TYPE_MISSING = "Content type header is missing in the request."
+BEARER_STRING = "Bearer "
+BEARER_STRING_MISSING = "Bearer keyword is missing in the access token"
+CONTENT_TYPE_HEADER = 'Content-Type'
+CONTENT_TYPE_VALUE = 'application/json'
+TOKEN_INVALID_STATUS = "Access token is invalid. Please try again with valid token."
+TOKEN_VALID_STATUS = "Access token validated successfully."
+TOKEN_STATUS_VALUE_ERROR = "Access token status value is invalid."
+SUCCESS_STATUS="Success"
+ERROR_STATUS="Error"
