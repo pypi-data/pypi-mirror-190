4d043c78a5aec1405d90896bb05d358fb3d53f20
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:13:54 +0000

    Add package pyroll-freiberg-flow-stress/2.0.0b1/pyroll_freiberg_flow_stress-2.0.0b1-py3-none-any.whl

---
+++ packages/pyroll-freiberg-flow-stress/pyroll_freiberg_flow_stress-2.0.0b1-py3-none-any.whl/pyroll/freiberg_flow_stress/__init__.py
@@ -0,0 +1,31 @@
+from pyroll.core import Profile, RollPass
+from pyroll.core.hooks import Hook
+from functools import partial
+
+from .freiberg_flow_stress import FreibergFlowStressCoefficients, flow_stress
+
+VERSION = "2.0.0b-1"
+
+Profile.freiberg_flow_stress_coefficients = Hook[FreibergFlowStressCoefficients]()
+
+
+@RollPass.Profile.flow_stress
+def freiberg_flow_stress(self: RollPass.Profile):
+    if hasattr(self, "freiberg_flow_stress_coefficients"):
+        return flow_stress(
+            self.freiberg_flow_stress_coefficients,
+            self.strain,
+            self.roll_pass().strain_rate,
+            self.temperature
+        )
+
+
+@RollPass.Profile.flow_stress_function
+def freiberg_flow_stress_function(self: RollPass.Profile):
+    return partial(
+        flow_stress,
+        coefficients=self.freiberg_flow_stress_coefficients
+    )
+
+
+from . import materials
