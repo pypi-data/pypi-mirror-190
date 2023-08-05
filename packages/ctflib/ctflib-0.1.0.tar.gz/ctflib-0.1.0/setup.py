821c413a37c982370333034c2d93f734fd57e915
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:14:48 +0000

    Add package ctflib/0.1.0/ctflib-0.1.0.tar.gz

---
+++ packages/ctflib/ctflib-0.1.0.tar.gz/ctflib-0.1.0/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['ctflib', 'ctflib.crypto']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'ctflib',
+    'version': '0.1.0',
+    'description': 'Tools for better CTF experience',
+    'long_description': '# ctflib\n',
+    'author': 'Alexey Tarasov',
+    'author_email': 'tarasovion2004@gmail.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.10,<4.0',
+}
+
+
+setup(**setup_kwargs)
