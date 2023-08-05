2b4053a1eed302945d799770ede1f7ad03b9b3ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:21:19 +0000

    Add package movie-bot-api/0.0.48/movie_bot_api-0.0.48-py3-none-any.whl

---
+++ packages/movie-bot-api/movie_bot_api-0.0.48-py3-none-any.whl/moviebotapi/auth.py
@@ -0,0 +1,23 @@
+from typing import Dict, List
+
+from moviebotapi import Session
+
+
+class AuthApi:
+    def __init__(self, session: Session):
+        self._session: Session = session
+
+    def get_default_ak(self, ) -> Dict:
+        return self._session.get('auth.get_default_ak')
+
+    def add_permission(self, role_code: List[int], uri: str):
+        """
+        为角色授权可访问的URI
+        :param role_code: 角色码 1为管理员 2为普通用户，目前固定不变
+        :param uri: 权限点URI
+        :return:
+        """
+        self._session.post('auth.add_permission', {
+            'role_code': role_code,
+            'uri': uri
+        })
