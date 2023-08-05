e1861a49ed5ce0f827919ffe19460fdbac39f773
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:22:44 +0000

    Add package django-jlog/1.0/django-jlog-1.0.tar.gz

---
+++ packages/django-jlog/django-jlog-1.0.tar.gz/django-jlog-1.0/setup.py
@@ -0,0 +1,21 @@
+import os
+from setuptools import setup
+
+here = os.path.abspath(os.path.dirname(__file__))
+README = open(os.path.join(here, 'README.rst')).read()
+
+setup(
+    name='django-jlog',
+    version='1.0',
+    packages=['jlog'],
+    long_description_content_type='text/x-rst',
+    description="""Log your request and response data""",
+    long_description=README,
+    author='Javid Aliyev',
+    author_email='jaliyev1987@gmail.com',
+    url='https://github.com/javiddo/django-jlog',
+    license='MIT',
+    install_requires=[
+        'Django>=1.8',
+    ]
+)
