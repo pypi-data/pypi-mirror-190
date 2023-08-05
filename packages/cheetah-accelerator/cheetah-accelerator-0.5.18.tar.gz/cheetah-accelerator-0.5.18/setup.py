ce06f56b3768906c0736f3f1c5124630b86aeb25
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:09:22 +0000

    Add package cheetah-accelerator/0.5.18/cheetah-accelerator-0.5.18.tar.gz

---
+++ packages/cheetah-accelerator/cheetah-accelerator-0.5.18.tar.gz/cheetah-accelerator-0.5.18/setup.py
@@ -0,0 +1,27 @@
+from pathlib import Path
+
+from setuptools import setup
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setup(
+    name="cheetah-accelerator",
+    version="0.5.18",
+    author="Jan Kaiser & Oliver Stein",
+    author_email="jan.kaiser@desy.de",
+    url="https://github.com/desy-ml/cheetah",
+    description=(
+        "Fast particle accelerator optics simulation for reinforcement learning and"
+        " optimisation applications."
+    ),
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=["cheetah"],
+    install_requires=[
+        "torch",
+        "matplotlib",
+        "numpy",
+        "scipy",
+    ],
+)
