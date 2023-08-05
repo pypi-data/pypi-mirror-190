036c5adc3f6e3a1a9c1614e82b55afd1f9ab2307
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:10:43 +0000

    Add package ocp-project-plugin/0.0.0.0.168/ocp_project_plugin-0.0.0.0.168-py3-none-any.whl

---
+++ packages/ocp-project-plugin/ocp_project_plugin-0.0.0.0.168-py3-none-any.whl/ocp_project_plugin/views/ocp_project.py
@@ -0,0 +1,78 @@
+from django.db.models import Sum, FloatField, IntegerField, DecimalField
+from django.db.models.functions import Cast
+
+from netbox.views.generic import ObjectView, ObjectListView, ObjectEditView, ObjectDeleteView, BulkImportView, \
+    BulkEditView, BulkDeleteView
+
+from ocp_project_plugin.filters import OCPProjectFilter
+from ocp_project_plugin.forms import (
+    OCPProjectImportForm,
+    OCPProjectFilterForm,
+    OCPProjectForm,
+    OCPProjectBulkEditForm
+)
+from ocp_project_plugin.models import OCPProject, AppEnvironment
+from ocp_project_plugin.tables import OCPProjectTable, AppEnvironmentTable
+from utilities.views import register_model_view
+
+
+class OCPProjectListView(ObjectListView):
+    queryset = OCPProject.objects.all()
+    filterset = OCPProjectFilter
+    filterset_form = OCPProjectFilterForm
+    table = OCPProjectTable
+
+
+class OCPProjectEditView(ObjectEditView):
+    """View for editing OCP Project instance."""
+
+    queryset = OCPProject.objects.all()
+    form = OCPProjectForm
+    default_return_url = "plugins:ocp_project_plugin:ocpproject_list"
+
+
+class OCPProjectDeleteView(ObjectDeleteView):
+    queryset = OCPProject.objects.all()
+    default_return_url = "plugins:ocp_project_plugin:ocpproject_list"
+
+
+class OCPProjectBulkImportView(BulkImportView):
+    queryset = OCPProject.objects.all()
+    model_form = OCPProjectImportForm
+    table = OCPProjectTable
+    default_return_url = "plugins:ocp_project_plugin:ocpproject_list"
+
+
+class OCPProjectBulkEditView(BulkEditView):
+    queryset = OCPProject.objects.all()
+    filterset = OCPProjectFilter
+    table = OCPProjectTable
+    form = OCPProjectBulkEditForm
+
+
+class OCPProjectBulkDeleteView(BulkDeleteView):
+    queryset = OCPProject.objects.all()
+    table = OCPProjectTable
+
+
+@register_model_view(OCPProject)
+class OCPProjectView(ObjectView):
+    template_name = 'ocp_project_plugin/ocp_project/ocp_project.html'
+    queryset = OCPProject.objects.all()
+
+    def get_extra_context(self, request, instance):
+        app_environment_assignments = AppEnvironment.objects.restrict(request.user, 'view').filter(
+            ocp_project=instance
+        )
+        assignments_table = AppEnvironmentTable(app_environment_assignments, user=request.user)
+        assignments_table.configure(request)
+
+        for app_env in app_environment_assignments:
+            app_env.calculate_cpu_cost()
+            #print(app_env.calculate_memory_cost())
+
+        return {
+            'assignments_table': assignments_table,
+            'assignment_count': AppEnvironment.objects.filter(ocp_project=instance).count(),
+            'app_env_list': AppEnvironment.objects.filter(ocp_project=instance)
+        }
