a293252b71c6b8fdcd4676f6460ac4a87fda7721
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:10:35 +0000

    Add package woomodule/1.43/woomodule-1.43-py3-none-any.whl

---
+++ packages/woomodule/woomodule-1.43-py3-none-any.whl/woomodule/main.py
@@ -0,0 +1,13 @@
+# from src.woomodule.app.app import get_str
+from app.app import get_str
+# from .app import app as ap
+
+
+def main():
+    print('from main')
+    print(get_str())
+
+
+if __name__ == '__main__':
+    main()
+
