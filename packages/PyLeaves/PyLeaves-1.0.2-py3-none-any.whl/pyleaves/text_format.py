94121ef47db8303795f15a08f44b8c887bf04ef9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:15:05 +0000

    Add package PyLeaves/1.0.2/PyLeaves-1.0.2-py3-none-any.whl

---
+++ packages/PyLeaves/PyLeaves-1.0.2-py3-none-any.whl/pyleaves/text_format.py
@@ -0,0 +1,15 @@
+# Copyright (©) 2023 https://github.com/MrMKN
+# Telegram Link : https://t.me/Mr_MKN 
+# Repo Link : https://github.com/MrMKN/PyLeaves
+# License Link : https://github.com/MrMKN/PyLeaves/blob/main/LICENSE
+
+
+
+#example template 
+PROGRESS_BAR = """\n
+╭━━━━❰ᴘʀᴏɢʀᴇss ʙᴀʀ❱━➣
+┣⪼ 🗂️ : {current} | {total}
+┣⪼ ⏳️ : {percentage}%
+┣⪼ 🚀 : {speed}/s
+┣⪼ ⏱️ : {est_time}
+╰━━━━━━━━━━━━━━━➣ """
