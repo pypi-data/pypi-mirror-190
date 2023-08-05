9774f85e42b674ebe868548f893df6e8b847cc05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:30 +0000

    Add package urm-validatetoken/0.0.7/urm_validatetoken-0.0.7-py3-none-any.whl

---
+++ packages/urm-validatetoken/urm_validatetoken-0.0.7-py3-none-any.whl/urm_validatetoken/validatetoken.py
@@ -0,0 +1,57 @@
+
+HEADER_AUTHORIZATION = "Authorization"
+HEADER_AUTHORIZATION_MISSING = "Authorization header is missing in the request."
+HEADER_CONTENT_TYPE_MISSING = "Content type header is missing in the request."
+BEARER_STRING = "Bearer "
+BEARER_STRING_MISSING = "Bearer keyword is missing in the access token"
+CONTENT_TYPE_HEADER = 'Content-Type'
+CONTENT_TYPE_VALUE = 'application/json'
+TOKEN_INVALID_STATUS = "Access token is invalid. Please try again with valid token."
+TOKEN_VALID_STATUS = "Access token validated successfully."
+TOKEN_STATUS_VALUE_ERROR = "Access token status value is invalid."
+SUCCESS_STATUS="Success"
+ERROR_STATUS="Error"
+
+import urllib3
+import json
+import logging
+from flask import jsonify, request
+from functools import wraps
+
+http = urllib3.PoolManager()
+    
+def validate_token(request_method,validate_token_url):
+    def decorator(function):
+        @wraps(function)
+        def wrapper(*args, **kwargs):
+            try:
+                access_token = request.headers.get(HEADER_AUTHORIZATION)
+                content_type = request.headers.get(CONTENT_TYPE_HEADER)
+                if access_token is None or access_token == "":
+                    logging.error(HEADER_AUTHORIZATION_MISSING)
+                    return jsonify({"error-code": 403, "error-desc": "header missing", "error-msg": HEADER_AUTHORIZATION_MISSING}), 403
+                elif BEARER_STRING not in access_token:
+                    logging.error(BEARER_STRING_MISSING)
+                    return jsonify({"error-code": 401, "error-desc": "unauthorized", "error-msg": BEARER_STRING_MISSING}), 401
+                if content_type is None or content_type == "":
+                    logging.error(HEADER_CONTENT_TYPE_MISSING)
+                    return jsonify({"error-code": 403, "error-desc": "header missing", "error-msg": HEADER_CONTENT_TYPE_MISSING}), 403
+
+                response=http.request(request_method,validate_token_url,headers={CONTENT_TYPE_HEADER:content_type, HEADER_AUTHORIZATION:access_token})
+                response_data = json.loads(response.data.decode('utf-8'))
+                logging.info(response_data)
+                logging.info(response.status)
+                token_status = response_data['active']
+                logging.info(token_status)
+                if token_status == False:
+                    logging.error(TOKEN_INVALID_STATUS)
+                    return jsonify({"error-code": 401, "error-desc": "unauthorized", "error-msg": TOKEN_INVALID_STATUS}), 401
+                elif token_status != True:
+                    logging.error(TOKEN_STATUS_VALUE_ERROR)
+                    return jsonify({"error-code": 401, "error-desc": "unauthorized", "error-msg": TOKEN_STATUS_VALUE_ERROR}), 401
+            except Exception as err:
+                logging.error("Error: {}".format(err))
+                return jsonify(str(err)), 405
+            return function(*args, **kwargs)
+        return wrapper
+    return decorator
