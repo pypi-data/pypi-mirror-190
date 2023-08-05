68d2131e66c161a8ef133dc7f4724fc23ebd82a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:14:00 +0000

    Add package eule/0.2.6/eule-0.2.6-py3-none-any.whl

---
+++ packages/eule/eule-0.2.6-py3-none-any.whl/eule/__init__.py
@@ -0,0 +1,13 @@
+""" Package Eule  """
+
+# -*- coding: utf-8 -*-
+
+from __future__ import annotations
+
+from .eule import spread_euler
+
+__author__ = """Bruno Peixoto"""
+__email__ = 'brunolnetto@gmail.com'
+__version__ = '0.2.0'
+
+__all__ = ['spread_euler', 'eule']
