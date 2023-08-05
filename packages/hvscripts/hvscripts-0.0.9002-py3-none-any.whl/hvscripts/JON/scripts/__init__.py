cb493b2a49f869b447a58687022547c35f704de5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:21:38 +0000

    Add package hvscripts/0.0.9002/hvscripts-0.0.9002-py3-none-any.whl

---
+++ packages/hvscripts/hvscripts-0.0.9002-py3-none-any.whl/hvscripts/JON/scripts/__init__.py
@@ -0,0 +1,2 @@
+from .JON import JONDefaultSchema
+from .JON_sup import String, Number, Boolean, Date, File, Enum, NotInEnum, ChosenType, Object, Array, Clone
---
+++ packages/hvscripts/hvscripts-0.0.9002-py3-none-any.whl/hvscripts/forms/__init__.py
@@ -0,0 +1,1 @@
+from .crudstruct_forms import *
