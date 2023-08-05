64dd23109d5739db57e9128cd0aeecdd4c3c39ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:50 +0000

    Add package pymediawikidocker/0.7.1/pymediawikidocker-0.7.1.tar.gz

---
+++ packages/pymediawikidocker/pymediawikidocker-0.7.1.tar.gz/pymediawikidocker-0.7.1/mwdocker/resources/templates/mwLocalSettings.php
@@ -0,0 +1,19 @@
+# general LocalSettings
+{% if smwVersion %}
+# enable Support for Semantic MediaWiki
+# see https://www.semantic-mediawiki.org/wiki/Help:EnableSemantics
+# Version of SemanticMediaWiki at install time: {{smwVersion}}
+{% if smwVersion >= "4" %}
+wfLoadExtension( 'SemanticMediaWiki' );
+{% endif %}
+enableSemantics();     
+{% endif %}
+{% for extension in extensions %}
+# {{ extension.name }}
+# {{ extension.url }}
+{{ extension.getLocalSettingsLine(mwShortVersion) }}{% endfor %}
+# modified defaults
+# https://www.mediawiki.org/wiki/Manual:$wgEnableUploads
+$wgEnableUploads = true;
+# https://github.com/WolfgangFahl/pymediawikidocker/issues/30:
+$wgDeprecationReleaseLimit = "1.35.0";
