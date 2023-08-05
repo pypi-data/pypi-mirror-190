cffeb636572635086272f8ad59155b6789e44206
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:13:43 +0000

    Add package decentgradients/0.0.1/decentgradients-0.0.1.tar.gz

---
+++ packages/decentgradients/decentgradients-0.0.1.tar.gz/decentgradients-0.0.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+classifiers = [
+    'Development Status :: 5 - Production/Stable',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: MIT License',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python :: 3',
+]
+
+setup(
+    name='decentgradients',
+    version='0.0.1',
+    description='Calculations',
+    long_description='Description to come',
+    url='',
+    author='Benjamin Kjær',
+    author_email='',
+    license='MIT',
+    classifiers=classifiers,
+    keywords='',
+    packages=find_packages(),
+    install_requires=['']
+)
