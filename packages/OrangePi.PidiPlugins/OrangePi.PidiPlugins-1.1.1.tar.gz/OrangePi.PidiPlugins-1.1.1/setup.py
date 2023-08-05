0caffb1415ea8c4e2a01dfc3c44bc2126b57d312
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:13:27 +0000

    Add package OrangePi.PidiPlugins/1.1.1/OrangePi.PidiPlugins-1.1.1.tar.gz

---
+++ packages/OrangePi.PidiPlugins/OrangePi.PidiPlugins-1.1.1.tar.gz/OrangePi.PidiPlugins-1.1.1/setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup, find_packages
+
+VERSION = "1.1.1"
+
+setup(
+    name="OrangePi.PidiPlugins",
+    version=VERSION,
+    author='Andriy Malyshenko',
+    author_email='andriy@sonocotta.com',
+    description="Pidi plugins for display output using SPI LCDs on the OrangePi.",
+    long_description=open("README.md").read() + "\n" + open("CHANGELOG.txt").read(),
+    long_description_content_type='text/markdown',
+    license="MIT",
+    url="https://github.com/sonocotta/pidi-plugins-python",
+    classifiers=[
+        "Environment :: X11 Applications",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python :: 3.6",
+    ],
+    packages=find_packages(),
+    install_requires=[
+        "fonts",
+        "font_roboto",
+        "Mopidy.OrangePi.Pidi",
+        "OrangePi.ST7789>=1.1.0",
+        "OrangePi.ILI9341>=1.1.1",
+        "Pillow",
+    ],
+    entry_points={
+        'pidi.plugin.display': [
+            'File = OrangePi_PidiPlugins:DisplayFile',
+            'DisplayST7789 = OrangePi_PidiPlugins.DisplayST7789:DisplayST7789',
+            'DisplayILI9341 = OrangePi_PidiPlugins.DisplayILI9341:DisplayILI9341',
+        ]
+    },
+    python_requires=">=2.7",
+    test_suite="tests",
+    include_package_data=True
+)
