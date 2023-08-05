265885255fb3b99a6ed22964b3ab246c72f079fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:13:20 +0000

    Add package shopify_client/3.1.6/shopify_client-3.1.6.tar.gz

---
+++ packages/shopify_client/shopify_client-3.1.6.tar.gz/shopify_client-3.1.6/shopify_client/versions.py
@@ -0,0 +1,11 @@
+from enum import Enum
+
+
+class APIVersion(Enum):
+    APRIL_2021 = '2021-04'
+    OCTOBER_2021 = '2021-10'
+    JANUARY_2022 = '2022-01'
+    JULY_2022 = '2022-07'
+
+
+BASE_VERSION = APIVersion.JULY_2022.value
