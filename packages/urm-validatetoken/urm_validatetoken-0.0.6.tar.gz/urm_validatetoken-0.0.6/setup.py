d1bc2631835970964bcc19627ba3cf7257420974
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:20 +0000

    Add package urm-validatetoken/0.0.6/urm_validatetoken-0.0.6.tar.gz

---
+++ packages/urm-validatetoken/urm_validatetoken-0.0.6.tar.gz/urm_validatetoken-0.0.6/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.6' 
+DESCRIPTION = 'urm_validatetoken package contains method validate_token() which takes access_token as input and returns status of the token as valid or invalid.'
+LONG_DESCRIPTION = 'Method validate_token() takes four required parameters and returns two values.\n\n syntax: <TOKEN_VALIDATION_STATUS>, <TOKEN_STATUS> = urm_validatetoken.validate_token(<REQUEST_METHOD>,<VALIDATE_TOKEN_URL>,<ACCESS_TOKEN>,<CONTENT_TYPE>).\n\n <REQUEST_METHOD>: POST, PUT, GET, PATCH, etc.\n <VALIDATE_TOKEN_URL>: url of validate_token api.\n <ACCESS_TOKEN>: unique access token for authorization.\n <CONTENT_TYPE>: for ex. application/json.\n <TOKEN_VALIDATION_STATUS>: validation status description.\n <TOKEN_STATUS>: it may contain value True or False or status_code(403 or 401).'
+
+# Setting up
+setup(
+        name="urm_validatetoken", 
+        version=VERSION,
+        author="Shubham Nalawade",
+        author_email="s.satish.nalawade@accenture.com",
+        description=DESCRIPTION,
+        long_description=LONG_DESCRIPTION,
+        long_description_content_type="text/markdown",
+        packages=find_packages(),
+        install_requires=["urllib3>=1.26.12","Flask>=2.2.2"], # add any additional packages that 
+        # needs to be installed along with your package. Eg: 'caer'
+        
+        keywords=['python', 'first package'],
+        classifiers= [
+            "Development Status :: 3 - Alpha",
+            "Intended Audience :: Developers",
+            "Programming Language :: Python :: 2",
+            "Programming Language :: Python :: 3",
+            "Operating System :: MacOS :: MacOS X",
+            "Operating System :: Microsoft :: Windows",
+        ]
+)
---
+++ packages/urm-validatetoken/urm_validatetoken-0.0.6.tar.gz/urm_validatetoken-0.0.6/urm_validatetoken/validatetoken.py
@@ -0,0 +1,44 @@
+
+import urllib3
+import json
+import logging
+from flask import jsonify, request
+from functools import wraps
+
+http = urllib3.PoolManager()
+    
+def validate_token(request_method,validate_token_url):
+    def decorator(function):
+        @wraps(function)
+        def wrapper(*args, **kwargs):
+            try:
+                access_token = request.headers.get(constants.HEADER_AUTHORIZATION)
+                content_type = request.headers.get(constants.CONTENT_TYPE_HEADER)
+                if access_token is None or access_token == "":
+                    logging.error(constants.HEADER_AUTHORIZATION_MISSING)
+                    return jsonify({"error-code": 403, "error-desc": "header missing", "error-msg": constants.HEADER_AUTHORIZATION_MISSING}), 403
+                elif constants.BEARER_STRING not in access_token:
+                    logging.error(constants.BEARER_STRING_MISSING)
+                    return jsonify({"error-code": 401, "error-desc": "unauthorized", "error-msg": constants.BEARER_STRING_MISSING}), 401
+                if content_type is None or content_type == "":
+                    logging.error(constants.HEADER_CONTENT_TYPE_MISSING)
+                    return jsonify({"error-code": 403, "error-desc": "header missing", "error-msg": constants.HEADER_CONTENT_TYPE_MISSING}), 403
+
+                response=http.request(request_method,validate_token_url,headers={constants.CONTENT_TYPE_HEADER:content_type, constants.HEADER_AUTHORIZATION:access_token})
+                response_data = json.loads(response.data.decode('utf-8'))
+                logging.info(response_data)
+                logging.info(response.status)
+                token_status = response_data['active']
+                logging.info(token_status)
+                if token_status == False:
+                    logging.error(constants.TOKEN_INVALID_STATUS)
+                    return jsonify({"error-code": 401, "error-desc": "unauthorized", "error-msg": constants.TOKEN_INVALID_STATUS}), 401
+                elif token_status != True:
+                    logging.error(constants.TOKEN_STATUS_VALUE_ERROR)
+                    return jsonify({"error-code": 401, "error-desc": "unauthorized", "error-msg": constants.TOKEN_STATUS_VALUE_ERROR}), 401
+            except Exception as err:
+                logging.error("Error: {}".format(err))
+                return jsonify(str(err)), 405
+            return function(*args, **kwargs)
+        return wrapper
+    return decorator
