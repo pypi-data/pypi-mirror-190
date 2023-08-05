c45b3eefcc9a5cbe42f1a5d1a56b821c08a9e62d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:14:50 +0000

    Add package tharos-pytools/0.0.1/tharos-pytools-0.0.1.tar.gz

---
+++ packages/tharos-pytools/tharos-pytools-0.0.1.tar.gz/tharos-pytools-0.0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+setup(
+    name='tharos-pytools',
+    version='0.1',
+    description='Testing installation of Package',
+    url='https://github.com/Tharos-ux/tharos-pytools',
+    author='Tharos',
+    author_email='dubois.siegfried@gmail.com',
+    license='MIT',
+    packages=['tharos-pytools'],
+    zip_safe=False
+)
