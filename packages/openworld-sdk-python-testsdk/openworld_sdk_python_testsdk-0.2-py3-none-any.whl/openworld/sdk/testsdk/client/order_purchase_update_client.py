39e617d0a6e0c9ec2822a651d9fb2b523354ec66
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:14:44 +0000

    Add package openworld-sdk-python-testsdk/0.2/openworld_sdk_python_testsdk-0.2-py3-none-any.whl

---
+++ packages/openworld-sdk-python-testsdk/openworld_sdk_python_testsdk-0.2-py3-none-any.whl/openworld/sdk/testsdk/client/order_purchase_update_client.py
@@ -0,0 +1,57 @@
+# Copyright 2022 Expedia, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from openworld.sdk.core.constant import header
+import platform
+
+from openworld.sdk.testsdk.model.order_purchase_update_request import OrderPurchaseUpdateRequest
+
+import platform
+from openworld.sdk.core.client.api import ApiClient
+from openworld.sdk.core.configuration.client_config import ClientConfig
+from furl import furl
+from uuid import UUID, uuid4
+
+
+class OrderPurchaseUpdateClient:
+    def __init__(self, client_config: ClientConfig):
+        python_version = platform.python_version()
+        os = platform.platform().split('-')
+        os_name = os[0]
+        os_version = os[1]
+        sdk_metadata = f'open-world-sdk-python-testsdk/0.2'
+
+        self.__api_client = ApiClient(client_config)
+        self.__user_agent = f'{sdk_metadata} (Python {python_version}; {os_name} {os_version})'
+
+    def update(self, order_purchase_update_request: OrderPurchaseUpdateRequest,
+            transaction_id: UUID = uuid4()) -> None:
+        """
+
+        Args:
+
+            order_purchase_update_request(OrderPurchaseUpdateRequest): An OrderPurchaseUpdate request may be of one of the following types `ORDER_UPDATE`, `CHARGEBACK_FEEDBACK`, `INSULT_FEEDBACK`, `REFUND_UPDATE`, `PAYMENT_UPDATE`. 
+        """
+        request_url = furl(self.__api_client.endpoint)
+        request_url /= '/order/purchase/update'
+        request_url.path.normalize()
+
+        return self.__api_client.call(
+            request_headers={  header.TRANSACTION_ID: str(transaction_id), header.USER_AGENT: header.OPENWORLD_SDK_PYTHON + str(self.__user_agent)},
+            method='post',
+            obj=order_purchase_update_request,
+            response_model=None,
+            url=request_url
+        )
+
---
+++ packages/openworld-sdk-python-testsdk/openworld_sdk_python_testsdk-0.2-py3-none-any.whl/openworld/sdk/testsdk/model/error.py
@@ -0,0 +1,44 @@
+# Copyright 2022 Expedia, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from dataclasses_json import config
+from dataclasses_json import dataclass_json
+from dataclasses import dataclass
+from dataclasses import field
+
+def require_value(var: str):
+    raise TypeError(f'None value not allowed for attribute {var}!')
+
+
+@dataclass_json
+@dataclass
+class Error:
+    """@dataclass Error The object used the describe an error, containing both human-readable and in a machine-readable information.
+
+    Attributes:
+        type(str):A URI reference, compliant with the standard EG error type format, which identifies the error type. It provides a machine-readable identifier for the error type. The error type will be aligned with the HTTP status code of the response. The URI can either be absolute or relative to the API's base URI. When dereferenced, it can also provide human-readable documentation for the error type. 
+
+        detail(str):A human-readable explanation of the error, specific to this error occurrence.
+    """
+    type: str = field(
+        default_factory=lambda: require_value("type"),
+        metadata=config(exclude=lambda f: f is None)
+    )
+    detail: str = field(
+        default_factory=lambda: require_value("detail"),
+        metadata=config(exclude=lambda f: f is None)
+    )
+
+
+
