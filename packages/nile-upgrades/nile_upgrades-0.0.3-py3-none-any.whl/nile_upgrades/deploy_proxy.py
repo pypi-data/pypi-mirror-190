128faf3c444da18529a41bcc358ccbcfa3a9e7ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:09:37 +0000

    Add package nile-upgrades/0.0.3/nile_upgrades-0.0.3-py3-none-any.whl

---
+++ packages/nile-upgrades/nile_upgrades-0.0.3-py3-none-any.whl/nile_upgrades/deploy_proxy.py
@@ -0,0 +1,76 @@
+import logging
+import os
+
+from starkware.starknet.compiler.compile import get_selector_from_name
+
+from nile_upgrades.common import declare_class, get_contract_abi
+
+
+async def deploy_proxy(
+    nre,
+    account,
+    contract_name,
+    initializer_args,
+    initializer='initializer',
+    salt=0,
+    unique=True,
+    alias=None,
+    max_fee_declare_impl=None,
+    max_fee_declare_proxy=None,
+    max_fee_deploy_proxy=None,
+    standalone_mode=None
+):
+    """
+    Deploy an upgradeable proxy for an implementation contract.
+
+    Returns a Nile Transaction instance representing the proxy deployment.
+
+    `nre` - the `NileRuntimeEnvironment` object.
+
+    `account` - the Account to use.
+
+    `contract_name` - the name of the implementation contract.
+
+    `initializer_args` - array of arguments for the initializer function.
+
+    `initializer` - initializer function name. Defaults to `'initializer'`.
+
+    `salt` - the salt for proxy address generation. Defaults to `0`.
+
+    `unique` - whether the account address should be taken into account for proxy address generation. Defaults to `True`.
+
+    `alias` - Unique identifier for your proxy. Defaults to `None`.
+
+    `max_fee_declare_impl` - Maximum fee for declaring the implementation contract. Defaults to `None`.
+
+    `max_fee_declare_proxy` - Maximum fee for declaring the proxy contract. Defaults to `None`.
+
+    `max_fee_deploy_proxy` - Maximum fee for deploying the proxy contract. Defaults to `None`.
+    """
+
+    # Declare implementation
+    impl_class_hash = await declare_class(nre.network, contract_name, account, max_fee_declare_impl)
+
+    # Declare proxy
+    await declare_class(nre.network, "Proxy", account, max_fee_declare_proxy, overriding_path=_get_proxy_artifact_path())
+
+    # Deploy proxy
+    logging.debug(f"Deploying proxy...")
+    selector = get_selector_from_name(initializer)
+    deploy_tx = await account.deploy_contract(
+        "Proxy",
+        salt=salt,
+        unique=unique,
+        calldata=[impl_class_hash, selector, len(initializer_args), *initializer_args],
+        max_fee=max_fee_deploy_proxy,
+        alias=alias,
+        overriding_path=_get_proxy_artifact_path(),
+        abi=get_contract_abi(contract_name),
+    )
+
+    return deploy_tx
+
+
+def _get_proxy_artifact_path():
+    package = os.path.dirname(os.path.realpath(__file__))
+    return (f"{package}/artifacts", f"{package}/artifacts/abis")
---
+++ packages/nile-upgrades/nile_upgrades-0.0.3-py3-none-any.whl/nile_upgrades/upgrade_proxy.py
@@ -0,0 +1,80 @@
+import logging
+
+from nile import deployments
+from nile.common import is_alias
+from nile.utils import normalize_number, hex_class_hash, hex_address
+
+from nile_upgrades.common import declare_class, get_contract_abi
+
+
+async def upgrade_proxy(
+    nre,
+    account,
+    proxy_address_or_alias,
+    contract_name,
+    max_fee_declare_impl=None,
+    max_fee_upgrade_proxy=None,
+    standalone_mode=None
+):
+    """
+    Upgrade a proxy to a different implementation contract.
+
+    Returns a Nile Transaction instance representing the upgrade operation.
+
+    `nre` - the `NileRuntimeEnvironment` object.
+
+    `account` - the Account to use.
+
+    `proxy_address_or_alias` - the proxy address or alias.
+
+    `contract_name` - the name of the implementation contract to upgrade to.
+
+    `max_fee_declare_impl` - Maximum fee for declaring the new implementation contract. Defaults to `None`.
+
+    `max_fee_upgrade_proxy` - Maximum fee for upgrading the proxy to the new implementation. Defaults to `None`.
+    """
+
+    proxy_address = _load_deployment(proxy_address_or_alias, nre.network)
+
+    # Declare new implementation
+    impl_class_hash = await declare_class(nre.network, contract_name, account, max_fee_declare_impl)
+
+    # Perform upgrade
+    logging.info(f"⏭️  Upgrading proxy {hex_address(proxy_address)} to class hash {hex_class_hash(impl_class_hash)}")
+    upgrade_tx = await account.send(
+        proxy_address, "upgrade", calldata=[impl_class_hash], max_fee=max_fee_upgrade_proxy
+    )
+
+    # Update ABI in deployments file
+    deployments.update_abi(
+        proxy_address, get_contract_abi(contract_name), nre.network
+    )
+
+    return upgrade_tx
+
+
+def _load_deployment(proxy_address_or_alias, network):
+    ids = None
+    if not is_alias(proxy_address_or_alias):
+        ids = deployments.load(normalize_number(proxy_address_or_alias), network)
+    else:
+        ids = deployments.load(proxy_address_or_alias, network)
+
+    id = next(ids, None)
+    if id is None:
+        raise Exception(
+            f"Deployment with address or alias {_normalize_string(proxy_address_or_alias)} not found"
+        )
+    if next(ids, None) is not None:
+        raise Exception(
+            f"Multiple deployments found with address or alias {_normalize_string(proxy_address_or_alias)}"
+        )
+
+    return id[0]
+
+
+def _normalize_string(proxy_address_or_alias):
+    identifier = proxy_address_or_alias
+    if type(identifier) is int:
+        identifier = hex_address(identifier)
+    return identifier
