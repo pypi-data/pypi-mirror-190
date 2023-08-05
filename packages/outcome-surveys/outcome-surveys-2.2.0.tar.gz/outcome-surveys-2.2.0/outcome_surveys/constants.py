740c716f6fa6a8d43399308b9b93552134a3b40f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:13:54 +0000

    Add package outcome-surveys/2.2.0/outcome-surveys-2.2.0.tar.gz

---
+++ packages/outcome-surveys/outcome-surveys-2.2.0.tar.gz/outcome-surveys-2.2.0/outcome_surveys/constants.py
@@ -0,0 +1,7 @@
+"""
+Constants for outcome_surveys.
+"""
+
+SEGMENT_LEARNER_PASSED_COURSE_FIRST_TIME_EVENT_TYPE = 'edx.course.learner.passed.first_time'
+SEGMENT_LEARNER_PASSED_COURSE_FIRST_TIME_FOLLOW_UP_EVENT_TYPE = 'edx.course.learner.passed.first_time.followup'
+OUTCOME_SURVEYS_FOLLOW_UP_DAYS_DEFAULT = 90
