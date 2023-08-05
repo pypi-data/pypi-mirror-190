a2e963258647aeb084dc899cb5e6a6e091228135
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:22:22 +0000

    Add package News-NLP/0.4/News_NLP-0.4.tar.gz

---
+++ packages/News-NLP/News_NLP-0.4.tar.gz/News_NLP-0.4/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+setup(name='News_NLP',
+version='0.4',
+description='This package aims to collect news data via Google News API, preporcessing, implement pretrained news type classifer and sentiment classifier and perform predictive model for news event and then implememnt multiple geolocator',
+url='https://github.com/HigherHoopern/News_NLP/blob/main/TestNoteBook.ipynb',
+author='JasonLu',
+author_email='luzhenxian@hotmail.com',
+license='MIT',
+packages=['News_NLP'],
+zip_safe=False)
