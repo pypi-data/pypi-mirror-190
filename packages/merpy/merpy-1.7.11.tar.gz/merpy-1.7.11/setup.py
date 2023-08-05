1b04369a36e1ec6f093b404099c8354add222533
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:11:25 +0000

    Add package merpy/1.7.11/merpy-1.7.11.tar.gz

---
+++ packages/merpy/merpy-1.7.11.tar.gz/merpy-1.7.11/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+setup(
+    name="merpy",
+    version="1.7.11",
+    description="Use Minimal Named-Entity Recognizer (MER) inside python",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Andre Lamurias",
+    author_email="alamurias@lasige.di.fc.ul.pt",
+    maintainer="Francisco M Couto",
+    maintainer_email="fjcouto@edu.ulisboa.pt",
+    packages=["merpy"],
+    keywords=["ner", "named-entity recognition", "entity linking", "ontologies"],
+    url="https://github.com/lasigeBioTM/merpy",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_data={"merpy": ["MER/*", "MER/data/*"]},
+    install_requires=["ssmpy"],
+    extras_require={"dev": ["setuptools-changelog"]},
+)
