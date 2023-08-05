6c2ae55f1e365d8459278aa062d26188763461ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:15:49 +0000

    Add package woomodule/1.42/woomodule-1.42-py3-none-any.whl

---
+++ packages/woomodule/woomodule-1.42-py3-none-any.whl/woomodule/app/app.py
@@ -0,0 +1,9 @@
+from random import randint
+
+
+def create_random_str():
+    return str(randint(1, 10))
+
+
+def get_str():
+    return create_random_str()
---
+++ packages/woomodule/woomodule-1.42-py3-none-any.whl/woomodule/main.py
@@ -0,0 +1,7 @@
+# from src.woomodule.app.app import get_str
+from .app.app import get_str
+
+
+def main():
+    get_str()
+
