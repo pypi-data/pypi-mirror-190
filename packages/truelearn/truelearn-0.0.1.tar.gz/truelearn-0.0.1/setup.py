2890cbcda2dc2bea4383934bc7fcdd9f90d6f8cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:37:09 +0000

    Add package truelearn/0.0.1/truelearn-0.0.1.tar.gz

---
+++ packages/truelearn/truelearn-0.0.1.tar.gz/truelearn-0.0.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='truelearn',
+    python_requires='>=3.7',
+    version='0.0.01',
+    license='MIT',
+    author="KD-7",
+    author_email='',
+    packages=['models', 'tests'],
+    url='',
+    keywords='',
+    install_requires=[],
+)
