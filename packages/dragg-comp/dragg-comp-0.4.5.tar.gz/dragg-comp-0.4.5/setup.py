40fee7dec8bab01bfe872da59f132ec61bed0b24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:34:06 +0000

    Add package dragg-comp/0.4.5/dragg-comp-0.4.5.tar.gz

---
+++ packages/dragg-comp/dragg-comp-0.4.5.tar.gz/dragg-comp-0.4.5/setup.py
@@ -0,0 +1,28 @@
+import os
+from setuptools import setup, find_packages
+
+setup(name='dragg-comp',
+      license='MIT',
+      version='0.4.5',
+      author='Aisling Pigott and Jacob Kravits',
+      author_email='aisling.pigott@colorado.edu',
+      packages=find_packages(),
+      install_requires=[
+        'asyncio',
+        'aioredis',
+        'gym',
+        'pathos',
+        'datetime',
+        'async-timeout',
+        'dragg',
+        'cvxopt',
+        'stable-baselines3',
+        'kaleido'
+        ],
+      py_modules=['dragg_comp'],
+      classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        ],
+     )
