a73829300a57bbe55032d167a812be931dff5a4f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:26:08 +0000

    Add package psu-base/0.5.3/psu_base-0.5.3-py3-none-any.whl

---
+++ packages/psu-base/psu_base-0.5.3-py3-none-any.whl/demo/sass_variables.py
@@ -0,0 +1,39 @@
+# Any SASS variables that you would like to override may be defined in this file
+# If you'd rather use a default value, either set the value to None, or remove it altogether.
+
+# Every time you change the values, delete your sass_build directory to re-generate the css
+
+# ---- COLORS ----
+# Colors may be defined as their HTML color name, hex code, or as RGB or RGBA tuples
+# Color names may contain non-word characters for readability, and are not case-sensitive
+#     Example: "Light_Slate-Gray" would be converted to "lightslategray"
+PRIMARY_BG_COLOR = 'psusienna'
+PRIMARY_FG_COLOR = None
+HEADING_COLOR = None
+TEXT_COLOR = None
+CODE_COLOR = None
+
+# By default, primary and default (i.e. btn-primary, btn-default) colors are based on primary bg and fg colors above
+# Depending on what you selected, the default variations of them may look awful!
+BOOTSTRAP_PRIMARY = None
+BOOTSTRAP_PRIMARY_COMPLIMENT = None
+BOOTSTRAP_DEFAULT = None
+BOOTSTRAP_DEFAULT_COMPLIMENT = None
+
+# Standard danger, warning, etc colors
+BOOTSTRAP_DANGER = None                 # red
+BOOTSTRAP_DANGER_COMPLIMENT = None      # white
+BOOTSTRAP_WARNING = None                # yellowish
+BOOTSTRAP_WARNING_COMPLIMENT = None     # maroon
+BOOTSTRAP_INFO = None                   # blue
+BOOTSTRAP_INFO_COMPLIMENT = None        # white
+BOOTSTRAP_SUCCESS = None                # green
+BOOTSTRAP_SUCCESS_COMPLIMENT = None     # white
+
+# ---- FONTS ----
+HEADING_FONT = None
+TEXT_FONT = None
+CODE_FONT = None
+
+
+
