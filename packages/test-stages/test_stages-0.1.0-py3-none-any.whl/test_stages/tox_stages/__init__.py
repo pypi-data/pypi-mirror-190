9adf20bae7773e60c95aa90864d437f24c010e8e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:56:34 +0000

    Add package test-stages/0.1.0/test_stages-0.1.0-py3-none-any.whl

---
+++ packages/test-stages/test_stages-0.1.0-py3-none-any.whl/test_stages/tox_stages/__init__.py
@@ -0,0 +1,5 @@
+"""A `test-stages` implementation for the Tox test runner.
+
+This module contains the configuration parsing and runtime glue to
+let Tox run its test environments grouped in stages.
+"""
