807abe630a0c20176a7ab24afc45537f0c411406
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:21:42 +0000

    Add package djangoldp-needle/0.1.18/djangoldp_needle-0.1.18.tar.gz

---
+++ packages/djangoldp-needle/djangoldp_needle-0.1.18.tar.gz/djangoldp_needle-0.1.18/djangoldp_needle/request_parser/request_parser.py
@@ -0,0 +1,22 @@
+from .parsers import OpenGraph
+from bs4 import BeautifulSoup
+from ..models import AnnotationTarget
+
+
+class RequestParser:
+    def __init__(self):
+        self.parsers = [
+            OpenGraph()
+        ]
+
+    def parse(self, targe_url, target_html):
+        beautiful_soup_document = BeautifulSoup(target_html, "html.parser")
+        annotation_target = AnnotationTarget()
+        parse_valid = False
+
+        for parser in self.parsers:
+            parser_parse_valid = parser.parse(annotation_target, targe_url, beautiful_soup_document,
+                         parse_valid)
+            parse_valid = parse_valid or parser_parse_valid
+
+        return parse_valid, annotation_target
