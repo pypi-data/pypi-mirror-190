88095494642f92dc8012017abeccd34151f4229b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:14:21 +0000

    Add package awscrt/0.16.10/awscrt-0.16.10.tar.gz

---
+++ packages/awscrt/awscrt-0.16.10.tar.gz/awscrt-0.16.10/crt/s2n/bindings/rust/s2n-tls/src/lib.rs
@@ -0,0 +1,22 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+#![allow(clippy::derive_partial_eq_without_eq)]
+
+extern crate alloc;
+
+#[macro_use]
+pub mod error;
+
+pub mod callbacks;
+pub mod config;
+pub mod connection;
+pub mod enums;
+pub mod init;
+pub mod pool;
+pub mod security;
+
+pub use s2n_tls_sys as ffi;
+
+#[cfg(any(feature = "testing", test))]
+pub mod testing;
