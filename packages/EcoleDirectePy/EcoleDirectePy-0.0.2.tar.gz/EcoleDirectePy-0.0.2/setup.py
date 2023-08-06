76550a64f92ffe79476e7dd437c6294f23e1e970
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:16:00 +0000

    Add package EcoleDirectePy/0.0.2/EcoleDirectePy-0.0.2.tar.gz

---
+++ packages/EcoleDirectePy/EcoleDirectePy-0.0.2.tar.gz/EcoleDirectePy-0.0.2/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+
+VERSION = '0.0.2'
+DESCRIPTION = 'a API Wrapper for EcoleDirecte.fr'
+
+# Setting up
+setup(
+    name="EcoleDirectePy",
+    version=VERSION,
+    author="DK16",
+    author_email="<dk16v2@gmail.com>",
+    description=DESCRIPTION,
+    packages=find_packages(),
+    install_requires=['requests', 'json'],
+    keywords=['python', 'ecoledirecte', 'api', 'Wrapper'],
+)
