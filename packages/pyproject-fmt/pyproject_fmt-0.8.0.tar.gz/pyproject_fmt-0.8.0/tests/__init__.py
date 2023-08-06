18f690dd60aeadaaa0b3ccb7b04cf31118056a81
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:14:32 +0000

    Add package pyproject-fmt/0.8.0/pyproject_fmt-0.8.0.tar.gz

---
+++ packages/pyproject-fmt/pyproject_fmt-0.8.0.tar.gz/pyproject_fmt-0.8.0/tests/__init__.py
@@ -0,0 +1,11 @@
+from __future__ import annotations
+
+from typing import Callable
+
+from tomlkit.toml_document import TOMLDocument
+
+from pyproject_fmt.formatter.config import Config
+
+Fmt = Callable[[Callable[[TOMLDocument, Config], None], str, str], None]
+
+__all__ = ["Fmt"]
