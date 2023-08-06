cb2f7640b2781ad3fb79346c710e5d7e12c94f52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:08:53 +0000

    Add package brevettiai/0.7.2/brevettiai-0.7.2-py3-none-any.whl

---
+++ packages/brevettiai/brevettiai-0.7.2-py3-none-any.whl/brevettiai/datamodel/__init__.py
@@ -0,0 +1,7 @@
+# Take care what you put in here. It will be loaded by all users of the library
+
+from .camelmodel import CamelModel
+from .image_annotation import ImageAnnotation, AnnotationTypes, PolygonAnnotation,\
+    RectangleAnnotation, LineAnnotation, PointAnnotation
+from .tag import Tag
+from .image import BBOX, SequenceShape, ImageRegion, SequenceRange
