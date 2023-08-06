5a5c07b59fa4e98af7a0e672fa3520beffa2e968
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:31:38 +0000

    Add package truelearn/0.0.2/truelearn-0.0.2.tar.gz

---
+++ packages/truelearn/truelearn-0.0.2.tar.gz/truelearn-0.0.2/README.rst
@@ -0,0 +1,16 @@
+
+
+TrueLearn
+===============
+Project for the Truelearn python library
+
+Installing
+============
+
+.. code-block:: bash
+
+    pip install truelearn
+
+Usage
+=====
+To be added
---
+++ packages/truelearn/truelearn-0.0.2.tar.gz/truelearn-0.0.2/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+setup(
+    name='truelearn',
+    python_requires='>=3.7',
+    version='0.0.2',
+    license='MIT',
+    author="KD-7",
+    author_email='',
+    long_description=open('README.rst').read(),
+    packages=['models', 'tests'],
+    url='',
+    keywords='',
+    install_requires=[],
+    classifiers=[
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "License :: OSI Approved :: MIT License", ]
+)
