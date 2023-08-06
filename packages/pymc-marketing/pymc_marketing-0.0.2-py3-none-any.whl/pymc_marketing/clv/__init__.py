7a7da35a194e9fc7d80638befacd92526b304813
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:13:44 +0000

    Add package pymc-marketing/0.0.2/pymc_marketing-0.0.2-py3-none-any.whl

---
+++ packages/pymc-marketing/pymc_marketing-0.0.2-py3-none-any.whl/pymc_marketing/clv/__init__.py
@@ -0,0 +1,13 @@
+from pymc_marketing.clv.models import (
+    BetaGeoModel,
+    GammaGammaModel,
+    GammaGammaModelIndividual,
+)
+from pymc_marketing.clv.utils import customer_lifetime_value
+
+__all__ = (
+    "BetaGeoModel",
+    "GammaGammaModel",
+    "GammaGammaModelIndividual",
+    "customer_lifetime_value",
+)
