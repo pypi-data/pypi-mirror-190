b12b03abcedc081ddea2b31c98a61573fd397140
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:17:46 +0000

    Add package hdict/0.230209.1/hdict-0.230209.1-py3-none-any.whl

---
+++ packages/hdict/hdict-0.230209.1-py3-none-any.whl/hdict/__init__.py
@@ -0,0 +1,48 @@
+#  Copyright (c) 2023. Davi Pereira dos Santos
+#  This file is part of the hdict project.
+#  Please respect the license - more about this in the section (*) below.
+#
+#  hdict is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  hdict is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with hdict.  If not, see <http://www.gnu.org/licenses/>.
+#
+#  (*) Removing authorship by any means, e.g. by distribution of derived
+#  works or verbatim, obfuscated, compiled or rewritten versions of any
+#  part of this work is illegal and it is unethical regarding the effort and
+#  time spent here.
+#
+from typing import Union
+
+from hdict.content.apply import apply
+from hdict.content.default import default
+from hdict.content.field import field
+from hdict.content.sample import sample
+from hdict.content.value import value
+from hdict.hdict_ import hdict
+from hosh import Hosh
+
+
+class _:
+    Ø = hdict()
+
+    def __call__(self, f: Union[callable, "apply", field], *applied_args, fhosh: Hosh = None, **applied_kwargs):
+        return apply(f, *applied_args, fhosh=fhosh, **applied_kwargs)
+
+    def __getattr__(self, item):
+        return field(item)
+
+    def __getitem__(self, item):
+        return sample(*item)
+
+
+_ = _()
+Ø = hdict()
