0b8caf06539daff8b08b676f0e968167e3743b16
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:31:46 +0000

    Add package gep3/0.0.42/gep3-0.0.42-py3-none-any.whl

---
+++ packages/gep3/gep3-0.0.42-py3-none-any.whl/pcsc/ml.py
@@ -0,0 +1,23 @@
+"""ml.py
+"""
+
+# Standard library imports
+from typing import Union
+
+# Third party imports
+
+# Local application imports
+from multos import GET_MULTOS_DATA as _GET_MULTOS_DATA, MultosData, MultosDataField
+from pcsc.card import Protocol, send_apdu
+
+
+def GET_MULTOS_DATA(hcard, protocol: Protocol) -> MultosData:
+    r, error = send_apdu(hcard, protocol, _GET_MULTOS_DATA())
+
+    if error is not None:
+        return None, error
+    elif r.SW12() != '9000':
+        return None, F"Response bytes to GET MULTOS DATA is not 9000 (SW12 = {r.SW12()})"
+    else:
+        multos_data = MultosData(r.data())
+        return multos_data, None
