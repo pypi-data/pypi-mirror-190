dc67a4af9a74b8b45bf91d24f0e1476d7bde309e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:26:07 +0000

    Add package psu-base/0.4.2/psu_base-0.4.2-py3-none-any.whl

---
+++ packages/psu-base/psu_base-0.4.2-py3-none-any.whl/demo/local_settings.py
@@ -0,0 +1,30 @@
+# Environment choices: {DEV, TEST, PROD}
+ENVIRONMENT = 'DEV'
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = '3c4zcw#^0jq7s$^@uw_#ej_n0!t1q*x*gmmf9nr$^x6$2u49sl'
+
+# Name of machine running the application
+ALLOWED_HOSTS = ['localhost']
+
+# Debug mode (probably only true in DEV)
+DEBUG = True
+
+# SSO URL
+CAS_SERVER_URL = 'https://sso-stage.oit.pdx.edu/idp/profile/cas/login'
+
+# Finti URL (dev, test, or prod)
+FINTI_URL = 'https://ws-test.oit.pdx.edu'
+
+# Finti Token (grants access to the APIs)
+# FINTI_TOKEN = '...'
+
+# Finti URLs (for reference)
+# http://localhost:8888
+# https://ws-test.oit.pdx.edu
+# https://ws.oit.pdx.edu
+
+ELEVATE_DEVELOPER_ACCESS = False
+
+# Session expiration
+SESSION_COOKIE_AGE = 4 * 60 * 60  # 4 hours
---
+++ packages/psu-base/psu_base-0.4.2-py3-none-any.whl/psu_base/__init__.py
@@ -0,0 +1,7 @@
+__version__ = '0.4.2'
+name = "psu_base"
+
+default_app_config = 'psu_base.apps.PsuTemplateConfig'
+
+
+
