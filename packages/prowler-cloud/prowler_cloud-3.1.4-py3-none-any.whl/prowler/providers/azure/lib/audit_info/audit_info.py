535b46a5adc613e7edf9e507f1b3821bc0c10107
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:39:58 +0000

    Add package prowler-cloud/3.1.4/prowler_cloud-3.1.4-py3-none-any.whl

---
+++ packages/prowler-cloud/prowler_cloud-3.1.4-py3-none-any.whl/prowler/providers/azure/lib/audit_info/audit_info.py
@@ -0,0 +1,8 @@
+from prowler.providers.azure.lib.audit_info.models import (
+    Azure_Audit_Info,
+    Azure_Identity_Info,
+)
+
+azure_audit_info = Azure_Audit_Info(
+    credentials=None, identity=Azure_Identity_Info(), audit_metadata=None
+)
