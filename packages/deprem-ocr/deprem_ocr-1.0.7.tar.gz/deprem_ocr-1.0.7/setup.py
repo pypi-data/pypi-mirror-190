a30c8677090bba5186168ea696e571355e04dbc1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:25:57 +0000

    Add package deprem-ocr/1.0.7/deprem_ocr-1.0.7.tar.gz

---
+++ packages/deprem-ocr/deprem_ocr-1.0.7.tar.gz/deprem_ocr-1.0.7/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="deprem_ocr",
+    version="1.0.7",
+    author="Kutsal Ozkurt",
+    author_email="kutsal_baran@hotmail.com",
+    description="Extract texts from images and screenshots",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Goodsea/deprem-ocr",
+    project_urls={
+        "Bug Tracker": "https://github.com/Goodsea/deprem-ocr/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    package_data={'deprem_ocr': ['src/deprem_ocr/ch_PP-OCRv3_det_infer/*', 'src/deprem_ocr/ch_PP-OCRv3_rec_infer/*', 'src/deprem_ocr/ppocr/ppocr_keys_v1.txt']},
+    python_requires=">=3.10",
+)
