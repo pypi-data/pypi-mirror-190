065bf96af43371474145a9662691f3e664f214de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:36:52 +0000

    Add package alibuild/1.14.0/alibuild-1.14.0.tar.gz

---
+++ packages/alibuild/alibuild-1.14.0.tar.gz/alibuild-1.14.0/alibuild_helpers/__init__.py
@@ -0,0 +1,9 @@
+# This file is needed to package build_template.sh.
+
+# Versions should comply with PEP440. For a discussion on single-sourcing the
+# version across setup.py and the project code, see
+# https://packaging.python.org/en/latest/single_source_version.html
+#
+# v1.14.0 is actually a placeholder which will be automatically replaced by
+# the release-alibuild pipeline in jenkins whenever we need a new release.
+__version__ = 'v1.14.0'
