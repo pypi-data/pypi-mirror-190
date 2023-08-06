0bece7aeedf1ba5ca7aade5a9de5db12b162544a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:08:24 +0000

    Add package pypi-pubpack-vin10/0.4.0/pypi_pubpack_vin10-0.4.0.tar.gz

---
+++ packages/pypi-pubpack-vin10/pypi_pubpack_vin10-0.4.0.tar.gz/pypi_pubpack_vin10-0.4.0/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['pypi_demo']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['numpy==1.19.5']
+
+setup_kwargs = {
+    'name': 'pypi-pubpack-vin10',
+    'version': '0.4.0',
+    'description': 'Upload Python package to PyPI (demo)',
+    'long_description': '# pypi-test',
+    'author': 'Your Name',
+    'author_email': 'you@example.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
