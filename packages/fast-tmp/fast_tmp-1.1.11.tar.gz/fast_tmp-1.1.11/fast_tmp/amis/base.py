745fdeb071adc487f72e2e5bfd86cd5e9769aa72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:14:20 +0000

    Add package fast-tmp/1.1.11/fast_tmp-1.1.11.tar.gz

---
+++ packages/fast-tmp/fast_tmp-1.1.11.tar.gz/fast_tmp-1.1.11/fast_tmp/amis/base.py
@@ -0,0 +1,42 @@
+from typing import List, Optional, TypeVar
+
+from pydantic.main import BaseModel
+
+from fast_tmp.amis.enums import ActionTypeEnum, ButtonLevelEnum, ButtonSize
+
+
+class SchemaNode(BaseModel):
+    """
+    所有amis类的父类
+    """
+
+    type: str
+
+
+SchemaArray = TypeVar("SchemaArray", bound=List[SchemaNode])
+
+
+class _Action(SchemaNode):
+    """
+    操作按钮
+    """
+
+    type = "button"
+    label: str
+    actionType: ActionTypeEnum
+    icon: Optional[str]
+    size: Optional[ButtonSize] = None
+    level: ButtonLevelEnum = ButtonLevelEnum.primary
+    tooltip: Optional[str]  # 鼠标挪上去的提示
+    className: Optional[str]
+
+
+class Message(BaseModel):
+    """
+    消息框
+    """
+
+    fetchSucss: Optional[str]
+    fetchFailed: Optional[str]
+    saveSuccess: Optional[str]
+    saveFailed: Optional[str]
