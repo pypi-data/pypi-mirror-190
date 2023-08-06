b8b5c6c3020f0395202e46b3b55f3f0573454d3f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:16:54 +0000

    Add package hai-gui-framework/0.0.3/hai-gui-framework-0.0.3.tar.gz

---
+++ packages/hai-gui-framework/hai-gui-framework-0.0.3.tar.gz/hai-gui-framework-0.0.3/HaiGF/__main__.py
@@ -0,0 +1,49 @@
+
+import sys, os
+from pathlib import Path
+from PySide2.QtWidgets import QApplication, QMainWindow
+from PySide2.QtCore import QTranslator
+from PySide2.QtGui import QFontDatabase
+here = Path(__file__).parent
+
+
+from .apis import HMainWindow
+# from .plugins import AppMainWindow
+from .apis import __version__, __appname__
+
+
+
+
+def run(name='framework'):
+    translator = QTranslator()
+    translator.load(f"{here}/gui_framework/translate/translate_zh_CN.qm")
+    
+    app = QApplication(sys.argv)
+    app.setApplicationName(__appname__)
+    app.installTranslator(translator)
+
+    font_path = f'{here}/gui_framework/translate/fonts/HarmonyOS_Sans_SC_Regular.ttf'
+    id = QFontDatabase.addApplicationFont(font_path)
+    # QFontDatabase.systemFont(QFontDatabase.GeneralFont)
+    # xx = QFontDatabase.applicationFontFamilies(id)
+    # print(xx)
+    # font_str = QFontDatabase.applicationFontFamilies(id)[0]
+
+    mw = HMainWindow()
+
+    from .plugins.hai_tools import AIPlugin
+    mw.install_plugin(AIPlugin)
+
+    from .plugins.load_img import LoadImagePlugin
+    mw.install_plugin(LoadImagePlugin)
+
+    from .plugins.annotation_tools import AnnoPlugin
+    mw.install_plugin(AnnoPlugin)
+
+    from .plugins.pyqtgraph import PyqtGraphPlugin
+    mw.install_plugin(PyqtGraphPlugin)
+
+    mw.show()
+    mw.raise_()
+    sys.exit(app.exec_())
+
---
+++ packages/hai-gui-framework/hai-gui-framework-0.0.3.tar.gz/hai-gui-framework-0.0.3/HaiGF/gui_framework/widgets/__init__.py
@@ -0,0 +1,29 @@
+
+from .common.hai_action import HAction
+from .common.hai_splitter import HSplitter
+from .common.hai_page import HPage
+from .common.hai_msb_widget import HMainSideBarWidget
+from .common.hai_plugin import HPlugin
+
+from .central_widgets.hai_tab_bar import HTabBar
+from .central_widgets.tab_widget import HTabWidget
+from .central_widgets.start_page.examples_page import HExamplesPage
+
+from .core_func_bar.tool_bar import get_toolbar, CoreFuncBar
+from .central_widgets.central_widget import get_central_widget, CentralWidget
+from .central_widgets.start_page.start_page import HStartPage
+
+from .main_side_bar.main_side_bar import get_main_side_bar, MainSideBar
+from .main_side_bar.explorer_widget import ExplorerWidget  # 资源浏览器
+from .main_side_bar.ai_widget.ai_widget import AIWidget  # AI工具
+
+from .aux_side_bar.aux_side_bar import get_aux_side_bar, AuxSideBar
+
+from .panel.panel_widget import get_panel_widget, PanelWidget
+
+from .status_bar.hai_status_bar import HStatusBar
+
+from .menu_bar.hai_menu_bar import HMenuBar
+
+
+
