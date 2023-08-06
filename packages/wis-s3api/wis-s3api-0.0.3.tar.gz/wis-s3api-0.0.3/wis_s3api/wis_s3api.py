ee272310285d678ab45225484c26e47dfc4d4476
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:18:32 +0000

    Add package wis-s3api/0.0.3/wis-s3api-0.0.3.tar.gz

---
+++ packages/wis-s3api/wis-s3api-0.0.3.tar.gz/wis-s3api-0.0.3/wis_s3api/wis_s3api.py
@@ -0,0 +1,7 @@
+"""Main module."""
+
+
+endpoint_url = 'http://minio.waterism.com:9000'
+access_key = 'JKhbLNL0jNKqbjn4'
+secret_key = '0RDubDRBIrC2WOHAP4nHtYP28TXtVj8H'
+bucket_path = 'test/geodata/'
