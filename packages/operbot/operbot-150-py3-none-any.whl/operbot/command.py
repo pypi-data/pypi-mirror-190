3c5a6599867961b2051c507e70cc6667b34fc610
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:16:14 +0000

    Add package operbot/150/operbot-150-py3-none-any.whl

---
+++ packages/operbot/operbot-150-py3-none-any.whl/operbot/command.py
@@ -0,0 +1,24 @@
+# This file is placed in the Public Domain.
+
+
+"handler"
+
+
+from .message import Message
+
+
+def __dir__():
+    return (
+            'Command',
+           )
+
+
+__all__ = __dir__()
+
+
+class Command(Message):
+
+
+    def __init__(self):
+        Message.__init__(self)
+        self.type = "command"
