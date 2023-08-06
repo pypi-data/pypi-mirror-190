d8f3283fcd815422b0d7b6344d5185515a2c3542
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:00:21 +0000

    Add package mobio-base-sdk-dev/1.0.25/mobio-base-sdk-dev-1.0.25.tar.gz

---
+++ packages/mobio-base-sdk-dev/mobio-base-sdk-dev-1.0.25.tar.gz/mobio-base-sdk-dev-1.0.25/setup.py
@@ -0,0 +1,37 @@
+import os
+
+from setuptools import setup, find_packages
+
+# read the contents of your README file
+
+this_directory = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+version_dev='1.0.25'
+version_prod='1.0.0'
+
+mode = '-dev'
+if os.environ.get('BUILD_MODE', 'dev') in ['prod']:
+    mode = ''
+
+setup(name='mobio-base-sdk' + mode,
+      version='1.0.25',
+      description='Mobio project SDK',
+      url='',
+      author='MOBIO',
+      author_email='contact@mobio.vn',
+      license='MIT',
+      package_dir={'': './'},
+      packages=find_packages('./'),
+      install_requires=['m-singleton',
+                        'm-monitor',
+                        'm-validator',
+                        'Flask>=1.1.2',
+                        'flask-cors',
+                        'configparser==3.5.0',
+                        'logstash_formatter==0.5.17'],
+      long_description=long_description,
+      long_description_content_type='text/markdown',
+      python_requires='>=3.7'
+      )
