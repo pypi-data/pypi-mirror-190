6ae5643c342be4946791a6b1ab9cca17f12e41e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:39 +0000

    Add package ansys-sphinx-theme/0.8.2/ansys-sphinx-theme-0.8.2.tar.gz

---
+++ packages/ansys-sphinx-theme/ansys-sphinx-theme-0.8.2.tar.gz/ansys-sphinx-theme-0.8.2/src/ansys_sphinx_theme/__init__.py
@@ -0,0 +1,58 @@
+"""This is the ansys-sphinx-theme module."""
+import os
+from pathlib import Path
+
+from ansys_sphinx_theme.latex import generate_404  # noqa: F401
+
+__version__ = "0.8.2"
+
+# get location of this directory
+_this_path = os.path.dirname(os.path.realpath(__file__))
+
+# make logo paths available
+pyansys_logo_black = os.path.join(_this_path, "static", "pyansys-logo-black-cropped.png")
+pyansys_logo_white = os.path.join(_this_path, "static", "pyansys-logo-white-cropped.png")
+ansys_favicon = os.path.join(_this_path, "static", "ansys-favicon.png")
+ansys_logo_white = os.path.join(_this_path, "static", "ansys_logo_white.pdf")
+ansys_logo_white_cropped = os.path.join(_this_path, "static", "ansys_logo_white_cropped.pdf")
+watermark = os.path.join(_this_path, "static", "watermark.pdf")
+ansys_logo_black = os.path.join(_this_path, "static", "ansys_logo_black_cropped.jpg")
+
+# Enable default 404 page
+page_404 = os.path.join(_this_path, "static", "404.rst")
+
+html_logo = pyansys_logo_black
+CSS_FILENAME = "ansys_sphinx_theme.css"
+
+
+def get_html_theme_path():
+    """Return list of HTML theme paths."""
+    return Path(__file__).parents[0].absolute()
+
+
+def get_version_match(semver):
+    """Evaluate the version match for the multi-documentation."""
+    if semver.endswith("dev0"):
+        return "dev"
+    major, minor, _ = semver.split(".")
+    return ".".join([major, minor])
+
+
+def setup(app):
+    """Connect to the sphinx theme app."""
+    theme_path = get_html_theme_path()
+    app.add_html_theme("ansys_sphinx_theme", theme_path)
+    theme_css_path = theme_path / "static" / "css" / CSS_FILENAME
+    if not theme_css_path.exists():
+        raise FileNotFoundError(f"Unable to locate ansys-sphinx theme at {theme_css_path}")
+    app.add_css_file(str(theme_css_path.relative_to(theme_path / "static")))
+
+    # add templates for autosummary
+    path_templates = os.path.join(_this_path, "_templates")
+    app.config.templates_path.append(path_templates)
+
+    return {
+        "version": __version__,
+        "parallel_read_safe": True,
+        "parallel_write_safe": True,
+    }
