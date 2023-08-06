230fec8082ae332952e34233a7077a1d3c83bb68
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:02:46 +0000

    Add package orzorng/0.0.1/orzorng-0.0.1.tar.gz

---
+++ packages/orzorng/orzorng-0.0.1.tar.gz/orzorng-0.0.1/orzorng/__init__.py
@@ -0,0 +1,10 @@
+from .file import (
+    exists_file,
+    write_file,
+    read_file,
+    get_file_line,
+    set_file_line
+)
+
+from .network import request_api
+from .judge import is_res, is_ip
