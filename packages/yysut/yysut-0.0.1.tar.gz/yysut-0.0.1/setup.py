fae4cf6c4143439f6a5d6f391d6e484d36215789
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:40 +0000

    Add package yysut/0.0.1/yysut-0.0.1.tar.gz

---
+++ packages/yysut/yysut-0.0.1.tar.gz/yysut-0.0.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+with open('README.md',mode="r",encoding="UTF-8") as f:
+    long_description = f.read()
+requirements = []
+setup(
+    name='yysut',
+    version='0.0.1',
+    author='SuperYY',
+    description='YYSuperUtils',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    #url='https://github.com/wkmyws/pysut',
+    packages=find_packages(),
+    install_requires=requirements,
+)
