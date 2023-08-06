e324a6ac15a9d61252657f393c2f8000305c0360
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:34:46 +0000

    Add package z3c.currency/2.0/z3c.currency-2.0-py3-none-any.whl

---
+++ packages/z3c.currency/z3c.currency-2.0-py3-none-any.whl/z3c/currency/tests.py
@@ -0,0 +1,26 @@
+##############################################################################
+#
+# Copyright (c) 2007-2013 Zope Foundation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""Currency Test Setup
+"""
+import doctest
+import unittest
+
+
+def test_suite():
+    return unittest.TestSuite([
+        doctest.DocFileSuite(
+            'README.rst',
+            optionflags=doctest.NORMALIZE_WHITESPACE | doctest.ELLIPSIS,
+        ),
+    ])
