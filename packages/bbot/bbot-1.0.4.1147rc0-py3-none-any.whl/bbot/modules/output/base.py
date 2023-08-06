93e1d6d92765f68d6fe1332bcc2f84feae6bf2fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:19:00 +0000

    Add package bbot/1.0.4.1147rc0/bbot-1.0.4.1147rc0-py3-none-any.whl

---
+++ packages/bbot/bbot-1.0.4.1147rc0-py3-none-any.whl/bbot/modules/output/base.py
@@ -0,0 +1,36 @@
+import logging
+
+from bbot.modules.base import BaseModule
+
+
+class BaseOutputModule(BaseModule):
+    accept_dupes = True
+    _type = "output"
+    emit_graph_trail = True
+    scope_distance_modifier = None
+    _stats_exclude = True
+
+    def _filter_event(self, event, precheck_only=False):
+        if event.type in ("FINISHED",):
+            return True, ""
+        if event._omit:
+            return False, "_omit is True"
+        if not precheck_only:
+            if event._force_output:
+                return True, "_force_output is True"
+            if event._internal:
+                return False, "_internal is True"
+        return True, ""
+
+    @property
+    def config(self):
+        config = self.scan.config.get("output_modules", {}).get(self.name, {})
+        if config is None:
+            config = {}
+        return config
+
+    @property
+    def log(self):
+        if self._log is None:
+            self._log = logging.getLogger(f"bbot.modules.output.{self.name}")
+        return self._log
---
+++ packages/bbot/bbot-1.0.4.1147rc0-py3-none-any.whl/bbot/modules/output/json.py
@@ -0,0 +1,45 @@
+import json
+from pathlib import Path
+from contextlib import suppress
+
+from bbot.modules.output.base import BaseOutputModule
+
+
+class JSON(BaseOutputModule):
+    watched_events = ["*"]
+    meta = {"description": "Output to JSON"}
+    options = {"output_file": "", "console": False}
+    options_desc = {"output_file": "Output to file", "console": "Output to console"}
+
+    def setup(self):
+        self.output_file = self.config.get("output_file", "")
+        if self.output_file:
+            self.output_file = Path(self.output_file)
+        else:
+            self.output_file = self.scan.home / "output.json"
+        self.helpers.mkdir(self.output_file.parent)
+        self._file = None
+        return True
+
+    @property
+    def file(self):
+        if self._file is None:
+            self._file = open(self.output_file, mode="a")
+        return self._file
+
+    def handle_event(self, event):
+        event_str = json.dumps(dict(event))
+        if self.file is not None:
+            self.file.write(event_str + "\n")
+            self.file.flush()
+        if self.config.get("console", False) or "human" not in self.scan.modules:
+            self.stdout(event_str)
+
+    def cleanup(self):
+        if self._file is not None:
+            with suppress(Exception):
+                self.file.close()
+
+    def report(self):
+        if self._file is not None:
+            self.info(f"Saved JSON output to {self.output_file}")
