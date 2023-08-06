01c31ca79477086121daa1507bd4c16394b15b11
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:39:35 +0000

    Add package NotebookRenderer/0.0.2/NotebookRenderer-0.0.2.tar.gz

---
+++ packages/NotebookRenderer/NotebookRenderer-0.0.2.tar.gz/NotebookRenderer-0.0.2/README.md
@@ -0,0 +1,3 @@
+# Notebook renderer python package
+
+- Subhasish Saha
---
+++ packages/NotebookRenderer/NotebookRenderer-0.0.2.tar.gz/NotebookRenderer-0.0.2/src/NotebookRenderer/__init__.py
@@ -0,0 +1,2 @@
+from NotebookRenderer.youtube import render_YouTube_video, get_time_info
+from NotebookRenderer.site import render_site, is_valid
