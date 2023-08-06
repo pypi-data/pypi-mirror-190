1ca6278c6bfa3b552504bcfef8b6528752ebd1ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:34:46 +0000

    Add package antm/0.0.7/antm-0.0.7.tar.gz

---
+++ packages/antm/antm-0.0.7.tar.gz/antm-0.0.7/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='antm',
+    version='0.0.7',
+    author='Hamed Rahimi',
+    author_email='hamed.rahimi@sorbonne-universite.fr',
+    description='Aligned Neural Topic Model for Exploring Evolving Topics',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/hamedR96/ANTM',
+    project_urls={
+        'Bug Tracker': 'https://github.com/hamedR96/ANTM/issues'
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={'': 'antm'},
+    packages=find_packages(where='antm'),
+    python_requires='>=3.6'
+)
