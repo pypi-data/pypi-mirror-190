e191de5efb0a0d842685f1627db67c58ac078666
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:00:42 +0000

    Add package anypay/1.0.6/anypay-1.0.6-py3-none-any.whl

---
+++ packages/anypay/anypay-1.0.6-py3-none-any.whl/anypay/exceptions.py
@@ -0,0 +1,26 @@
+from functools import wraps
+from typing import Callable, Awaitable
+
+
+class AnyPayAPIError(Exception):
+    """
+    Base AnyPay Exception.
+    Exception codes and their meanings can be found here: https://anypay.io/doc/api/errors
+    """
+    
+    def __init__(self, exception: dict) -> None:
+        """
+        AnyPay API Exception.
+        Docs: https://anypay.io/doc/api/errors
+        
+        :param exception: Exception data (code and message).
+        
+        :raises: AnyPayAPIError
+        """
+
+        self.message = exception['message']
+        self.code = exception['code']
+
+        super().__init__(
+            '[%(code)s] AnyPayAPI Error: %(message)s' % exception,
+        )
