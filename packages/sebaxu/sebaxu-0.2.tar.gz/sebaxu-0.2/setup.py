e42b646482f7be04e081e999d225fcb784f38588
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:16:40 +0000

    Add package sebaxu/0.2/sebaxu-0.2.tar.gz

---
+++ packages/sebaxu/sebaxu-0.2.tar.gz/sebaxu-0.2/setup.py
@@ -0,0 +1,12 @@
+import setuptools
+
+setuptools.setup(
+
+    name='sebaxu',
+    version='0.2',
+    author='Sahraoui Tarek Ziad',
+    description='simple lib in py for simple MLP',
+    packages=setuptools.find_packages(),
+    classifiers=[    "Programming Language :: Python :: 3.0",    "Operating System :: OS Independent",    "License :: OSI Approved :: MIT License"]
+
+)
