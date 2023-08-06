87c11284a209e77c34303a6dc7c244dedc3b410a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:19:29 +0000

    Add package typegraph/0.0.3.dev2/typegraph-0.0.3.dev2-py3-none-any.whl

---
+++ packages/typegraph/typegraph-0.0.3.dev2-py3-none-any.whl/typegraph/providers/google/runtimes/googleapis.py
@@ -0,0 +1,23 @@
+# Copyright Metatype OÜ under the Elastic License 2.0 (ELv2). See LICENSE.md for usage.
+
+from attrs import field
+from attrs import frozen
+from typegraph.effects import Effect
+from typegraph.runtimes.base import Materializer
+from typegraph.runtimes.base import Runtime
+from typegraph.utils.attrs import always
+from typegraph.utils.attrs import required
+
+
+@frozen
+class GoogleapisRuntime(Runtime):
+    runtime_name: str = always("googleapis")
+
+
+@frozen
+class RestMat(Materializer):
+    verb: str
+    url: str
+    effect: Effect = required()
+    runtime: Runtime = field(kw_only=True, factory=GoogleapisRuntime)
+    materializer_name: str = always("restmat")
