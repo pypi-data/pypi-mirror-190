0dc500e78a199b1859065d6ad8a15c30083c5374
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:16:52 +0000

    Add package proxygen-cli/2.0.6/proxygen_cli-2.0.6-py3-none-any.whl

---
+++ packages/proxygen-cli/proxygen_cli-2.0.6-py3-none-any.whl/proxygen_cli/cli/command_main.py
@@ -0,0 +1,56 @@
+import sys
+from typing import get_args
+from urllib import parse
+import click
+from proxygen_cli.lib.settings import SETTINGS
+
+from proxygen_cli.lib import constants, proxygen_api, settings, output, version
+from proxygen_cli.cli import (
+    command_credentials,
+    command_settings,
+    command_instance,
+    command_spec,
+    command_secret,
+)
+
+
+@click.group()
+def main():
+    pass
+
+
+main.add_command(command_settings.settings)
+main.add_command(command_credentials.credentials)
+main.add_command(command_instance.instance)
+main.add_command(command_spec.spec_cmd, name="spec")
+main.add_command(command_secret.secret)
+
+
+@main.command()
+def status():
+    """
+    Query the proxygen service status endpoint.
+    """
+    status = proxygen_api.status()
+    output.print_json(
+        {"proxygen_url": str(settings.SETTINGS.endpoint_url), "response": status}
+    )
+
+
+@main.command()
+@click.option(
+    "--api", default=SETTINGS.api, help="Override the default API", show_default=True
+)
+def docker_get_login(api):
+    """
+    Generate docker login command for AWS ECR repo with credentials from proxygen.
+    """
+    if api is None:
+        raise click.UsageError("You must set the API before using this command: see `proxygen settings`")
+
+    resp = proxygen_api.get_docker_login(api)
+    click.echo(f"docker login -u {resp['user']} --password {resp['password']} {resp['registry']}")
+
+
+if __name__ == "__main__":
+    main()
