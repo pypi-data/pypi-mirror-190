6365f97a10ed0ce53c4f3fdbe8bdf02dd08b678b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:56:22 +0000

    Add package dolead-entry-points/0.0.10/dolead_entry_points-0.0.10.tar.gz

---
+++ packages/dolead-entry-points/dolead_entry_points-0.0.10.tar.gz/dolead_entry_points-0.0.10/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['dolead_entry_points']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['celery>=3.1.17,<5', 'requests>=2.13.0']
+
+setup_kwargs = {
+    'name': 'dolead-entry-points',
+    'version': '0.0.10',
+    'description': 'Multiple entry points generator',
+    'long_description': '# Dolead Simple multi entry point lib\n',
+    'author': 'François Schmidts',
+    'author_email': 'francois@schmidts.fr',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
