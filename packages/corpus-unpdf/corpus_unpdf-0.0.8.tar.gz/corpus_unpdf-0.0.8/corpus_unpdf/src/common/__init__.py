1320e08ab23c82e3a307b08fe7bdcb3b855f08a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:52:26 +0000

    Add package corpus-unpdf/0.0.8/corpus_unpdf-0.0.8.tar.gz

---
+++ packages/corpus-unpdf/corpus_unpdf-0.0.8.tar.gz/corpus_unpdf-0.0.8/corpus_unpdf/src/common/__init__.py
@@ -0,0 +1,12 @@
+from .fetch import (
+    get_img_from_page,
+    get_page_and_img,
+    get_pages_and_imgs,
+    get_reverse_pages_and_imgs,
+)
+from .slice import (
+    PageCut,
+    get_centered_coordinates,
+    get_contours,
+    is_match_text,
+)
