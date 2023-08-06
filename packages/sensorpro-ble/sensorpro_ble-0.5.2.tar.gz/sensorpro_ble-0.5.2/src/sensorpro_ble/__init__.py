e1b6e18b534fb5be34c3ace08e4ab355ad0b2a8f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:33:43 +0000

    Add package sensorpro-ble/0.5.2/sensorpro_ble-0.5.2.tar.gz

---
+++ packages/sensorpro-ble/sensorpro_ble-0.5.2.tar.gz/sensorpro_ble-0.5.2/src/sensorpro_ble/__init__.py
@@ -0,0 +1,32 @@
+"""Parser for SensorPro BLE advertisements."""
+from __future__ import annotations
+
+from sensor_state_data import (
+    BinarySensorDeviceClass,
+    BinarySensorValue,
+    DeviceKey,
+    SensorDescription,
+    SensorDeviceClass,
+    SensorDeviceInfo,
+    SensorUpdate,
+    SensorValue,
+    Units,
+)
+
+from .parser import SensorProBluetoothDeviceData
+
+__version__ = "0.5.2"
+
+__all__ = [
+    "SensorProBluetoothDeviceData",
+    "BinarySensorDeviceClass",
+    "BinarySensorValue",
+    "SensorDescription",
+    "SensorDeviceInfo",
+    "DeviceKey",
+    "SensorUpdate",
+    "SensorDeviceClass",
+    "SensorDeviceInfo",
+    "SensorValue",
+    "Units",
+]
