bbd9d1e3670d3a40081ffb7402c102a65e049dff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:34:37 +0000

    Add package py-clob-client/0.1.13/py_clob_client-0.1.13.tar.gz

---
+++ packages/py-clob-client/py_clob_client-0.1.13.tar.gz/py_clob_client-0.1.13/py_clob_client/clob_types.py
@@ -0,0 +1,78 @@
+from dataclasses import dataclass
+from typing import Any
+
+from .constants import ZERO_ADDRESS
+
+
+@dataclass
+class ApiCreds:
+    api_key: str
+    api_secret: str
+    api_passphrase: str
+
+
+@dataclass
+class RequestArgs:
+    method: str
+    request_path: str
+    body: Any = None
+
+
+@dataclass
+class OrderArgs:
+    token_id: str
+    """
+    TokenID of the Conditional token asset being traded
+    """
+
+    price: float
+    """
+    Price used to create the order
+    """
+
+    size: float
+    """
+    Size in terms of the ConditionalToken
+    """
+
+    side: str
+    """
+    Side of the order
+    """
+
+    fee_rate_bps: int = 0
+    """
+    Fee rate, in basis points, charged to the order maker, charged on proceeds
+    """
+
+    nonce: int = 0
+    """
+    Nonce used for onchain cancellations
+    """
+
+    expiration: int = 0
+    """
+    Timestamp after which the order is expired.
+    """
+
+    taker: str = ZERO_ADDRESS
+    """
+    Address of the order taker. The zero address is used to indicate a public order
+    """
+
+
+@dataclass
+class FilterParams:
+    market: str = None
+    maker: str = None
+    taker: str = None
+    id: str = None
+    limit: int = None
+    before: int = None
+    after: int = None
+    owner: str = None
+
+
+@dataclass
+class TradeNotificationParams:
+    index: int = None
