020c1735919cb2fdbdc26037c8ca29318fab1e4b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:56 +0000

    Add package seleccioninstancias/0.2/seleccioninstancias-0.2.tar.gz

---
+++ packages/seleccioninstancias/seleccioninstancias-0.2.tar.gz/seleccioninstancias-0.2/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='seleccioninstancias',
+    version=0.2,
+    license="MIT",
+    description="Paquete con una funcion",
+    author='Victor Martinez Santiago @vicbox01',
+#    author_email='victor.santiago@cimat.mx',
+    url='https://github.com/VicBoxMS/SeleccionInstancias',
+    install_requires=['numpy'],
+    packages=find_packages()
+)
