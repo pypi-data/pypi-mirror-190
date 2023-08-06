4dc62e5886ebba5604a11bb946d0e507543447b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:29:38 +0000

    Add package jaws-libp/4.5.0/jaws_libp-4.5.0-py3-none-any.whl

---
+++ packages/jaws-libp/jaws_libp-4.5.0-py3-none-any.whl/jaws_libp/references/__init__.py
@@ -0,0 +1,5 @@
+"""
+   Experimental code aiding development of Schema References in the Python Confluent Kafka library.
+
+       See: `Schema reference support <https://github.com/confluentinc/confluent-kafka-python/pull/1088>`_
+"""
