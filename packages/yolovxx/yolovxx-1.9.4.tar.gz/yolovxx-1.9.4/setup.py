3b5b659dc76ce77935356b2a1ee7f3d5bd28e82e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:11:10 +0000

    Add package yolovxx/1.9.4/yolovxx-1.9.4.tar.gz

---
+++ packages/yolovxx/yolovxx-1.9.4.tar.gz/yolovxx-1.9.4/setup.py
@@ -0,0 +1,44 @@
+from distutils.core import setup
+setup(
+  name = 'yolovxx',         # How you named your package folder (MyLib)
+  packages = ['yolovxx', 'yolovxx.yolov7', 'yolovxx.yolov7.nets',
+               'yolovxx.yolov7.utils', 'yolovxx.yolox', 'yolovxx.yolox.nets',
+               'yolovxx.yolox.utils', 'yolovxx.yolov5', 'yolovxx.yolov5.nets',
+               'yolovxx.yolov5.utils'],  # Chose the same as "name"
+  version = '1.9.4',      # Start with a small number and increase it with every change you make
+  license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
+  description = 'run all yolo detectors (training and inference)',   # Give a short description about your library
+  author = 'NasAir',                   # Type in your name
+  author_email = 'me@domain.com',      # Type in your E-Mail
+  url = 'https://hoolme.ai',   # Provide either the link to your github or to your website
+  download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
+  keywords = ['SOME', 'MEANINGFULL', 'KEYWORDS'],   # Keywords that define your package best
+  install_requires=[            # I get to this in a second
+        'scipy',   
+        'numpy',   
+        'matplotlib',
+        'opencv_python',
+        'torch',
+        'torchvision',
+        'tqdm',
+        'Pillow',
+        'h5py',
+        'tensorboard',
+        'thop',
+        'onnx',
+        'onnxsim',
+        'onnxruntime',
+        'pycocotools',
+        'pascal_voc_writer',
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+    'Intended Audience :: Developers',      # Define that your audience are developers
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',   # Again, pick a license
+    'Programming Language :: Python :: 3.6',      #Specify which pyhton versions that you want to support
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+    'Programming Language :: Python :: 3.9',
+  ],
+)
