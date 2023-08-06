ab50b078d9d53351ee77784089029fe0cb646156
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:00:30 +0000

    Add package ddd-interface/0.0.2/ddd-interface-0.0.2.tar.gz

---
+++ packages/ddd-interface/ddd-interface-0.0.2.tar.gz/ddd-interface-0.0.2/src/ddd_interface/objects/dto.py
@@ -0,0 +1,40 @@
+from typing import List, Optional, Dict, Tuple, Union
+from pydantic import BaseModel
+
+
+
+class DatapipeServerInfoDTO(BaseModel):
+    id: str
+    secret: str
+    endpoint: str
+
+
+
+class DatapipeDataInfoDTO(BaseModel):
+    bucket: str
+    remote_path: str
+    local_path: str
+    timeout: int = 3
+
+
+
+class ClusterConfigDataDTO(BaseModel):
+    data_server: DatapipeServerInfoDTO
+    data: List[DatapipeDataInfoDTO]
+
+
+
+class ClusterConfigDTO(BaseModel):
+    cluster_name: str
+    region_id: str
+    config_data: Optional[ClusterConfigDataDTO] = None
+    entry_point: Optional[List[str]] = None
+    timeout: int = 20
+
+
+
+class BootstrapDTO(BaseModel):
+    cluster_config: ClusterConfigDTO
+    template: str = 'normal'
+    platform: str = 'aliyun'
+    patch_setting: Optional[Dict] = None
