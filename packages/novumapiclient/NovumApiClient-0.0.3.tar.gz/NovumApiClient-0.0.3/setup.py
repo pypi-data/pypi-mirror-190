b74265d9951140960d73624701a980001fefff3f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:00:31 +0000

    Add package novumapiclient/0.0.3/NovumApiClient-0.0.3.tar.gz

---
+++ packages/novumapiclient/NovumApiClient-0.0.3.tar.gz/NovumApiClient-0.0.3/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="NovumApiClient",
+    version="0.0.3",
+    description="This is a client API to access Novum Service Center",
+    url="https://github.com/novum-engineering",
+    author="Leonardo Biz",
+    author_email="l.biz@novum-engineering.com",
+    license="BSD 2-clause",
+    packages=find_packages(),
+    classifiers=[
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: BSD License",
+        "Programming Language :: Python :: 3.10",
+    ],
+)
