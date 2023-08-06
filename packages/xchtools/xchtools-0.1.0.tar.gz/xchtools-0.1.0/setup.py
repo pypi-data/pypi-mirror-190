7c6c4eb7f9155ddee9ce56cb657a73f6034dac44
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:58:46 +0000

    Add package xchtools/0.1.0/xchtools-0.1.0.tar.gz

---
+++ packages/xchtools/xchtools-0.1.0.tar.gz/xchtools-0.1.0/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+from setuptools import find_packages
+
+
+VERSION = '0.1.0'
+
+setup(
+    name='xchtools',  # package name
+    version=VERSION,  # package version
+    description="xch's tools",  # package description
+    author='xch',
+    packages=find_packages(),
+    zip_safe=False,
+)
