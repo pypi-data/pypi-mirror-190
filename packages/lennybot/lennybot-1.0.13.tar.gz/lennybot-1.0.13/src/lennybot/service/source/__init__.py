c05b9e97ad6bcc412705baa3ef1bc7e7c2e48166
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:12:21 +0000

    Add package lennybot/1.0.13/lennybot-1.0.13.tar.gz

---
+++ packages/lennybot/lennybot-1.0.13.tar.gz/lennybot-1.0.13/src/lennybot/service/source/__init__.py
@@ -0,0 +1,12 @@
+from .source_github import GithubSource
+from .source_github_query import GithubQuerySource
+from .isource import ISource
+from ...config import LennyBotSourceConfig
+
+def create_source(name, config: LennyBotSourceConfig, github) -> ISource:
+    source_type = config.type
+    if source_type == "github":
+        return GithubSource(name, config, github)
+    if source_type == "github-query":
+        return GithubQuerySource(name, config, github)
+    raise Exception(f"Unknown Source Type: {source_type}")
