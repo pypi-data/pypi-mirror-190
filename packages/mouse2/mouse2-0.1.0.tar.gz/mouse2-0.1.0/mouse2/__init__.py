6fe84ad238491de4e385de7b635cbb3dd22b62f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:19:04 +0000

    Add package mouse2/0.1.0/mouse2-0.1.0.tar.gz

---
+++ packages/mouse2/mouse2-0.1.0.tar.gz/mouse2-0.1.0/mouse2/__init__.py
@@ -0,0 +1,10 @@
+"""
+mouse2
+
+A library for processing of molecular dynamics simulation data
+with a focus on chiral ordering.
+"""
+
+__version__ = "0.1.0"
+__author__ = 'Mikhail Glagolev, Anna Glagoleva'
+__credits__ = 'A. N. Nesmeyanov Institute of Organoelement Compounds RAS'
---
+++ packages/mouse2/mouse2-0.1.0.tar.gz/mouse2-0.1.0/setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+@author: Mikhail Glagolev
+"""
+from setuptools import setup
+
+setup(
+    name='mouse2',
+    version='0.1.0',    
+    description='A example Python package',
+    url='https://github.com/mglagolev/mouse2',
+    author='Mikhail Glagolev, Anna Glagoleva',
+    author_email='mikhail.glagolev@gmail.com',
+    license='GNU GPL v3',
+    packages=['mouse2'],
+    install_requires=['numpy',
+                      'MDAnalysis',
+                      'networkx',
+                      ],
+
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',  
+        'Operating System :: POSIX :: Linux',        
+        'Programming Language :: Python :: 3',
+    ],
+)
