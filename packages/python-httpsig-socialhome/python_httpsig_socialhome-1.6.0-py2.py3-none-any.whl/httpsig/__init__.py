13bfcb9ee5fe048c98ca2bc6c9aae751d0961794
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:34:36 +0000

    Add package python-httpsig-socialhome/1.6.0/python_httpsig_socialhome-1.6.0-py2.py3-none-any.whl

---
+++ packages/python-httpsig-socialhome/python_httpsig_socialhome-1.6.0-py2.py3-none-any.whl/httpsig/__init__.py
@@ -0,0 +1,13 @@
+from pkg_resources import get_distribution, DistributionNotFound
+
+from .sign import Signer, HeaderSigner
+from .verify import Verifier, HeaderVerifier
+from .sign_algorithms import *
+
+try:
+    __version__ = get_distribution(__name__).version
+except DistributionNotFound:
+    # package is not installed
+    pass
+
+__all__ = (Signer, HeaderSigner, Verifier, HeaderVerifier)
