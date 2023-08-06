00fba75c4484e34c144cf1dc42ea4d874eaf0292
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:36:04 +0000

    Add package SUSspLbrr-0.1/0.1/SUSspLbrr_0.1-0.1.tar.gz

---
+++ packages/SUSspLbrr-0.1/SUSspLbrr_0.1-0.1.tar.gz/SUSspLbrr_0.1-0.1/setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+
+setuptools.setup(
+    name="SUSspLbrr_0.1",
+    version="0.1",
+    author="Mohamed Elaissoui",
+    author_email="mohammadelaissaoui@gmail.com",
+    description="A simple python package",
+    packages=setuptools.find_packages(),
+    classifiers=[
+    "Programming Language :: Python :: 3",
+    "License :: OSI Approved :: MIT License",
+    "Operating System :: OS Independent",
+    ]
+)
