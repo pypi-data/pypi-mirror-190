00ac6ac0483ccffeaf1a5fe0da254f0e093a5b74
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:31:31 +0000

    Add package prodot/0.1.9/prodot-0.1.9.tar.gz

---
+++ packages/prodot/prodot-0.1.9.tar.gz/prodot-0.1.9/prodot/_path_object.py
@@ -0,0 +1,28 @@
+from __future__ import annotations
+import typing as t
+from ._base_object import BaseObject
+from jsonpath_ng.ext import parse
+from .json_tools import get_all_paths
+from .json_tools import Filter
+
+class PathObject(BaseObject):
+    __filter_object = Filter
+
+    @property
+    def filter(self)->Filter:
+        return self.__filter_object(self.main_object, self)
+        
+    def __getitem__(self, name:str)->PathObject:
+        try:
+            return PathObject(parse('$.'+name).find(self.main_object)[0].value)
+        except IndexError:
+            raise Exception(f"path <<{name}>> not found at the main_object")
+
+    def __setitem__(self, name:str, value:t.Any):
+        path = parse('$.'+name)
+        path.update_or_create(self.main_object, value)
+
+    def get_all_paths(self, **kwargs)->t.Iterable:
+        return get_all_paths(self.main_object, **kwargs)
+
+
