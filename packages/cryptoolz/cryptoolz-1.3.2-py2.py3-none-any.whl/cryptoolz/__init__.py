7373849b476c7248f2a17becb021e1767c7905a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:35:16 +0000

    Add package cryptoolz/1.3.2/cryptoolz-1.3.2-py2.py3-none-any.whl

---
+++ packages/cryptoolz/cryptoolz-1.3.2-py2.py3-none-any.whl/cryptoolz/__init__.py
@@ -0,0 +1,8 @@
+"""Top-level package for cryptoolz."""
+
+import cryptoolz.crypto as crypto
+
+__author__ = """Tanats Nir"""
+__email__ = "tanats_nir@disroot.org"
+__version__ = "1.3.2"
+__all__ = ["crypto"]
---
+++ packages/cryptoolz/cryptoolz-1.3.2-py2.py3-none-any.whl/cryptoolz/app/cli/__main__.py
@@ -0,0 +1,6 @@
+from .cryptoolz import cryptoolz
+from .primes import primes
+from .keys import keys, create, reveal
+
+if __name__ == "__main__":
+    cryptoolz()
