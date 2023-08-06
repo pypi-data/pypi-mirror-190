53eee7ca996f6f1163015103617533d63ac5a198
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:28:22 +0000

    Add package bunting/0.1.4/bunting-0.1.4-py3-none-any.whl

---
+++ packages/bunting/bunting-0.1.4-py3-none-any.whl/bunting/__init__.py
@@ -0,0 +1,45 @@
+import urllib.parse
+import urllib.request
+import json
+
+OSM_EXTRACT_ENDPOINT = 'http://osm.buntinglabs.com/v1/osm/extract'
+BUILDING_SELECT_ENDPOINT = 'http://osm.buntinglabs.com/v1/buildings/select'
+
+class BuntingClient:
+
+    def __init__(self, api_key):
+        self.api_key = api_key
+
+    def osm_extract(self, tags, bbox=None):
+        # list of tags
+        if isinstance(tags, str):
+            tags = [tags]
+        if not (isinstance(bbox, tuple) and len(bbox) == 4):
+            raise TypeError('bbox must be tuple with 4 floats')
+        for tag in tags:
+            if '=' not in tag:
+                raise TypeError('tags argument should be of form "key=value", but got tag "%s" without =' % tag)
+
+        params = {
+            'tags': '&'.join(tags),
+            'api_key': self.api_key,
+            'bbox': '%f,%f,%f,%f' % bbox
+        }
+
+        response = urllib.request.urlopen(OSM_EXTRACT_ENDPOINT + "?" + urllib.parse.urlencode(params))
+
+        return json.loads(response.read().decode())
+
+    def building_select(self, bbox=None):
+        # list of tags
+        if not (isinstance(bbox, tuple) and len(bbox) == 4):
+            raise TypeError('bbox must be tuple with 4 floats')
+
+        params = {
+            'api_key': self.api_key,
+            'bbox': '%f,%f,%f,%f' % bbox
+        }
+
+        response = urllib.request.urlopen(BUILDING_SELECT_ENDPOINT + "?" + urllib.parse.urlencode(params))
+
+        return json.loads(response.read().decode())
