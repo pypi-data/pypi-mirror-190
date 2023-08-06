5f349743fdf9bebeb7df4ae23ca4d1489a45b444
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:33:49 +0000

    Add package antm/0.0.6/antm-0.0.6.tar.gz

---
+++ packages/antm/antm-0.0.6.tar.gz/antm-0.0.6/tests/test.py
@@ -0,0 +1,16 @@
+from antm import ANTM
+import pandas as pd
+
+#load data
+df=pd.read_parquet("./data/dblpFullSchema_2000_2020_extract_big_data_1K.parquet")
+df=df[["abstract","year"]].rename(columns={"abstract":"content","year":"time"}).dropna().reset_index()
+
+#choosing the windows size and overlapping length for time frames
+window_size=3
+overlap=1
+
+#initialize model
+model=ANTM(df,overlap,window_size,mode="data2vec",num_words=10,path="./saved_data")
+
+#learn the model and save it
+model.fit(save=True)
