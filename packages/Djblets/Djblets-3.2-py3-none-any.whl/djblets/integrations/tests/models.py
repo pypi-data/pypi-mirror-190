687770ed540c2d72f37e4cb3cfe34fb15ba1cf7d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:56:56 +0000

    Add package Djblets/3.2/Djblets-3.2-py3-none-any.whl

---
+++ packages/Djblets/Djblets-3.2-py3-none-any.whl/djblets/integrations/tests/models.py
@@ -0,0 +1,6 @@
+from djblets.integrations.models import BaseIntegrationConfig
+
+
+class IntegrationConfig(BaseIntegrationConfig):
+    def get_integration_manager(self):
+        return self.manager
---
+++ packages/Djblets/Djblets-3.2-py3-none-any.whl/djblets/integrations/tests/testcases.py
@@ -0,0 +1,28 @@
+from django.conf import settings
+from django.core.cache import cache
+
+from djblets.integrations.manager import shutdown_integration_managers
+from djblets.testing.testcases import TestCase, TestModelsLoaderMixin
+
+
+class IntegrationsTestCase(TestModelsLoaderMixin, TestCase):
+    """Base class for unit tests that work with integrations."""
+
+    tests_app = 'djblets.integrations.tests'
+
+    def setUp(self):
+        super(IntegrationsTestCase, self).setUp()
+
+        self.old_middleware_classes = list(settings.MIDDLEWARE)
+        settings.MIDDLEWARE = self.old_middleware_classes + [
+            'djblets.integrations.middleware.IntegrationsMiddleware',
+        ]
+
+        cache.clear()
+
+    def tearDown(self):
+        super(IntegrationsTestCase, self).tearDown()
+
+        settings.MIDDLEWARE = self.old_middleware_classes
+
+        shutdown_integration_managers()
