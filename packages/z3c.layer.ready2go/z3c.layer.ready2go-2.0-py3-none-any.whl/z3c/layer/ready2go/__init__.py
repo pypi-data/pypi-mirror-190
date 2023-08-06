4cde200f21e22f6dc3eed9a84daf81fb08efe1cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:35:01 +0000

    Add package z3c.layer.ready2go/2.0/z3c.layer.ready2go-2.0-py3-none-any.whl

---
+++ packages/z3c.layer.ready2go/z3c.layer.ready2go-2.0-py3-none-any.whl/z3c/layer/ready2go/__init__.py
@@ -0,0 +1,42 @@
+##############################################################################
+#
+# Copyright (c) 2007 Zope Foundation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""Ready2Go Browser Layer
+"""
+import z3c.form.interfaces
+import z3c.formui.interfaces
+import z3c.layer.pagelet
+from zope.viewlet.interfaces import IViewletManager
+
+
+class IReady2GoBrowserLayer(
+        z3c.layer.pagelet.IPageletBrowserLayer,
+        z3c.formui.interfaces.IDivFormLayer,
+        z3c.form.interfaces.IFormLayer):
+    """A ready 2 go layer useful for custom applications."""
+
+
+class ICSS(IViewletManager):
+    """CSS viewlet manager."""
+
+
+class IJavaScript(IViewletManager):
+    """JavaScript viewlet manager."""
+
+
+class IBreadcrumb(IViewletManager):
+    """Breadcrumb viewlet manager."""
+
+
+class ISideBar(IViewletManager):
+    """SideBar viewlet manager."""
