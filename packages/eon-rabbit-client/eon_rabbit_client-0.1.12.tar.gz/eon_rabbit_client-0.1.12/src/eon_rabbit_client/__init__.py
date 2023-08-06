4e068d7b4990e5c18138a7dbf83f705620962ba1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:23:41 +0000

    Add package eon-rabbit-client/0.1.12/eon_rabbit_client-0.1.12.tar.gz

---
+++ packages/eon-rabbit-client/eon_rabbit_client-0.1.12.tar.gz/eon_rabbit_client-0.1.12/src/eon_rabbit_client/__init__.py
@@ -0,0 +1,16 @@
+from eon_broker_client.connection_controller import ConnectionController
+from eon_broker_client.channel_controller import ChannelController
+from eon_broker_client.queue_controller import QueueController
+from eon_broker_client.consume_controller import ConsumeController
+from eon_broker_client.exchange_controller import ExchangeController
+from eon_broker_client.publish_controller import PublishController
+
+
+__all__ = (
+    "ConnectionController",
+    "ChannelController",
+    "QueueController",
+    "ConsumeController",
+    "ExchangeController",
+    "PublishController",
+)
