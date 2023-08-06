67c44a658d441978e0feb88122632e35b8e19cb2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:02:00 +0000

    Add package bokeh/3.1.0.dev2/bokeh-3.1.0.dev2-py3-none-any.whl

---
+++ packages/bokeh/bokeh-3.1.0.dev2-py3-none-any.whl/bokeh/server/static/js/lib/models/widgets/text_input.js
@@ -0,0 +1,33 @@
+import { TextLikeInput, TextLikeInputView } from "./text_like_input";
+import { input, div } from "../../core/dom";
+import * as inputs from "../../styles/widgets/inputs.css";
+export class TextInputView extends TextLikeInputView {
+    static __name__ = "TextInputView";
+    connect_signals() {
+        super.connect_signals();
+        const { prefix, suffix } = this.model.properties;
+        this.on_change([prefix, suffix], () => this.render());
+    }
+    _render_input() {
+        this.input_el = input({ type: "text", class: inputs.input });
+        const { prefix, suffix } = this.model;
+        const prefix_el = prefix != null ? div({ class: "bk-input-prefix" }, prefix) : null;
+        const suffix_el = suffix != null ? div({ class: "bk-input-suffix" }, suffix) : null;
+        const container_el = div({ class: "bk-input-container" }, prefix_el, this.input_el, suffix_el);
+        return container_el;
+    }
+}
+export class TextInput extends TextLikeInput {
+    static __name__ = "TextInput";
+    constructor(attrs) {
+        super(attrs);
+    }
+    static {
+        this.prototype.default_view = TextInputView;
+        this.define(({ String, Nullable }) => ({
+            prefix: [Nullable(String), null],
+            suffix: [Nullable(String), null],
+        }));
+    }
+}
+//# sourceMappingURL=text_input.js.map
