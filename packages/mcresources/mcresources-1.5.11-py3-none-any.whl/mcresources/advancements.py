91d488eb42aa0310f4a4397ffd33ed9b447ab9c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:31:08 +0000

    Add package mcresources/1.5.11/mcresources-1.5.11-py3-none-any.whl

---
+++ packages/mcresources/mcresources-1.5.11-py3-none-any.whl/mcresources/advancements.py
@@ -0,0 +1,60 @@
+#  Part of mcresources by Alex O'Neill
+#  Work under copyright. Licensed under MIT
+#  For more information see the project LICENSE file
+
+"""
+A collection of helper functions for creating common vanilla advancement JSONs.
+
+Usage:
+```python
+from mcresources import advancements
+```
+"""
+
+from mcresources.type_definitions import Json, ResourceIdentifier
+from mcresources import utils
+
+from typing import Optional
+
+
+class AdvancementCategory:
+    def __init__(self, rm, category: str, background: str):
+        self.rm = rm
+        self.category = category
+        self.background = background
+
+    def advancement(self, name: str, icon: Json, title: str, description: str, parent: Optional[str], criteria: Json, requirements: Json = None, frame: str = 'task', toast: bool = True, chat: bool = True, hidden: bool = False):
+        key = '%s.advancements.%s.%s' % (self.rm.domain, self.category, name)
+
+        if parent is not None:
+            parent = utils.resource_location(self.rm.domain, self.category + '/' + parent).join()
+
+        self.rm.advancement((self.category, name), {
+            'icon': utils.item_stack(icon),
+            'title': {'translate': key + '.title'},
+            'description': {'translate': key + '.description'},
+            'frame': frame,
+            'show_toast': toast,
+            'announce_to_chat': chat,
+            'hidden': hidden,
+            'background': self.background
+        }, parent, criteria, requirements)
+        self.rm.lang(key + '.title', title)
+        self.rm.lang(key + '.description', description)
+
+
+def inventory_changed(*item_predicates: Json) -> Json:
+    return {
+        'trigger': 'minecraft:inventory_changed',
+        'conditions': {
+            'items': [utils.item_predicate(ip) for ip in item_predicates]
+        }
+    }
+
+def recipe_unlocked(recipe: ResourceIdentifier) -> Json:
+    return {
+        'trigger': 'minecraft:recipe_unlocked',
+        'conditions': {
+            'recipe': utils.resource_location(recipe).join()
+        }
+    }
---
+++ packages/mcresources/mcresources-1.5.11-py3-none-any.whl/mcresources/loot_tables.py
@@ -0,0 +1,148 @@
+#  Part of mcresources by Alex O'Neill
+#  Work under copyright. Licensed under MIT
+#  For more information see the project LICENSE file
+
+"""
+Common functions for creating loot table related objects, such as loot conditions, functions, or loot pools.
+
+Usage:
+```python
+from mcresources import loot_tables
+```
+"""
+
+from mcresources.type_definitions import Json, JsonObject
+from mcresources import utils
+
+from typing import Optional, Sequence
+
+# General utils for creating loot pools, and loot entries
+
+def pool(*entries: Json, conditions: Optional[Json] = None, functions: Optional[Json] = None, rolls: int = 1, bonus_rolls: Optional[int] = None) -> JsonObject:
+    return {
+        'entries': entries,
+        'conditions': conditions,
+        'functions': functions,
+        'rolls': rolls,
+        'bonus_rolls': bonus_rolls
+    }
+
+def alternatives(*entries: Json, conditions: Optional[Json] = None, functions: Optional[Json] = None) -> JsonObject:
+    return {
+        'type': 'minecraft:alternatives',
+        'children': entries,
+        'conditions': conditions,
+        'functions': functions
+    }
+
+
+# Loot Conditions
+
+def or_condition(*terms: Json) -> JsonObject:
+    return {
+        'condition': 'minecraft:alternative',
+        'terms': utils.loot_conditions(terms)
+    }
+
+def inverted_condition(term: Json) -> JsonObject:
+    condition = utils.loot_conditions(term)
+    assert len(condition) == 1, 'Expected one condition for inverted_condition() term'
+    return {
+        'condition': 'minecraft:inverted',
+        'term': condition[0]
+    }
+
+def random_chance(chance: float) -> JsonObject:
+    return {
+        'condition': 'minecraft:random_chance',
+        'chance': chance
+    }
+
+def block_state_property(data: Json) -> JsonObject:
+    """ Accepts a block like 'minecraft:grass[snowy=true]', or a dictionary with 'Name' and 'Properties' entries """
+    block_state = utils.block_state(data)
+    assert 'Name' in block_state, 'Missing Name'
+    assert 'Properties' in block_state and len(block_state['Properties']) > 0, 'Requires at least one property'
+    return {
+        'condition': 'minecraft:block_state_property',
+        'block': block_state['Name'],
+        'properties': block_state['Properties']
+    }
+
+def match_tag(tag: str) -> JsonObject:
+    return {
+        'condition': 'minecraft:match_tool',
+        'predicate': {'tag': tag}
+    }
+
+def silk_touch() -> JsonObject:
+    return {
+        'condition': 'minecraft:match_tool',
+        'predicate': {
+            'enchantments': [{
+                'enchantment': 'minecraft:silk_touch',
+                'levels': {'min': 1}
+            }]
+        }
+    }
+
+def fortune_table(chances: Sequence[float]) -> JsonObject:
+    return {
+        'condition': 'minecraft:table_bonus',
+        'enchantment': 'minecraft:fortune',
+        'chances': chances
+    }
+
+def survives_explosion() -> JsonObject:
+    return {
+        'condition': 'minecraft:survives_explosion'
+    }
+
+
+# Loot Functions
+
+def set_count(min_inclusive: int = 1, max_inclusive: int = -1) -> JsonObject:
+    """ `minecraft:set_count` function """
+    if max_inclusive == -1:
+        count = min_inclusive
+    else:
+        count = {'min': min_inclusive, 'max': max_inclusive, 'type': 'minecraft:uniform'}
+    return {
+        'function': 'minecraft:set_count',
+        'count': count
+    }
+
+def fortune_bonus(multiplier: int = 1) -> JsonObject:
+    """ `minecraft:apply_bonus` function with the fortune enchantment, with a uniform bonus count
+    :param multiplier: the bonusMultiplier
+    """
+    return {
+        'function': 'minecraft:apply_bonus',
+        'enchantment': 'minecraft:fortune',
+        'formula': 'minecraft:uniform_bonus_count',
+        'parameters': {
+            'bonusMultiplier': multiplier
+        }
+    }
+
+def explosion_decay() -> JsonObject:
+    return {
+        'function': 'minecraft:explosion_decay'
+    }
+
+def copy_block_entity_name() -> JsonObject:
+    return {
+        'function': 'minecraft:copy_name',
+        'source': 'block_entity'
+    }
+
+def copy_block_entity_nbt() -> JsonObject:
+    return {
+        'function': 'minecraft:copy_nbt',
+        'source': 'block_entity',
+        'ops': [{
+            'source': '',
+            'target': 'BlockEntityTag',
+            'op': 'replace'
+        }]
+    }
