3c936d15696e81aae7910d35f6c641772e0b761f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:58:46 +0000

    Add package network-tom/2.0.1/network-tom-2.0.1.tar.gz

---
+++ packages/network-tom/network-tom-2.0.1.tar.gz/network-tom-2.0.1/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="network-tom",
+    version="2.0.1",
+    author="Schooldevops",
+    author_email="schooldevops@gmail.com",
+    description="schooldevops sample lib",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/schooldevops/python-tutorials",
+    project_urls={
+        "Bug Tracker": "https://github.com/schooldevops/python-tutorials/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+    entry_points={
+        'console_scripts': [
+            'testfunc = testfunc:add',
+        ],
+    },
+
+)
