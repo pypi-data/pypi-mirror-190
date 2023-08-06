37ae9d6470618693b30ae82843772f19b4963568
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:31:29 +0000

    Add package distributions-1pint/0.1/distributions_1pint-0.1.tar.gz

---
+++ packages/distributions-1pint/distributions_1pint-0.1.tar.gz/distributions_1pint-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions_1pint',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions_1pint'],
+      zip_safe=False)
