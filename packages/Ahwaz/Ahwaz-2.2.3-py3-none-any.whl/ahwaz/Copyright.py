4e2aac71853164c5f508503a406885f1ee17e7eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:02:12 +0000

    Add package Ahwaz/2.2.3/Ahwaz-2.2.3-py3-none-any.whl

---
+++ packages/Ahwaz/Ahwaz-2.2.3-py3-none-any.whl/ahwaz/Copyright.py
@@ -0,0 +1,5 @@
+class copyright:
+	__Version__ = "2.2.3"
+	CopyRight = print(f"\n\nAhwaz library version {__Version__} \n\n" + "Ahwaz Copyright (C) 2022 raibak Team Ahwaz\n\n\n\n" + "در حال فعال شدن کمی صبور باشید...")
+	print("........")
+	print(" ")
---
+++ packages/Ahwaz/Ahwaz-2.2.3-py3-none-any.whl/ahwaz/__init__.py
@@ -0,0 +1,1 @@
+from ahwaz.Arsein import *
