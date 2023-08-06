84a9b6a85478063b6d9ef048503d86aa321c10b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:38:50 +0000

    Add package fawltydeps/0.1.1/fawltydeps-0.1.1.tar.gz

---
+++ packages/fawltydeps/fawltydeps-0.1.1.tar.gz/fawltydeps-0.1.1/fawltydeps/utils.py
@@ -0,0 +1,25 @@
+"""Common utilities"""
+
+import os
+from pathlib import Path
+from typing import Iterator
+
+
+def walk_dir(path: Path) -> Iterator[Path]:
+    """Walk a directory structure and yield Path objects for each file within.
+
+    Wrapper around os.walk() that yields Path objects for files found (directly
+    or transitively) under the given directory. Directories whose name start
+    with a dot are skipped.
+    """
+    for root, dirs, files in os.walk(path):
+        dirs[:] = [d for d in dirs if not d.startswith(".")]
+        for filename in files:
+            yield Path(root, filename)
+
+
+def dirs_between(parent: Path, child: Path) -> Iterator[Path]:
+    """Yield directories between 'parent' and 'child', inclusive."""
+    yield child
+    if child != parent:
+        yield from dirs_between(parent, child.parent)
