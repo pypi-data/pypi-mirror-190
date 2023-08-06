6d57390a81ec735d7687e5dfb450bc1ca4aa3745
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:33:40 +0000

    Add package blazetest-beta/0.0.0.14/blazetest_beta-0.0.0.14-py3-none-any.whl

---
+++ packages/blazetest-beta/blazetest_beta-0.0.0.14-py3-none-any.whl/blazetest/core/project_config/yaml_model.py
@@ -0,0 +1,21 @@
+from dataclasses import dataclass, field
+
+
+@dataclass
+class BuildConfig:
+    ecr_repository_name: str
+
+
+@dataclass
+class DeployConfig:
+    stack_name: str
+    s3_bucket: str
+
+
+@dataclass
+class BlazetestConfig:
+    aws_region: str
+    build: BuildConfig
+    deploy: DeployConfig
+    license_key: str = field(default_factory=lambda: None)
+    license_file: str = field(default_factory=lambda: None)
