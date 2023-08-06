fdfcee73e74f169d2864befd349b357463384b6b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:16:20 +0000

    Add package google-cloud-documentai-toolbox/0.1.1a0/google_cloud_documentai_toolbox-0.1.1a0-py2.py3-none-any.whl

---
+++ packages/google-cloud-documentai-toolbox/google_cloud_documentai_toolbox-0.1.1a0-py2.py3-none-any.whl/google/cloud/documentai_toolbox/wrappers/document.py
@@ -0,0 +1,346 @@
+# -*- coding: utf-8 -*-
+# Copyright 2023 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+"""Wrappers for Document AI Document type."""
+
+import dataclasses
+import os
+import re
+from typing import Dict, List, Optional
+
+from google.api_core import client_info
+from google.cloud import documentai
+from google.cloud import storage
+from google.cloud import documentai_toolbox
+
+from google.cloud.documentai_toolbox import constants
+from google.cloud.documentai_toolbox.wrappers.page import Page
+from google.cloud.documentai_toolbox.wrappers.entity import Entity
+
+
+def _entities_from_shards(
+    shards: List[documentai.Document],
+) -> List[Entity]:
+    r"""Returns a list of Entities from a list of documentai.Document shards.
+
+    Args:
+        shards (List[google.cloud.documentai.Document]):
+            Required. List of document shards.
+
+    Returns:
+        List[Entity]:
+            a list of Entities.
+    """
+    result = []
+    for shard in shards:
+        for entity in shard.entities:
+            result.append(Entity(documentai_entity=entity))
+    return result
+
+
+def _pages_from_shards(shards: List[documentai.Document]) -> List[Page]:
+    r"""Returns a list of Pages from a list of documentai.Document shards.
+
+    Args:
+        shards (List[google.cloud.documentai.Document]):
+            Required. List of document shards.
+
+    Returns:
+        List[Page]:
+            A list of Pages.
+    """
+    result = []
+    for shard in shards:
+        text = shard.text
+        for page in shard.pages:
+            result.append(Page(documentai_page=page, text=text))
+
+    return result
+
+
+def _get_storage_client():
+    r"""Returns a Storage client with custom user agent header.
+
+    Returns:
+        storage.Client.
+
+    """
+    user_agent = f"{constants.USER_AGENT_PRODUCT}/{documentai_toolbox.__version__}"
+
+    info = client_info.ClientInfo(
+        client_library_version=documentai_toolbox.__version__,
+        user_agent=user_agent,
+    )
+
+    return storage.Client(client_info=info)
+
+
+def _get_bytes(gcs_bucket_name: str, gcs_prefix: str) -> List[bytes]:
+    r"""Returns a list of bytes of json files from Cloud Storage.
+
+    Args:
+        gcs_bucket_name (str):
+            Required. The name of the gcs bucket.
+
+            Format: `gs://bucket/optional_folder/target_folder/` where gcs_bucket_name=`bucket`.
+        gcs_prefix (str):
+            Required. The prefix of the json files in the target_folder
+
+            Format: `gs://bucket/optional_folder/target_folder/` where gcs_prefix=`optional_folder/target_folder`.
+    Returns:
+        List[bytes]:
+            A list of bytes.
+
+    """
+    result = []
+
+    storage_client = _get_storage_client()
+    blob_list = storage_client.list_blobs(gcs_bucket_name, prefix=gcs_prefix)
+
+    for blob in blob_list:
+        if (
+            blob.name.endswith(constants.JSON_EXTENSION)
+            or blob.content_type == constants.JSON_MIMETYPE
+        ):
+            result.append(blob.download_as_bytes())
+
+    return result
+
+
+def _get_shards(gcs_bucket_name: str, gcs_prefix: str) -> List[documentai.Document]:
+    r"""Returns a list of documentai.Document shards from a Cloud Storage folder.
+
+    Args:
+        gcs_bucket_name (str):
+            Required. The name of the gcs bucket.
+
+            Format: `gs://bucket/optional_folder/target_folder/` where gcs_bucket_name=`bucket`.
+        gcs_prefix (str):
+            Required. The prefix of the json files in the target_folder.
+
+            Format: `gs://bucket/optional_folder/target_folder/` where gcs_prefix=`optional_folder/target_folder`.
+    Returns:
+        List[google.cloud.documentai.Document]:
+            A list of documentai.Documents.
+
+    """
+    shards = []
+
+    file_check = re.match(constants.FILE_CHECK_REGEX, gcs_prefix)
+
+    if file_check is not None:
+        raise ValueError("gcs_prefix cannot contain file types")
+
+    byte_array = _get_bytes(gcs_bucket_name, gcs_prefix)
+
+    for byte in byte_array:
+        shards.append(documentai.Document.from_json(byte, ignore_unknown_fields=True))
+
+    return shards
+
+
+def print_gcs_document_tree(gcs_bucket_name: str, gcs_prefix: str) -> None:
+    r"""Prints a tree of filenames in Cloud Storage folder.
+
+    Args:
+        gcs_bucket_name (str):
+            Required. The name of the gcs bucket.
+
+            Format: `gs://bucket/optional_folder/target_folder/` where gcs_bucket_name=`bucket`.
+        gcs_prefix (str):
+            Required. The prefix of the json files in the target_folder.
+
+            Format: `gs://bucket/optional_folder/target_folder/` where gcs_prefix=`optional_folder/target_folder`.
+    Returns:
+        None.
+
+    """
+    FILENAME_TREE_MIDDLE = "├──"
+    FILENAME_TREE_LAST = "└──"
+    FILES_TO_DISPLAY = 4
+
+    file_check = re.match(constants.FILE_CHECK_REGEX, gcs_prefix)
+
+    if file_check is not None:
+        raise ValueError("gcs_prefix cannot contain file types")
+
+    storage_client = _get_storage_client()
+    blob_list = storage_client.list_blobs(gcs_bucket_name, prefix=gcs_prefix)
+
+    path_list: Dict[str, List[str]] = {}
+
+    for blob in blob_list:
+        directory, file_name = os.path.split(blob.name)
+
+        if directory in path_list:
+            path_list[directory].append(file_name)
+        else:
+            path_list[directory] = [file_name]
+
+    for directory, files in path_list.items():
+        print(f"{directory}")
+        dir_size = len(files)
+        for idx, file_name in enumerate(files):
+            if idx == dir_size - 1:
+                if dir_size > FILES_TO_DISPLAY:
+                    print("│  ....")
+                print(f"{FILENAME_TREE_LAST}{file_name}\n")
+            elif idx <= FILES_TO_DISPLAY:
+                print(f"{FILENAME_TREE_MIDDLE}{file_name}")
+
+
+@dataclasses.dataclass
+class Document:
+    r"""Represents a wrapped Document.
+
+    This class hides away the complexities of using Document protobuf
+    response outputted by BatchProcessDocuments or ProcessDocument
+    methods and implements convenient methods for searching and
+    extracting information within the Document.
+
+    Attributes:
+        shards: (List[google.cloud.documentai.Document]):
+            Optional. A list of documentai.Document shards of the same Document.
+            Each shard consists of a number of pages in the Document.
+        gcs_bucket_name (Optional[str]):
+            Optional. The name of the gcs bucket.
+
+            Format: `gs://bucket/optional_folder/target_folder/` where gcs_bucket_name=`bucket`.
+        gcs_prefix (Optional[str]):
+            Optional. The prefix of the json files in the target_folder.
+
+            Format: `gs://bucket/optional_folder/target_folder/` where gcs_prefix=`optional_folder/target_folder`.
+
+            For more information please take a look at https://cloud.google.com/storage/docs/json_api/v1/objects/list .
+        pages: (List[Page]):
+            A list of Pages in the Document.
+        entities: (List[Entity]):
+            A list of Entities in the Document.
+    """
+
+    shards: List[documentai.Document] = dataclasses.field(repr=False)
+    gcs_bucket_name: Optional[str] = dataclasses.field(default=None, repr=False)
+    gcs_prefix: Optional[str] = dataclasses.field(default=None, repr=False)
+
+    pages: List[Page] = dataclasses.field(init=False, repr=False)
+    entities: List[Entity] = dataclasses.field(init=False, repr=False)
+
+    def __post_init__(self):
+        self.pages = _pages_from_shards(shards=self.shards)
+        self.entities = _entities_from_shards(shards=self.shards)
+
+    @classmethod
+    def from_document_path(
+        cls,
+        document_path: str,
+    ):
+        r"""Loads Document from local document_path.
+
+        Args:
+            document_path (str):
+                Required. The path to the document.json file.
+        Returns:
+            Document:
+                A document from local document_path.
+        """
+
+        with open(document_path, "r", encoding="utf-8") as f:
+            doc = documentai.Document.from_json(f.read(), ignore_unknown_fields=True)
+
+        return cls(shards=[doc])
+
+    @classmethod
+    def from_documentai_document(
+        cls,
+        documentai_document: documentai.Document,
+    ):
+        r"""Loads Document from local documentai_document.
+
+        Args:
+            documentai_document (documentai.Document):
+                Optional. The Document.proto response.
+        Returns:
+            Document:
+                A document from local documentai_document.
+        """
+
+        return cls(shards=[documentai_document])
+
+    @classmethod
+    def from_gcs(cls, gcs_bucket_name: str, gcs_prefix: str):
+        r"""Loads Document from Cloud Storage.
+
+        Args:
+            gcs_bucket_name (str):
+                Required. The gcs bucket.
+
+                Format: Given `gs://{bucket_name}/{optional_folder}/{operation_id}/` where gcs_bucket_name=`{bucket_name}`.
+            gcs_prefix (str):
+                Required. The prefix to the location of the target folder.
+
+                Format: Given `gs://{bucket_name}/optional_folder/target_folder` where gcs_prefix=`{optional_folder}/{target_folder}`.
+        Returns:
+            Document:
+                A document from gcs.
+        """
+        shards = _get_shards(gcs_bucket_name=gcs_bucket_name, gcs_prefix=gcs_prefix)
+        return cls(
+            shards=shards, gcs_prefix=gcs_prefix, gcs_bucket_name=gcs_bucket_name
+        )
+
+    def search_pages(
+        self, target_string: Optional[str] = None, pattern: Optional[str] = None
+    ) -> List[Page]:
+        r"""Returns the list of Pages containing target_string or text matching pattern.
+
+        Args:
+            target_string (Optional[str]):
+                Optional. target str.
+            pattern (Optional[str]):
+                Optional. regex str.
+
+        Returns:
+            List[Page]:
+                A list of Pages.
+
+        """
+        if (target_string and pattern) or (not target_string and not pattern):
+            raise ValueError(
+                "Exactly one of target_string and pattern must be specified."
+            )
+
+        found_pages = []
+        for page in self.pages:
+            for paragraph in page.paragraphs:
+                if (target_string and target_string in paragraph.text) or (
+                    pattern and re.search(pattern, paragraph.text)
+                ):
+                    found_pages.append(page)
+                    break
+        return found_pages
+
+    def get_entity_by_type(self, target_type: str) -> List[Entity]:
+        r"""Returns the list of Entities of target_type.
+
+        Args:
+            target_type (str):
+                Required. target_type.
+
+        Returns:
+            List[Entity]:
+                A list of Entity matching target_type.
+
+        """
+        return [entity for entity in self.entities if entity.type_ == target_type]
---
+++ packages/google-cloud-documentai-toolbox/google_cloud_documentai_toolbox-0.1.1a0-py2.py3-none-any.whl/tests/unit/test_document.py
@@ -0,0 +1,338 @@
+# -*- coding: utf-8 -*-
+# Copyright 2022 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os
+
+# try/except added for compatibility with python < 3.8
+try:
+    from unittest import mock
+except ImportError:  # pragma: NO COVER
+    import mock
+
+import pytest
+import glob
+
+from google.cloud.documentai_toolbox import document
+
+from google.cloud import documentai
+from google.cloud import storage
+
+
+def get_bytes(file_name):
+    result = []
+    for filename in glob.glob(os.path.join(file_name, "*.json")):
+        with open(os.path.join(os.getcwd(), filename), "rb") as f:
+            result.append(f.read())
+
+    return result
+
+
+@pytest.fixture
+def get_bytes_single_file_mock():
+    with mock.patch.object(document, "_get_bytes") as byte_factory:
+        byte_factory.return_value = get_bytes("tests/unit/resources/0")
+        yield byte_factory
+
+
+@pytest.fixture
+def get_bytes_multiple_files_mock():
+    with mock.patch.object(document, "_get_bytes") as byte_factory:
+        byte_factory.return_value = get_bytes("tests/unit/resources/1")
+        yield byte_factory
+
+
+def test_get_shards_with_gcs_uri_contains_file_type():
+    with pytest.raises(ValueError, match="gcs_prefix cannot contain file types"):
+        document._get_shards(
+            gcs_bucket_name="test-directory",
+            gcs_prefix="documentai/output/123456789/0.json",
+        )
+
+
+def test_get_shards_with_valid_gcs_uri(get_bytes_single_file_mock):
+    actual = document._get_shards(
+        gcs_bucket_name="test-directory", gcs_prefix="documentai/output/123456789/0/"
+    )
+
+    get_bytes_single_file_mock.assert_called_once()
+    # We are testing only one of the fields to make sure the file content could be loaded.
+    assert actual[0].pages[0].page_number == 1
+
+
+def test_pages_from_shards():
+    shards = []
+    for byte in get_bytes("tests/unit/resources/0"):
+        shards.append(documentai.Document.from_json(byte))
+
+    actual = document._pages_from_shards(shards=shards)
+    assert len(actual[0].paragraphs) == 31
+
+
+def test_entities_from_shard():
+    shards = []
+    for byte in get_bytes("tests/unit/resources/0"):
+        shards.append(documentai.Document.from_json(byte))
+
+    actual = document._entities_from_shards(shards=shards)
+
+    assert actual[0].mention_text == "$140.00"
+    assert actual[0].type_ == "vat"
+
+
+def test_document_from_document_path_with_single_shard():
+    actual = document.Document.from_document_path(
+        document_path="tests/unit/resources/0/toolbox_invoice_test-0.json"
+    )
+    assert len(actual.pages) == 1
+
+
+def test_document_from_documentai_document_with_single_shard():
+    with open("tests/unit/resources/0/toolbox_invoice_test-0.json", "r") as f:
+        doc = documentai.Document.from_json(f.read())
+
+    actual = document.Document.from_documentai_document(documentai_document=doc)
+    assert len(actual.pages) == 1
+
+
+def test_document_from_gcs_with_single_shard(get_bytes_single_file_mock):
+    actual = document.Document.from_gcs(
+        gcs_bucket_name="test-directory", gcs_prefix="documentai/output/123456789/0/"
+    )
+
+    get_bytes_single_file_mock.assert_called_once()
+    assert len(actual.pages) == 1
+
+
+def test_document_from_gcs_with_multiple_shards(get_bytes_multiple_files_mock):
+    actual = document.Document.from_gcs(
+        gcs_bucket_name="test-directory", gcs_prefix="documentai/output/123456789/1/"
+    )
+    get_bytes_multiple_files_mock.assert_called_once()
+
+    assert len(actual.pages) == 48
+
+
+@mock.patch("google.cloud.documentai_toolbox.wrappers.document.storage")
+def test_print_gcs_document_tree_with_one_folder(mock_storage, capfd):
+
+    client = mock_storage.Client.return_value
+
+    mock_bucket = mock.Mock()
+
+    client.Bucket.return_value = mock_bucket
+
+    blobs = [
+        storage.Blob(
+            name="gs://test-directory/1/test_shard1.json",
+            bucket="gs://test-directory/1",
+        ),
+        storage.Blob(
+            name="gs://test-directory/1/test_shard2.json",
+            bucket="gs://test-directory/1",
+        ),
+        storage.Blob(
+            name="gs://test-directory/1/test_shard3.json",
+            bucket="gs://test-directory/1",
+        ),
+    ]
+
+    client.list_blobs.return_value = blobs
+
+    document.print_gcs_document_tree(gcs_bucket_name="test-directory", gcs_prefix="/")
+
+    mock_storage.Client.assert_called_once()
+
+    out, err = capfd.readouterr()
+    assert (
+        out
+        == """gs://test-directory/1
+├──test_shard1.json
+├──test_shard2.json
+└──test_shard3.json\n\n"""
+    )
+
+
+@mock.patch("google.cloud.documentai_toolbox.wrappers.document.storage")
+def test_print_gcs_document_tree_with_3_documents(mock_storage, capfd):
+
+    client = mock_storage.Client.return_value
+
+    mock_bucket = mock.Mock()
+
+    client.Bucket.return_value = mock_bucket
+
+    blobs = [
+        storage.Blob(
+            name="gs://test-directory/documentai/output/123456789/1/test_shard1.json",
+            bucket="gs://test-directory/documentai/output/123456789/1",
+        ),
+        storage.Blob(
+            name="gs://test-directory/documentai/output/123456789/1/test_shard2.json",
+            bucket="gs://test-directory/documentai/output/123456789/1",
+        ),
+        storage.Blob(
+            name="gs://test-directory/documentai/output/123456789/1/test_shard3.json",
+            bucket="gs://test-directory/documentai/output/123456789/1",
+        ),
+    ]
+
+    client.list_blobs.return_value = blobs
+
+    document.print_gcs_document_tree(
+        gcs_bucket_name="test-directory", gcs_prefix="documentai/output/123456789/1/"
+    )
+
+    mock_storage.Client.assert_called_once()
+
+    out, err = capfd.readouterr()
+    assert (
+        out
+        == """gs://test-directory/documentai/output/123456789/1
+├──test_shard1.json
+├──test_shard2.json
+└──test_shard3.json\n\n"""
+    )
+
+
+@mock.patch("google.cloud.documentai_toolbox.wrappers.document.storage")
+def test_print_gcs_document_tree_with_more_than_5_document(mock_storage, capfd):
+
+    client = mock_storage.Client.return_value
+
+    mock_bucket = mock.Mock()
+
+    client.Bucket.return_value = mock_bucket
+
+    blobs = [
+        storage.Blob(
+            name="gs://test-directory/documentai/output/123456789/1/test_shard1.json",
+            bucket="gs://test-directory/documentai/output/123456789/1",
+        ),
+        storage.Blob(
+            name="gs://test-directory/documentai/output/123456789/1/test_shard2.json",
+            bucket="gs://test-directory/documentai/output/123456789/1",
+        ),
+        storage.Blob(
+            name="gs://test-directory/documentai/output/123456789/1/test_shard3.json",
+            bucket="gs://test-directory/documentai/output/123456789/1",
+        ),
+        storage.Blob(
+            name="gs://test-directory/documentai/output/123456789/1/test_shard4.json",
+            bucket="gs://test-directory/documentai/output/123456789/1",
+        ),
+        storage.Blob(
+            name="gs://test-directory/documentai/output/123456789/1/test_shard5.json",
+            bucket="gs://test-directory/documentai/output/123456789/1",
+        ),
+        storage.Blob(
+            name="gs://test-directory/documentai/output/123456789/1/test_shard6.json",
+            bucket="gs://test-directory/documentai/output/123456789/1",
+        ),
+    ]
+    client.list_blobs.return_value = blobs
+
+    document.print_gcs_document_tree(
+        gcs_bucket_name="test-directory", gcs_prefix="documentai/output/123456789/1/"
+    )
+
+    mock_storage.Client.assert_called_once()
+
+    out, err = capfd.readouterr()
+    assert (
+        out
+        == """gs://test-directory/documentai/output/123456789/1
+├──test_shard1.json
+├──test_shard2.json
+├──test_shard3.json
+├──test_shard4.json
+├──test_shard5.json
+│  ....
+└──test_shard6.json\n\n"""
+    )
+
+
+def test_print_gcs_document_tree_with_gcs_uri_contains_file_type():
+    with pytest.raises(ValueError, match="gcs_prefix cannot contain file types"):
+        document.print_gcs_document_tree(
+            gcs_bucket_name="test-directory",
+            gcs_prefix="documentai/output/123456789/1/test_file.json",
+        )
+
+
+def test_search_page_with_target_string(get_bytes_single_file_mock):
+
+    doc = document.Document.from_gcs(
+        gcs_bucket_name="test-directory", gcs_prefix="documentai/output/123456789/0/"
+    )
+
+    actual_string = doc.search_pages(target_string="contract")
+
+    get_bytes_single_file_mock.assert_called_once()
+    assert len(actual_string) == 1
+
+
+def test_search_page_with_target_pattern(get_bytes_single_file_mock):
+    doc = document.Document.from_gcs(
+        gcs_bucket_name="test-directory", gcs_prefix="documentai/output/123456789/0/"
+    )
+
+    actual_regex = doc.search_pages(pattern=r"\$\d+(?:\.\d+)?")
+
+    get_bytes_single_file_mock.assert_called_once()
+    assert len(actual_regex) == 1
+
+
+def test_search_page_with_regex_and_str(get_bytes_single_file_mock):
+    with pytest.raises(
+        ValueError,
+        match="Exactly one of target_string and pattern must be specified.",
+    ):
+
+        doc = document.Document.from_gcs(
+            gcs_bucket_name="test-directory",
+            gcs_prefix="documentai/output/123456789/0/",
+        )
+        doc.search_pages(pattern=r"^\$?(\d*(\d\.?|\.\d{1,2}))$", target_string="hello")
+
+        get_bytes_single_file_mock.assert_called_once()
+
+
+def test_search_page_with_none(get_bytes_single_file_mock):
+    with pytest.raises(
+        ValueError,
+        match="Exactly one of target_string and pattern must be specified.",
+    ):
+        doc = document.Document.from_gcs(
+            gcs_bucket_name="test-directory",
+            gcs_prefix="documentai/output/123456789/0/",
+        )
+        doc.search_pages()
+
+        get_bytes_single_file_mock.assert_called_once()
+
+
+def test_get_entity_by_type(get_bytes_single_file_mock):
+
+    doc = document.Document.from_gcs(
+        gcs_bucket_name="test-directory", gcs_prefix="documentai/output/123456789/0"
+    )
+
+    actual = doc.get_entity_by_type(target_type="receiver_address")
+
+    get_bytes_single_file_mock.assert_called_once()
+
+    assert len(actual) == 1
+    assert actual[0].type_ == "receiver_address"
+    assert actual[0].mention_text == "222 Main Street\nAnytown, USA"
