16062368b1766c8d25745e13df9fda8becd914d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:17:13 +0000

    Add package hail/0.2.109/hail-0.2.109-py3-none-any.whl

---
+++ packages/hail/hail-0.2.109-py3-none-any.whl/hail/backend/backend.py
@@ -0,0 +1,183 @@
+from typing import Mapping, List, Union, Tuple, Dict, Optional, Any
+import abc
+import orjson
+import pkg_resources
+import zipfile
+from ..fs.fs import FS
+from ..builtin_references import BUILTIN_REFERENCE_RESOURCE_PATHS
+from ..expr import Expression
+from ..expr.types import HailType
+from ..ir import BaseIR
+from ..utils.java import FatalError
+
+
+def fatal_error_from_java_error_triplet(short_message, expanded_message, error_id):
+    from .. import __version__
+    if error_id != -1:
+        return FatalError(f'Error summary: {short_message}', error_id)
+    return FatalError(f'''{short_message}
+
+Java stack trace:
+{expanded_message}
+Hail version: {__version__}
+Error summary: {short_message}''',
+                      error_id)
+
+
+class Backend(abc.ABC):
+    @abc.abstractmethod
+    def __init__(self):
+        self._persisted_locations = dict()
+
+    @abc.abstractmethod
+    def stop(self):
+        pass
+
+    @abc.abstractmethod
+    def execute(self, ir: BaseIR, timed: bool = False) -> Any:
+        pass
+
+    @abc.abstractmethod
+    async def _async_execute(self, ir, timed=False):
+        pass
+
+    @abc.abstractmethod
+    def value_type(self, ir):
+        pass
+
+    @abc.abstractmethod
+    def table_type(self, tir):
+        pass
+
+    @abc.abstractmethod
+    def matrix_type(self, mir):
+        pass
+
+    @abc.abstractmethod
+    def add_reference(self, config):
+        pass
+
+    @abc.abstractmethod
+    def load_references_from_dataset(self, path):
+        pass
+
+    @abc.abstractmethod
+    def from_fasta_file(self, name, fasta_file, index_file, x_contigs, y_contigs, mt_contigs, par):
+        pass
+
+    @abc.abstractmethod
+    def remove_reference(self, name):
+        pass
+
+    def get_reference(self, name):
+        if name in BUILTIN_REFERENCE_RESOURCE_PATHS:
+            path_in_jar = BUILTIN_REFERENCE_RESOURCE_PATHS[name]
+            jar_path = pkg_resources.resource_filename(__name__, 'hail-all-spark.jar')
+            return orjson.loads(zipfile.ZipFile(jar_path).open(path_in_jar).read())
+        return self._get_non_builtin_reference(name)
+
+    @abc.abstractmethod
+    def _get_non_builtin_reference(self, name):
+        pass
+
+    def get_references(self, names):
+        return [self.get_reference(name) for name in names]
+
+    @abc.abstractmethod
+    def add_sequence(self, name, fasta_file, index_file):
+        pass
+
+    @abc.abstractmethod
+    def remove_sequence(self, name):
+        pass
+
+    @abc.abstractmethod
+    def add_liftover(self, name, chain_file, dest_reference_genome):
+        pass
+
+    @abc.abstractmethod
+    def remove_liftover(self, name, dest_reference_genome):
+        pass
+
+    @abc.abstractmethod
+    def parse_vcf_metadata(self, path):
+        pass
+
+    @property
+    @abc.abstractmethod
+    def logger(self):
+        pass
+
+    @property
+    @abc.abstractmethod
+    def fs(self) -> FS:
+        pass
+
+    @abc.abstractmethod
+    def index_bgen(self,
+                   files: List[str],
+                   index_file_map: Dict[str, str],
+                   referenceGenomeName: Optional[str],
+                   contig_recoding: Dict[str, str],
+                   skip_invalid_loci: bool):
+        pass
+
+    @abc.abstractmethod
+    def import_fam(self, path: str, quant_pheno: bool, delimiter: str, missing: str):
+        pass
+
+    def persist_table(self, t):
+        from hail.context import TemporaryFilename
+        tf = TemporaryFilename(prefix='persist_table')
+        self._persisted_locations[t] = tf
+        return t.checkpoint(tf.__enter__())
+
+    def unpersist_table(self, t):
+        try:
+            self._persisted_locations[t].__exit__(None, None, None)
+        except KeyError as err:
+            raise ValueError(f'{t} is not persisted') from err
+
+    def persist_matrix_table(self, mt):
+        from hail.context import TemporaryFilename
+        tf = TemporaryFilename(prefix='persist_matrix_table')
+        self._persisted_locations[mt] = tf
+        return mt.checkpoint(tf.__enter__())
+
+    def unpersist_matrix_table(self, mt):
+        try:
+            self._persisted_locations[mt].__exit__(None, None, None)
+        except KeyError as err:
+            raise ValueError(f'{mt} is not persisted') from err
+
+    def unpersist_block_matrix(self, id):
+        pass
+
+    @abc.abstractmethod
+    def register_ir_function(self,
+                             name: str,
+                             type_parameters: Union[Tuple[HailType, ...], List[HailType]],
+                             value_parameter_names: Union[Tuple[str, ...], List[str]],
+                             value_parameter_types: Union[Tuple[HailType, ...], List[HailType]],
+                             return_type: HailType,
+                             body: Expression):
+        pass
+
+    @abc.abstractmethod
+    def persist_expression(self, expr: Expression) -> Expression:
+        pass
+
+    @abc.abstractmethod
+    def set_flags(self, **flags: Mapping[str, str]):
+        """Set Hail flags."""
+        pass
+
+    @abc.abstractmethod
+    def get_flags(self, *flags) -> Mapping[str, str]:
+        """Mapping of Hail flags."""
+        pass
+
+    @property
+    @abc.abstractmethod
+    def requires_lowering(self):
+        pass
---
+++ packages/hail/hail-0.2.109-py3-none-any.whl/hail/backend/py4j_backend.py
@@ -0,0 +1,133 @@
+from typing import Mapping, Union, Tuple, List
+import abc
+
+import py4j
+import py4j.java_gateway
+
+import hail
+from hail.expr import construct_expr
+from hail.ir import JavaIR, finalize_randomness
+from hail.ir.renderer import CSERenderer
+from hail.utils.java import FatalError, Env
+from .backend import Backend, fatal_error_from_java_error_triplet
+from ..expr import Expression
+from ..expr.types import HailType
+
+
+def handle_java_exception(f):
+    def deco(*args, **kwargs):
+        import pyspark
+        try:
+            return f(*args, **kwargs)
+        except py4j.protocol.Py4JJavaError as e:
+            s = e.java_exception.toString()
+
+            # py4j catches NoSuchElementExceptions to stop array iteration
+            if s.startswith('java.util.NoSuchElementException'):
+                raise
+
+            tpl = Env.jutils().handleForPython(e.java_exception)
+            deepest, full, error_id = tpl._1(), tpl._2(), tpl._3()
+            raise fatal_error_from_java_error_triplet(deepest, full, error_id) from None
+        except pyspark.sql.utils.CapturedException as e:
+            raise FatalError('%s\n\nJava stack trace:\n%s\n'
+                             'Hail version: %s\n'
+                             'Error summary: %s' % (e.desc, e.stackTrace, hail.__version__, e.desc)) from None
+
+    return deco
+
+
+class Py4JBackend(Backend):
+    _jbackend: py4j.java_gateway.JavaObject
+
+    @abc.abstractmethod
+    def __init__(self):
+        super(Py4JBackend, self).__init__()
+        import base64
+
+        def decode_bytearray(encoded):
+            return base64.standard_b64decode(encoded)
+
+        # By default, py4j's version of this function does extra
+        # work to support python 2. This eliminates that.
+        py4j.protocol.decode_bytearray = decode_bytearray
+
+    @abc.abstractmethod
+    def jvm(self):
+        pass
+
+    @abc.abstractmethod
+    def hail_package(self):
+        pass
+
+    @abc.abstractmethod
+    def utils_package_object(self):
+        pass
+
+    @abc.abstractmethod
+    def _parse_value_ir(self, code, ref_map={}, ir_map={}):
+        pass
+
+    @abc.abstractmethod
+    def _to_java_value_ir(self, ir):
+        pass
+
+    def register_ir_function(self,
+                             name: str,
+                             type_parameters: Union[Tuple[HailType, ...], List[HailType]],
+                             value_parameter_names: Union[Tuple[str, ...], List[str]],
+                             value_parameter_types: Union[Tuple[HailType, ...], List[HailType]],
+                             return_type: HailType,
+                             body: Expression):
+        r = CSERenderer(stop_at_jir=True)
+        code = r(finalize_randomness(body._ir))
+        jbody = (self._parse_value_ir(code, ref_map=dict(zip(value_parameter_names, value_parameter_types)), ir_map=r.jirs))
+
+        Env.hail().expr.ir.functions.IRFunctionRegistry.pyRegisterIR(
+            name,
+            [ta._parsable_string() for ta in type_parameters],
+            value_parameter_names,
+            [pt._parsable_string() for pt in value_parameter_types],
+            return_type._parsable_string(),
+            jbody)
+
+    def execute(self, ir, timed=False):
+        jir = self._to_java_value_ir(ir)
+        stream_codec = '{"name":"StreamBufferSpec"}'
+        # print(self._hail_package.expr.ir.Pretty.apply(jir, True, -1))
+        try:
+            result_tuple = self._jbackend.executeEncode(jir, stream_codec, timed)
+            (result, timings) = (result_tuple._1(), result_tuple._2())
+            value = ir.typ._from_encoding(result)
+
+            return (value, timings) if timed else value
+        except FatalError as e:
+            raise e.maybe_user_error(ir) from None
+
+    async def _async_execute(self, ir, timed=False):
+        raise NotImplementedError('no async available in Py4JBackend')
+
+    def persist_expression(self, expr):
+        return construct_expr(
+            JavaIR(self._jbackend.executeLiteral(self._to_java_value_ir(expr._ir))),
+            expr.dtype
+        )
+
+    def set_flags(self, **flags: Mapping[str, str]):
+        available = self._jbackend.availableFlags()
+        invalid = []
+        for flag, value in flags.items():
+            if flag in available:
+                self._jbackend.setFlag(flag, value)
+            else:
+                invalid.append(flag)
+        if len(invalid) != 0:
+            raise FatalError("Flags {} not valid. Valid flags: \n    {}"
+                             .format(', '.join(invalid), '\n    '.join(available)))
+
+    def get_flags(self, *flags) -> Mapping[str, str]:
+        return {flag: self._jbackend.getFlag(flag) for flag in flags}
+
+    @property
+    def requires_lowering(self):
+        return True
