a98ba065acb9a7921bac861d9ccd21927ccafa5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:34:02 +0000

    Add package yc_as_client_v2/67.0.1/yc_as_client_v2-67.0.1.tar.gz

---
+++ packages/yc_as_client_v2/yc_as_client_v2-67.0.1.tar.gz/yc_as_client_v2-67.0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+   name='yc_as_client_v2',
+   version='67.0.1',
+   author='Yandex',
+   author_email='security@yandex-team.ru',
+   url='https://ya.ru',
+   readme="README.md",
+   long_description="""This is a security placeholder package.""",
+   long_description_content_type='text/plain',
+   description='A package to prevent Dependency Confusion attacks against Yandex.',
+   )
