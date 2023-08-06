c8ae78424eee1f90f8d09689b1b37d8b482baca9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:20:31 +0000

    Add package distpacktest/0.1/distpacktest-0.1.tar.gz

---
+++ packages/distpacktest/distpacktest-0.1.tar.gz/distpacktest-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='distpacktest',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distpacktest'],
+      author = 'Jenny Jang',
+      author_email = 'eppunijyj@gmail.com',
+      zip_safe=False)
