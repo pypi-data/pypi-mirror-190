4f4048843f3077543d46cdab88f761173f4a1b30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:33:46 +0000

    Add package labelbase/0.0.54/labelbase-0.0.54.tar.gz

---
+++ packages/labelbase/labelbase-0.0.54.tar.gz/labelbase-0.0.54/labelbase/__init__.py
@@ -0,0 +1,10 @@
+name = "labelbase"
+__version__ = "0.0.51"
+
+from labelbase import converters
+from labelbase.uploader import *
+from labelbase.metadata import *
+from labelbase.ontology import *
+from labelbase.dataset import *
+from labelbase.annotate import *
+from labelbase.connector import *
