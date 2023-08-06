eba8b03ec1cbdf90ef9505c07f30ced04ce500d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:27 +0000

    Add package lXtractor/0.1.dev5/lxtractor-0.1.dev5-py3-none-any.whl

---
+++ packages/lXtractor/lxtractor-0.1.dev5-py3-none-any.whl/lXtractor/core/structure.py
@@ -0,0 +1,277 @@
+"""
+Module defines basic interfaces to interact with macromolecular structures.
+"""
+from __future__ import annotations
+
+import logging
+import typing as t
+from collections import abc
+from dataclasses import dataclass
+from os import PathLike
+from pathlib import Path
+
+import biotite.structure as bst
+import biotite.structure.io as strio
+import numpy as np
+from typing_extensions import Self
+
+from lXtractor.core.base import AminoAcidDict
+from lXtractor.core.config import EMPTY_PDB_ID
+from lXtractor.core.exceptions import NoOverlap, InitError, LengthMismatch, MissingData
+from lXtractor.core.segment import Segment
+from lXtractor.util.structure import filter_selection
+
+LOGGER = logging.getLogger(__name__)
+
+
+class GenericStructure:
+    """
+    A generic macromolecular structure with possibly many chains holding
+    a single :class:`biotite.structure.AtomArray` instance.
+    """
+
+    __slots__ = ('_array', 'pdb_id')
+
+    def __init__(self, array: bst.AtomArray, pdb_id: str | None = None):
+        """
+        :param array: Atom array object.
+        :param pdb_id: PDB ID of a structure in `array`.
+        """
+        #: Atom array object.
+        self._array: bst.AtomArray = array
+        #: PDB ID of a structure in `array`.
+        self.pdb_id: str | None = pdb_id
+
+    def __len__(self) -> int:
+        return len(self.array)
+
+    def __eq__(self, other: t.Any) -> bool:
+        if isinstance(other, GenericStructure):
+            return self.pdb_id == other.pdb_id and np.all(self.array == other.array)
+        return False
+
+    def __hash__(self):
+        atoms = tuple(
+            (a.chain_id, a.res_id, a.res_name, a.atom_name, tuple(a.coord))
+            for a in self.array
+        )
+        return hash(self.pdb_id) + hash(atoms)
+
+    @property
+    def array(self) -> bst.AtomArray:
+        """
+        :return: Atom array object.
+        """
+        return self._array
+
+    @property
+    def is_empty(self) -> bool:
+        """
+        :return: ``True`` if the :meth:`array` is empty and ``False``
+            otherwise.
+        """
+        return len(self) == 0
+
+    @classmethod
+    def read(
+        cls, path: Path, path2id: abc.Callable[[Path], str] = lambda p: p.stem
+    ) -> Self:
+        """
+        :param path: Path to a structure in supported format.
+        :param path2id: A callable obtaining a PDB ID from the file path.
+            By default, it's a ``Path.stem``.
+        :return: Parsed structure.
+        """
+        array = strio.load_structure(str(path))
+        pdb_id = path2id(path)
+        if not len(pdb_id) == 4:
+            pdb_id = EMPTY_PDB_ID
+            # LOGGER.warning(f'Did not obtain a valid PDB ID {pdb_id} from {path}')
+        if isinstance(array, bst.AtomArrayStack):
+            raise InitError(
+                f'{path} is likely an NMR structure. '
+                f'NMR structures are not supported.'
+            )
+        return cls(array, pdb_id)
+
+    @classmethod
+    def make_empty(cls, pdb_id: str | None = None) -> Self:
+        """
+        :param pdb_id: (Optional) PDB ID of the created array.
+        :return: An instance with empty :meth:`array`.
+        """
+        return cls(bst.AtomArray(0), pdb_id)
+
+    def write(self, path: Path | PathLike | str):
+        """
+        A one-line wrapper around :func:`biotite.structure.io.save_structure`.
+
+        :param path: A path or a path-like object compatible with :func:`open`.
+        :return: Nothing
+        """
+        strio.save_structure(path, self.array)
+
+    def get_sequence(self) -> abc.Iterable[tuple[str, str, int]]:
+        """
+        :return: A tuple with (1) one-letter code, (2) three-letter code,
+            (3) residue number.
+        """
+        # TODO: rm specialization towards protein seq
+        if self.is_empty:
+            return []
+
+        mapping = AminoAcidDict()
+        for r in bst.residue_iter(self.array):
+            atom = r[0]
+            try:
+                one_letter_code = mapping.three21[atom.res_name]
+            except KeyError:
+                one_letter_code = 'X'
+            yield one_letter_code, atom.res_name, atom.res_id
+
+    def split_chains(self, *, copy: bool = False) -> abc.Iterator[Self]:
+        """
+        Split into separate chains. Splitting is done using
+        :func:`biotite.structure.get_chain_starts`.
+
+        :param copy: Copy atom arrays resulting from subsetting based on
+            chain annotation.
+        :return: An iterable over chains found in :attr:`array`.
+        """
+        a = self.array
+        arrays = (a[a.chain_id == c] for c in sorted(set(a.chain_id)))
+        chains = (self.__class__(a.copy() if copy else a, self.pdb_id) for a in arrays)
+        yield from chains
+
+    def sub_structure(self, start: int, end: int) -> Self:
+        """
+        Create a sub-structure encompassing some continuous segment.
+
+        :param start: Residue number to start from (inclusive).
+        :param end: Residue number to stop at (inclusive).
+        :return: A new Generic structure with residues in ``[start, end]``.
+        """
+        if self.is_empty:
+            raise NoOverlap('Attempting to sub an empty structure')
+
+        self_start, self_end = self.array.res_id.min(), self.array.res_id.max()
+
+        # This is needed when some coordinates are <= 0 which can occur
+        # in PDB structures but unsupported for a Segment.
+        offset_self = abs(self_start) + 1 if self_start <= 0 else 0
+        offset_start = abs(start) + 1 if start <= 0 else 0
+        offset = max(offset_start, offset_self)
+        seg_self = Segment(self_start + offset, self_end + offset)
+        seg_sub = Segment(start + offset, end + offset)
+        if not seg_self.bounds(seg_sub):
+            raise NoOverlap(
+                f'Provided positions {start, end} lie outside '
+                f'of the structure positions {self_start, self_end}'
+            )
+        idx = (self.array.res_id >= start) & (self.array.res_id <= end)
+        return self.__class__(self.array[idx], self.pdb_id)
+
+    def superpose(
+        self,
+        other: GenericStructure | bst.AtomArray,
+        res_id_self: abc.Iterable[int] | None = None,
+        res_id_other: abc.Iterable[int] | None = None,
+        atom_names_self: abc.Iterable[abc.Sequence[str]]
+        | abc.Sequence[str]
+        | None = None,
+        atom_names_other: abc.Iterable[abc.Sequence[str]]
+        | abc.Sequence[str]
+        | None = None,
+        mask_self: np.ndarray | None = None,
+        mask_other: np.ndarray | None = None,
+    ) -> tuple[GenericStructure, float, tuple[np.ndarray, np.ndarray, np.ndarray]]:
+        """
+        Superpose other structure to this one.
+        Arguments to this function all serve a single purpose: to correctly
+        subset both structures so the resulting selections have the same number
+        of atoms.
+
+        The subsetting achieved either by speficiying residue numbers and atom
+        names or by suppliying a binary mask of the same length as the number
+        of atoms in the structure.
+
+        :param other: Other :class:`GenericStructure` or atom array.
+        :param res_id_self: Residue numbers to select in this structure.
+        :param res_id_other: Residue numbers to select in other structure.
+        :param atom_names_self: Atom names to select in this structure given
+            either per-residue or broadcasted to selected residues.
+        :param atom_names_other: Same as `self`.
+        :param mask_self: Binary mask to select atoms. Takes precedence over
+            other selection arguments.
+        :param mask_other: Same as `self`.
+        :return:
+        """
+
+        def _get_mask(a, res_id, atom_names):
+            if res_id:
+                m = filter_selection(a, res_id, atom_names)
+            else:
+                if atom_names:
+                    m = np.isin(a.atom_name, atom_names)
+                else:
+                    m = np.ones_like(a, bool)
+            return m
+
+        if self.is_empty or other.is_empty:
+            raise MissingData('Superposing empty structures is not supported')
+
+        if mask_self is None:
+            mask_self = _get_mask(self.array, res_id_self, atom_names_self)
+        if mask_other is None:
+            mask_other = _get_mask(other.array, res_id_other, atom_names_other)
+
+        num_self, num_other = mask_self.sum(), mask_other.sum()
+        if num_self != num_other:
+            raise LengthMismatch(
+                f'To superpose, the number of atoms must match. '
+                f'Got {num_self} in self and {num_other} in other.'
+            )
+
+        if num_self == num_other == 0:
+            raise MissingData('No atoms selected')
+
+        superposed, transformation = bst.superimpose(
+            self.array[mask_self], other.array[mask_other]
+        )
+        other_transformed = bst.superimpose_apply(other.array, transformation)
+
+        rmsd_target = bst.rmsd(self.array[mask_self], superposed)
+
+        return (
+            GenericStructure(other_transformed, other.pdb_id),
+            rmsd_target,
+            transformation,
+        )
+
+
+@dataclass
+class PDB_Chain:
+    """
+    A container to hold the data of a single structural chain.
+    """
+
+    id: str
+    chain: str
+    structure: GenericStructure | None
+
+
+def _validate_chain(pdb: PDB_Chain):
+    if pdb.structure.is_empty:
+        return
+    chains = set(pdb.structure.array.chain_id)
+    if len(chains) > 1:
+        raise InitError('The structure must contain a single chain')
+    chain_id = chains.pop()
+    if chain_id != pdb.chain:
+        raise InitError(
+            f'Actual chain {chain_id} does not match .chain attribute {pdb.chain}'
+        )
+
+
+if __name__ == '__main__':
+    raise RuntimeError
---
+++ packages/lXtractor/lxtractor-0.1.dev5-py3-none-any.whl/lXtractor/util/structure.py
@@ -0,0 +1,238 @@
+"""
+Low-level utilities to work with structures.
+"""
+from __future__ import annotations
+
+import logging
+from collections import abc
+from itertools import repeat, starmap
+
+import biotite.structure as bst
+import biotite.structure.info as bstinfo
+import numpy as np
+from more_itertools import unzip
+
+from lXtractor.core.exceptions import LengthMismatch, MissingData
+from lXtractor.util.typing import is_sequence_of
+
+LOGGER = logging.getLogger(__name__)
+
+_BASIC_COMPARISON_ATOMS = {'N', 'CA', 'C', 'CB'}
+
+
+# def read_fast_pdb(path: Path, model: int = 1) -> bst.AtomArray:
+#     file = fastpdb.PDBFile.read(str(path))
+#     return file.get_structure(model=model)
+
+
+def calculate_dihedral(
+    atom1: np.ndarray, atom2: np.ndarray, atom3: np.ndarray, atom4: np.ndarray
+) -> float:
+    """
+    Calculate angle between planes formed by
+    [a1, a2, atom3] and [a2, atom3, atom4].
+
+    Each atom is an array of shape (3, ) with XYZ coordinates.
+
+    Calculation method inspired by
+    https://math.stackexchange.com/questions/47059/how-do-i-calculate-a-
+    dihedral-angle-given-cartesian-coordinates
+    """
+
+    def vnorm(v: np.ndarray) -> np.ndarray:
+        return v / np.linalg.norm(v)  # type: ignore  # thinks is "Any"
+
+    v1, v2, v3 = map(vnorm, [atom2 - atom1, atom3 - atom2, atom4 - atom3])
+
+    n1 = np.cross(v1, v2)
+    n2 = np.cross(v2, v3)
+    x = n1.dot(n2)
+    y = np.cross(n1, n2).dot(v2)
+
+    res: float = np.arctan2(y, x)
+    return res
+
+
+def filter_selection(
+    array: bst.AtomArray,
+    res_id: abc.Sequence[int] | None,
+    atom_names: abc.Sequence[abc.Sequence[str]] | abc.Sequence[str] | None = None,
+) -> np.ndarray:
+    """
+    Filter :class:`AtomArray` by residue numbers and atom names.
+
+    :param array: Arbitrary structure.
+    :param res_id: A sequence of residue numbers.
+    :param atom_names: A sequence of atom names (broadcasted to each position
+        in `res_id`) or an iterable over such sequences for each position in
+        `res_id`.
+    :return: A binary mask that is ``True`` for filtered atoms.
+    """
+
+    if res_id is None:
+        res_id, _ = bst.get_residues(array)
+
+    _atom_names: abc.Iterable[None] | abc.Iterable[abc.Sequence[str]]
+
+    if atom_names is None:
+        _atom_names = repeat(None, len(res_id))
+    elif is_sequence_of(atom_names, str):
+        _atom_names = repeat(atom_names, len(res_id))
+    else:
+        _atom_names = atom_names
+
+    mask = np.zeros_like(array, bool)
+
+    for r_id, a_names in zip(res_id, _atom_names, strict=True):
+        mask_local = array.res_id == r_id
+        if a_names is not None:
+            mask_local &= np.isin(array.atom_name, a_names)
+        mask |= mask_local
+    return mask
+
+
+def filter_to_common_atoms(
+    a1: bst.AtomArray, a2: bst.AtomArray, allow_residue_mismatch: bool = False
+) -> tuple[np.ndarray, np.ndarray]:
+    """
+    Filter to atoms common between residues of atom arrays a1 and a2.
+
+    :param a1: Arbitrary atom array.
+    :param a2: Arbitrary atom array.
+    :param allow_residue_mismatch: If ``True``, when residue names mismatch,
+        the common atoms are derived from the intersection
+        ``a1.atoms & a2.atoms & {"C", "N", "CA", "CB"}``.
+    :return: A pair of masks for a1 and a2, ``True`` for matching atoms.
+    :raises ValueError:
+
+        1. If `a1` and `a2` have different number of residues.
+        2. If the selection for some residue produces different number
+            of atoms.
+    """
+
+    def preprocess_array(a: bst.AtomArray) -> tuple[int, bst.AtomArray]:
+        num_res = bst.get_residue_count(a)
+        r_it = bst.residue_iter(a)
+        return num_res, r_it
+
+    def process_pair(
+        r1: bst.AtomArray, r2: bst.AtomArray
+    ) -> tuple[np.ndarray, np.ndarray]:
+        r1_name, r2_name = r1.res_name[0], r2.res_name[0]
+        atom_names = set(r1.atom_name) & set(r2.atom_name)
+
+        if r1_name != r2_name:
+            if not allow_residue_mismatch:
+                raise ValueError(
+                    f'Residue names must match. Got {r1_name} from the first array '
+                    f'and {r2_name} from the second one. Use `allow_residue_mismatch` '
+                    'to allow name mismatches.'
+                )
+            atom_names &= _BASIC_COMPARISON_ATOMS
+
+        m1, m2 = map(lambda r: np.isin(r.atom_name, list(atom_names)), [r1, r2])
+        if m1.sum() != m2.sum():
+            raise ValueError(
+                f'Obtained different sets of atoms {atom_names}. '
+                f'Residue 1: {r1[m1]}. Residue 2: {r2[m2]}'
+            )
+
+        return m1, m2
+
+    (a1_l, a1_it), (a2_l, a2_it) = map(preprocess_array, [a1, a2])
+
+    if a1_l != a2_l:
+        raise LengthMismatch(
+            'The number of residues must match between structures. '
+            f'Got {a1_l} for `a1` and {a2_l} for `a2`.'
+        )
+
+    mask1, mask2 = map(
+        # numpy + mypy = confusion
+        lambda x: np.concatenate(list(x)),  # type: ignore
+        unzip(starmap(process_pair, zip(a1_it, a2_it, strict=True))),
+    )
+
+    return mask1, mask2
+
+
+def iter_canonical(a: bst.AtomArray) -> abc.Generator[bst.AtomArray | None, None, None]:
+    """
+    :param a: Arbitrary atom array.
+    :return: Generator of canonical versions of residues in `a` or ``None``
+        if no such residue found in CCD.
+    """
+    for name in bst.get_residues(a)[1]:
+        try:
+            r_can = bstinfo.residue(name)
+            yield r_can
+        except KeyError:
+            yield None
+
+
+def _exclude(a, names, elements):
+    if names:
+        a = a[~np.isin(a.atom_name, names)]
+    if elements:
+        a = a[~np.isin(a.element, elements)]
+    return a
+
+
+def get_missing_atoms(
+    a: bst.AtomArray,
+    excluding_names: abc.Sequence[str] | None = ('OXT',),
+    excluding_elements: abc.Sequence[str] | None = ('H',),
+) -> abc.Generator[list[str | None] | None, None, None]:
+    """
+    For each residue, compare with the one stored in CCD, and find missing
+    atoms.
+
+    :param a: Non-empty atom array.
+    :param excluding_names: A sequence of atom names to exclude
+        for calculation.
+    :param excluding_elements: A sequence of element names to exclude
+        for calculation.
+    :return: A generator of lists of missing atoms (excluding hydrogens)
+        per residue in `a` or ``None`` if not such residue was found in CCD.
+    """
+    if len(a) == 0:
+        raise MissingData('Array is empty')
+    for r_can, r_obs in zip(iter_canonical(a), bst.residue_iter(a)):
+        if r_can is None:
+            yield None
+        else:
+            r_can = _exclude(r_can, excluding_names, excluding_elements)
+            m_can, _ = filter_to_common_atoms(r_can, r_obs)
+            yield list(r_can[~m_can].atom_name)
+
+
+def get_observed_atoms_frac(
+    a: bst.AtomArray,
+    excluding_names: abc.Sequence[str] | None = ('OXT',),
+    excluding_elements: abc.Sequence[str] | None = ('H',),
+) -> abc.Generator[list[str | None] | None, None, None]:
+    """
+    Find fractions of observed atoms compared to canonical residue versions
+    stored in CCD.
+
+    :param a: Non-empty atom array.
+    :param excluding_names: A sequence of atom names to exclude
+        for calculation.
+    :param excluding_elements: A sequence of element names to exclude
+        for calculation.
+    :return: A generator of observed atom fractions per residue in
+        `a` or ``None`` if a residue was not found in CCD.
+    """
+    if len(a) == 0:
+        raise MissingData('Array is empty')
+    for r_can, r_obs in zip(iter_canonical(a), bst.residue_iter(a)):
+        if r_can is None:
+            yield None
+        else:
+            r_can = _exclude(r_can, excluding_names, excluding_elements)
+            _, m_obs = filter_to_common_atoms(r_can, r_obs)
+            yield m_obs.sum() / len(r_can)
+
+
+if __name__ == '__main__':
+    raise RuntimeError
