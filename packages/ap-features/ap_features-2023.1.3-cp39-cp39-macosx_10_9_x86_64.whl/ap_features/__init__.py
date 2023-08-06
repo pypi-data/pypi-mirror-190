579b53f4ac80fdafbed5b58b3a8d0a5c36703dca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:36:30 +0000

    Add package ap-features/2023.1.3/ap_features-2023.1.3-cp39-cp39-macosx_10_9_x86_64.whl

---
+++ packages/ap-features/ap_features-2023.1.3-cp39-cp39-macosx_10_9_x86_64.whl/ap_features/__init__.py
@@ -0,0 +1,70 @@
+__author__ = """Henrik Finsberg"""
+__email__ = "henriknf@simula.no"
+__version__ = "2023.1.3"
+
+import logging as _logging
+
+from . import (
+    background,
+    chopping,
+    features,
+    average,
+    filters,
+    lib,
+    utils,
+    _c,
+    _numba,
+    plot,
+)
+from ._c import NUM_COST_TERMS
+from ._numba import transpose_trace_array
+from .beat import Beat, Beats, Trace, BeatCollection, StateCollection, State
+from .features import all_cost_terms, apd, apd_up_xy, cost_terms, cost_terms_trace
+from .utils import Backend, list_cost_function_terms, list_cost_function_terms_trace
+from .average import average_and_interpolate
+from .background import BackgroundCorrection as BC
+
+
+def set_log_level(level=_logging.INFO):
+    for logger in [
+        _c.logger,
+        _numba.logger,
+        background.logger,
+        chopping.logger,
+        features.logger,
+        filters.logger,
+        lib.logger,
+        utils.logger,
+    ]:
+        logger.setLevel(level)
+
+
+set_log_level()
+
+__all__ = [
+    "features",
+    "apd",
+    "Backend",
+    "apd_up_xy",
+    "cost_terms",
+    "cost_terms_trace",
+    "all_cost_terms",
+    "peak_and_repolarization",
+    "transpose_trace_array",
+    "list_cost_function_terms_trace",
+    "list_cost_function_terms",
+    "NUM_COST_TERMS",
+    "Beat",
+    "Trace",
+    "Beats",
+    "background",
+    "chopping",
+    "BeatCollection",
+    "StateCollection",
+    "State",
+    "average",
+    "average_and_interpolate",
+    "filters",
+    "BC",
+    "plot",
+]
