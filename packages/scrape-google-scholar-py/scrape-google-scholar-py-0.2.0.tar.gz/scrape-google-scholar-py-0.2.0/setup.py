ff2d7ae1f3f1976406321f6e0b27a269fdd17a14
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:32:55 +0000

    Add package scrape-google-scholar-py/0.2.0/scrape-google-scholar-py-0.2.0.tar.gz

---
+++ packages/scrape-google-scholar-py/scrape-google-scholar-py-0.2.0.tar.gz/scrape-google-scholar-py-0.2.0/setup.py
@@ -0,0 +1,60 @@
+from setuptools import setup
+
+
+setup(
+    name='scrape-google-scholar-py',
+    description = 'Extract data from all Google Scholar pages in Python. Sponsored by SerpApi.',
+    url='https://github.com/dimitryzub/scrape-google-scholar',
+    version='0.2.0',
+    license='MIT',
+    author='Dmitiry Zub',
+    author_email='dimitryzub@gmail.com',
+    maintainer='Dmitiry Zub',
+    maintainer_email='dimitryzub@gmail.com',
+    long_description_content_type='text/markdown',
+    include_package_data=True,
+    classifiers = [
+        'Intended Audience :: Developers',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: MacOS',
+        'Operating System :: POSIX :: Linux',
+        'Topic :: Internet',
+        'Natural Language :: English',
+        'Topic :: Utilities',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+    ],
+    keywords=[
+            'google scholar',
+            'serpapi',
+            'scraper',
+            'python',
+            'python google scholar',
+            'python google scholar api',
+            'web scraping',
+            'python web scraping',
+            'research',
+            'lexbor',
+            'selectolax',
+            'selenium',
+            'selenium-stealth',
+            'pandas',
+        ],
+    install_requires=[
+          'google-search-results',
+          'selectolax',
+          'selenium-stealth',
+          'pandas',
+    ],
+    extras_require={
+        'dev':[
+            'ruff', #TODO: add test dev dependencies
+        ]
+    },
+    project_urls={
+        'Documentation': 'https://github.com/dimitryzub/scrape-google-scholar#example-usage-custom-backend',
+        'Source': 'https://github.com/dimitryzub/scrape-google-scholar',
+        'Tracker': 'https://github.com/dimitryzub/scrape-google-scholar/issues',
+    },
+)
