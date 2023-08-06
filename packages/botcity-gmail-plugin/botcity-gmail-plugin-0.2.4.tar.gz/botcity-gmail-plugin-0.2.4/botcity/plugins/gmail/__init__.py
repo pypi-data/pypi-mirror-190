1438b465b4b03b5767b5f737813f65cf3ff3da9a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:30:52 +0000

    Add package botcity-gmail-plugin/0.2.4/botcity-gmail-plugin-0.2.4.tar.gz

---
+++ packages/botcity-gmail-plugin/botcity-gmail-plugin-0.2.4.tar.gz/botcity-gmail-plugin-0.2.4/botcity/plugins/gmail/__init__.py
@@ -0,0 +1,5 @@
+from . import _version
+from .plugin import BotGmailPlugin  # noqa: F401, F403
+from .utils import GmailDefaultLabels  # noqa: F401, F403
+
+__version__ = _version.get_versions()['version']
---
+++ packages/botcity-gmail-plugin/botcity-gmail-plugin-0.2.4.tar.gz/botcity-gmail-plugin-0.2.4/botcity/plugins/gmail/utils.py
@@ -0,0 +1,53 @@
+from enum import Enum
+
+from imap_tools import MailMessage
+
+
+class BotGmailMessage(MailMessage):
+
+    def _set_message_response(self, msg_response):
+        self._msg_response = msg_response
+
+    def _set_message_labels(self, labels_name):
+        self._msg_labels = labels_name
+
+    @property
+    def id_(self):
+        """
+        The id of the gmail message
+        """
+        return str(self._msg_response.get('id'))
+
+    @property
+    def thread_id(self):
+        """
+        The thread id of the gmail message
+        """
+        return str(self._msg_response.get('threadId'))
+
+    @property
+    def labels(self):
+        """
+        The message labels
+        """
+        return self._msg_labels
+
+
+class GmailDefaultLabels(str, Enum):
+    """
+    The class with the enumerated Gmail dafault labels
+    Usage: GmailDefaultLabels.<LABEL_NAME>
+    """
+    INBOX = "INBOX"
+    SPAM = "SPAM"
+    TRASH = "TRASH"
+    UNREAD = "UNREAD"
+    STARRED = "STARRED"
+    IMPORTANT = "IMPORTANT"
+    SENT = "SENT"
+    DRAFT = "DRAFT"
+    CATEGORY_PERSONAL = "CATEGORY_PERSONAL"
+    CATEGORY_SOCIAL = "CATEGORY_SOCIAL"
+    CATEGORY_PROMOTIONS = "CATEGORY_PROMOTIONS"
+    CATEGORY_UPDATES = "CATEGORY_UPDATES"
+    CATEGORY_FORUMS = "CATEGORY_FORUMS"
