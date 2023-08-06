7b83efab9b25ee779a0776d7210c2f1dde846263
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:00:11 +0000

    Add package MSTR-Robotics-magerdaniel/0.0.64/MSTR_Robotics_magerdaniel-0.0.64.tar.gz

---
+++ packages/MSTR-Robotics-magerdaniel/MSTR_Robotics_magerdaniel-0.0.64.tar.gz/MSTR_Robotics_magerdaniel-0.0.64/setup.py
@@ -0,0 +1,21 @@
+#[tool.poetry]
+description = "Demo Python scripts to automating MicroStrategy devOps processes"
+
+from distutils.core import setup
+#from setuptools import setup
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+setup(name='MSTR_Robotics_magerdaniel',
+      version= "0.0.64",
+      description="MicroStrateg(P)ython",
+      author="Daniel Mager",
+      author_email='"danielmager@gmx.de"',
+      url="https://github.com/magerdaniel",
+      long_description=long_description,
+      long_description_content_type='text/markdown',
+      packages=['mstr_robotics'],
+      exclude = ["hello_beer.py","zzz_mig_create_short_cut.py","IBCS_WIZZARD.py"]
+
+)
+
