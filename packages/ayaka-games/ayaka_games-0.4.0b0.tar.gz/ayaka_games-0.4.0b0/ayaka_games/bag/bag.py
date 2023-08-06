ab53292814139c824e2639ceebfb745a0d670b40
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:18:52 +0000

    Add package ayaka-games/0.4.0b0/ayaka_games-0.4.0b0.tar.gz

---
+++ packages/ayaka-games/ayaka_games-0.4.0b0.tar.gz/ayaka_games-0.4.0b0/ayaka_games/bag/bag.py
@@ -0,0 +1,27 @@
+from ayaka import AyakaCat, get_adapter
+from ..utils import subscribe, db
+
+cat = AyakaCat("背包",db=db)
+adapter = get_adapter()
+
+
+@subscribe.cls_property_watch
+class Money(db.UserDBBase, table=True):
+    money: int = 0
+
+
+@cat.on_cmd(cmds=["bag", "背包"], always=True)
+async def show_bag():
+    '''展示背包；你还可以 bag @xx 查看其他人的背包'''
+    uid = cat.user.id
+    name = cat.user.name
+
+    if cat.event.at:
+        user = await cat.get_user(cat.event.at)
+        if not user:
+            return await cat.send("查无此人")
+        uid = user.id
+        name = user.name
+
+    money = Money.get_or_create(cat.group.id, uid)
+    await cat.send(f"[{name}]当前有 {money.money}金")
