e7a00d80ed5a22084d0ccebb144e23d35f270242
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:37:56 +0000

    Add package SUSspLb/0.2/SUSspLb-0.2.tar.gz

---
+++ packages/SUSspLb/SUSspLb-0.2.tar.gz/SUSspLb-0.2/code/__init__.py
@@ -0,0 +1,11 @@
+def somme(x,y):
+    return x+y
+
+def sost(x,y):
+    return x-y
+
+def multi(x,y):
+    return x*y
+
+def divs(x,y):
+    return x/y
---
+++ packages/SUSspLb/SUSspLb-0.2.tar.gz/SUSspLb-0.2/setup.py
@@ -0,0 +1,15 @@
+import setuptools
+
+setuptools.setup(
+    name="SUSspLb",
+    version="0.2",
+    author="Mohamed Elaissoui",
+    author_email="mohammadelaissaoui@gmail.com",
+    description="A simple python package",
+    packages=setuptools.find_packages(),
+    classifiers=[
+    "Programming Language :: Python :: 3",
+    "License :: OSI Approved :: MIT License",
+    "Operating System :: OS Independent",
+    ]
+)
