c3f0c5300d8d27b1b4588c9f391e9c64820a8b79
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:37:20 +0000

    Add package oda-data/0.4.1/oda_data-0.4.1-py3-none-any.whl

---
+++ packages/oda-data/oda_data-0.4.1-py3-none-any.whl/oda_data/__init__.py
@@ -0,0 +1,19 @@
+__version__ = "0.4.1"
+
+from oda_data.classes.oda_data import ODAData
+from oda_data.get_data.crs import download_crs
+from oda_data.get_data.dac1 import download_dac1
+from oda_data.get_data.dac2a import download_dac2a
+from oda_data.get_data.multisystem import download_multisystem
+from pydeflate import set_pydeflate_path
+
+
+def set_data_path(path):
+    from pathlib import Path
+    from oda_data.config import OdaPATHS
+
+    """Set the path to the data folder."""
+    global OdaPATHS
+
+    OdaPATHS.raw_data = Path(path).resolve()
+    set_pydeflate_path(OdaPATHS.raw_data)
