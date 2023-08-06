393fb4070c09f4093c06408f583d03e4a8596b9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:18:51 +0000

    Add package aleph-alpha-client/2.10.0/aleph_alpha_client-2.10.0-py3-none-any.whl

---
+++ packages/aleph-alpha-client/aleph_alpha_client-2.10.0-py3-none-any.whl/aleph_alpha_client/__init__.py
@@ -0,0 +1,61 @@
+from .aleph_alpha_client import (
+    AlephAlphaClient,
+    QuotaError,
+    POOLING_OPTIONS,
+    AsyncClient,
+    Client,
+)
+from .aleph_alpha_model import AlephAlphaModel
+from .image import ImagePrompt
+from .prompt import Prompt
+from .explanation import ExplanationRequest
+from .embedding import (
+    EmbeddingRequest,
+    EmbeddingResponse,
+    SemanticEmbeddingRequest,
+    SemanticEmbeddingResponse,
+    SemanticRepresentation,
+)
+from .completion import CompletionRequest, CompletionResponse
+from .qa import QaRequest, QaResponse
+from .evaluation import EvaluationRequest, EvaluationResponse
+from .tokenization import TokenizationRequest, TokenizationResponse
+from .detokenization import DetokenizationRequest, DetokenizationResponse
+from .summarization import SummarizationRequest, SummarizationResponse
+from .search import SearchRequest, SearchResponse, SearchResult
+from .utils import load_base64_from_url, load_base64_from_file
+from .document import Document
+from .version import __version__
+
+__all__ = [
+    "POOLING_OPTIONS",
+    "AlephAlphaClient",
+    "AlephAlphaModel",
+    "AsyncClient",
+    "Client",
+    "CompletionRequest",
+    "CompletionResponse",
+    "DetokenizationRequest",
+    "DetokenizationResponse",
+    "Document",
+    "EmbeddingRequest",
+    "EmbeddingResponse",
+    "EvaluationRequest",
+    "EvaluationResponse",
+    "ExplanationRequest",
+    "ImagePrompt",
+    "Prompt",
+    "QaRequest",
+    "QaResponse",
+    "QuotaError",
+    "SearchRequest",
+    "SearchResponse",
+    "SearchResult",
+    "SemanticEmbeddingRequest",
+    "SemanticEmbeddingResponse",
+    "SemanticRepresentation",
+    "SummarizationRequest",
+    "SummarizationResponse",
+    "TokenizationRequest",
+    "TokenizationResponse",
+]
