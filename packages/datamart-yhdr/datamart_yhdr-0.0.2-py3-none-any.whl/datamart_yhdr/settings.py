29e3b0f343ba147a83c295c804354eca1783973f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:11:52 +0000

    Add package datamart-yhdr/0.0.2/datamart_yhdr-0.0.2-py3-none-any.whl

---
+++ packages/datamart-yhdr/datamart_yhdr-0.0.2-py3-none-any.whl/datamart_yhdr/settings.py
@@ -0,0 +1,6 @@
+from datamart_yhdr import const
+
+const.HALO_URL = "http://192.168.10.111:82/base/token/create"
+const.WORKSPACE = "YHDR-DM"
+const.QUERY_URL = "http://192.168.10.111:82/query/query"
+const.COMMAND_URL = "http://192.168.10.111:82/message/message/submit"
