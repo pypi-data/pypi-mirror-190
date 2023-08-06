1be0bdccc6498f8e9bfee533a675275b3de5a386
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:16:43 +0000

    Add package slutil/0.8.1/slutil-0.8.1-py3-none-any.whl

---
+++ packages/slutil/slutil-0.8.1-py3-none-any.whl/slutil/model/Record.py
@@ -0,0 +1,21 @@
+from dataclasses import dataclass
+from functools import total_ordering
+from datetime import datetime
+
+
+@dataclass
+@total_ordering
+class Record:
+    slurm_id: int
+    submitted_timestamp: datetime
+    git_tag: str
+    sbatch: str
+    status: str
+    description: str
+    deleted: bool = False
+
+    def __eq__(self, other):
+        return self.slurm_id == other.slurm_id
+
+    def __gt__(self, other):
+        return self.slurm_id > other.slurm_id
