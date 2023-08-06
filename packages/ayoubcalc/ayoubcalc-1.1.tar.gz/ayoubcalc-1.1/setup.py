6fe6145bd7a28422e9df13f0290d4cec59397259
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:16:37 +0000

    Add package ayoubcalc/1.1/ayoubcalc-1.1.tar.gz

---
+++ packages/ayoubcalc/ayoubcalc-1.1.tar.gz/ayoubcalc-1.1/setup.py
@@ -0,0 +1,13 @@
+import setuptools
+setuptools.setup(
+    name= 'ayoubcalc',
+    version= '1.1',
+    author= 'ayoub slassi',
+    description= 'simpl library by python ',
+    packages=setuptools.find_packages(),
+    classifiers=[
+    "Programming Language :: Python :: 3",
+    "Operating System :: OS Independent",
+    "License :: OSI Approved :: MIT License"
+    ]
+)
