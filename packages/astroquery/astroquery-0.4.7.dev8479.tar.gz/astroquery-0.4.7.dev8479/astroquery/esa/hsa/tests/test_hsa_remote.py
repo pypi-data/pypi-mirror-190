8c5486296f5c6be9532233d846413c3c8c29ba82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:36:03 +0000

    Add package astroquery/0.4.7.dev8479/astroquery-0.4.7.dev8479.tar.gz

---
+++ packages/astroquery/astroquery-0.4.7.dev8479.tar.gz/astroquery-0.4.7.dev8479/astroquery/esa/hsa/tests/test_hsa_remote.py
@@ -0,0 +1,170 @@
+# Licensed under a 3-clause BSD style license - see LICENSE.rst
+from pathlib import Path
+import tarfile
+
+import pytest
+from requests.exceptions import ChunkedEncodingError
+
+from ..core import HSAClass
+
+spire_chksum = [10233, 10762, 9019, 10869, 3944, 11328, 3921, 10999, 10959,
+                11342, 10974, 3944, 11335, 11323, 11078, 11321, 11089, 11314, 11108, 6281]
+
+pacs_chksum = [10208, 10755, 8917, 10028, 3924, 3935, 6291]
+
+
+@pytest.mark.remote_data
+class TestHSARemote:
+    retries = 2
+
+    def access_archive_with_retries(self, f, params):
+        for _ in range(self.retries):
+            try:
+                res = f(**params)
+                return res
+            except ChunkedEncodingError:
+                pass
+        return None
+
+    def test_download_data_observation_pacs(self, tmp_path):
+        obs_id = "1342191813"
+        parameters = {'retrieval_type': "OBSERVATION",
+                      'observation_id': obs_id,
+                      'instrument_name': "PACS",
+                      'product_level': 'LEVEL3',
+                      'cache': False,
+                      'download_dir': tmp_path}
+        expected_res = Path(tmp_path, obs_id + ".tar")
+        hsa = HSAClass()
+        res = self.access_archive_with_retries(hsa.download_data, parameters)
+        if res is None:
+            pytest.xfail(f"Archive broke the connection {self.retries} times, unable to test")
+        assert Path(res) == expected_res
+        assert Path(res).is_file()
+        with tarfile.open(res) as tar:
+            chksum = [m.chksum for m in tar.getmembers()]
+        assert chksum.sort() == pacs_chksum.sort()
+
+    def test_download_data_observation_pacs_filename(self, tmp_path):
+        obs_id = "1342191813"
+        fname = "output_file"
+        parameters = {'retrieval_type': "OBSERVATION",
+                      'observation_id': obs_id,
+                      'instrument_name': "PACS",
+                      'product_level': 'LEVEL3',
+                      'filename': fname,
+                      'cache': False,
+                      'download_dir': tmp_path}
+        expected_res = Path(tmp_path, fname + ".tar")
+        hsa = HSAClass()
+        res = self.access_archive_with_retries(hsa.download_data, parameters)
+        if res is None:
+            pytest.xfail(f"Archive broke the connection {self.retries} times, unable to test")
+        assert Path(res) == expected_res
+        assert Path(res).is_file()
+        with tarfile.open(res) as tar:
+            chksum = [m.chksum for m in tar.getmembers()]
+        assert chksum.sort() == pacs_chksum.sort()
+
+    def test_download_data_observation_pacs_compressed(self, tmp_path):
+        obs_id = "1342191813"
+        parameters = {'retrieval_type': "OBSERVATION",
+                      'observation_id': obs_id,
+                      'instrument_name': "PACS",
+                      'product_level': 'LEVEL3',
+                      'compress': 'true',
+                      'cache': False,
+                      'download_dir': tmp_path}
+        expected_res = Path(tmp_path, obs_id + ".tgz")
+        hsa = HSAClass()
+        res = self.access_archive_with_retries(hsa.download_data, parameters)
+        if res is None:
+            pytest.xfail(f"Archive broke the connection {self.retries} times, unable to test")
+        assert Path(res) == expected_res
+        assert Path(res).is_file()
+        with tarfile.open(res) as tar:
+            chksum = [m.chksum for m in tar.getmembers()]
+        assert chksum.sort() == pacs_chksum.sort()
+
+    def test_download_data_observation_spire(self, tmp_path):
+        obs_id = "1342191188"
+        parameters = {'retrieval_type': "OBSERVATION",
+                      'observation_id': obs_id,
+                      'instrument_name': "SPIRE",
+                      'product_level': 'LEVEL2',
+                      'cache': False,
+                      'download_dir': tmp_path}
+        expected_res = Path(tmp_path, obs_id + ".tar")
+        hsa = HSAClass()
+        res = self.access_archive_with_retries(hsa.download_data, parameters)
+        if res is None:
+            pytest.xfail(f"Archive broke the connection {self.retries} times, unable to test")
+        assert Path(res) == expected_res
+        assert Path(res).is_file()
+        with tarfile.open(res) as tar:
+            chksum = [m.chksum for m in tar.getmembers()]
+        assert chksum.sort() == spire_chksum.sort()
+
+    def test_download_data_postcard_pacs(self, tmp_path):
+        obs_id = "1342191813"
+        parameters = {'retrieval_type': "POSTCARD",
+                      'observation_id': obs_id,
+                      'instrument_name': "PACS",
+                      'cache': False,
+                      'download_dir': tmp_path}
+        expected_res = Path(tmp_path, obs_id + ".jpg")
+        hsa = HSAClass()
+        res = self.access_archive_with_retries(hsa.download_data, parameters)
+        if res is None:
+            pytest.xfail(f"Archive broke the connection {self.retries} times, unable to test")
+        assert Path(res) == expected_res
+        assert Path(res).is_file()
+
+    def test_download_data_postcard_pacs_filename(self, tmp_path):
+        obs_id = "1342191813"
+        fname = "output_file"
+        parameters = {'retrieval_type': "POSTCARD",
+                      'observation_id': obs_id,
+                      'instrument_name': "PACS",
+                      'filename': fname,
+                      'cache': False,
+                      'download_dir': tmp_path}
+        expected_res = Path(tmp_path, fname + ".jpg")
+        hsa = HSAClass()
+        res = self.access_archive_with_retries(hsa.download_data, parameters)
+        if res is None:
+            pytest.xfail(f"Archive broke the connection {self.retries} times, unable to test")
+        assert Path(res) == expected_res
+        assert Path(res).is_file()
+
+    def test_get_observation(self, tmp_path):
+        obs_id = "1342191813"
+        parameters = {'observation_id': obs_id,
+                      'instrument_name': "PACS",
+                      'product_level': 'LEVEL3',
+                      'cache': False,
+                      'download_dir': tmp_path}
+        expected_res = Path(tmp_path, obs_id + ".tar")
+        hsa = HSAClass()
+        res = self.access_archive_with_retries(hsa.get_observation, parameters)
+        if res is None:
+            pytest.xfail(f"Archive broke the connection {self.retries} times, unable to test")
+        assert Path(res) == expected_res
+        assert Path(res).is_file()
+        with tarfile.open(res) as tar:
+            chksum = [m.chksum for m in tar.getmembers()]
+        assert chksum.sort() == pacs_chksum.sort()
+
+    def test_get_postcard(self, tmp_path):
+        obs_id = "1342191813"
+        parameters = {'observation_id': obs_id,
+                      'instrument_name': "PACS",
+                      'cache': False,
+                      'download_dir': tmp_path}
+        expected_res = Path(tmp_path, obs_id + ".jpg")
+        hsa = HSAClass()
+        res = self.access_archive_with_retries(hsa.get_postcard, parameters)
+        if res is None:
+            pytest.xfail(f"Archive broke the connection {self.retries} times, unable to test")
+        assert Path(res) == expected_res
+        assert Path(res).is_file()
---
+++ packages/astroquery/astroquery-0.4.7.dev8479.tar.gz/astroquery-0.4.7.dev8479/docs/sdss/sdss.rst
@@ -0,0 +1,94 @@
+.. _astroquery.sdss:
+
+********************************
+SDSS Queries (`astroquery.sdss`)
+********************************
+
+Default Data Release
+====================
+
+The default data release is set to Data Release 17 (DR17), which is
+the final data release of the Sloan Digital Sky Survey IV.  DR17
+contains new optical and infrared spectra from both Apache Point
+Observatory and Las Campanas Observatory. Previously released
+integral-field datacubes and maps, stellar library spectra, as well as
+images, are also included in DR17.  Users may select alternate DR's.
+
+Getting started
+===============
+
+This example shows how to perform an object cross-ID with SDSS. We'll start
+with the position of a source found in another survey, and search within a 5
+arcsecond radius for optical counterparts in SDSS. Note use of the keyword
+argument spectro, which requires matches to have spectroscopy, not just
+photometry:
+
+.. doctest-remote-data::
+
+    >>> from astroquery.sdss import SDSS
+    >>> from astropy import coordinates as coords
+    >>> pos = coords.SkyCoord('0h8m05.63s +14d50m23.3s', frame='icrs')
+    >>> xid = SDSS.query_region(pos, spectro=True)
+    >>> print(xid)
+           ra              dec        ...     specobjid      run2d
+    ---------------- ---------------- ... ------------------ -----
+    2.02344596573482 14.8398237551311 ... 845594848269461504    26
+
+The result is an astropy.Table.
+
+Downloading data
+================
+If we'd like to download spectra and/or images for our match, we have all
+the information we need in the elements of "xid" from the above example.
+
+.. doctest-remote-data::
+
+    >>> sp = SDSS.get_spectra(matches=xid)
+    >>> im = SDSS.get_images(matches=xid, band='g')
+
+The variables "sp" and "im" are lists of `~astropy.io.fits.HDUList` objects, one entry for
+each corresponding object in xid.
+
+Note that in SDSS, image downloads retrieve the entire plate, so further
+processing will be required to excise an image centered around the point of
+interest (*i.e.*, the object(s) returned by
+`~astroquery.sdss.SDSSClass.query_region`).
+
+Spectral templates
+==================
+
+.. warning::
+
+    These templates are from the SDSS-I/II spectroscopic pipeline
+    (DR7 and earlier). SDSS-III/IV (DR8 and later) spectroscopic processing
+    pipelines use different templates.
+
+It is also possible to download spectral templates from SDSS. To see what is
+available, do
+
+.. doctest-remote-data::
+
+    >>> from astroquery.sdss import SDSS
+    >>> print(SDSS.AVAILABLE_TEMPLATES)    # doctest: +IGNORE_OUTPUT
+    {'star_O': 0, 'star_OB': 1, 'star_B': 2, 'star_A': [3, 4], 'star_FA': 5,
+    'star_F': [6, 7], 'star_G': [8, 9], 'star_K': 10, 'star_M1': 11, 'star_M3': 12,
+    'star_M5': 13, 'star_M8': 14, 'star_L1': 15, 'star_wd': [16, 20, 21], 'star_carbon': [17, 18, 19],
+    'star_Ksubdwarf': 22, 'galaxy_early': 23, 'galaxy': [24, 25, 26], 'galaxy_late': 27, 'galaxy_lrg': 28,
+    'qso': 29, 'qso_bal': [30, 31], 'qso_bright': 32}
+
+Then, to download your favorite template, do something like
+
+.. code-block:: python
+
+    >>> template = SDSS.get_spectral_template('qso')    # doctest: +REMOTE_DATA
+
+The variable "template" is a list of `~astropy.io.fits.HDUList` objects
+(same object as "sp" in the above example). In this case there is only one
+result, but in a few cases there are multiple templates available to choose
+from (*e.g.*, the "galaxy" spectral template will actually return 3 templates).
+
+Reference/API
+=============
+
+.. automodapi:: astroquery.sdss
+    :no-inheritance-diagram:
