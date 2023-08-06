bc45ccc603d85336a5de087c81625a3890173975
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:31:36 +0000

    Add package simontg6/0.0.1/simontg6-0.0.1-py3-none-any.whl

---
+++ packages/simontg6/simontg6-0.0.1-py3-none-any.whl/hello/sayhello.py
@@ -0,0 +1,5 @@
+def sayhelloto(name):
+    if name in ["Klément", "Simon", "Ewen"]:
+        print("Bonjour " + name)
+    else:
+        print("Je ne dis pas bonjour aux gens que je ne connais pas !")
