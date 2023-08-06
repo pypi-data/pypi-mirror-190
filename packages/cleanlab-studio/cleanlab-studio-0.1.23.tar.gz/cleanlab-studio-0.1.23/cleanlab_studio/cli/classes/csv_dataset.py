d974f0c28afd9ffd3cdf017f6d91fc279b76760a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:33:54 +0000

    Add package cleanlab-studio/0.1.23/cleanlab-studio-0.1.23.tar.gz

---
+++ packages/cleanlab-studio/cleanlab-studio-0.1.23.tar.gz/cleanlab-studio-0.1.23/cleanlab_studio/cli/classes/csv_dataset.py
@@ -0,0 +1,42 @@
+import csv
+from typing import List, Generator, Any
+
+import pandas as pd
+
+from .dataset import Dataset
+from ..types import RecordType
+
+
+class CsvDataset(Dataset):
+    def count_rows(self) -> int:
+        with open(self.filepath, "r", encoding="utf-8", errors="ignore") as f:
+            reader = csv.reader(f)
+
+            # handle case where CSV is empty
+            return max(
+                len(list(reader)) - 1,  # first row is headers
+                0,
+            )
+
+    def get_columns(self) -> List[str]:
+        with open(self.filepath, "r", encoding="utf-8", errors="ignore") as f:
+            reader = csv.reader(f)
+            columns = next(reader)
+            return columns
+
+    def read_streaming_records(self) -> Generator[RecordType, None, None]:
+        with open(self.filepath, "r", encoding="utf-8", errors="ignore") as f:
+            reader = csv.reader(f)
+            columns = next(reader)
+            for row in reader:
+                yield dict(zip(columns, row))
+
+    def read_streaming_values(self) -> Generator[List[Any], None, None]:
+        with open(self.filepath, "r", encoding="utf-8", errors="ignore") as f:
+            reader = csv.reader(f)
+            next(reader)
+            for row in reader:
+                yield row
+
+    def read_file_as_dataframe(self) -> pd.DataFrame:
+        return pd.read_csv(self.filepath, keep_default_na=True)
