74c4a7e93e7991acf357e22b2fbac706fb273607
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:28:31 +0000

    Add package mms-networkx/1.12rc1/mms_networkx-1.12rc1-py2.py3-none-any.whl

---
+++ packages/mms-networkx/mms_networkx-1.12rc1-py2.py3-none-any.whl/networkx/version.py
@@ -0,0 +1,25 @@
+"""
+Version information for NetworkX, created during installation.
+
+Do not add this file to the repository.
+
+"""
+
+import datetime
+
+version = '1.12rc1'
+date = 'Mon Feb  6 15:11:34 2023'
+
+# Was NetworkX built from a development version? If so, remember that the major
+# and minor versions reference the "target" (rather than "current") release.
+dev = False
+
+# Format: (name, major, min, revision)
+version_info = ('mms-networkx', '1', '12rc1', None)
+
+# Format: a 'datetime.datetime' instance
+date_info = datetime.datetime(2023, 2, 6, 15, 11, 34, 601576)
+
+# Format: (vcs, vcs_tuple)
+vcs_info = (None, (None, None))
+
