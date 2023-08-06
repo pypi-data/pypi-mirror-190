5c125750a1681455187289c01493d15d28ff8c2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:52:56 +0000

    Add package mevo/0.0.1/mevo-0.0.1.tar.gz

---
+++ packages/mevo/mevo-0.0.1.tar.gz/mevo-0.0.1/src/mevo/__init__.py
@@ -0,0 +1,6 @@
+from mevo import chromosomes
+from mevo import individuals
+from mevo import mtype
+from mevo import population
+from mevo.chromosomes.chromosome import BinaryChromo, IntChromo, FloatChromo
+from mevo.population import GeneticAlgoFloat, GeneticAlgoNet, GeneticAlgoOrder, GeneticAlgoInt, EvolutionStrategyNet
