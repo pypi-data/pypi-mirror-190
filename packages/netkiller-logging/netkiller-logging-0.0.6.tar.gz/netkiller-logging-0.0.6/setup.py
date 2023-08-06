ec41dbf3729496b4cc17dd11c5d5cddcfa9d7aef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:31:22 +0000

    Add package netkiller-logging/0.0.6/netkiller-logging-0.0.6.tar.gz

---
+++ packages/netkiller-logging/netkiller-logging-0.0.6.tar.gz/netkiller-logging-0.0.6/setup.py
@@ -0,0 +1,56 @@
+import os,sys
+from setuptools import setup,find_packages
+sys.path.insert(0, os.path.abspath('lib'))
+#from library import __version__, __author__
+#print (__version__)
+with open("README.md", "r") as file:
+  long_description = file.read()
+
+with open("CHANGES.md", "r") as file:
+  changes = file.read()
+
+setup(
+	name='netkiller-logging',
+	#version=__version__,
+	version='0.0.6',
+	#author=__author__,
+	author_email='netkiller@msn.com',
+	description="log send to remote",
+	long_description=long_description + '\n\n' + changes,
+	long_description_content_type="text/markdown",
+	keywords='logging',
+
+	url='http://netkiller.github.io',
+	download_url='https://github.com/netkiller/logging',
+	license='BSD',
+	classifiers=[
+		'Environment :: Console',
+		'Intended Audience :: Developers',
+		'License :: OSI Approved :: BSD License',
+		'Operating System :: OS Independent',
+		'Programming Language :: Python',
+		'Programming Language :: Python :: 3.4',
+	],
+	install_requires = ['netkiller-devops'],
+	#py_modules=[
+	#	'library.rsync',
+	#	'library.whiptail'
+	#],
+	
+	#package_dir={ '': 'src' },
+	packages=find_packages(),
+	scripts=[
+		'bin/auditlog',
+		'bin/rlog',
+		'bin/collection',
+		'bin/cisco'
+	],
+	data_files = [
+		('etc', ['etc/auditlog.ini']),
+		('etc', ['etc/logging.ini']),
+		#('log', ['log/auditlog.log']),
+		('share', ['share/dmesg.reg']),
+		('etc', ['etc/cisco.conf']),
+		('libexec', ['libexec/switch.exp', 'libexec/route.exp'])
+	]
+)
