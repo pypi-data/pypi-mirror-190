28ff0943d24ce7cc33fd93e8c30888a6e36c59e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:10:00 +0000

    Add package uhslc-station-tools/0.0.13/uhslc_station_tools-0.0.13.tar.gz

---
+++ packages/uhslc-station-tools/uhslc_station_tools-0.0.13.tar.gz/uhslc_station_tools-0.0.13/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='uhslc_station_tools',
+    version='0.0.13',
+    python_requires='>=3.6',
+    description='Set of tools for loading, processing and saving sea level data from tide gauges',
+    url='https://github.com/uhsealevelcenter/station_tools',
+    author='Nemanja Komar',
+    author_email='komar@hawaii.edu',
+    license='MIT',
+    packages=find_packages(),
+    install_requires=[
+        'numpy==1.15.0',
+        'pandas==0.24.1',
+        'scipy==1.4.1',
+        'utide==0.2.5'
+      ],
+
+    package_data={'': [
+        '*.txt',
+        '*.dat'
+            ]},
+
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Science/Research',
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
