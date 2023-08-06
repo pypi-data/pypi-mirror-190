83a96bf900931d48c7822a35014ccacb6906c5d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:13:54 +0000

    Add package ddd-interface/0.0.4/ddd-interface-0.0.4.tar.gz

---
+++ packages/ddd-interface/ddd-interface-0.0.4.tar.gz/ddd-interface-0.0.4/src/ddd_interface/domain/entity.py
@@ -0,0 +1,4 @@
+class Entity:
+    def __repr__(self) -> str:
+        attrs = {a:self.__dict__[a] for a in self.__dict__ if not a.startswith('__') and not a.startswith('_')}
+        return str(attrs)
