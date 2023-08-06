c68a2ac0dccb4dd12e4209b7cc088fe141822b6b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:03:17 +0000

    Add package rpaframework-assistant/1.0.1/rpaframework_assistant-1.0.1.tar.gz

---
+++ packages/rpaframework-assistant/rpaframework_assistant-1.0.1.tar.gz/rpaframework_assistant-1.0.1/src/RPA/Assistant/types.py
@@ -0,0 +1,28 @@
+from enum import Enum, auto
+from typing import Any, Dict, List, Union
+
+
+Element = Dict[str, Any]
+Options = Union[List[str], str]
+Result = Dict[str, Any]
+
+
+class Size(Enum):
+    """Element size options"""
+
+    Small = "small"
+    Medium = "medium"
+    Large = "large"
+
+
+class Icon(Enum):
+    """Icon variants"""
+
+    Success = "success"
+    Warning = "warning"
+    Failure = "failure"
+
+
+class Location(Enum):
+    Center = auto()
+    TopLeft = auto()
