033dcca209e491b62001223563f694eb647be9d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:28:33 +0000

    Add package mclass/1.3.2/mclass-1.3.2.tar.gz

---
+++ packages/mclass/mclass-1.3.2.tar.gz/mclass-1.3.2/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+# read the contents of your README file
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setup(
+    name="mclass",
+    version="1.3.2",
+    author="Moses Dastmard",
+    description="converts python dictoinary to class object",
+    long_description=long_description,
+    long_description_content_type='text/markdown'
+)
