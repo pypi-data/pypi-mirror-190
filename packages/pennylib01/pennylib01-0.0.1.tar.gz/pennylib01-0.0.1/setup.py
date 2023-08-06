02b7134077c20baa2a03213cdbe8f2dc056e9c53
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:52:44 +0000

    Add package pennylib01/0.0.1/pennylib01-0.0.1.tar.gz

---
+++ packages/pennylib01/pennylib01-0.0.1.tar.gz/pennylib01-0.0.1/setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+
+
+setuptools.setup(
+    name="pennylib01",
+    version="0.0.1",
+    author="Penny",
+    author_email="penny@gmail.com",
+    description="A small package of heyoo for whatsapp",
+    long_description= '',
+    long_description_content_type="text/markdown",
+    url="https://github.com/shadaenx/pennylib",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
