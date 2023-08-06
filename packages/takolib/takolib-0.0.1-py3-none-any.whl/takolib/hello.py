edd23674936b51dee729be15ba0a961297701fce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:17:28 +0000

    Add package takolib/0.0.1/takolib-0.0.1-py3-none-any.whl

---
+++ packages/takolib/takolib-0.0.1-py3-none-any.whl/takolib/hello.py
@@ -0,0 +1,7 @@
+def hello(txt="World"):
+    """ Prints Hello World to the console
+
+    Args:
+        txt (str, optional): Prints the text with hello. Defaults to "World".
+    """
+    print(f"Hello {txt}")
