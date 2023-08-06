1297e6cc260acb5656db6666052fe57bf1ae71dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:35:52 +0000

    Add package hashwd/0.5.112b0/hashwd-0.5.112b0.tar.gz

---
+++ packages/hashwd/hashwd-0.5.112b0.tar.gz/hashwd-0.5.112b0/hashwd/__init__.py
@@ -0,0 +1,12 @@
+# __init__.py
+
+
+from generate import generate_password
+from clear import clear_clipboard
+from parameters import MIN_WORDS, MAX_WORDS, MIN_NUMBERS, MAX_NUMBERS, MIN_SYMBOLS, MAX_SYMBOLS, SYMBOLS, DICTIONARY_FILE, WORDS_DEFAULT, NUMBERS_DEFAULT, SYMBOLS_DEFAULT
+from pyperclip import copy
+from random import randint, random, sample
+from argparse import ArgumentParser
+from sys import exit
+from os import path
+from re import match
