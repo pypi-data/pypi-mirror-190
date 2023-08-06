6162aaa3cfb7c6281e7ad573c722866c09a046ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:39:39 +0000

    Add package alminer/0.1.3/alminer-0.1.3.tar.gz

---
+++ packages/alminer/alminer-0.1.3.tar.gz/alminer-0.1.3/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+version = {}
+with open('alminer/version.py') as v:
+    exec(v.read(), version)
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name='alminer',
+    version=version['__version__'],
+    author='Aida Ahmadi',
+    author_email='aahmadi@strw.leidenuniv.nl',
+    description='ALminer: ALMA archive mining and visualization toolkit',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=find_packages(include=['alminer']),
+    url='https://github.com/emerge-erc/ALminer',
+    project_urls={
+        "Bug Tracker": "https://github.com/emerge-erc/ALminer/issues"
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    license='MIT',
+    install_requires=['numpy>=1.15', 'pandas>1.0', 'matplotlib>=3.3.0', 'pyvo==1.2.1',
+                      'astropy>=3.1.2', 'astroquery>=0.4.2'],
+    python_requires='>=3.6'
+)
