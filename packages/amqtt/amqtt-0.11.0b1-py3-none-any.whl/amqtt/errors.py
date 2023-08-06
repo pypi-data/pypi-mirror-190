0e3f92d68700a8fe29b456c191e5a7cf91ec1e2e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:20:01 +0000

    Add package amqtt/0.11.0b1/amqtt-0.11.0b1-py3-none-any.whl

---
+++ packages/amqtt/amqtt-0.11.0b1-py3-none-any.whl/amqtt/errors.py
@@ -0,0 +1,35 @@
+# Copyright (c) 2015 Nicolas JOUANIN
+#
+# See the file license.txt for copying permission.
+
+
+class AMQTTException(Exception):
+    """
+    aMQTT base exception
+    """
+
+    pass
+
+
+class MQTTException(Exception):
+    """
+    Base class for all errors refering to MQTT specifications
+    """
+
+    pass
+
+
+class CodecException(Exception):
+    """
+    Exceptions thrown by packet encode/decode functions
+    """
+
+    pass
+
+
+class NoDataException(Exception):
+    """
+    Exceptions thrown by packet encode/decode functions
+    """
+
+    pass
