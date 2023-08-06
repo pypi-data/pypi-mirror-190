c72b3c854cba15a2edc18df15239a8fac7ad9aae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:36:01 +0000

    Add package bempy/0.3.1/bempy-0.3.1.tar.gz

---
+++ packages/bempy/bempy-0.3.1.tar.gz/bempy-0.3.1/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='bempy',
+    version='0.3.1',
+    description='BEM-based class construction',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='http://github.com/fefa4ka/bempy',
+    author='Alexander Kondratev',
+    author_email='alex@nder.work',
+    license='GPLv3+',
+    packages=find_packages(exclude=["tests"]),
+    test_suite="tests",
+    classifiers=[
+        'Intended Audience :: Developers',
+        "Programming Language :: Python :: 3",
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+        "Operating System :: OS Independent",
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+)
