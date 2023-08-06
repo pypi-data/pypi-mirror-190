77eab9d5dd949d7421748dc5d9f31a73f62a7e36
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:11:42 +0000

    Add package streamlit-file-browser/0.0.3/streamlit_file_browser-0.0.3-py3-none-any.whl

---
+++ packages/streamlit-file-browser/streamlit_file_browser-0.0.3-py3-none-any.whl/st_file_browser/frontend/build/precache-manifest.38b17f7c92fbd206a9c805b585b2002e.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c52fae9b513dc3de65d9a6d355143646",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e4caace3f7b2cb859e26",
+    "url": "./static/css/2.5a3919a9.chunk.css"
+  },
+  {
+    "revision": "e4caace3f7b2cb859e26",
+    "url": "./static/js/2.9d6cd207.chunk.js"
+  },
+  {
+    "revision": "53ec02d0e91d8bc718b5061335433fac",
+    "url": "./static/js/2.9d6cd207.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "57bf8161704e0db71c31",
+    "url": "./static/js/main.6865ad64.chunk.js"
+  },
+  {
+    "revision": "44e7e8c245c21e8f352f",
+    "url": "./static/js/runtime-main.5606d314.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "./static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "./static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "./static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "./static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "./static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
