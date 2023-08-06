99b44e170e32aefe784f54eb7997ae0552d535d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:16:19 +0000

    Add package excelbird/0.0.12/excelbird-0.0.12-py3-none-any.whl

---
+++ packages/excelbird/excelbird-0.0.12-py3-none-any.whl/excelbird/__init__.py
@@ -0,0 +1,6 @@
+from excelbird.core import *
+from excelbird.styles import *
+from excelbird.schema import *
+from excelbird.exceptions import *
+from excelbird._layout_references import *
+from excelbird._base.dotdict import *
