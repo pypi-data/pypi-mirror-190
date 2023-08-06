427fc2c13db27acbad6d8a592328e171ea6037b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:19:51 +0000

    Add package pastas/1.0.1/pastas-1.0.1-py3-none-any.whl

---
+++ packages/pastas/pastas-1.0.1-py3-none-any.whl/pastas/typing/__init__.py
@@ -0,0 +1,17 @@
+# flake8: noqa
+from .types import (
+    ArrayLike,
+    Axes,
+    CallBack,
+    Figure,
+    Function,
+    Model,
+    NoiseModel,
+    Recharge,
+    Reservoir,
+    RFunc,
+    Solver,
+    StressModel,
+    TimeSeries,
+    TimestampType,
+)
