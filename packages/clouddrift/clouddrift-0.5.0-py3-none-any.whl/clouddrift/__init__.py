145e5fa05869990a701c181be35383b5ac8cc3e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:36:51 +0000

    Add package clouddrift/0.5.0/clouddrift-0.5.0-py3-none-any.whl

---
+++ packages/clouddrift/clouddrift-0.5.0-py3-none-any.whl/clouddrift/__init__.py
@@ -0,0 +1,8 @@
+from ._version import version
+
+__version__ = version
+
+from clouddrift.dataformat import RaggedArray
+import clouddrift.adapters
+import clouddrift.analysis
+import clouddrift.haversine
