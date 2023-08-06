3b8ac196e661a04565bd8d67f377c93792f66977
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:17:33 +0000

    Add package Amino.py/3.0.9/Amino.py-3.0.9.tar.gz

---
+++ packages/Amino.py/Amino.py-3.0.9.tar.gz/Amino.py-3.0.9/setup.py
@@ -0,0 +1,42 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as stream:
+    long_description = stream.read()
+
+setup(
+    name = "Amino.py",
+    version = "3.0.9",
+    url = "https://github.com/Slimakoi/Amino.py",
+    download_url = "https://github.com/Slimakoi/Amino.py/tarball/master",
+    license = "MIT",
+    author = "Slimakoi",
+    author_email = "slimeytoficial@gmail.com",
+    description = "A library to create Amino bots.",
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
+    keywords = [
+        "aminoapps",
+        "amino-py",
+        "amino",
+        "amino-bot",
+        "narvii",
+        "api",
+        "python",
+        "python3",
+        "python3.x",
+        "slimakoi",
+        "official"
+    ],
+    install_requires = [
+        "setuptools",
+        "requests",
+        "six",
+        "websockets",
+        "websocket-client==1.3.1",
+        "aiohttp"
+    ],
+    setup_requires = [
+        "wheel"
+    ],
+    packages = find_packages()
+)
