8391362416f681e274e8610ca11ec24f12938554
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:32:41 +0000

    Add package y-tal/0.8.2/y_tal-0.8.2-py3-none-any.whl

---
+++ packages/y-tal/y_tal-0.8.2-py3-none-any.whl/tal/reconstruct/bp/__init__.py
@@ -0,0 +1,28 @@
+from tal.io.capture_data import NLOSCaptureData
+from tal.enums import VolumeFormat, CameraSystem
+import numpy as np
+
+
+def solve(data: NLOSCaptureData,
+          volume_xyz: NLOSCaptureData.VolumeXYZType = None,
+          volume_format: VolumeFormat = None,
+          camera_system: CameraSystem = CameraSystem.STEADY) -> np.array:  # FIXME type
+    """
+    NOTE: Does _NOT_ attempt to compensate effects caused by attenuation:
+      - cos decay i.e. {sensor|laser}_grid_normals are ignored
+      - 1/d^2 decay
+    TODO(diego): docs
+    """
+    from tal.reconstruct.utils import convert_to_N_3, convert_reconstruction_from_N_3
+    H, laser_grid_xyz, sensor_grid_xyz, volume_xyz_n3 = \
+        convert_to_N_3(data, volume_xyz, volume_format)
+
+    from tal.reconstruct.bp.backprojection import backproject
+    reconstructed_volume_n3 = backproject(
+        H, laser_grid_xyz, sensor_grid_xyz, volume_xyz_n3,
+        camera_system, data.t_accounts_first_and_last_bounces,
+        data.t_start, data.delta_t,
+        data.laser_xyz, data.sensor_xyz,
+        progress=True)
+
+    return convert_reconstruction_from_N_3(data, reconstructed_volume_n3, volume_xyz, volume_format, camera_system)
