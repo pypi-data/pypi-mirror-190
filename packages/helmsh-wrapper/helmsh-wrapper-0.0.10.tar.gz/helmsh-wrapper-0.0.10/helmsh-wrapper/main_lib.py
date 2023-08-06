1b96aa55fd26daae6fdc88ac8aab743923bf290b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:13:46 +0000

    Add package helmsh-wrapper/0.0.10/helmsh-wrapper-0.0.10.tar.gz

---
+++ packages/helmsh-wrapper/helmsh-wrapper-0.0.10.tar.gz/helmsh-wrapper-0.0.10/helmsh-wrapper/main_lib.py
@@ -0,0 +1,14 @@
+def test_method(x: str):
+    """
+        Returns a string, which will be appended with X variable.
+
+        >>> test_method('foo')
+        'Hello foo rwret'
+
+        :param input:
+        :param length: The return string's desired length.
+        :param fill_character:
+        :rtype str:
+        """
+
+    print(f'Hello {x}')
---
+++ packages/helmsh-wrapper/helmsh-wrapper-0.0.10.tar.gz/helmsh-wrapper-0.0.10/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='helmsh-wrapper',
+    version='0.0.10',
+    packages=['helmsh-wrapper'],
+    url='',
+    license='',
+    author='Pavel Starostin',
+    author_email='pavel.starostin@gmail.com',
+    description='Helm wrapper'
+)
