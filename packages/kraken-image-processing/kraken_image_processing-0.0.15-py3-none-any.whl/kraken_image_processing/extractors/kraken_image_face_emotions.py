c1a9974039c6fb0220e4b1684acae75171ea9fdb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:32:53 +0000

    Add package kraken-image-processing/0.0.15/kraken_image_processing-0.0.15-py3-none-any.whl

---
+++ packages/kraken-image-processing/kraken_image_processing-0.0.15-py3-none-any.whl/kraken_image_processing/extractors/kraken_image_face_emotions.py
@@ -0,0 +1,21 @@
+import cv2
+import uuid
+#from fer import FER
+
+
+
+def get(image):
+    return get_emotions(image)
+
+def get_emotions(image):
+
+    '''
+    emo_detector = FER()
+    # Capture all the emotions on the image
+    captured_emotions = emo_detector.detect_emotions(image)
+    top_emotion = emo_detector.top_emotion(image)
+
+    e, p = top_emotion
+    '''
+
+    return None
