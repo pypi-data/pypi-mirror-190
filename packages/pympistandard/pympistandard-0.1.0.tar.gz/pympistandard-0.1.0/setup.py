b781367a042ad42f0d9da9a0a9abdb45b22dfc1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:14:17 +0000

    Add package pympistandard/0.1.0/pympistandard-0.1.0.tar.gz

---
+++ packages/pympistandard/pympistandard-0.1.0.tar.gz/pympistandard-0.1.0/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+package_dir = \
+{'': 'src'}
+
+packages = \
+['pympistandard', 'pympistandard.data']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'pympistandard',
+    'version': '0.1.0',
+    'description': 'Python API to the MPI Standard',
+    'long_description': 'None',
+    'author': 'Martin Ruefenacht',
+    'author_email': 'martin.ruefenacht@lrz.de',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'package_dir': package_dir,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
