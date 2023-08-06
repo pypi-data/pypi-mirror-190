be3a6df5a564e736cb81aa0bbba1b6fadf3d0322
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:19:36 +0000

    Add package switch-guides/0.1.18/switch_guides-0.1.18.tar.gz

---
+++ packages/switch-guides/switch_guides-0.1.18.tar.gz/switch_guides-0.1.18/switch_guides/extensions/__init__.py
@@ -0,0 +1,14 @@
+# -------------------------------------------------------------------------
+# Copyright (c) Switch Automation Pty Ltd. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for
+# license information.
+# --------------------------------------------------------------------------
+
+
+
+from .component import forms
+
+
+__all__ = [
+    'forms',
+]
