ba9f74080eca5449f79334c91f371ceba88eba87
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:14:08 +0000

    Add package culculator/0.1/culculator-0.1.tar.gz

---
+++ packages/culculator/culculator-0.1.tar.gz/culculator-0.1/setup.py
@@ -0,0 +1,13 @@
+import setuptools
+setuptools.setup(
+    name= 'culculator',
+    version= '0.1',
+    author= 'Amdjed',
+    description= 'culculator ',
+    packages=setuptools.find_packages(),
+    classifiers=[
+    "Programming Language :: Python :: 3",
+    "Operating System :: OS Independent",
+    "License :: OSI Approved :: MIT License"
+    ]
+)
