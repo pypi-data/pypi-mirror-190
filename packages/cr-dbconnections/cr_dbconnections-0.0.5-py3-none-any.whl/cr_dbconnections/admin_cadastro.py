57baaa3395723fb7959468d7c5ca216d162263d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:13:49 +0000

    Add package cr-dbconnections/0.0.5/cr_dbconnections-0.0.5-py3-none-any.whl

---
+++ packages/cr-dbconnections/cr_dbconnections-0.0.5-py3-none-any.whl/cr_dbconnections/admin_cadastro.py
@@ -0,0 +1,32 @@
+from cr_dbconnections.core_mongo import CoreMongo
+from dotenv import load_dotenv
+import os
+import yaml
+
+class MongoCr(CoreMongo):
+    
+    def __init__(self):
+        
+        try:
+            with open('config.yaml', 'r') as f:
+                db_connections = yaml.load(f.read(), Loader=yaml.Loader)
+        except:
+            raise 'Arquivo config.yaml não encontrado.'
+        
+        self.user = db_connections.get('mongo_cr')['user']
+        self.password = db_connections.get('mongo_cr')['pwd']
+        self.cluster = db_connections.get('mongo_cr')['cluster']
+        self.auth_db = db_connections.get('mongo_cr')['auth_db']
+        self.db = db_connections.get('mongo_cr')['db']
+        self.options = db_connections.get('mongo_cr')['options']
+        self.cert = db_connections.get('mongo_cr')['cert']
+
+        super().__init__(
+            user=self.user, 
+            password=self.password, 
+            cluster=self.cluster, 
+            db=self.db, 
+            auth_db=self.auth_db,
+            options=self.options,
+            cert=self.cert
+        )
