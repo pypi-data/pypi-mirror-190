67d979a2be05b299fb641236d6d55cfb713993d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:18:41 +0000

    Add package deephys/0.11.0/deephys-0.11.0.tar.gz

---
+++ packages/deephys/deephys-0.11.0.tar.gz/deephys-0.11.0/deephys/__init__.py
@@ -0,0 +1,23 @@
+from deephys.deephys import (
+    Neuron,
+    Layer,
+    Model,
+    model,
+    import_torch_dataset,
+    import_test_data,
+    ImageFile,
+    Test,
+    Stopwatch,
+)
+
+__all__ = [
+    "Neuron",
+    "Layer",
+    "Model",
+    "model",
+    "import_torch_dataset",
+    "import_test_data",
+    "ImageFile",
+    "Test",
+    "Stopwatch",
+]
