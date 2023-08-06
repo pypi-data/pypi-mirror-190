fa83471ea7095431781ba14819f188590f125724
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:11:13 +0000

    Add package issubclass/0.1.2/issubclass-0.1.2.tar.gz

---
+++ packages/issubclass/issubclass-0.1.2.tar.gz/issubclass-0.1.2/setup.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+modules = \
+['issubclass']
+setup_kwargs = {
+    'name': 'issubclass',
+    'version': '0.1.2',
+    'description': "issubclass() builtin that doesn't raise TypeError when arguments are not classes",
+    'long_description': 'None',
+    'author': 'Stanislav Zmiev',
+    'author_email': 'szmiev2000@gmail.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'py_modules': modules,
+    'python_requires': '>=3.8',
+}
+
+
+setup(**setup_kwargs)
