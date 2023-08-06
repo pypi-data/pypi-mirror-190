32cea6d2c4e13b29a08ec851a356fba2bedcb3b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:17:21 +0000

    Add package odd-collector-sdk/0.3.15/odd_collector_sdk-0.3.15.tar.gz

---
+++ packages/odd-collector-sdk/odd_collector_sdk-0.3.15.tar.gz/odd_collector_sdk-0.3.15/odd_collector_sdk/api/http_client.py
@@ -0,0 +1,13 @@
+from aiohttp import ClientSession, ClientTimeout
+
+
+class HttpClient:
+    def __init__(self, token: str, connection_timeout_seconds: int = 300) -> None:
+        self.headers = {
+            "content-type": "application/json",
+            "Authorization": f"Bearer {token}",
+        }
+        self.timeout = ClientTimeout(total=connection_timeout_seconds)
+
+    async def post(self, url: str, data: any, session: ClientSession, **kwargs):
+        return await session.post(url=url, data=data, headers=self.headers, **kwargs)
