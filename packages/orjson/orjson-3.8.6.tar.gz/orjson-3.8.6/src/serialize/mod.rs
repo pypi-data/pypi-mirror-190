3e54c1edfd286e30071bc2252cd9f265c119042c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:18:57 +0000

    Add package orjson/3.8.6/orjson-3.8.6.tar.gz

---
+++ packages/orjson/orjson-3.8.6.tar.gz/orjson-3.8.6/src/serialize/mod.rs
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+mod dataclass;
+mod datetime;
+#[macro_use]
+mod datetimelike;
+mod default;
+mod dict;
+mod error;
+mod float;
+mod int;
+mod list;
+mod numpy;
+mod pyenum;
+mod serializer;
+mod str;
+mod tuple;
+mod uuid;
+mod writer;
+
+mod json;
+
+pub use serializer::serialize;
