7c7f68562f71c87af1264b7bfcebeb9ffbb3612b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:31:22 +0000

    Add package distributions-mde1177/0.1/distributions_mde1177-0.1.tar.gz

---
+++ packages/distributions-mde1177/distributions_mde1177-0.1.tar.gz/distributions_mde1177-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='distributions_mde1177',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions_mde1177'],
+      author='Martin D. Edson',
+      author_email='m.edson373@gmail.com',
+      zip_safe=False)
