1e2068f9ba0c6e968fad852e1549b2e48827da22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:33:34 +0000

    Add package odd-collector-sdk/0.3.12/odd_collector_sdk-0.3.12-py3-none-any.whl

---
+++ packages/odd-collector-sdk/odd_collector_sdk-0.3.12-py3-none-any.whl/odd_collector_sdk/domain/collector_config_loader.py
@@ -0,0 +1,27 @@
+from pathlib import Path
+from typing import Dict, Type, Union
+
+from pyaml_env import parse_config
+
+from ..logger import logger
+from .collector_config import CollectorConfig
+from .plugin import Plugin
+
+
+class CollectorConfigLoader:
+    def __init__(
+        self, config_path: Union[str, Path], plugin_factory: Dict[str, Type[Plugin]]
+    ) -> None:
+        self.plugin_factory = plugin_factory
+        self.path = Path(config_path).resolve()
+
+    def load(self) -> CollectorConfig:
+        logger.debug("Start reading config")
+        parsed = parse_config(str(self.path))
+
+        parsed["plugins"] = [
+            self.plugin_factory[plugin["type"]].parse_obj(plugin)
+            for plugin in parsed["plugins"]
+        ]
+
+        return CollectorConfig.parse_obj(parsed)
