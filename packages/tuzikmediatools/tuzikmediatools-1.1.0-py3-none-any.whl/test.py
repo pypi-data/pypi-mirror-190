435a0e88331c8ee591d7a857a526c0d10454c9db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:12:18 +0000

    Add package tuzikmediatools/1.1.0/tuzikmediatools-1.1.0-py3-none-any.whl

---
+++ packages/tuzikmediatools/tuzikmediatools-1.1.0-py3-none-any.whl/test.py
@@ -0,0 +1,7 @@
+from words_count import words_count
+from is_prime import is_prime
+
+print(words_count('Ghbdtn f sdf d   ahf')) # 5
+print(words_count('Ghbdastn assf sdff d  x ahf')) # 6
+print(words_count('Ghbdtn f sdf d   ahf', '264 52 4')) # (5, 3)
+
