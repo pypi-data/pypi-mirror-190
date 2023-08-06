369aef31db18bb020b6c9200219b9518e3a71b6b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:17:21 +0000

    Add package HyFetch/1.4.7rc1/HyFetch-1.4.7rc1.tar.gz

---
+++ packages/HyFetch/HyFetch-1.4.7rc1.tar.gz/HyFetch-1.4.7rc1/hyfetch/distros/mageia_small.py
@@ -0,0 +1,13 @@
+
+from . import AsciiArt
+
+mageia_small = AsciiArt(match=r'''"mageia_small"*''', color='6 7', ascii=r"""
+${c1}   *
+    *
+   **
+${c2} /\__/\
+/      \
+\      /
+ \____/
+""")
+    
---
+++ packages/HyFetch/HyFetch-1.4.7rc1.tar.gz/HyFetch-1.4.7rc1/hyfetch/distros/manjaro_small.py
@@ -0,0 +1,13 @@
+
+from . import AsciiArt
+
+manjaro_small = AsciiArt(match=r'''"manjaro_small"*''', color='2 7', ascii=r"""
+${c1}||||||||| ||||
+||||||||| ||||
+||||      ||||
+|||| |||| ||||
+|||| |||| ||||
+|||| |||| ||||
+|||| |||| ||||
+""")
+    
