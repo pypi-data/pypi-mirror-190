c73598097b3bfc67e80daddb72b17f37dec04ad4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:34:34 +0000

    Add package numalogic-prometheus/0.1.5a1/numalogic_prometheus-0.1.5a1-py3-none-any.whl

---
+++ packages/numalogic-prometheus/numalogic_prometheus-0.1.5a1-py3-none-any.whl/numaprom/__init__.py
@@ -0,0 +1,20 @@
+import logging
+import os
+
+LOGGER = logging.getLogger(__name__)
+
+stream_handler = logging.StreamHandler()
+
+if os.getenv("DEBUG", False):
+    LOGGER.setLevel(logging.DEBUG)
+    stream_handler.setLevel(logging.DEBUG)
+else:
+    LOGGER.setLevel(logging.INFO)
+    stream_handler.setLevel(logging.INFO)
+
+formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
+stream_handler.setFormatter(formatter)
+
+
+LOGGER.addHandler(stream_handler)
+logging.getLogger("pytorch_lightning").setLevel(logging.WARNING)
