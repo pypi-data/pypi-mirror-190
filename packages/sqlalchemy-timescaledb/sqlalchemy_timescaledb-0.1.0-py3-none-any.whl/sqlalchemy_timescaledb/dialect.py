8dc305eec0cc6ed3ac0470ac0907e078c52c5052
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:30:34 +0000

    Add package sqlalchemy-timescaledb/0.1.0/sqlalchemy_timescaledb-0.1.0-py3-none-any.whl

---
+++ packages/sqlalchemy-timescaledb/sqlalchemy_timescaledb-0.1.0-py3-none-any.whl/sqlalchemy_timescaledb/dialect.py
@@ -0,0 +1,36 @@
+from sqlalchemy import schema
+from sqlalchemy.dialects.postgresql.base import PGDDLCompiler
+from sqlalchemy.dialects.postgresql.psycopg2 import PGDialect_psycopg2
+
+
+class TimescaledbDDLCompiler(PGDDLCompiler):
+    def post_create_table(self, table):
+        # There is exists some problems: https://stackoverflow.com/a/72508098
+
+        hypertable = table.kwargs.get('timescaledb_hypertable', {})
+
+        if hypertable:
+            hypertable_sql = \
+                ';\n\nSELECT create_hypertable(' \
+                f"    '{table.name}', '{hypertable['time_column_name']}'" \
+                ");"
+
+            return super().post_create_table(table) + hypertable_sql
+
+        return super().post_create_table(table)
+
+
+class TimescaledbDialect:
+    name = 'timescaledb'
+    ddl_compiler = TimescaledbDDLCompiler
+    construct_arguments = [
+        (
+            schema.Table, {
+                "hypertable": {}
+            }
+        )
+    ]
+
+
+class TimescaledbPsycopg2Dialect(TimescaledbDialect, PGDialect_psycopg2):
+    driver = 'psycopg2'
