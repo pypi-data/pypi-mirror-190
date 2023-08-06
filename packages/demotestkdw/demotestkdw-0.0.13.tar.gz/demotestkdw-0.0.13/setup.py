0935bbf7c75e31c91972ba19704dd47bf1689af1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:09:16 +0000

    Add package demotestkdw/0.0.13/demotestkdw-0.0.13.tar.gz

---
+++ packages/demotestkdw/demotestkdw-0.0.13.tar.gz/demotestkdw-0.0.13/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "readme.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+VERSION = '0.0.13'
+DESCRIPTION = 'Demo file'
+LONG_DESCRIPTION = 'A basic package'
+
+# Setting up
+setup(
+    name="demotestkdw",
+    version=VERSION,
+    author="kdwivedi",
+    author_email="<kdwivedi@materialise.be>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['python'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
