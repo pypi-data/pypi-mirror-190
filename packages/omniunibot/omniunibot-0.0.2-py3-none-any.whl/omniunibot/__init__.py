46dd515028c7e3ba0aab2e8bff7f00e7e0e8f0e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:27:24 +0000

    Add package omniunibot/0.0.2/omniunibot-0.0.2-py3-none-any.whl

---
+++ packages/omniunibot/omniunibot-0.0.2-py3-none-any.whl/omniunibot/__init__.py
@@ -0,0 +1,6 @@
+from .wrapper.dingtalk import DingTalkBot
+from .wrapper.wecom import WeComBot
+from .wrapper.feishu import FeishuBot
+
+from .server import OmniUniBotServer
+from .client import OmniUniBotClient
