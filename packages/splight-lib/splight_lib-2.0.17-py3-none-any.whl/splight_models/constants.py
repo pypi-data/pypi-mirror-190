da29024d34f62f7c232b8b76749f3a4b9bf6661b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:32:10 +0000

    Add package splight-lib/2.0.17/splight_lib-2.0.17-py3-none-any.whl

---
+++ packages/splight-lib/splight_lib-2.0.17-py3-none-any.whl/splight_models/constants.py
@@ -0,0 +1,87 @@
+# TODO remove this file
+import logging
+from enum import Enum, IntEnum
+
+
+class SeverityType(str, Enum):
+    system = 'system'
+    info = 'info'
+    low = 'low'
+    medium = 'medium'
+    high = 'high'
+    critical = 'critical'
+
+
+class ComponentSize(str, Enum):
+    small = "small"
+    medium = "medium"
+    large = "large"
+    very_large = "very_large"
+
+    def __str__(self):
+        return self.value
+
+
+class LogginLevel(IntEnum):
+    critical = logging.CRITICAL
+    error = logging.ERROR
+    warning = logging.WARNING
+    info = logging.INFO
+    debug = logging.DEBUG
+    notset = logging.NOTSET
+
+    def __str__(self):
+        return str(self.value)
+
+    @classmethod
+    def _missing_(cls, value):
+        return super()._missing_(int(value))
+
+
+class RestartPolicy(str, Enum):
+    ALWAYS = "Always"
+    ON_FAILURE = "OnFailure"
+    NEVER = "Never"
+
+
+class DeploymentStatus(str, Enum):
+    STOPPED = "Stopped"
+    PENDING = "Pending"
+    RUNNING = "Running"
+    SUCCEEDED = "Succeeded"
+    FAILED = "Failed"
+    UNKNOWN = "Unknown"
+
+
+class ComponentStatus(str, Enum):
+    STOPPED = "Stopped"
+    PENDING = "Pending"
+    RUNNING = "Running"
+    SUCCEEDED = "Succeeded"
+    FAILED = "Failed"
+    UNKNOWN = "Unknown"
+
+
+class PrivacyPolicy(str, Enum):
+    PUBLIC = "public"
+    PRIVATE = "private"
+
+
+class VerificationLevel(str, Enum):
+    VERIFIED = "verified"
+    UNVERIFIED = "unverified"
+    OFFICIAL = "official"
+
+
+class BuildStatus(str, Enum):
+    PENDING = "pending"
+    BUILDING = "building"
+    FAILED = "failed"
+    SUCCESS = "success"
+    UNKNOWN = "unknown"
+
+class ComponentType(str,Enum):
+    ALGORITHM = "algorithm"
+    NETWORK = "network"
+    CONNECTOR = "connector"
+    SIMULATOR = "simulator"
