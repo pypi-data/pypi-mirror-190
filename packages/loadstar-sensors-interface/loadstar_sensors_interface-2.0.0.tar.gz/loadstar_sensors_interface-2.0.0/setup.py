ed7bea735ff3e0b61ba5cfa3e68800c3f2108fa5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:39:09 +0000

    Add package loadstar-sensors-interface/2.0.0/loadstar_sensors_interface-2.0.0.tar.gz

---
+++ packages/loadstar-sensors-interface/loadstar_sensors_interface-2.0.0.tar.gz/loadstar_sensors_interface-2.0.0/setup.py
@@ -0,0 +1,8 @@
+"""Python async interface to Loadstar Sensors USB devices."""
+# This file is generated automatically from metadata
+# File edits may be overwritten!
+from setuptools import setup
+
+
+if __name__ == '__main__':
+    setup()
