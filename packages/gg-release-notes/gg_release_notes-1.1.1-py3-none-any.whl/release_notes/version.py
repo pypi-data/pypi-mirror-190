09d427c178044b58d1a1b7b948205ceb47db74b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:03:20 +0000

    Add package gg-release-notes/1.1.1/gg_release_notes-1.1.1-py3-none-any.whl

---
+++ packages/gg-release-notes/gg_release_notes-1.1.1-py3-none-any.whl/release_notes/version.py
@@ -0,0 +1,63 @@
+from typing import Optional
+
+from ghapi.all import GhApi
+
+from gg_release_notes.config.github_config import GithubAPIConfig
+
+
+class ReleaseVersion:
+    """
+    Class for getting and incrementing the version number of the current release in Github.
+    """
+
+    def __init__(self, github_configuration: GithubAPIConfig):
+        self.github_configuration = github_configuration
+        self.github_api: GhApi = github_configuration.github_api
+
+    @property
+    def _version_mapping(self) -> dict:
+        """Maps the versioning type to the correct incrementation function."""
+        return {
+            "None": 2,
+            "major": 0,
+            "minor": 1,
+            "patch": 2,
+        }
+
+    @property
+    def current_version(self) -> str:
+        """Gets the current version number of the release."""
+        try:
+            release_req = self.github_api.repos.get_latest_release()
+            version = release_req.get("tag_name", "1.0.0")
+        except:
+            # No release exists yet
+            version = "1.0.0"
+        
+        return str(version)
+
+    def increment_version(
+        self, current_version: str, versioning_type: Optional[str]
+    ) -> str:
+        """Increments the version number of the current release."""
+        print(f"Current version: {current_version}")
+        if self._version_mapping.get(versioning_type) == 2:
+            version_list = current_version.split(".")
+            version_list[self._version_mapping.get(str(versioning_type))] = str(
+                int(version_list[self._version_mapping.get(str(versioning_type))]) + 1
+            )
+            print(f"New version: {version_list}")
+        else:
+            version_list = current_version.split(".")
+            version_list[self._version_mapping.get(str(versioning_type))] = str(
+                int(version_list[self._version_mapping.get(str(versioning_type))]) + 1
+            )
+            print(f"New version: {version_list}")
+
+            # Reset all the other version numbers to 0
+            for i in range(self._version_mapping.get(str(versioning_type)) + 1, 3):
+                version_list[i] = "0"
+            print(f"New version: {version_list}")
+                
+        version = ".".join(version_list)
+        return str(version)
