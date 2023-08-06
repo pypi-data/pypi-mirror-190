4191853971029b0b5ef21ba29f3200ef54534f9a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:26:39 +0000

    Add package fenlmagic/0.1.0a1/fenlmagic-0.1.0a1.tar.gz

---
+++ packages/fenlmagic/fenlmagic-0.1.0a1.tar.gz/fenlmagic-0.1.0a1/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+package_dir = \
+{'': 'src'}
+
+packages = \
+['fenlmagic']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['kaskada==0.1.0a0']
+
+setup_kwargs = {
+    'name': 'fenlmagic',
+    'version': '0.1.0a1',
+    'description': '',
+    'long_description': '',
+    'author': 'Kevin J Nguyen',
+    'author_email': 'kevin.nguyen@datastax.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'package_dir': package_dir,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
