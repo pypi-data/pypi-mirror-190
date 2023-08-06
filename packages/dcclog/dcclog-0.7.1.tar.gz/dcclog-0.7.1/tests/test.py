3897535e76e334c3e963126d4f838e444f82eb7f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:39:52 +0000

    Add package dcclog/0.7.1/dcclog-0.7.1.tar.gz

---
+++ packages/dcclog/dcclog-0.7.1.tar.gz/dcclog-0.7.1/tests/test.py
@@ -0,0 +1,22 @@
+import dcclog
+from dcclog.cipher.rsa import RSAEncryption
+
+dcclog.default_config(
+    level=dcclog.INFO,
+    filename=".logs/app.log",
+    cipher=RSAEncryption("pubkey.pem"),
+)
+logger = dcclog.getLogger(name=__name__)
+
+logger.error("error message.")
+logger.warning("warning message.")
+logger.info("info message.")
+logger.debug("debug message.")
+
+
+@dcclog.log
+def logged_function(x: int, y: int) -> int:
+    return x + y
+
+
+logged_function(4, 6)
