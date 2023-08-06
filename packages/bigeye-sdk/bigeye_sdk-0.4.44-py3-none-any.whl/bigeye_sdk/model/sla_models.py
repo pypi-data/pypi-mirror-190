e87ae1e1ca6671faa1a6a233aae2b8a17e51fdc9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:13:47 +0000

    Add package bigeye-sdk/0.4.44/bigeye_sdk-0.4.44-py3-none-any.whl

---
+++ packages/bigeye-sdk/bigeye_sdk-0.4.44-py3-none-any.whl/bigeye_sdk/model/sla_models.py
@@ -0,0 +1,23 @@
+from __future__ import annotations
+
+import json
+from dataclasses import dataclass
+
+from bigeye_sdk.generated.com.bigeye.models.generated import Collection, MetricInfoList
+
+
+@dataclass
+class SlaMetrics:
+    """TODO: move to proto?  Reusing Message because it has nice features for converting json camel -> snake."""
+    sla: Collection
+    metrics: MetricInfoList
+
+    def as_dict(self) -> dict:
+        return {"sla": self.sla.to_dict(), "metrics": self.metrics.to_dict()}
+
+    @classmethod
+    def from_json(cls, sla_metrics_json: str) -> SlaMetrics:
+        d = json.loads(sla_metrics_json)
+        r = SlaMetrics(sla=Collection().from_dict(d['sla']), metrics=MetricInfoList().from_dict(d['metrics']))
+        return r
+
