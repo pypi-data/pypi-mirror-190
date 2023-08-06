770d9ae02996d83e66220b43685ee888acc5d50f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:14:20 +0000

    Add package pyqchem/0.9.2/pyqchem-0.9.2-py3-none-any.whl

---
+++ packages/pyqchem/pyqchem-0.9.2-py3-none-any.whl/pyqchem/parsers/parser_rasci.py
@@ -0,0 +1,346 @@
+__author__ = 'Abel Carreras'
+
+import re
+import operator
+import numpy as np
+from pyqchem.structure import Structure
+from pyqchem.parsers.common import read_basic_info, get_rasci_occupations_list, search_bars, standardize_vector
+
+
+def _read_simple_matrix(header, output, maxchar=10000, foot='-------'):
+    matrix_list = []
+    for m in re.finditer(header, output):
+        section_state = output[m.end():m.end() + maxchar]  # 10000: assumed to max of section
+        section_state = section_state[:section_state.find(foot)]
+        dim = len(section_state.split('\n')[1].split())
+        matrix = section_state.split('\n')[1:dim + 1]
+        matrix = [[float(n) for n in l.split()] for l in matrix]
+        matrix_list.append(matrix)
+
+    return matrix_list
+
+
+def _read_soc_matrix(lines, dimensions):
+    # for line in lines:
+    #     print(line)
+
+    col_per_line = 5
+    matrix = []
+    for ib in range(dimensions[0]):
+        real = []
+        complex = []
+        for j in range((dimensions[1] - 1) // col_per_line + 1):
+            real += lines[j*dimensions[0] + 1 * (j+1) + ib][11:].split()[0::2]
+            complex += lines[j*dimensions[0] + 1 * (j+1) +ib][11:].split()[1::2]
+
+        row = [float(r) + float(c[:-1]) * 1j for r, c in zip(real, complex)]
+        matrix.append(row)
+
+    return matrix
+
+
+def parser_rasci(output):
+    """
+    Parser for RAS-CI calculations
+    Include:
+    - Diabatization scheme data
+    - Structure
+    - Adiabatic states
+    - SOC
+
+    :param output:
+    :return:
+    """
+
+    data_dict = {}
+    # Molecule
+    n = output.find('$molecule')
+    n2 = output[n:].find('$end')
+
+    molecule_region = output[n:n+n2-1].replace('\t', ' ').split('\n')[1:]
+    charge, multiplicity = [int(num) for num in molecule_region[0].split()]
+    coordinates = [[float(l) for l in line.split()[1:4]] for line in molecule_region[1:]]
+    symbols = [line.split()[0].capitalize() for line in molecule_region[1:]]
+    n_atoms = len(symbols)
+
+    # structure
+    structure_input = Structure(coordinates=coordinates,
+                                symbols=symbols,
+                                charge=charge,
+                                multiplicity=multiplicity)
+
+    enum = output.find('Standard Nuclear Orientation')
+    section_structure = output[enum:enum + 200*structure_input.get_number_of_atoms()].split('\n')
+    section_structure = section_structure[3:structure_input.get_number_of_atoms()+3]
+    coordinates = [[float(num) for num in s.split()[2:]] for s in section_structure]
+
+    data_dict['structure'] = Structure(coordinates=coordinates,
+                                       symbols=symbols,
+                                       charge=charge,
+                                       multiplicity=multiplicity)
+
+    # basic info
+    enum = output.find('Nuclear Repulsion Energy')
+    basic_data = read_basic_info(output[enum:enum + 5000])
+
+    # scf_energy
+    enum = output.find('SCF   energy in the final basis set')
+    scf_energy = float(output[enum:enum+100].split()[8])
+
+    data_dict['scf_energy'] = scf_energy
+    # total energy
+    # enum = output.find('Total energy in the final basis set')
+    # total_energy = float(output[enum:enum+100].split()[8])
+
+    # RASCI dimensions
+    ini_section = output.find('RAS-CI Dimensions')
+    end_section = search_bars(output, from_position=ini_section, bar_type='\*\*\*')[0]
+    dimension_section = output[ini_section: end_section]
+
+    enum = dimension_section.find('Doubly Occ')
+    doubly_occ = int(dimension_section[enum: enum+50].split()[3])
+    enum = dimension_section.find('Doubly Vir')
+    doubly_vir = int(dimension_section[enum: enum+50].split()[2])
+    enum = dimension_section.find('Frozen Occ')
+    frozen_occ = int(dimension_section[enum: enum+50].split()[3])
+    enum = dimension_section.find('Frozen Vir')
+    frozen_vir = int(dimension_section[enum: enum+50].split()[2])
+
+    enum = dimension_section.find('Total CI configurations')
+    total_conf = int(dimension_section[enum: enum+50].split()[3])
+    enum = dimension_section.find('Active configurations')
+    active_conf = int(dimension_section[enum: enum+50].split()[2])
+    enum = dimension_section.find('Hole configurations')
+    hole_conf = int(dimension_section[enum: enum+50].split()[2])
+    enum = dimension_section.find('Particle configurations')
+    particle_conf = int(dimension_section[enum: enum+50].split()[2])
+
+    rasci_dimensions = {'doubly_occupied': doubly_occ,
+                        'doubly_virtual': doubly_vir,
+                        'frozen_occupied': frozen_occ,
+                        'frozen_virtual': frozen_vir,
+                        'total_configurations': total_conf,
+                        'active_configurations': active_conf,
+                        'hole_configurations': hole_conf,
+                        'particle_configurations': particle_conf}
+
+    data_dict.update({'rasci_dimensions': rasci_dimensions})
+
+    # Diabatization scheme
+    done_diabat = bool(output.find('RASCI DIABATIZATION')+1)
+    if done_diabat:
+        rot_matrix = _read_simple_matrix('showmatrix final adiabatic -> diabatic', output)[-1]
+        adiabatic_matrix = _read_simple_matrix('showing H in adiabatic representation: NO coupling elements', output)[-1]
+        diabatic_matrix = _read_simple_matrix('showing H in diabatic representation: WITH coupling elements', output)[-1]
+
+        mulliken_adiabatic = []
+        enum = output.find('Mulliken analysis of Adiabatic State')
+        for m in re.finditer('Mulliken analysis of Adiabatic State', output[enum:]):
+            section_mulliken = output[m.end() + enum: m.end() + 10000 + enum]  # 10000: assumed to max of section
+            section_mulliken = section_mulliken[:section_mulliken.find('Natural Orbitals stored in FCHK')]
+            section_attachment = section_mulliken.split('\n')[9+n_atoms:9+n_atoms*2]
+
+            mulliken_adiabatic.append({'attach': [float(l.split()[1]) for l in section_attachment],
+                                       'detach': [float(l.split()[2]) for l in section_attachment],
+                                       'total': [float(l.split()[3]) for l in section_attachment]})
+
+        mulliken_diabatic = []
+        enum = output.find('showing H in diabatic representation')
+        for m in re.finditer('Mulliken Analysis of Diabatic State', output[enum:]):
+            section_mulliken = output[m.end() + enum: m.end() + 10000 + enum]  # 10000: assumed to max of section
+            section_mulliken = section_mulliken[:section_mulliken.find('Natural Orbitals stored in FCHK')]
+            section_attachment = section_mulliken.split('\n')[9+n_atoms:9+n_atoms*2]
+
+            mulliken_diabatic.append({'attach': [float(l.split()[1]) for l in section_attachment],
+                                      'detach': [float(l.split()[2]) for l in section_attachment],
+                                      'total': [float(l.split()[3]) for l in section_attachment]})
+
+        enum = output.find('Transition dipole moment - diabatic states')
+
+        tdm_section = output[enum: enum + 70 * len(rot_matrix)]
+
+        diabatic_tdm = []
+        for m in re.finditer('TDM', tdm_section):
+            diabatic_tdm.append([float(n) for n in tdm_section[m.end(): m.end()+70][14:].split()[:3]])
+
+        diabatic_states = []
+        for i, tdm in enumerate(diabatic_tdm):
+            diabatic_states.append({'excitation_energy': diabatic_matrix[i][i],
+                                    'excitation_energy_units': 'eV',
+                                    'transition_moment': tdm,
+                                    'dipole_moment_units': 'ua',
+                                    'mulliken': mulliken_diabatic[i]})
+
+        data_dict['diabatization'] = {'rot_matrix': rot_matrix,
+                                      'adiabatic_matrix': adiabatic_matrix,
+                                      'diabatic_matrix': diabatic_matrix,
+                                      'diabatic_states': diabatic_states,
+                                      'mulliken_adiabatic': mulliken_adiabatic}
+
+    # excited states data
+    excited_states = []
+    for m in re.finditer('RAS-CI total energy for state', output):
+        # print('ll found', m.start(), m.end())
+
+        end_section = search_bars(output, from_position=m.start(), bar_type='\*\*\*\*\*\*\*')[0]
+        section_state = output[m.start():end_section]
+
+        # energies
+        enum = section_state.find('total energy for state')
+        tot_energy = float(section_state[enum: enum + 50].split()[5])
+        enum = section_state.find('Excitation energy')
+        exc_energy_units = section_state[enum: enum + 30].split()[2].strip('(').strip(')')
+        exc_energy = float(section_state[enum: enum + 30].split()[4])
+
+        # multiplicity
+        n_multi = section_state.find('<S^2>')
+        multi_data = section_state[n_multi:n_multi + 30].split(':')[1]
+        state_multiplicity = float(multi_data.split()[0])
+
+        # dipole moment
+        enum = section_state.find('Dipole Moment')
+        dipole_mom = [float(section_state[enum:].split()[2]) + 0.0,
+                      float(section_state[enum:].split()[4]) + 0.0,
+                      float(section_state[enum:].split()[6]) + 0.0]
+
+        # Transition moment
+        enum = section_state.find('Trans. Moment')
+        trans_mom = strength = None
+        if enum > -1:
+            trans_mom = [float(section_state[enum:].split()[2]) + 0.0,
+                         float(section_state[enum:].split()[4]) + 0.0,
+                         float(section_state[enum:].split()[6]) + 0.0]
+            trans_mom = standardize_vector(trans_mom)
+            strength = float(section_state[enum:].split()[10])
+
+        # Natural orbitals
+        nato_occ = None
+        enum = section_state.find('NATURAL OCCUPATION NUMBERS')
+        if enum > -1:
+            lines = []
+            for line in section_state[enum:].split('\n')[2::2]:
+                if len(line) == 0:
+                    break
+                if line.split()[0].isnumeric():
+                    lines += line.split()[1:]
+                else:
+                    break
+            nato_occ = [float(num) for num in lines]
+
+        # configurations table
+        enum = section_state.find('AMPLITUDE')
+        enum2 = section_state.find('Contributions')
+        section_table = section_state[enum: enum2].split('\n')[2:-2]
+
+        # ' HOLE  | ALPHA | BETA  | PART | AMPLITUDE'
+        table = []
+        for row in section_table:
+            table.append({'hole': row.split('|')[1].strip(),
+                          'alpha': row.split('|')[2].strip(),
+                          'beta': row.split('|')[3].strip(),
+                          'part': row.split('|')[4].strip(),
+                          'amplitude': float(row.split('|')[5]) + 0.0})
+            table[-1]['occupations'] = get_rasci_occupations_list(table[-1],
+                                                                  doubly_occ,
+                                                                  basic_data['n_basis_functions'])
+
+        table = sorted(table, key=operator.itemgetter('hole', 'alpha', 'beta', 'part'))
+
+        # Contributions RASCI wfn
+        contributions_section = section_state[enum2:]
+        contributions = {'active' : float(contributions_section.split()[4]),
+                         'hole': float(contributions_section.split()[6]),
+                         'part': float(contributions_section.split()[8])}
+
+        # complete dictionary
+        tot_energy_units = 'au'
+
+        state_dict = {'total_energy': tot_energy,
+                      'total_energy_units': tot_energy_units,
+                      'excitation_energy': exc_energy,
+                      'excitation_energy_units': exc_energy_units,
+                      'multiplicity': state_multiplicity,
+                      'dipole_moment': dipole_mom,
+                      'transition_moment': trans_mom,
+                      'dipole_moment_units': 'ua',
+                      'oscillator_strength': strength,
+                      'configurations': table,
+                      'contributions_fwn': contributions}
+
+        if nato_occ is not None:
+            state_dict.update({'natural_occupation_numbers': nato_occ})
+
+        excited_states.append(state_dict)
+
+    data_dict.update({'excited_states': excited_states})
+
+    # Interstate transition properties
+    done_interstate = bool(output.find('Interstate Transition Properties')+1)
+    if done_interstate:
+        ini_section = output.find('Interstate Transition Properties')
+        end_section = search_bars(output, from_position=ini_section)[1]
+        interstate_section = output[ini_section: end_section]
+
+        interstate_dict = {}
+        for m in re.finditer('State A: Root', interstate_section):
+            section_pair = interstate_section[m.start():m.start() + 10000]
+            section_pair = section_pair[:section_pair.find('********')]
+
+            lines = section_pair.split('\n')
+
+            state_a = int(lines[0].split()[-1])
+            state_b = int(lines[1].split()[-1])
+
+            pair_dict = {'state_a': state_a,
+                         'state_b': state_b}
+
+            s_a = s_b = 0
+            for i, line in enumerate(lines):
+                # RAS-CI SOC section
+                if 'Angular momentum components' in line:
+                    pair_dict['angular_momentum'] = [complex(lines[i+1+k].split()[-1].replace('i', 'j')) for k in range(3)]
+
+                if '||gamma^AB||_total' in line:
+                    pair_dict['gamma_total'] = float(lines[i+0].split()[-1])
+                    pair_dict['gamma_sym'] = float(lines[i+1].split()[-1])
+                    pair_dict['gamma_anti_sym'] = float(lines[i+2].split()[-1])
+
+                if "KET: S',Sz'" in line:
+                    s_a = float(lines[i].split('=')[1].split()[0])
+                    s_b = float(lines[i+1].split('=')[1].split()[0])
+
+                na = int(2 * s_a + 1)
+                nb = int(2 * s_b + 1)
+
+                if 'Spin Matrices' in line:
+                    spinmat_x = _read_soc_matrix(lines[i + 2:], [nb, na])
+                    spinmat_y = _read_soc_matrix(lines[i + 4 + nb:], [nb, na])
+                    spinmat_z = _read_soc_matrix(lines[i + 6 + 2*nb:], [nb, na])
+                    pair_dict['spin_matrices'] = [spinmat_x, spinmat_y, spinmat_z]
+
+                if 'Spin matrices Sx, Sy and Sz for states' in line:
+                    pair_dict['spin_matrices'] = [np.zeros((nb, na)).tolist()]*3
+
+                if '1-elec SOC matrix (cm-1)' in line:
+                    pair_dict['1e_soc_mat'] = _read_soc_matrix(lines[i+1:], [nb, na])
+                    pair_dict['1e_socc'] = float(lines[i+2 + nb].split()[-2:][0])
+                if '2e-SOMF Reduced matrix elements (cm-1)' in line:
+                    r, c = lines[i+1].split()[-2:]
+                    pair_dict['hso_l-'] = float(r) + float(c) * 1j
+                    r, c = lines[i+2].split()[-2:]
+                    pair_dict['hso_l0'] = float(r) + float(c) * 1j
+                    r, c = lines[i+3].split()[-2:]
+                    pair_dict['hso_l+'] = float(r) + float(c) * 1j
+
+                if '2-elec mean-field SOC matrix (cm-1)' in line:
+                    pair_dict['2e_soc_mat'] = _read_soc_matrix(lines[i + 1:], [nb, na])
+                if 'Total mean-field SOC matrix (cm-1)' in line:
+                    pair_dict['total_soc_mat'] = _read_soc_matrix(lines[i + 1:], [nb, na])
+                if 'Mean-Field SOCC' in line:
+                    pair_dict['mf_socc'] = float(line.split()[-2])
+                    pair_dict['units'] = line.split()[-1]
+
+            interstate_dict[(state_a, state_b)] = pair_dict
+        data_dict.update({'interstate_properties': interstate_dict})
+
+    return data_dict
