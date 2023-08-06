d62c7d88ffe2f3bd7f15e2ecb85400ac600f07f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:29:24 +0000

    Add package mode-behave/1.0.6/mode_behave-1.0.6.tar.gz

---
+++ packages/mode-behave/mode_behave-1.0.6.tar.gz/mode_behave-1.0.6/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='mode_behave',
+      version='1.0.6',
+      description='Estimation and simulation of discrete choice models',
+      author='Julian Reul',
+      author_email='j.reul@fz-juelich.de',
+      url='https://github.com/julianreul/mode_behave',
+      license='MIT',
+      packages=['mode_behave_public'],
+      include_package_data=True,
+      zip_safe=False)
