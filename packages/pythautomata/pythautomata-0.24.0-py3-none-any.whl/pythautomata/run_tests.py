edef6768cbebf86dd01300a1d35e3451c961b4ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:32:04 +0000

    Add package pythautomata/0.24.0/pythautomata-0.24.0-py3-none-any.whl

---
+++ packages/pythautomata/pythautomata-0.24.0-py3-none-any.whl/pythautomata/run_tests.py
@@ -0,0 +1,43 @@
+from unittest import TestLoader, TestSuite, TextTestRunner
+
+from pythautomata.tests.test_minimizer import TestMinimizer
+from pythautomata.tests.test_automata_convertor import TestAutomataConvertor
+from pythautomata.tests.test_moore_machines_last_symbol_queries import TestMooreMachinesLastSymbolQueries
+from pythautomata.tests.test_pdfa_generator import TestPDFAGenerator
+from pythautomata.tests.test_pdfa_serialization import TestPDFASerialization
+from pythautomata.tests.test_dfa_serialization import TestDFASerialization
+from pythautomata.tests.test_dfa_generators import TestDFAGenerators
+from pythautomata.tests.test_automata_comparison import TestAutomataComparison
+from pythautomata.tests.test_sequence import TestSequence
+from pythautomata.tests.test_dfa_operations import TestDFAOperations
+from pythautomata.tests.test_automata_definitions import TestAutomataDefinitions
+from pythautomata.tests.test_regex_generator import TestRegexGenerator
+from pythautomata.tests.test_pdfa_metrics import TestPDFAMetrics
+from pythautomata.tests.test_simple_DFA_generator import TestSimpleDFAGenerator
+from pythautomata.tests.test_pdfa_operations import TestPDFAOperations
+from pythautomata.tests.test_pdfa_last_token_queries import TestPDFALastTokenQueries
+from pythautomata.tests.test_dfa_to_moore_machine import TestDFAToMooreMachine
+from pythautomata.tests.test_dfa_exporting import TestDFAExporting
+
+
+def get_all_tests():
+    return [TestDFAExporting, TestSimpleDFAGenerator, TestMinimizer, TestAutomataConvertor,
+            TestDFAGenerators, TestAutomataComparison, TestSequence, TestDFAOperations,
+            TestAutomataDefinitions, TestPDFAGenerator, TestRegexGenerator, TestPDFASerialization,
+            TestDFASerialization, TestPDFAMetrics, TestPDFAOperations, TestPDFALastTokenQueries,
+            TestMooreMachinesLastSymbolQueries, TestDFAToMooreMachine]
+
+
+def run():
+    test_classes_to_run = get_all_tests()
+    loader = TestLoader()
+    suites_list = []
+    for test_class in test_classes_to_run:
+        suite = loader.loadTestsFromTestCase(test_class)
+        suites_list.append(suite)
+    meta_suite = TestSuite(suites_list)
+    TextTestRunner().run(meta_suite)
+
+
+if __name__ == '__main__':
+    run()
