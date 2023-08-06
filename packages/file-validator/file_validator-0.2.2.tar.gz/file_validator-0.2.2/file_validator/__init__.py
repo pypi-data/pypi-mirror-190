b3f1c5bfae77c753d1ed868a222dc1a50b7ca8dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:11:55 +0000

    Add package file-validator/0.2.2/file_validator-0.2.2.tar.gz

---
+++ packages/file-validator/file_validator-0.2.2.tar.gz/file_validator-0.2.2/file_validator/__init__.py
@@ -0,0 +1,9 @@
+"""Top-level package for File Validator."""
+import file_validator.exceptions
+import file_validator.validators
+import file_validator.constants
+import file_validator.models
+
+__author__ = """Reza Shakeri"""
+__email__ = "rzashakeri@outlook.com"
+__version__ = "0.2.2"
