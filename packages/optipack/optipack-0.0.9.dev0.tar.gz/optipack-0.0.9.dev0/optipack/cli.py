0e10274297915b6bc57efa154ac3b4abd791dd1c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:59:25 +0000

    Add package optipack/0.0.9.dev0/optipack-0.0.9.dev0.tar.gz

---
+++ packages/optipack/optipack-0.0.9.dev0.tar.gz/optipack-0.0.9.dev0/optipack/cli.py
@@ -0,0 +1,45 @@
+import typer 
+
+""" 
+Optipack can be used in 2 ways: 
+    - init via cli: optipack init --env <dev/staging/prod> --project <project_name> 
+    - setup in code: 
+
+        import optipack 
+        optipack.setup(env, project_name) 
+"""
+
+app = typer.Typer()
+
+@app.command(help = 'Initialize project structure')
+def init(
+    env: str = '', 
+    project_name: str = '', 
+    parent_dir: str = '', 
+    structure_dir: str = '', # use __path__ + modify this using typer arguments 
+): 
+    import optipack
+    import os
+    # TODO: create env var. this one is for testing purpose only! 
+    path = optipack.__path__
+    if not structure_dir:
+        structure_dir = os.path.join(path, '.config/project_structure.yaml')
+    
+    optipack.init(
+        env = env, 
+        project_name = project_name, 
+        parent_dir=parent_dir, 
+        structure_dir= structure_dir
+    )
+
+@app.command(help='Setup')
+def setup(
+
+): 
+    ...
+
+def run(): 
+    app()
+
+if __name__ == '__main__': 
+    app()
