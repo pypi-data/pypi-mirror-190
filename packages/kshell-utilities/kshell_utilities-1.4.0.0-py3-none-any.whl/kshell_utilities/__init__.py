04b68d24af7b811884d7ea40e2425527af76f518
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:59:38 +0000

    Add package kshell-utilities/1.4.0.0/kshell_utilities-1.4.0.0-py3-none-any.whl

---
+++ packages/kshell-utilities/kshell_utilities-1.4.0.0-py3-none-any.whl/kshell_utilities/__init__.py
@@ -0,0 +1,22 @@
+__version__ = "1.4.0.0"
+__author__ = "Jon Kristian Dahl"
+__credits__ = "Noritaka Shimizu, Jørgen Eriksson Midtbø"
+
+"""
+Version legend:
+a.b.c.d
+
+a: major release
+b: new functionality added
+c: new feature to existing functionality
+d: bug fixes
+"""
+
+from .kshell_utilities import *
+from .general_utilities import *
+from .kshell_exceptions import *
+from .count_dim import *
+from .parameters import *
+from .collect_logs import collect_logs, check_multipolarities
+from .script_editing import edit_and_queue_executables
+# from .low_energy_enhancement import * # Not ready for ver. 1.0
