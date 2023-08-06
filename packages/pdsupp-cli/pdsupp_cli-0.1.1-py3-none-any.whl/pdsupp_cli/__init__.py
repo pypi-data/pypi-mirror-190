5db8af7dbae69a311eb60bec90251fa485521780
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:17:48 +0000

    Add package pdsupp-cli/0.1.1/pdsupp_cli-0.1.1-py3-none-any.whl

---
+++ packages/pdsupp-cli/pdsupp_cli-0.1.1-py3-none-any.whl/pdsupp_cli/__init__.py
@@ -0,0 +1,7 @@
+"""
+Proto-Type repo for migrating PagerDuty Public Support Scripts to a convenient CLI App.
+And exploring expansion of testing and developer documentation.
+
+
+.. include:: ../../README.md
+"""
