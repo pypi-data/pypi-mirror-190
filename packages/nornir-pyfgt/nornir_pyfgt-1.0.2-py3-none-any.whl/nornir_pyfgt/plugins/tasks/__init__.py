01de8f177e17068b84052f42a87c69c4e3eff303
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:18:10 +0000

    Add package nornir-pyfgt/1.0.2/nornir_pyfgt-1.0.2-py3-none-any.whl

---
+++ packages/nornir-pyfgt/nornir_pyfgt-1.0.2-py3-none-any.whl/nornir_pyfgt/plugins/tasks/__init__.py
@@ -0,0 +1,52 @@
+from .pyfgt_send_command import pyfgt_send_command
+from .pyfgt_address import pyfgt_address
+from .pyfgt_address_grp import pyfgt_address_grp
+from .pyfgt_antivirus import pyfgt_antivirus
+from .pyfgt_application import pyfgt_application
+from .pyfgt_dhcp_server import pyfgt_dhcp_server
+from .pyfgt_interface import pyfgt_interface
+from .pyfgt_internet_service import pyfgt_internet_service
+from .pyfgt_ip_pool import pyfgt_ip_pool
+from .pyfgt_policy_get import pyfgt_policy_get
+from .pyfgt_service import pyfgt_service
+from .pyfgt_service_category import pyfgt_service_category
+from .pyfgt_service_group import pyfgt_service_group
+from .pyfgt_virtual_ip import pyfgt_virtual_ip
+from .pyfgt_zone import pyfgt_zone
+from .pyfgt_get_url import pyfgt_get_url
+from .pyfgt_post_url import pyfgt_post_url
+from .pyfgt_put_url import pyfgt_put_url
+from .pyfgt_delete import pyfgt_delete
+from .pyfgt_policy_get import pyfgt_policy_get
+from .pyfgt_policy_move import pyfgt_policy_move
+from .pyfgt_policy_create import pyfgt_policy_create
+from .pyfgt_policy_delete import pyfgt_policy_delete
+
+
+__all__ = (
+    "pyfgt_send_command",
+    "pyfgt_address",
+    "pyfgt_address_grp",
+    "pyfgt_antivirus",
+    "pyfgt_application",
+    "pyfgt_dhcp_server",
+    "pyfgt_interface",
+    "pyfgt_internet_service",
+    "pyfgt_ip_pool",
+    "pyfgt_policy_get",
+    "pyfgt_service",
+    "pyfgt_service_category",
+    "pyfgt_service_group",
+    "pyfgt_virtual_ip",
+    "pyfgt_zone",
+    "pyfgt_get_url",
+    "pyfgt_post_url",
+    "pyfgt_put_url",
+    "pyfgt_delete",
+    "pyfgt_policy_move",
+    "pyfgt_policy_rename",
+    "pyfgt_policy_create",
+    "pyfgt_policy_delete",
+    "pyfgt_policy_get",
+
+)
