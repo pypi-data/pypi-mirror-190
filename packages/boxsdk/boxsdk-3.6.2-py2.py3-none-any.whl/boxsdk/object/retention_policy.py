b571b285cb19c7d59b77c86c09f85bb77f56ee50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:34:13 +0000

    Add package boxsdk/3.6.2/boxsdk-3.6.2-py2.py3-none-any.whl

---
+++ packages/boxsdk/boxsdk-3.6.2-py2.py3-none-any.whl/boxsdk/object/retention_policy.py
@@ -0,0 +1,89 @@
+import json
+from typing import Any, Union, TYPE_CHECKING, Iterable, Optional
+from .base_object import BaseObject
+from ..pagination.marker_based_object_collection import MarkerBasedObjectCollection
+from ..util.api_call_decorator import api_call
+
+if TYPE_CHECKING:
+    from boxsdk.object.metadata_template import MetadataTemplate
+    from boxsdk.object.enterprise import Enterprise
+    from boxsdk.object.folder import Folder
+    from boxsdk.object.retention_policy_assignment import RetentionPolicyAssignment
+    from boxsdk.pagination.box_object_collection import BoxObjectCollection
+
+
+class RetentionPolicy(BaseObject):
+    """Represents a Box retention policy."""
+    _item_type = 'retention_policy'
+
+    def get_url(self, *args: Any) -> str:
+        """
+        Returns the url for this retention policy.
+        """
+        return self._session.get_url('retention_policies', self._object_id, *args)
+
+    @api_call
+    def assign(
+            self,
+            assignee: Union['Folder', 'Enterprise', 'MetadataTemplate'],
+            fields: Iterable[str] = None
+    ) -> 'RetentionPolicyAssignment':
+        """Assign a retention policy to a Box item
+
+        :param assignee:
+            The item to assign the retention policy on.
+        :param fields:
+            List of fields to request.
+        :returns:
+            A :class:`RetentionPolicyAssignment` object.
+        """
+        url = self._session.get_url('retention_policy_assignments')
+        body = {
+            'policy_id': self.object_id,
+            'assign_to': {
+                'type': assignee.object_type,
+                'id': assignee.object_id,
+            }
+        }
+        params = {}
+        if fields is not None:
+            params['fields'] = ','.join(fields)
+        response = self._session.post(url, data=json.dumps(body), params=params).json()
+        return self.translator.translate(
+            session=self._session,
+            response_object=response,
+        )
+
+    @api_call
+    def assignments(
+            self,
+            assignment_type: Optional[str] = None,
+            limit: Optional[int] = None,
+            marker: Optional[str] = None,
+            fields: Iterable[str] = None
+    ) -> 'BoxObjectCollection':
+        """Get the assignments for the retention policy.
+
+        :param assignment_type:
+            The type of retention policy assignment to retrieve. Can be set to 'folder', 'enterprise', or 'metadata_template'.
+        :param limit:
+            The maximum number of items to return.
+        :param marker:
+            The position marker at which to begin the response.
+        :param fields:
+            List of fields to request.
+        :returns:
+            An iterable of assignments in the retention policy.
+        """
+        additional_params = {}
+        if assignment_type is not None:
+            additional_params['assignment_type'] = assignment_type
+        return MarkerBasedObjectCollection(
+            session=self._session,
+            url=self.get_url('assignments'),
+            additional_params=additional_params,
+            limit=limit,
+            marker=marker,
+            fields=fields,
+            return_full_pages=False,
+        )
---
+++ packages/boxsdk/boxsdk-3.6.2-py2.py3-none-any.whl/boxsdk/util/chunked_uploader.py
@@ -0,0 +1,179 @@
+import hashlib
+from typing import TYPE_CHECKING, IO, Optional
+from boxsdk.exception import BoxException
+
+if TYPE_CHECKING:
+    from boxsdk.object.upload_session import UploadSession
+    from boxsdk.object.file import File
+
+
+class ChunkedUploader:
+
+    def __init__(self, upload_session: 'UploadSession', content_stream: IO[bytes], file_size: int):
+        """
+        The initializer for the :class:`ChunkedUploader`
+
+        :param upload_session:
+            The upload session for doing the chunked uploader.
+        :param content_stream:
+            The file-like object to upload.
+        :param file_size:
+            The total size of the file for the chunked upload.
+        :returns:
+            An intialized`ChunkedUploader` object.
+        """
+        self._upload_session = upload_session
+        self._content_stream = content_stream
+        self._file_size = file_size
+        self._part_array = []
+        self._sha1 = hashlib.sha1()
+        self._part_definitions = {}
+        self._inflight_part = None
+        self._is_aborted = False
+
+    def start(self) -> Optional['File']:
+        """
+        Starts the process of chunk uploading a file. Should return file. If commit was not processed will return None.
+        You can call ChunkedUploader.resume to retry committing upload.
+
+        :returns:
+            An uploaded :class:`File` or None if session was not processed
+        """
+        if self._is_aborted:
+            raise BoxException('The upload has been previously aborted. Please retry upload with a new upload session.')
+        self._upload()
+        return self._commit_and_erase_stream_reference_when_succeed()
+
+    def resume(self) -> Optional['File']:
+        """
+        Resumes the process of chunk uploading a file from where upload failed.
+        Should return file. If commit was not processed will return None.
+        You can call ChunkedUploader.resume to retry committing upload.
+
+        :returns:
+            An uploaded :class:`File` or None if session was not processed
+        """
+        if self._is_aborted:
+            raise BoxException('The upload has been previously aborted. Please retry upload with a new upload session.')
+        parts = self._upload_session.get_parts()
+        self._part_array = []
+        # Construct a part array that is the first consecutive run of uploaded parts up to an inflight part so resume
+        # has a previous state to start from for in process uploads and cross process uploads.
+        # Construct a part definition to be used later to determine if a part has been uploaded by offset.
+        for part in parts:
+            if self._inflight_part and part['offset'] <= self._inflight_part.offset:
+                self._part_array.append(part)
+            if self._inflight_part and part['offset'] == self._inflight_part.offset:
+                self._inflight_part = None
+            self._part_definitions[part['offset']] = part
+        self._upload()
+        return self._commit_and_erase_stream_reference_when_succeed()
+
+    def abort(self) -> bool:
+        """
+        Abort an upload session, cancelling the upload and removing any parts that have already been uploaded.
+
+        :returns:
+            A boolean indication success of the upload abort.
+        """
+        self._content_stream = None
+        self._part_array = []
+        self._inflight_part = None
+        self._is_aborted = True
+        return self._upload_session.abort()
+
+    def _upload(self) -> None:
+        """
+        Utility function for looping through all parts of the upload session and uploading them.
+        """
+        while len(self._part_array) < self._upload_session.total_parts:
+            # Retrieve the part inflight if it exists, if it does not exist then get the next part from the stream.
+            next_part = self._inflight_part or self._get_next_part()
+            # Set the retrieve part to the current part inflight.
+            self._inflight_part = next_part
+            self._sha1.update(next_part.chunk)
+            # Retrieve the uploaded part if the part has already been uploaded. If not upload the current part.
+            uploaded_part = self._part_definitions.get(next_part.offset) or next_part.upload()
+            self._inflight_part = None
+            # Record that the part has been uploaded.
+            self._part_array.append(uploaded_part)
+            self._part_definitions[next_part.offset] = uploaded_part
+
+    def _get_next_part(self) -> 'InflightPart':
+        """
+        Retrieves the next :class:`InflightPart` that needs to be uploaded
+
+        :returns:
+            The :class:`InflightPart` object to be uploaded next.
+        """
+        copied_length = 0
+        chunk = b''
+        offset = len(self._part_array) * self._upload_session.part_size
+        while copied_length < self._upload_session.part_size:
+            bytes_read = self._content_stream.read(self._upload_session.part_size - copied_length)
+            if bytes_read is None:
+                # stream returns none when no bytes are ready currently but there are
+                # potentially more bytes in the stream to be read.
+                continue
+            if not bytes_read:
+                # stream is exhausted.
+                break
+            chunk += bytes_read
+            copied_length += len(bytes_read)
+        return InflightPart(offset, chunk, self._upload_session, self._file_size)
+
+    def _commit_and_erase_stream_reference_when_succeed(self):
+        content_sha1 = self._sha1.digest()
+        commit_result = self._upload_session.commit(content_sha1=content_sha1, parts=self._part_array)
+        # Remove file stream reference when uploading file succeeded
+        if commit_result is not None:
+            self._content_stream = None
+        return commit_result
+
+
+class InflightPart:
+
+    def __init__(self, offset: int, chunk: bytes, upload_session: 'UploadSession', total_size: int):
+        """
+        The initializer for the :class:`InflightPart` object.
+
+        :param offset:
+            The offset for the :class:`InflightPart` that represents the position of the part to be uploaded
+        :param chunk:
+            The chunk in bytes to be uploaded.
+        :param upload_session:
+            The :class:`UploadSession` for the :class:`InflightPart`.
+        :param total_size:
+            The total size of the file to be chunked uploaded.
+        """
+        self._offset = offset
+        self._chunk = chunk
+        self._upload_session = upload_session
+        self._total_size = total_size
+
+    @property
+    def offset(self) -> int:
+        """
+        Getter for the offset of the :class:`InflightPart`
+        """
+        return self._offset
+
+    @property
+    def chunk(self) -> bytes:
+        """
+        Getter for the chunk of the :class:`InflightPart`
+        """
+        return self._chunk
+
+    def upload(self) -> dict:
+        """
+        Upload method for the :class:`InflightPart`
+
+        :returns:
+            The uploaded part record.
+        """
+        return self._upload_session.upload_part_bytes(
+            part_bytes=self.chunk,
+            offset=self.offset,
+            total_size=self._total_size
+        )
