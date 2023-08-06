458ee014d09f00491e256ecc124079cd4d2dcdaf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:30:26 +0000

    Add package mara-db/4.9.0/mara-db-4.9.0.tar.gz

---
+++ packages/mara-db/mara-db-4.9.0.tar.gz/mara-db-4.9.0/mara_db/databricks.py
@@ -0,0 +1,20 @@
+"""Easy access to Databricks databases via databricks-sql-connector"""
+
+import contextlib
+import typing
+from warnings import warn
+
+import mara_db.dbs
+
+
+@contextlib.contextmanager
+def databricks_cursor_context(db: typing.Union[str, mara_db.dbs.DatabricksDB]) \
+        -> 'databricks.sql.client.Cursor':
+    warn('Function databricks_cursor_context(db) is deprecated. Please use db.cursor_context() instead.')
+
+    if isinstance(db, str):
+        db = mara_db.dbs.db(db)
+
+    assert (isinstance(db, mara_db.dbs.DatabricksDB))
+
+    return mara_db.dbs.cursor_context(db)
---
+++ packages/mara-db/mara-db-4.9.0.tar.gz/mara-db-4.9.0/mara_db/mysql.py
@@ -0,0 +1,20 @@
+"""Easy access to MySQL databases via mysql-client"""
+
+import contextlib
+import typing
+from warnings import warn
+
+import mara_db.dbs
+
+
+@contextlib.contextmanager
+def mysql_cursor_context(db: typing.Union[str, mara_db.dbs.MysqlDB]) -> 'MySQLdb.cursors.Cursor':
+    """Creates a context with a mysql-client cursor for a database alias or database"""
+    warn('Function mysql_cursor_context(db) is deprecated. Please use db.cursor_context() instead.')
+
+    if isinstance(db, str):
+        db = mara_db.dbs.db(db)
+
+    assert (isinstance(db, mara_db.dbs.MysqlDB))
+
+    return mara_db.dbs.cursor_context(db)
