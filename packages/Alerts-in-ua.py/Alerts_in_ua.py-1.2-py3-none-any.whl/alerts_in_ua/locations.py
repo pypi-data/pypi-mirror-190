d94bc5cf3282e556e3f9a18cf90c250979bc6c77
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:13:58 +0000

    Add package Alerts-in-ua.py/1.2/Alerts_in_ua.py-1.2-py3-none-any.whl

---
+++ packages/Alerts-in-ua.py/Alerts_in_ua.py-1.2-py3-none-any.whl/alerts_in_ua/locations.py
@@ -0,0 +1,32 @@
+from alerts_in_ua.location import Location
+
+
+class Locations(list):
+    """
+    Список для місць з тревогою для більш зручної маніпуляції над ними
+    """
+
+    def __init__(self, *__locations: Location, disclaimer: str, last_updated_at: str):
+        super(Locations, self).__init__(__locations)
+
+        self.disclaimer = disclaimer
+        self.last_updated_at = last_updated_at
+        self.type = "full"  # Ну потому что v1
+
+    def append(self, __location: Location) -> None:
+        super(Locations, self).append(__location)
+
+    def filter(self, **filters) -> list[Location]:
+        """
+        Повертає список місць які підпадають під вказані фільтри
+
+        :param filters: Фільтри
+        """
+
+        def location_filter(location: Location):
+            for _filter in filters:
+                if getattr(location, _filter) != filters[_filter]:
+                    return False
+            return True
+
+        return list(filter(location_filter, self))
