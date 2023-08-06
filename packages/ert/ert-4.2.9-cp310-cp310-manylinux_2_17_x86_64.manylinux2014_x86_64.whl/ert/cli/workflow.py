ca75a6fef91916ca0b0ede8565a10441b140244a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:32:06 +0000

    Add package ert/4.2.9/ert-4.2.9-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/ert/ert-4.2.9-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/ert/cli/workflow.py
@@ -0,0 +1,17 @@
+import logging
+
+
+def execute_workflow(ert, workflow_name):
+    logger = logging.getLogger(__name__)
+    workflow_list = ert.getWorkflowList()
+    try:
+        workflow = workflow_list[workflow_name]
+    except KeyError:
+        msg = "Workflow {} is not in the list of available workflows"
+        logger.error(msg.format(workflow_name))
+        return
+    context = ert.get_context()
+    workflow.run(ert=ert, verbose=True, context=context)
+    all_successful = all((v["completed"] for k, v in workflow.getJobsReport().items()))
+    if not all_successful:
+        logger.error(f"Workflow {workflow_name} failed!")
