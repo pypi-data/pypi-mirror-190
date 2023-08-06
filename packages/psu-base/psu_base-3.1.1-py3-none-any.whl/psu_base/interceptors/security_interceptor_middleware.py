2bede7c60c5589448fdd7a1fa83f8b6cb4a533e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:15:44 +0000

    Add package psu-base/3.1.1/psu_base-3.1.1-py3-none-any.whl

---
+++ packages/psu-base/psu_base-3.1.1-py3-none-any.whl/psu_base/interceptors/security_interceptor_middleware.py
@@ -0,0 +1,103 @@
+from django.shortcuts import redirect
+from psu_base.services import utility_service, auth_service, message_service
+from psu_base.classes.Log import Log
+from psu_base.models.xss import Xss
+from django.urls import reverse
+from django.http import HttpResponseForbidden, HttpResponse
+
+log = Log()
+
+
+def xss_prevention(get_response):
+    def script_response(param, value, is_ajax, path):
+
+        # Log the suspicious parameter
+        log.error(f"Potential XSS attempt in '{param}' parameter")
+        log.info(f"\n{value}\n")
+
+        # In prod, store the attempt as being from the CAS user
+        # In non-prod, use the impersonated user for testing
+        auth_object = auth_service.get_auth_object()
+        if utility_service.is_production():
+            xss_username = auth_object.sso_user.username
+        else:
+            xss_username = auth_object.get_user().username
+
+        # Store attempt in database.
+        xss_instance = Xss(
+            app_code=utility_service.get_app_code(),
+            path=path,
+            username=xss_username,
+            parameter_name=param,
+            parameter_value=value
+        )
+        xss_instance.save()
+
+        # Also log it in the audit table.
+        auth_service.audit_event('xss_attempt', comments=f"Created XSS attempt record #{xss_instance.id}")
+
+        if is_ajax:
+            # Generate a "flash message" to display on the view
+            message_service.post_error("Suspicious input detected. Unable to process request.")
+
+            # Return as failure for AJAX calls
+            return HttpResponseForbidden()
+        else:
+            # Redirect to "suspicious input" page for non-AJAX requests
+            return redirect('psu:xss')
+
+    def xss_middleware(request):
+
+        # Gather conditions and values used later
+        is_ajax = request.is_ajax()
+        is_health_check = utility_service.is_health_check()
+        is_flash_messages = request.path == reverse('psu:messages')
+        is_xss_lock = request.path == reverse('psu:xss_lock')
+        is_terminating_impersonation = request.path == reverse('psu:stop_impersonating')
+        is_logging_out = request.path == reverse('psu:logout')
+        username = auth_service.get_user().username
+
+        # Script_response will return a redirect.  If there are multiple XSS attempts, all attempts
+        # should be logged, but only one return is needed. Store it in a variable while iterating.
+        script_response_value = None
+
+        # Iterate through GET parameters
+        for param, value in request.GET.items():
+            if utility_service.contains_script(value):
+                # If XSS attempt found, log it and get a Redirect
+                script_response_value = script_response(param, value, is_ajax, request.path)
+
+        # Iterate through POST parameters
+        for param, value in request.POST.items():
+            if utility_service.contains_script(value):
+                # If XSS attempt found, log it and get a Redirect
+                script_response_value = script_response(param, value, is_ajax, request.path)
+
+        # If xss was found, return the Redirect to the blocking page
+        if script_response_value is not None:
+            return script_response_value
+
+        # If not already loading the lock page (and not an AWS health check)
+        if username is not None and not (is_xss_lock or is_health_check or is_flash_messages):
+            # Locked out users may logout or stop impersonating
+            if not(is_terminating_impersonation or is_logging_out):
+                # Count the number of un-reviewed XSS attempts for this user
+                attempts = len(Xss.objects.filter(username=username, review_username__isnull=True))
+                # After 3 attempts, user is locked out of site
+                if attempts >= 3:
+                    return redirect('psu:xss_lock')
+
+        # Otherwise, continue normally (and add XSS-Protection header)
+        response = get_response(request)
+        if type(response) is HttpResponse:
+            response['X-XSS-Protection'] = "1"
+
+            # Also add Cache-control: no-store and Pragma: no-cache headers (recommended by security team)
+            response['Cache-Control'] = "no-store"
+            response['Pragma'] = "no-cache"
+        # else:
+        #     log.info(f"Security headers not added to {type(response)}")
+
+        return response
+
+    return xss_middleware
---
+++ packages/psu-base/psu_base-3.1.1-py3-none-any.whl/psu_base/tests/test_dates.py
@@ -0,0 +1,137 @@
+import unittest
+import datetime
+from psu_base.classes.ConvenientDate import ConvenientDate
+from psu_base.services import date_service 
+
+
+class TestDates(unittest.TestCase):
+    """
+    Test the functions used by the PSU module
+    """
+
+    def test_date_strings(self):
+        """
+        Test the Date functions of psu_base
+        """
+
+        self.assertTrue(
+            date_service.string_to_date("31-JAN-2018") == datetime.datetime(2018, 1, 31, 0, 0, 0, 0),
+            "String-to_date #1"
+        )
+        self.assertTrue(
+            date_service.string_to_date("2018-01-31") == datetime.datetime(2018, 1, 31, 0, 0, 0, 0),
+            "String-to_date #2"
+        )
+        self.assertTrue(
+            date_service.string_to_date("1/31/2018") == datetime.datetime(2018, 1, 31, 0, 0, 0, 0),
+            "String-to_date #3"
+        )
+        self.assertTrue(
+            date_service.string_to_date("1/31/18") == datetime.datetime(2018, 1, 31, 0, 0, 0, 0),
+            "String-to_date #4"
+        )
+        self.assertTrue(
+            date_service.string_to_date("01/31/18") == datetime.datetime(2018, 1, 31, 0, 0, 0, 0),
+            "String-to_date #5"
+        )
+        self.assertTrue(
+            date_service.string_to_date("01/31/18 1:30:45") == datetime.datetime(2018, 1, 31, 1, 30, 45, 0),
+            "String-to_date #6"
+        )
+        self.assertTrue(
+            date_service.string_to_date("01/31/18 1:30") == datetime.datetime(2018, 1, 31, 1, 30, 0),
+            "String-to_date #7"
+        )
+        self.assertTrue(
+            date_service.string_to_date("01/31/18 1:30 PM") == datetime.datetime(2018, 1, 31, 13, 30, 0),
+            "String-to_date #8"
+        )
+        self.assertTrue(
+            date_service.string_to_date('April 29, 2020, 8:24 a.m.') == datetime.datetime(2020, 4, 29, 8, 24, 0),
+            "String-to_date #8b"
+        )
+        self.assertTrue(
+            date_service.string_to_date("01/31/18 1:30 pm") == datetime.datetime(2018, 1, 31, 13, 30, 0),
+            "String-to_date #9"
+        )
+        self.assertTrue(
+            date_service.string_to_date(
+                "2020-03-19 07:12:14.165927"
+            ) == datetime.datetime(2020, 3, 19, 7, 12, 14, 165927),
+            "String-to_date #10"
+        )
+        self.assertTrue(
+            date_service.string_to_date(
+                "2020-08-13T10:15"
+            ) == datetime.datetime(2020, 8, 13, 10, 15, 00),
+            "String-to_date #11"
+        )
+        # An error message will be printed here. Suppress it.
+        self.assertTrue(
+            date_service.string_to_date("Today at 7:30") is None,
+            "String-to_date #12"
+        )
+
+    def test_convenient_dates(self):
+        """
+        Test the ConvenientDate class
+        """
+        dd = ConvenientDate('01/31/2019')
+
+        self.assertTrue(dd.datetime_instance, "ConvenientDate #1")
+        self.assertTrue(dd.arrow_instance, "ConvenientDate #2")
+        self.assertTrue(dd.banner_date() == "31-JAN-2019", "ConvenientDate #3")
+        self.assertTrue(dd.date_field() == "2019-01-31", "ConvenientDate #4")
+        self.assertTrue(dd.timestamp() == "2019-01-31 00:00:00", "ConvenientDate #5")
+        self.assertTrue(str(dd) == "2019-01-31 00:00:00")
+
+        dd = ConvenientDate('06/08/2019 15:37')
+
+        self.assertTrue(dd.banner_date() == "08-JUN-2019", "ConvenientDate #6")
+        self.assertTrue(dd.timestamp() == "2019-06-08 15:37:00", "ConvenientDate #7")
+        self.assertTrue(dd.minus(2).timestamp() == "2019-06-06 15:37:00", "ConvenientDate #8")
+        self.assertTrue(dd.plus(2).timestamp() == "2019-06-08 15:37:00", "ConvenientDate #8")
+
+    def test_convenient_date_from_datetime(self):
+        """
+        Test creating ConvenientDate class from datetime instance
+        """
+        dt = datetime.datetime.now()
+        dd = ConvenientDate(dt)
+
+        self.assertTrue(dd.datetime_instance)
+        self.assertTrue(dd.datetime_instance == dt)
+
+    def test_convenient_date_from_none(self):
+        """
+        Test creating ConvenientDate class from None
+        """
+
+        # Create a null ConvenientDate
+        dd = ConvenientDate(None)
+        self.assertTrue(dd)
+        self.assertTrue(dd.datetime_instance is None)
+        self.assertTrue(dd.arrow_instance is None)
+
+        # Create a ConvenientDate for right now
+        dd = ConvenientDate('now')
+        self.assertTrue(dd)
+        self.assertTrue(dd.datetime_instance is not None)
+        self.assertTrue(dd.arrow_instance is not None)
+
+    def test_convenient_date_failure(self):
+        """
+        Test the ConvenientDate class
+        """
+        dd = ConvenientDate('1/2/3/4')
+
+        print(f"DDDDDDDDD: {dd}")
+        self.assertTrue(dd)
+        self.assertTrue(dd.datetime_instance is None)
+        self.assertTrue(dd.arrow_instance is None)
+        self.assertTrue(dd.conversion_error is not None)
+        self.assertTrue(dd.original_value == '1/2/3/4')
+
+
+if __name__ == '__main__':
+    unittest.main()
