65fb9d076cf2ec76fa2f6bf44eb079bc9bce27ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:01:14 +0000

    Add package chilitools/0.1.70/chilitools-0.1.70.tar.gz

---
+++ packages/chilitools/chilitools-0.1.70.tar.gz/chilitools-0.1.70/setup.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['chilitools', 'chilitools.api', 'chilitools.settings', 'chilitools.utilities']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['beautifulsoup4>=4.11.1,<5.0.0',
+ 'coloredlogs>=15.0.1,<16.0.0',
+ 'packaging>=21.3,<22.0',
+ 'pyperclip>=1.8.2,<2.0.0',
+ 'requests>=2.27.1,<3.0.0',
+ 'xmltodict>=0.12.0,<0.13.0']
+
+setup_kwargs = {
+    'name': 'chilitools',
+    'version': '0.1.70',
+    'description': 'A collection of tools for working with the CHILI publish REST API',
+    'long_description': None,
+    'author': 'Austin',
+    'author_email': 'austin.meier@hotmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.6,<4.0',
+}
+
+
+setup(**setup_kwargs)
