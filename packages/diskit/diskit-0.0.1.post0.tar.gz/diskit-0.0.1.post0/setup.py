d43b07135134d1f1dbba547fb86fa6a310d638d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:16:08 +0000

    Add package diskit/0.0.1.post0/diskit-0.0.1.post0.tar.gz

---
+++ packages/diskit/diskit-0.0.1.post0.tar.gz/diskit-0.0.1.post0/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="diskit",
+    version="0.0.1.post0",
+    license='Apache',
+    keywords=['quantum', 'distributed', 'simulator', 'qiskit', 'diskit'],
+    author="Anuranan Das, Stephen DiAdamo",
+    author_email="stephen.diadamo@gmail.com",
+    description="Distributed circuit remapping for Qiskit",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Interlin-q/diskit",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+    packages=setuptools.find_packages(),
+    python_requires=">=3.6"
+)
