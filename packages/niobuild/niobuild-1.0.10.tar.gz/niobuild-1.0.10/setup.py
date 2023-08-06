3a1afd045902edf0cfa4c83f69f958f3bb83b4b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:30:49 +0000

    Add package niobuild/1.0.10/niobuild-1.0.10.tar.gz

---
+++ packages/niobuild/niobuild-1.0.10.tar.gz/niobuild-1.0.10/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+Version = "v1.0.10"
+
+setup(
+    name="niobuild",
+    version=Version,
+    install_requires=[
+        # "conan==1.58.0",
+        "fbtftp",
+    ],
+    entry_points = {
+        'console_scripts': [
+            'niobuild = conannio.build:main'
+        ]
+    }
+)
