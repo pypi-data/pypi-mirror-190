6d434e6cbbf2c15af0b47bc356a737758ea54fdc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:26:07 +0000

    Add package psu-base/0.2.10/psu_base-0.2.10-py3-none-any.whl

---
+++ packages/psu-base/psu_base-0.2.10-py3-none-any.whl/psu_base/templates/local_settings_template.py
@@ -0,0 +1,26 @@
+# Environment choices: {DEV, TEST, PROD}
+ENVIRONMENT = 'DEV'
+
+# Name of machine running the application
+ALLOWED_HOSTS = ['localhost']
+
+# Debug mode (probably only true in DEV)
+DEBUG = True
+
+# SSO URL
+CAS_URL = 'https://sso-stage.oit.pdx.edu/idp/profile/cas/login'
+
+# Finti URL (dev, test, or prod)
+FINTI_URL = 'https://ws-test.oit.pdx.edu'
+# Finti URLs (for reference)
+# http://localhost:8888
+# https://ws-test.oit.pdx.edu
+# https://ws.oit.pdx.edu
+
+# FINTI_TOKEN = << put your Finti token here >>
+
+# You may disable elevated developer access in non-production
+# ELEVATE_DEVELOPER_ACCESS = False
+
+# You may extend session expiration during development
+# SESSION_COOKIE_AGE = 4 * 60 * 60  # 4 hours
