b6c509d20eb1283363d39b1b180298b334a6d0ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:30:08 +0000

    Add package kyuml/2023.2.7rc3/kyuml-2023.2.7rc3-py3-none-any.whl

---
+++ packages/kyuml/kyuml-2023.2.7rc3-py3-none-any.whl/kyuml/models/contrib/__init__.py
@@ -0,0 +1,9 @@
+# import eureka_model as EurekaModel
+import autogluon_model as AutogluonTrainable
+import big_model as DeepSeqEurekaModel
+
+__all__ = [
+    # "EurekaModel",
+    "DeepSeqEurekaModel",
+    "AutogluonTrainable",
+]
---
+++ packages/kyuml/kyuml-2023.2.7rc3-py3-none-any.whl/kyuml/utils/__init__.py
@@ -0,0 +1,3 @@
+from .logic_converter import LogicConverter
+
+__all__ = ["LogicConverter"]
