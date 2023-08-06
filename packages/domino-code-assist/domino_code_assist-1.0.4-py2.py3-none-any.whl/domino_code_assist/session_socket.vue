b39f5b0988ed068c64d012a6304daee1f74e82cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:10:20 +0000

    Add package domino-code-assist/1.0.4/domino_code_assist-1.0.4-py2.py3-none-any.whl

---
+++ packages/domino-code-assist/domino_code_assist-1.0.4-py2.py3-none-any.whl/domino_code_assist/session_socket.vue
@@ -0,0 +1,11 @@
+<script>
+  module.exports = {
+    created() {
+      if (!window.sessionSockets) {
+        window.sessionSockets = {};
+      }
+      const current = window.sessionSockets[this.notebookId];
+
+    }
+  }
+</script>
