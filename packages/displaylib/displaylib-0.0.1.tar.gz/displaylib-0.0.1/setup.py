92afeee4d1012e96e4bf72281976db9fcfc64647
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:16:52 +0000

    Add package displaylib/0.0.1/displaylib-0.0.1.tar.gz

---
+++ packages/displaylib/displaylib-0.0.1.tar.gz/displaylib-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+from displaylib import __version__, __author__
+
+
+setup(
+   name="displaylib",
+   version=__version__,
+   description="Display an ASCII world",
+   author=__author__,
+   packages=["displaylib"]
+)
