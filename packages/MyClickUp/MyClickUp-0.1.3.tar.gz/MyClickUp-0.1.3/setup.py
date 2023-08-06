e1e0f5d37cd240685dacecfcf4775316ad83325f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:23:45 +0000

    Add package MyClickUp/0.1.3/MyClickUp-0.1.3.tar.gz

---
+++ packages/MyClickUp/MyClickUp-0.1.3.tar.gz/MyClickUp-0.1.3/setup.py
@@ -0,0 +1,17 @@
+from setuptools import find_packages, setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='MyClickUp',
+    packages=find_packages(include=['MyClickUp']),
+    version='0.1.3',
+    description='The MyClickUp class allows you to encapsulate the integration with the ClickUp API',
+    description_content_type='text/plain',
+    long_description=long_description,
+    long_description_content_type='text/x-rst',
+    author='Jose Cordeiro',
+    license='MIT',
+    setup_requires=['requests'],
+)
