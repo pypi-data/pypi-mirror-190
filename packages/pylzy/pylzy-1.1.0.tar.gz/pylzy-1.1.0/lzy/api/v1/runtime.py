1b246e17ec3b70936205c10140928dbdc51be6df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:43:59 +0000

    Add package pylzy/1.1.0/pylzy-1.1.0.tar.gz

---
+++ packages/pylzy/pylzy-1.1.0.tar.gz/pylzy-1.1.0/lzy/api/v1/runtime.py
@@ -0,0 +1,41 @@
+from abc import ABC, abstractmethod
+from enum import Enum
+from typing import TYPE_CHECKING, Callable, List, Optional
+
+if TYPE_CHECKING:
+    from lzy.api.v1 import LzyWorkflow, WorkflowServiceClient
+
+from lzy.api.v1.call import LzyCall
+
+
+class ProgressStep(Enum):
+    WAITING = "WAITING"
+    EXECUTING = "EXECUTING"
+    COMPLETED = "COMPLETED"
+    FAILED = "FAILED"
+
+
+class Runtime(ABC):
+    @abstractmethod
+    def workflow_client(self) -> Optional["WorkflowServiceClient"]:
+        pass
+
+    @abstractmethod
+    async def start(self, workflow: "LzyWorkflow") -> str:
+        pass
+
+    @abstractmethod
+    async def exec(
+        self,
+        calls: List[LzyCall],
+        progress: Callable[[ProgressStep], None],
+    ) -> None:
+        pass
+
+    @abstractmethod
+    async def abort(self) -> None:
+        pass
+
+    @abstractmethod
+    async def destroy(self) -> None:
+        pass
