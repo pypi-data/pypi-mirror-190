96b728f85fc70bdb6bad5d8b793a22b891c1f46d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:13:51 +0000

    Add package fastapi-paginator/0.1.0/fastapi_paginator-0.1.0.tar.gz

---
+++ packages/fastapi-paginator/fastapi_paginator-0.1.0.tar.gz/fastapi_paginator-0.1.0/fastapi_paginator/__init__.py
@@ -0,0 +1,5 @@
+"""FastAPI Paginator."""
+from fastapi_paginator._models import Page, PageParameters
+from fastapi_paginator._paginator import Paginator
+
+__all__ = ("Page", "PageParameters", "Paginator")
