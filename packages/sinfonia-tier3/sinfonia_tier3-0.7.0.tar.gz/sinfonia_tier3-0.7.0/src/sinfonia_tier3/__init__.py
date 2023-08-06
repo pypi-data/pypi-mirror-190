6775f347eac534838ad6eccae7bf63ad0226f823
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:13:22 +0000

    Add package sinfonia-tier3/0.7.0/sinfonia_tier3-0.7.0.tar.gz

---
+++ packages/sinfonia-tier3/sinfonia_tier3-0.7.0.tar.gz/sinfonia_tier3-0.7.0/src/sinfonia_tier3/__init__.py
@@ -0,0 +1,15 @@
+#
+# Sinfonia
+#
+# deploy helm charts to a cloudlet kubernetes cluster for edge-native applications
+#
+# Copyright (c) 2021-2022 Carnegie Mellon University
+#
+# SPDX-License-Identifier: MIT
+#
+
+__version__ = "0.7.0"
+
+from .cli import sinfonia_tier3
+
+__all__ = ["sinfonia_tier3"]
