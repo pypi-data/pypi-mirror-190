0fbaffc3b8c3811163294cf4f6f626123d1bb52a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:58:45 +0000

    Add package quri-parts-honeywell/0.6.0/quri_parts_honeywell-0.6.0.tar.gz

---
+++ packages/quri-parts-honeywell/quri_parts_honeywell-0.6.0.tar.gz/quri_parts_honeywell-0.6.0/setup.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['quri_parts',
+ 'quri_parts.honeywell',
+ 'quri_parts.honeywell.circuit',
+ 'quri_parts.honeywell.circuit.transpile']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['numpy>=1.22.0', 'typing-extensions>=4.1.1,<5.0.0']
+
+setup_kwargs = {
+    'name': 'quri-parts-honeywell',
+    'version': '0.6.0',
+    'description': 'A plugin to use Honeywell with QIRI Parts',
+    'long_description': '# QURI Parts Honeywell\n\nQURI Parts Honeywell is a support library for using Honeywell with QURI Parts.\n\n## Documentation\n\n[QURI Parts Documentation](https://quri-parts.qunasys.com)\n\n## Installation\n\n```\npip install quri-parts-honeywell\n```\n\n## License\n\nApache License 2.0\n',
+    'author': 'QURI Parts Authors',
+    'author_email': 'support@qunasys.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'https://github.com/QunaSys/quri-parts',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.9.8,<4.0.0',
+}
+
+
+setup(**setup_kwargs)
