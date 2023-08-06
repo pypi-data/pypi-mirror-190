ac33538201b97df0caf11a9e4131d2161d5de0d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:33:31 +0000

    Add package agiocli/0.5.1/agiocli-0.5.1-py3-none-any.whl

---
+++ packages/agiocli/agiocli-0.5.1-py3-none-any.whl/agiocli/__main__.py
@@ -0,0 +1,275 @@
+"""
+A command line interface to autograder.io.
+
+Andrew DeOrio <awdeorio@umich.edu>
+"""
+import sys
+import click
+from agiocli import APIClient, TokenFileNotFound, utils
+
+
+@click.group(context_settings={"help_option_names": ["-h", "--help"]})
+@click.version_option()
+@click.option("-d", "--debug", is_flag=True, help="Debug output")
+@click.pass_context
+def main(ctx, debug):
+    """Autograder.io command line interface."""
+    # Pass global flags to subcommands via Click context
+    # https://click.palletsprojects.com/en/latest/commands/#nested-handling-and-contexts
+    ctx.ensure_object(dict)
+    ctx.obj["DEBUG"] = debug
+
+
+@main.command()
+@click.pass_context
+def login(ctx):
+    """Show current authenticated user."""
+    try:
+        client = APIClient.make_default(debug=ctx.obj["DEBUG"])
+    except TokenFileNotFound as err:
+        sys.exit(err)
+    user = client.get("/api/users/current/")
+    print(f"{user['username']} {user['first_name']} {user['last_name']}")
+
+
+@main.command()
+@click.argument("course_arg", required=False)
+@click.option("-l", "--list", "show_list", is_flag=True,
+              help="List courses and exit.")
+@click.option("-w", "--web", is_flag=True, help="Open course in browser.")
+@click.pass_context
+# The \b character in the docstring prevents Click from rewraping a paragraph.
+# We need to tell pycodestyle to ignore it.
+# https://click.palletsprojects.com/en/8.0.x/documentation/#preventing-rewrapping
+def courses(ctx, course_arg, show_list, web):  # noqa: D301
+    """Show course detail or list courses.
+
+    COURSE_ARG is a primary key, name, or shorthand.
+
+    \b
+    EXAMPLES:
+    agio courses --list
+    agio courses
+    agio courses 109
+    agio courses eecs485sp21
+    agio courses eecs485[cur|current]
+
+    """
+    try:
+        client = APIClient.make_default(debug=ctx.obj["DEBUG"])
+    except TokenFileNotFound as err:
+        sys.exit(err)
+
+    # Handle --list: list courses and exit
+    if show_list:
+        course_list = utils.get_current_course_list(client)
+        for i in course_list:
+            print(utils.course_str(i))
+        return
+
+    # Select a course and print or open it
+    course = utils.get_course_smart(course_arg, client)
+    if web:
+        utils.open_web(f"https://autograder.io/web/course/{course['pk']}")
+        return
+    print(utils.dict_str(course))
+
+
+@main.command()
+@click.argument("project_arg", required=False)
+@click.option("-c", "--course", "course_arg",
+              help="Course pk, name, or shorthand.")
+@click.option("-l", "--list", "show_list", is_flag=True,
+              help="List projects and exit.")
+@click.option("-w", "--web", is_flag=True, help="Open project in browser.")
+@click.option("--config", is_flag=True, help="Get test suite config.")
+@click.pass_context
+# The \b character in the docstring prevents Click from rewraping a paragraph.
+# We need to tell pycodestyle to ignore it.
+# https://click.palletsprojects.com/en/8.0.x/documentation/#preventing-rewrapping
+def projects(ctx, project_arg, course_arg, show_list, web, config):  # noqa: D301
+    """Show project detail or list projects.
+
+    PROJECT_ARG is a primary key, name, or shorthand.
+
+    \b
+    EXAMPLES:
+    agio projects --list
+    agio projects
+    agio projects 1005
+    agio projects --course 109 p1
+    agio projects --course eecs485sp21 p1
+    agio projects p1
+    agio projects --course eecs485sp21 p1 --config
+
+    """
+    # pylint: disable=too-many-arguments
+    try:
+        client = APIClient.make_default(debug=ctx.obj["DEBUG"])
+    except TokenFileNotFound as err:
+        sys.exit(err)
+
+    # Handle --list: list projects and exit
+    if show_list:
+        course = utils.get_course_smart(course_arg, client)
+        project_list = utils.get_course_project_list(course, client)
+        for i in project_list:
+            print(utils.project_str(i))
+        return
+
+    # Select a project
+    project = utils.get_project_smart(project_arg, course_arg, client)
+
+    # Print test suite config if --config flag
+    if config:
+        config_json = client.get(
+            f"/api/projects/{project['pk']}/ag_test_suites/"
+        )
+        print(utils.dict_str(config_json))
+        return
+
+    # Open project if --web flag
+    if web:
+        utils.open_web(f"https://autograder.io/web/project/{project['pk']}")
+        return
+
+    # Otherwise print project info
+    print(utils.dict_str(project))
+
+
+@main.command()
+@click.argument("group_arg", required=False)
+@click.option("-c", "--course", "course_arg",
+              help="Course pk, name, or shorthand.")
+@click.option("-p", "--project", "project_arg",
+              help="Project pk, name, or shorthand.")
+@click.option("-l", "--list", "show_list", is_flag=True,
+              help="List groups and exit.")
+@click.option("-j", "--list-json", "list_json", is_flag=True,
+              help="List groups in JSON format (2D array) and exit.")
+@click.option("-w", "--web", is_flag=True, help="Open group in browser.")
+@click.pass_context
+# The \b character in the docstring prevents Click from rewraping a paragraph.
+# We need to tell pycodestyle to ignore it.
+# https://click.palletsprojects.com/en/8.0.x/documentation/#preventing-rewrapping
+def groups(ctx, group_arg, project_arg, course_arg, show_list, list_json, web):  # noqa: D301
+    """Show group detail or list groups.
+
+    GROUP_ARG is a primary key, name, or member uniqname.
+
+    \b
+    EXAMPLES:
+    agio groups --list
+    agio groups --list-json
+    agio groups
+    agio groups 246965
+    agio groups awdeorio
+    agio groups awdeorio --project 1005
+    agio groups awdeorio --course eecs485sp21 --project p1
+
+    """
+    # We must have an function argument for each CLI argument or option
+    # pylint: disable=too-many-arguments
+
+    try:
+        client = APIClient.make_default(debug=ctx.obj["DEBUG"])
+    except TokenFileNotFound as err:
+        sys.exit(err)
+
+    # Handle --list: list groups and exit
+    if show_list:
+        project = utils.get_project_smart(project_arg, course_arg, client)
+        group_list = utils.get_group_list(project, client)
+        for i in group_list:
+            print(utils.group_str(i))
+        return
+
+    # Handle --queue: list groups in OH Queue format and exit
+    if list_json:
+        project = utils.get_project_smart(project_arg, course_arg, client)
+        group_list = utils.get_group_list(project, client)
+        output = [utils.group_emails(group) for group in group_list]
+        print(utils.dict_str(output))
+        return
+
+    # Select a group and print or open it
+    group = utils.get_group_smart(group_arg, project_arg, course_arg, client)
+    if web:
+        utils.open_web(
+            "https://autograder.io/web/"
+            f"project/{group['project']}"
+            f"?current_tab=student_lookup"
+            f"&current_student_lookup={group['pk']}"
+        )
+        return
+    print(utils.dict_str(group))
+
+
+@main.command()
+@click.argument("submission_arg", required=False)
+@click.option("-c", "--course", "course_arg",
+              help="Course pk, name, or shorthand.")
+@click.option("-p", "--project", "project_arg",
+              help="Project pk, name, or shorthand.")
+@click.option("-g", "--group", "group_arg",
+              help="Group pk or member uniqname.")
+@click.option("-l", "--list", "show_list", is_flag=True,
+              help="List groups and exit.")
+@click.option("-d", "--download", is_flag=True,
+              help="Download submission files.")
+@click.pass_context
+# The \b character in the docstring prevents Click from rewraping a paragraph.
+# We need to tell pycodestyle to ignore it.
+# https://click.palletsprojects.com/en/8.0.x/documentation/#preventing-rewrapping
+def submissions(ctx, submission_arg, group_arg,
+                project_arg, course_arg, show_list, download):  # noqa: D301
+    """Show submission detail or list submissions.
+
+    SUBMISSION_ARG is a primary key, 'best', or 'last'
+
+    \b
+    EXAMPLES:
+    agio submissions --list
+    agio submissions
+    agio submissions 1128572
+    agio submissions --course eecs485sp21 --project p1 --group awdeorio
+    agio submissions [...] best
+    agio submissions [...] last
+    agio submissions [...] --download
+    """
+    # We must have an function argument for each CLI argument or option
+    # pylint: disable=too-many-arguments
+
+    try:
+        client = APIClient.make_default(debug=ctx.obj["DEBUG"])
+    except TokenFileNotFound as err:
+        sys.exit(err)
+
+    # Handle --list: list submissions and exit
+    if show_list:
+        group = utils.get_group_smart(
+            group_arg, project_arg, course_arg, client
+        )
+        submission_list = utils.get_submission_list(group, client)
+        for i in submission_list:
+            print(utils.submission_str(i))
+        return
+
+    # Select a submission
+    submission = utils.get_submission_smart(
+        submission_arg, group_arg, project_arg, course_arg, client
+    )
+
+    # Handle --download: download the submission and exit
+    if download:
+        utils.download_submission(submission, group_arg, client)
+        return
+
+    # Default: print submission
+    print(utils.dict_str(submission))
+
+
+if __name__ == "__main__":
+    # These errors are endemic to click
+    # pylint: disable=no-value-for-parameter,unexpected-keyword-arg
+    main(obj={})
---
+++ packages/agiocli/agiocli-0.5.1-py3-none-any.whl/agiocli/api_client.py
@@ -0,0 +1,202 @@
+"""
+Autograder REST API client, a wrapper around the requests library.
+
+Based on HTTPClient by James Perretta
+https://github.com/eecs-autograder/autograder-contrib/
+"""
+import copy
+import os
+import json
+import sys
+from typing import Iterator
+from urllib.parse import urljoin
+import requests
+
+
+class APIClient:
+    """Send authenticated requests to the autograder.io REST API.
+
+    APIClient is a wrapper around the requests library that adds an
+    authentication token to the headers.  It supports all the arguments
+    accepted by the corresponding requests library methods.
+    https://requests.readthedocs.io/
+
+    Avoid constructing an APIClient directly.  Instead, use
+    APIClient.make_default().
+
+    """
+
+    @staticmethod
+    def make_default(
+            token_filename='.agtoken',
+            base_url='https://autograder.io/',
+            debug=False
+    ):
+        """Create an APIClient instance with API token found in token_filename.
+
+        Token file discovery works as follows:
+        - If token_filename is just a filename (no path information),
+        the current directory and every upward directory until the home
+        directory will be searched for a file with that name.
+        - If token_filename is an absolute path or a relative path that
+        contains at least one directory, that file will be opened and
+        the token read to it.
+
+        base_url will be prepended to all URLs passed to the client's
+        request methods and defaults to https://autograder.io/.
+        """
+        return APIClient(get_api_token(token_filename), base_url, debug)
+
+    def __init__(self, api_token, base_url, debug=False):
+        """Create an APIClient instance using a raw api_token.
+
+        Most users should use HTTPClient.make_default() instead.
+        """
+        self.api_token = api_token
+        self.base_url = base_url
+        self.debug = debug
+
+    def get(self, path, *args, **kwargs):
+        """Call requests.get with authentication headers and base URL."""
+        return self.do_request(requests.get, path, *args, **kwargs)
+
+    def get_paginated(self, path, *args, **kwargs):
+        """Iterate over paginated list route, yielding one page at a time."""
+        page_url = path
+        while page_url:
+            response = self.get(page_url, *args, **kwargs)
+            response.raise_for_status()
+            assert "results" in response.json()
+            assert "next" in response.json()
+            for item in response.json()["results"]:
+                yield item
+            page_url = response.json()['next']
+
+    def post(self, path, *args, **kwargs):
+        """Call requests.post with authentication headers and base URL."""
+        return self.do_request(requests.post, path, *args, **kwargs)
+
+    def put(self, path, *args, **kwargs):
+        """Call requests.put with authentication headers and base URL."""
+        return self.do_request(requests.put, path, *args, **kwargs)
+
+    def patch(self, path, *args, **kwargs):
+        """Call requests.patch with authentication headers and base URL."""
+        return self.do_request(requests.patch, path, *args, **kwargs)
+
+    def delete(self, path, *args, **kwargs):
+        """Call requests.delete with authentication headers and base URL."""
+        return self.do_request(requests.delete, path, *args, **kwargs)
+
+    def do_request(self, method_func, path, *args, **kwargs):
+        """Add authentication, base URL, call method, parse JSON.
+
+        - Append path to autograder REST API base URL
+        - Add token authentication headers
+        - Call method_func
+        - Check HTTP status code
+        - Parse JSON
+        """
+        # Append path to base URL
+        url = urljoin(self.base_url, path)
+
+        # Print request method and url
+        if self.debug:
+            method = method_func.__name__.upper()
+            print(f"{method} {url}")
+
+        # Call the underlying requests library function
+        headers = copy.deepcopy(kwargs.pop('headers', {}))
+        headers['Authorization'] = f'Token {self.api_token}'
+        response = method_func(url, *args, headers=headers, **kwargs)
+
+        # Print the response
+        if self.debug:
+            print_response(response)
+
+        # Check response status code
+        if not response.ok:
+            sys.exit(
+                f"Error: {response.status_code} {response.reason} "
+                f"for url {response.url}"
+            )
+
+        # Decode JSON
+        if "Content-Type" not in response.headers:
+            sys.exit(f"Error: no Content-Type from: {response.url}")
+        if 'application/json' in response.headers['Content-Type']:
+            try:
+                return response.json()
+            except json.JSONDecodeError:
+                sys.exit(
+                    f"Error: JSON decoding failed for url {response.url}\n"
+                    f"{response.text}"
+                )
+        elif 'application/octet-stream' in response.headers['Content-Type']:
+            return response.content
+        else:
+            sys.exit(
+                "Error: Unknown Content-Type "
+                f"'{response.headers['Content-Type']}' for url {response.url}"
+            )
+
+
+def get_api_token(token_filename: str) -> str:
+    """Search for autograder.io token.
+
+    Token file discovery works as follows:
+    - If token_filename is just a filename (no path information), the current
+      directory and every upward directory until the home directory will be
+      searched for a file with that name.
+    - If token_filename is an absolute path or a relative path that contains
+      at least one directory, that file will be opened and the token read.
+    """
+    # Token filename provided and it does not exist
+    if os.path.dirname(token_filename) and not os.path.isfile(token_filename):
+        raise TokenFileNotFound("Token file does not exist: {token_filename}")
+
+    # Make sure that we're starting in a subdir of the home directory
+    curdir = os.path.abspath(os.curdir)
+    if os.path.expanduser('~') not in curdir:
+        raise TokenFileNotFound(f"Invalid search path: {curdir}")
+
+    # Search, walking up the directory structure from PWD to home
+    for dirname in walk_up_to_home_dir():
+        filename = os.path.join(dirname, token_filename)
+        if os.path.isfile(filename):
+            with open(filename, encoding="utf8") as tokenfile:
+                return tokenfile.read().strip()
+
+    # Didn't find a token file
+    raise TokenFileNotFound(
+        f"Token file not found: {token_filename}.  Download a token from "
+        f"https://autograder.io/web/__apitoken__ and save it to "
+        f"~/{token_filename} or ./{token_filename}"
+    )
+
+
+def walk_up_to_home_dir() -> Iterator[str]:
+    """Iterate up the directory structure from pwd to home directory."""
+    current_dir = os.path.abspath(os.curdir)
+    home_dir = os.path.expanduser('~')
+
+    while current_dir != home_dir:
+        yield current_dir
+        current_dir = os.path.dirname(current_dir)
+
+    yield home_dir
+
+
+def print_response(response):
+    """Print a response object."""
+    try:
+        parsed = response.json()
+    except json.JSONDecodeError:
+        print(response.text)
+    else:
+        formatted = json.dumps(parsed, indent=4)
+        print(formatted)
+
+
+class TokenFileNotFound(Exception):
+    """Exception type indicating failure to locate user token file."""
