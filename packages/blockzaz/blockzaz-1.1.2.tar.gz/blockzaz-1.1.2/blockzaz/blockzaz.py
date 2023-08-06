7e8f859cc918437448e945bbdb1e9f7765905cd6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:18:50 +0000

    Add package blockzaz/1.1.2/blockzaz-1.1.2.tar.gz

---
+++ packages/blockzaz/blockzaz-1.1.2.tar.gz/blockzaz-1.1.2/blockzaz/blockzaz.py
@@ -0,0 +1,26 @@
+from typing import Optional
+
+from prefect.blocks.core import Block
+from pydantic import SecretStr
+
+class CubeJs(Block):
+    urlCube: Optional[str] = None
+    apiSecret: Optional[SecretStr] = None
+    urlAtualizacao: Optional[str] = None
+
+class Pentaho(Block):
+    urlPentaho: Optional[str] = None
+    apiSecret: Optional[SecretStr] = None
+    urlAtualizacao: Optional[str] = None
+
+class DataBase(Block):
+    nome: Optional[str] = None
+    host: Optional[str] = None
+    username: Optional[str] = None
+    password: Optional[SecretStr] = None
+    database: Optional[str] = None
+    port: Optional[str] = None
+    
+    def password(self):        
+        from pydantic import SecretStr as ss
+        return ss.get_secret_value(self.password)
