434a4fc799af24907d8e143ba5391f60aa45fc0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:19:09 +0000

    Add package setup1nter/0.0.1/setup1nter-0.0.1.tar.gz

---
+++ packages/setup1nter/setup1nter-0.0.1.tar.gz/setup1nter-0.0.1/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+
+
+setup(
+    name="setup1nter",
+    version='0.0.1',
+    license='Eclipse Public License 2.0',
+    authors=["myeggs", "myeggs", "myeggs"],
+    author_email="<myeggs@gmail.com>",
+    description="by myeggs, myeggs and myeggs",
+    long_description='Documentation: https://github.com/myeggs/setup1nter',
+    keywords=['setup1nter', 'colorama'],
+    packages=['setup1nter']
+)
+
+
