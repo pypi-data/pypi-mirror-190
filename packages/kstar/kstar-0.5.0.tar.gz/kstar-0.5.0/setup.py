0faca115fe5fc603e8ae0cb1b1cba10bbf9ff985
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:59:23 +0000

    Add package kstar/0.5.0/kstar-0.5.0.tar.gz

---
+++ packages/kstar/kstar-0.5.0.tar.gz/kstar-0.5.0/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+
+# Run setup
+setup(
+    name="kstar",
+    version="0.5.0",
+    author="Naegle Lab",
+    author_email="kmn4mj@virginia.edu",
+    url="https://github.com/NaegleLab/KSTAR",
+    install_requires=['pandas', 'numpy', 'scipy', 'matplotlib', 'seaborn', 'statsmodels', 'biopython','requests'],
+    license='GNU General Public License v3',
+    description='KSTAR: Kinase-Substrate Transfer to Activity Relationships',
+    long_description="""KSTAR is an open-source software for estimating kinase activities from phosphoproteomic data. 
+    KSTAR implements statistical and graph-theoretic approaches to produce a robust activity score that increases with increasing evidence from a kinase's network.""",
+    project_urls = {'Documentation': 'https://naeglelab.github.io/KSTAR/'},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Intended Audience :: Science/Research",
+        "Operating System :: OS Independent",
+    ],
+    packages=find_packages(),
+    include_package_data = True,
+    python_requires=">=3.6",
+    zip_safe = False
+)
+
