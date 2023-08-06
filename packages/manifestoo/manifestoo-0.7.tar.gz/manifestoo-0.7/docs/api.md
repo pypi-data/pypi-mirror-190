08c798520dd8b7dbd70cbe374e12e6fed57d8efd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:36:26 +0000

    Add package manifestoo/0.7/manifestoo-0.7.tar.gz

---
+++ packages/manifestoo/manifestoo-0.7.tar.gz/manifestoo-0.7/docs/api.md
@@ -0,0 +1,7 @@
+# Programmatic API
+
+This project does not expose a programmatic API. Only the command line interface is
+stable and documented.
+
+A public programmatic API is being developped in `manifestoo_core
+<https://pypi.org/project/manifestsoo-core>` on which this project relies.
---
+++ packages/manifestoo/manifestoo-0.7.tar.gz/manifestoo-0.7/src/manifestoo/commands/list_depends.py
@@ -0,0 +1,26 @@
+from typing import Iterable, Set, Tuple
+
+from manifestoo_core.addons_set import AddonsSet
+
+from ..addons_selection import AddonsSelection
+from ..dependency_iterator import dependency_iterator
+
+
+def list_depends_command(
+    addons_selection: AddonsSelection,
+    addons_set: AddonsSet,
+    transitive: bool = False,
+    include_selected: bool = False,
+) -> Tuple[Iterable[str], Iterable[str]]:
+    result: Set[str] = set()
+    missing: Set[str] = set()
+    for addon_name, addon in dependency_iterator(
+        addons_selection, addons_set, transitive
+    ):
+        if include_selected:
+            result.add(addon_name)
+        if not addon:
+            missing.add(addon_name)
+        else:
+            result.update(set(addon.manifest.depends) - set(addons_selection))
+    return sorted(result), missing
