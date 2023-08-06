53f537b0f74f430250ea0418dad6810ee4c3db16
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:09:37 +0000

    Add package datclass/0.0.3/datclass-0.0.3.tar.gz

---
+++ packages/datclass/datclass-0.0.3.tar.gz/datclass-0.0.3/src/datclass/__init__.py
@@ -0,0 +1,16 @@
+__title__ = 'datclass'
+__author__ = 'foyoux'
+__version__ = '0.0.3'
+__all__ = ['main', 'DatClass', 'MetaClass']
+
+import argparse
+
+from datclass.datclass import DatClass, MetaClass
+
+
+def main():
+    epilog = f'%(prog)s({__version__}) by foyoux(https://github.com/foyoux/datclass)'
+    parser = argparse.ArgumentParser(prog='datclass', description='', epilog=epilog)
+    parser.add_argument('-v', '--version', action='version', version=epilog)
+
+    parser.parse_args()
