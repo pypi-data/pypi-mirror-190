d4891ec1879849c11f3101850b82e6c710941966
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:59:29 +0000

    Add package hamed-hack/0.1/hamed_hack-0.1.tar.gz

---
+++ packages/hamed-hack/hamed_hack-0.1.tar.gz/hamed_hack-0.1/code/__init__.py
@@ -0,0 +1,8 @@
+def gamaa(a,b):
+    return a + b
+def tarah(a,b):
+    return a - b
+def darb(a,b):
+    return a * b
+def qasma(a,b):
+    return a / b
---
+++ packages/hamed-hack/hamed_hack-0.1.tar.gz/hamed_hack-0.1/setup.py
@@ -0,0 +1,13 @@
+import setuptools
+setuptools.setup(
+    name= 'hamed_hack',
+    version= '0.1',
+    author= 'hamed_sarhan',
+    description= 'Simple library in python for test',
+    packages=setuptools.find_packages(),
+    classifiers=[
+    "Programming Language :: Python :: 3",
+    "Operating System :: OS Independent",
+    "License :: OSI Approved :: MIT License"
+    ]
+)
