ea4344f8575803fe198c29715d49d2935b4c1ff9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:14:01 +0000

    Add package fastposter/1.1.8/fastposter-1.1.8-py3-none-any.whl

---
+++ packages/fastposter/fastposter-1.1.8-py3-none-any.whl/fastposter/__version__.py
@@ -0,0 +1,10 @@
+
+__title__ = 'fastposter'
+__description__ = 'Python client for fastposter cloud.'
+__url__ = 'https://cloud.fastposter.net/doc/'
+__version__ = '1.1.8'
+__author__ = 'Alex'
+__author_email__ = 'service@fastposter.net'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2023 fastposter.net'
+
