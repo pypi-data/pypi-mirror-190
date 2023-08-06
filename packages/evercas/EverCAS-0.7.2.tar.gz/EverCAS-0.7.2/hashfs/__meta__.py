a4e1b409d6a4582bb28468d7ff7dd9aeecfea431
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:16:21 +0000

    Add package evercas/0.7.2/EverCAS-0.7.2.tar.gz

---
+++ packages/evercas/EverCAS-0.7.2.tar.gz/EverCAS-0.7.2/hashfs/__meta__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""Define project metadata
+"""
+
+__title__ = "EverCAS"
+__summary__ = "Cloneable (with rclone) content-addressable storage for Python"
+__url__ = "https://github.com/weedonandscott/evercas"
+
+__version__ = "0.7.2"
+
+__install_requires__ = []
+__tests_require__ = ["tox"]
+
+__author__ = "Weedon & Scott Studios"
+__email__ = "studios@weedonandscott.com"
+
+__license__ = "MIT License"
