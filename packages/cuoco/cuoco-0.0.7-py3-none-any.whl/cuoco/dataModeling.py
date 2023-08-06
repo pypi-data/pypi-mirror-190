96cad2569d7e31f600953f7b20a409c8963a4cbb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:09:09 +0000

    Add package cuoco/0.0.7/cuoco-0.0.7-py3-none-any.whl

---
+++ packages/cuoco/cuoco-0.0.7-py3-none-any.whl/cuoco/dataModeling.py
@@ -0,0 +1,30 @@
+
+# Function for data normalization
+def normalize(col, df, method):
+    if method == "max_bas":
+        df[col] = df[col] / df[col].abs().max()
+    elif method == "min_max":
+        df[col] = (df[col] - df[col].min()) / (df[col].max() - df[col].min())
+    elif method == "z_score":
+        df[col] = (df[col] - df[col].mean()) / df[col].std()
+
+    return df
+
+"""
+# Function to manage oversampling method
+def oversampling(df, method, ycol):
+
+    if method == "random":
+        df = randomOverSampling(df, ycol)
+    elif method == "smote":
+        df = SMOTE(df, ycol)
+    return df
+
+# Perform random oversampling of the dataframe
+def randomOverSampling(df, ycol):
+    return df
+
+# Perform SMOTE in the dataframe
+def SMOTE(df, ycol):
+    return df
+"""
