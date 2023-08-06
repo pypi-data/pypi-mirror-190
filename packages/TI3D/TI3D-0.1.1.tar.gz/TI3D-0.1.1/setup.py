c508393fcd33d21eedd1322dd773c403bec1d7fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:30:48 +0000

    Add package TI3D/0.1.1/TI3D-0.1.1.tar.gz

---
+++ packages/TI3D/TI3D-0.1.1.tar.gz/TI3D-0.1.1/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name="TI3D",
+    version="0.1.1",
+    description="A simple 3D rendering libary for python",
+    long_description="Warning! this package is still in develpment, excpect bugs and incomplete fueters.\n\nThis is a simple package for 3d rendering\nin python. Documentation will be coming soon.\n\ninstalation: pip install TI3D\n\nmade by: Legolars2800",
+    author="legolars2800"
+    )
