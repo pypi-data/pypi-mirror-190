d2fd9b9be9aa65221e89d3ff71dfa51e17d5696a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:31:37 +0000

    Add package basictokenizer/0.0.3/basictokenizer-0.0.3.tar.gz

---
+++ packages/basictokenizer/basictokenizer-0.0.3.tar.gz/basictokenizer-0.0.3/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+ 
+classifiers = [
+  'Development Status :: 5 - Production/Stable',
+  'Intended Audience :: Education',
+  'Operating System :: Microsoft :: Windows :: Windows 10',
+  'License :: OSI Approved :: MIT License',
+  'Programming Language :: Python :: 3'
+]
+ 
+setup(
+  name='basictokenizer',
+  version='0.0.3',
+  description='A basic and useful tokenizer.',
+  long_description=open('README.txt').read(),
+  url='',  
+  author='UesleiDev',
+  author_email='uesleibros@gmail.com',
+  license='MIT', 
+  classifiers=classifiers,
+  keywords=['tokenizer',' token', 'basic-tokenizer', 'basic', 'easy-and-useful'], 
+  packages=find_packages(),
+  install_requires=['unidecode'] 
+)
