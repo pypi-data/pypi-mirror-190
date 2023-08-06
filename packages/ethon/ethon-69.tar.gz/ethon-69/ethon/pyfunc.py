749e26839165a9455d8ad95a4b637f3172737c41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:44:00 +0000

    Add package ethon/69/ethon-69.tar.gz

---
+++ packages/ethon/ethon-69.tar.gz/ethon-69/ethon/pyfunc.py
@@ -0,0 +1,36 @@
+"""This file is part of the ethon distribution.
+Copyright (c) 2021 vasusen-code
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, version 3.
+
+This program is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+General Public License for more details.
+
+License can be found in < https://github.com/vasusen-code/ethon/blob/main/LICENSE > ."""
+
+#vasusen-code/thechariotoflight/dronebots
+#__TG:ChauhanMahesh__
+
+import subprocess 
+import cv2
+
+#fastest way to get total number of frames in a video
+def total_frames(video_path):
+    cap = cv2.VideoCapture(f"{video_path}")
+    tf = int(cap.get(cv2.CAP_PROP_FRAME_COUNT)) 
+    return tf        
+
+#makes a subprocess handy
+def bash(cmd):    
+    bashCommand = f"{cmd}"
+    process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE) 
+    output, error = process.communicate()
+    return output, error
+
+#to get width, height and duration(in sec) of a video
+def video_metadata(file):
+    break
