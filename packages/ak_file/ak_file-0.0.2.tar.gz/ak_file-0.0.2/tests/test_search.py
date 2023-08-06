c2c8c8054e2cedd34a5b17628c9f8ed5633851e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:26:01 +0000

    Add package ak_file/0.0.2/ak_file-0.0.2.tar.gz

---
+++ packages/ak_file/ak_file-0.0.2.tar.gz/ak_file-0.0.2/tests/test_search.py
@@ -0,0 +1,8 @@
+from ak_file.search import *
+from pathlib import Path
+
+parent_dir = Path(__file__).parent.parent
+
+def test_find_files_w_extension():
+    assert len(list(find_files_w_extension(parent_dir, 'py', False))) == 0
+    assert len(list(find_files_w_extension(parent_dir, 'toml', False))) == 1
