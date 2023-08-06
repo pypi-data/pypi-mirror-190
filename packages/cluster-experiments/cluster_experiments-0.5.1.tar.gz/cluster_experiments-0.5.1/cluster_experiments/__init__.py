6ca99777bc88198c9097fa257aaee3a2173c7f91
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:38:49 +0000

    Add package cluster-experiments/0.5.1/cluster_experiments-0.5.1.tar.gz

---
+++ packages/cluster-experiments/cluster_experiments-0.5.1.tar.gz/cluster_experiments-0.5.1/cluster_experiments/__init__.py
@@ -0,0 +1,50 @@
+from cluster_experiments.cupac import EmptyRegressor, TargetAggregation
+from cluster_experiments.experiment_analysis import (
+    ClusteredOLSAnalysis,
+    ExperimentAnalysis,
+    GeeExperimentAnalysis,
+    OLSAnalysis,
+    PairedTTestClusteredAnalysis,
+    TTestClusteredAnalysis,
+)
+from cluster_experiments.perturbator import (
+    BinaryPerturbator,
+    Perturbator,
+    UniformPerturbator,
+)
+from cluster_experiments.power_analysis import PowerAnalysis
+from cluster_experiments.power_config import PowerConfig
+from cluster_experiments.random_splitter import (
+    BalancedClusteredSplitter,
+    BalancedSwitchbackSplitter,
+    ClusteredSplitter,
+    NonClusteredSplitter,
+    RandomSplitter,
+    StratifiedClusteredSplitter,
+    StratifiedSwitchbackSplitter,
+    SwitchbackSplitter,
+)
+
+__all__ = [
+    "ExperimentAnalysis",
+    "GeeExperimentAnalysis",
+    "OLSAnalysis",
+    "BinaryPerturbator",
+    "Perturbator",
+    "UniformPerturbator",
+    "PowerAnalysis",
+    "PowerConfig",
+    "EmptyRegressor",
+    "TargetAggregation",
+    "BalancedClusteredSplitter",
+    "ClusteredSplitter",
+    "RandomSplitter",
+    "NonClusteredSplitter",
+    "StratifiedClusteredSplitter",
+    "SwitchbackSplitter",
+    "BalancedSwitchbackSplitter",
+    "StratifiedSwitchbackSplitter",
+    "ClusteredOLSAnalysis",
+    "TTestClusteredAnalysis",
+    "PairedTTestClusteredAnalysis",
+]
