e00298a633880c99561b4d0996755b7aef2ccb50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:00:35 +0000

    Add package airphin/0.0.11/airphin-0.0.11.tar.gz

---
+++ packages/airphin/airphin-0.0.11.tar.gz/airphin-0.0.11/src/airphin/constants.py
@@ -0,0 +1,46 @@
+class TOKEN:
+    """Constants token for airphin."""
+
+    IMPORT: str = "import"
+    COMMA: str = ","
+    POINT: str = "."
+    STRING: str = "str"
+    CODE: str = "code"
+    NEW_LINE: str = "\n"
+
+
+class KEYWORD:
+    """Constants keywords for airphin."""
+
+    MIGRATE_MARK: str = "-airphin"
+
+
+class REGEXP:
+    """Constants regular expression for airphin."""
+
+    PATH_YAML: str = "*.yaml"
+    PATH_PYTHON: str = "*.py"
+    ALL: str = "**/*"
+
+
+class CONFIG:
+    """Constants config file for airphin."""
+
+    EXAMPLE: str = "examples"
+
+    MIGRATION: str = "migration"
+    MODULE: str = "module"
+    PARAMETER: str = "parameter"
+
+    ACTION: str = "action"
+    SOURCE: str = "src"
+    DESTINATION: str = "dest"
+
+    KW_REPLACE: str = "replace"
+    KW_ADD: str = "add"
+    KW_REMOVE: str = "remove"
+
+    ARGUMENT: str = "arg"
+    DEFAULT: str = "default"
+    TYPE: str = "type"
+    VALUE: str = "value"
