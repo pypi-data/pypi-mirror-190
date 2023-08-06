0c02a60e47ea4efa9d2810e116464d7b46d5e42b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:08:48 +0000

    Add package protontherapy/0.1.0/protontherapy-0.1.0.tar.gz

---
+++ packages/protontherapy/protontherapy-0.1.0.tar.gz/protontherapy-0.1.0/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['protontherapy']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['pendulum>=2.1.2,<3.0.0']
+
+setup_kwargs = {
+    'name': 'protontherapy',
+    'version': '0.1.0',
+    'description': '',
+    'long_description': '',
+    'author': 'Your Name',
+    'author_email': 'you@example.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
