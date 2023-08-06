01df275df2befaadf5b7df9b9e9492f5f449244e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:18:35 +0000

    Add package txtai/5.3.0/txtai-5.3.0-py3-none-any.whl

---
+++ packages/txtai/txtai-5.3.0-py3-none-any.whl/txtai/embeddings/__init__.py
@@ -0,0 +1,13 @@
+"""
+Embeddings imports
+"""
+
+from .archive import Archive
+from .base import Embeddings
+from .cloud import Cloud
+from .documents import Documents
+from .explain import Explain
+from .query import Query
+from .reducer import Reducer
+from .search import Search
+from .transform import Transform
---
+++ packages/txtai/txtai-5.3.0-py3-none-any.whl/txtai/workflow/task/factory.py
@@ -0,0 +1,73 @@
+"""
+Task factory module
+"""
+
+import functools
+
+from ...util import Resolver
+
+
+class TaskFactory:
+    """
+    Task factory. Creates new Task instances.
+    """
+
+    @staticmethod
+    def get(task):
+        """
+        Gets a new instance of task class.
+
+        Args:
+            task: Task instance class
+
+        Returns:
+            Task class
+        """
+
+        # Local task if no package
+        if "." not in task:
+            # Get parent package
+            task = ".".join(__name__.split(".")[:-1]) + "." + task.capitalize() + "Task"
+
+        # Attempt to load custom task
+        return Resolver()(task)
+
+    @staticmethod
+    def create(config, task):
+        """
+        Creates a new Task instance.
+
+        Args:
+            config: Task configuration
+            task: Task instance class
+
+        Returns:
+            Task
+        """
+
+        # Create lambda function if additional arguments present
+        if "args" in config:
+            args = config.pop("args")
+            action = config["action"]
+            if action:
+                if isinstance(action, list):
+                    config["action"] = [Partial(a, *args[i]) if args[i] else Partial(a) for i, a in enumerate(action)]
+                else:
+                    config["action"] = lambda x: action(x, *args)
+
+        # Get Task instance
+        return TaskFactory.get(task)(**config)
+
+
+class Partial(functools.partial):
+    """
+    Modifies functools.partial to prepend arguments vs append.
+    """
+
+    def __call__(self, *args, **kwargs):
+        # Update keyword arguments
+        kw = self.keywords.copy()
+        kw.update(kwargs)
+
+        # Execute function with new arguments prepended to default arguments
+        return self.func(*(args + self.args), **kw)
