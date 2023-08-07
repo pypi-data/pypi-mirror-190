77db7dc1c36533c7d5dfdb0c68068e5011dc3919
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 00:39:56 +0000

    Add package wmpaws/1.3/wmpaws-1.3.tar.gz

---
+++ packages/wmpaws/wmpaws-1.3.tar.gz/wmpaws-1.3/setup.py
@@ -0,0 +1,42 @@
+"""
+wmpaws
+-------------
+
+This is the description for that library
+"""
+import setuptools
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='wmpaws',
+    version='1.3',
+    license='GNU',
+    author='Martin Urbanec',
+    author_email='martin@urbanec.cz',
+    description='A library for Wikimedia\'s PAWS service.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    package_dir={"": 'src'},
+    packages=setuptools.find_packages(where="src"),
+    zip_safe=False,
+    include_package_data=True,
+    platforms='any',
+    install_requires=[
+        'requests',
+        'requests_oauthlib',
+        'pymysql',
+        'pandas',
+        'ipython',
+        'sqlalchemy'
+    ],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+)
---
+++ packages/wmpaws/wmpaws-1.3.tar.gz/wmpaws-1.3/src/wmpaws/__init__.py
@@ -0,0 +1,105 @@
+"""
+Part of wmpaws library
+
+Copyright (C) 2021 Martin Urbanec <martin.urbanec@wikimedia.cz>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
+import os
+import pymysql
+import sqlalchemy
+import pandas as pd
+import requests
+from requests_oauthlib import OAuth1
+from IPython.display import display, HTML
+
+def connect(dbname: str, cluster: str = 'analytics') -> pymysql.connections.Connection:
+    if cluster not in ['analytics', 'web']:
+        raise ValueError('"cluster" must be one of: "analytics", "web"')
+
+    if dbname.endswith('_p'):
+        dbname = dbname[:-2]
+    return pymysql.connect(
+        host="{dbname}.{cluster}.db.svc.wikimedia.cloud".format(dbname=dbname, cluster=cluster),
+        read_default_file=os.path.expanduser("~/.my.cnf"),
+        database="{dbname}_p".format(dbname=dbname),
+        charset='utf8'
+    )
+
+def _get_connection_string(dbname: str) -> str:
+    if dbname.endswith('_p'):
+        dbname = dbname[:-2]
+    return 'mysql+pymysql://{dbname}.analytics.db.svc.wikimedia.cloud/{dbname}_p?read_default_file={config}&charset=utf8'.format(
+        dbname=dbname,
+        config=os.path.expanduser("~/.my.cnf")
+    )
+
+def _run_sql(query: str, connection):
+    df = pd.read_sql_query(sqlalchemy.sql.text(query), connection)
+    for x in df:
+        df[x] = df[x].apply(lambda y: y.decode('utf-8') if isinstance(y, bytes) else y)
+    return df
+
+def run_sql(query: str, connection_or_dbname) -> pd.DataFrame:
+    if isinstance(connection_or_dbname, str):
+        engine = sqlalchemy.create_engine(_get_connection_string(connection_or_dbname))
+
+        with engine.connect() as connection:
+            df = _run_sql(query, connection)
+
+        engine.dispose()
+        return df
+    else:
+        return _run_sql(query, connection_or_dbname)
+
+def get_dblist(dblist):
+    r = requests.get('https://noc.wikimedia.org/conf/dblists/{dblist}.dblist'.format(dblist=dblist))
+    res = r.content.decode('utf-8').split('\n')
+    res.pop(0)
+    res.pop()
+    return res
+
+def get_oauth1():
+    return OAuth1(os.environ["CLIENT_ID"], os.environ["CLIENT_SECRET"], os.environ["ACCESS_KEY"], os.environ["ACCESS_SECRET"])
+
+def hide_code_button():
+    # Based on a StackOverflow answer by harshil
+    # https://stackoverflow.com/a/28073228/2509972
+    display(HTML("""
+    <form action="javascript:code_toggle()">
+        <input
+        id="code_toggle"
+        type="submit"
+        value="Hide code"
+        style="font-size: 1.4em"
+        >
+    </form>
+    <script>
+    code_shown = true;
+    function code_toggle() {
+        if (code_shown) {
+            $('div.input, div.output_prompt').hide();
+            //$('div.output_prompt').css('visibility', 'hidden');
+            $('#code_toggle').attr("value", "Show code");
+        } else {
+            $('div.input, div.output_prompt').show();
+            //$('div.output_prompt').css('visibility', 'visible');
+            $('#code_toggle').attr("value", "Hide code");
+        }
+        code_shown = !code_shown
+    }
+    $(document).ready(code_toggle);
+    </script>
+    """))
