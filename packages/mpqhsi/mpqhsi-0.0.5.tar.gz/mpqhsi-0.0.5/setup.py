397ee4b53f893d13fc0ac9b4e5a50a91eda029ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 02:36:02 +0000

    Add package mpqhsi/0.0.5/mpqhsi-0.0.5.tar.gz

---
+++ packages/mpqhsi/mpqhsi-0.0.5.tar.gz/mpqhsi-0.0.5/setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages, setup
+setup(
+    name='mpqhsi',
+    version='0.0.5',
+    description='hsi for MPQ_code',
+    author='MPQ',#作者
+    author_email='miaopeiqi@163.com',
+    url='https://github.com/miaopeiqi',
+    #packages=find_packages(),
+    packages=['mpqhsi'],  #这里是所有代码所在的文件夹名称
+    install_requires=[''],
+)
