4f3ae4a754d6ec133d9330cbdf0f72acf2f7eaf9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 00:45:29 +0000

    Add package downloader-for-reels/0.1.2/downloader_for_reels-0.1.2.tar.gz

---
+++ packages/downloader-for-reels/downloader_for_reels-0.1.2.tar.gz/downloader_for_reels-0.1.2/setup.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['downloader-for-reels',
+ 'downloader-for-reels.app',
+ 'downloader-for-reels.app.common',
+ 'downloader-for-reels.app.common.model',
+ 'downloader-for-reels.app.main']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['aiohttp==3.8.3',
+ 'aiosignal==1.3.1',
+ 'async-timeout==4.0.2',
+ 'attrs==22.2.0',
+ 'beautifulsoup4==4.11.2',
+ 'charset-normalizer==2.1.1',
+ 'frozenlist==1.3.3',
+ 'idna==3.4',
+ 'multidict==6.0.4',
+ 'soupsieve==2.3.2.post1',
+ 'yarl==1.8.2']
+
+setup_kwargs = {
+    'name': 'downloader-for-reels',
+    'version': '0.1.2',
+    'description': 'Reels Download Module',
+    'long_description': '',
+    'author': 'Николай Витальевич Никоноров',
+    'author_email': 'nnv@bitt.moe',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.10,<4.0',
+}
+
+
+setup(**setup_kwargs)
