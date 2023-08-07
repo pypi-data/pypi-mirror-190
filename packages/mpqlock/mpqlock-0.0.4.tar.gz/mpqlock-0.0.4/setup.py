c7587e2308c4771d9ea2e8249aa05895e0dc066d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 02:35:58 +0000

    Add package mpqlock/0.0.4/mpqlock-0.0.4.tar.gz

---
+++ packages/mpqlock/mpqlock-0.0.4.tar.gz/mpqlock-0.0.4/setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages, setup
+setup(
+    name='mpqlock',
+    version='0.0.4',
+    description='lock for MPQ_code',
+    author='MPQ',#作者
+    author_email='miaopeiqi@163.com',
+    url='https://github.com/miaopeiqi',
+    #packages=find_packages(),
+    packages=['mpqlock'],  #这里是所有代码所在的文件夹名称
+    install_requires=[''],
+)
