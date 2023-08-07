bbac2b15ab7894062648ab1f61827e45921d2d22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 00:43:51 +0000

    Add package this-django/0.1.0/this-django-0.1.0.tar.gz

---
+++ packages/this-django/this-django-0.1.0.tar.gz/this-django-0.1.0/README.md
@@ -0,0 +1,14 @@
+# this-django
+
+
+## Getting started
+```bash
+pip install this-django
+```
+
+
+## Using Guide
+```python
+# manage.py
+import this_dajngo
+```
---
+++ packages/this-django/this-django-0.1.0.tar.gz/this-django-0.1.0/this_django/__init__.py
@@ -0,0 +1,23 @@
+text = """
+Zen Django by 1PaCHEK1
+
+Ugly is better than beautiful.
+Implicit is better than explicit.
+Complex is better than simple.
+Complex is better than simple.
+Nested is better than flat.
+Dense is better than sparse.
+Readability doesn't matter.
+Special occasions, special enough to break the rules.
+Errors should always pass silently.
+In the face of ambiguity, give in to the temptation to guess.
+There shouldn't be one obvious way to do this.
+Though it may not be obvious at first, unless you are Dutch.
+Never better than now.
+If the implementation is hard to explain, that's a good idea.
+If the implementation is easy to explain, this might be a bad idea.
+Namespaces are one of the ideas - let's make them smaller!
+Annotation types don't exist for you if you are a real django developer
+"""
+
+print(text)
