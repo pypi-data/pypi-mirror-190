d319ea5e23a277cb1df3c99b7c3985c14e40fb35
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 00:44:57 +0000

    Add package econpizza/0.4.4/econpizza-0.4.4-py3-none-any.whl

---
+++ packages/econpizza/econpizza-0.4.4-py3-none-any.whl/econpizza/__init__.py
@@ -0,0 +1,88 @@
+#!/bin/python
+# -*- coding: utf-8 -*-
+
+from .solvers.solve_linear_state_space import *
+from .solvers.solve_linear import find_path_linear
+from .solvers.stacking import find_path_stacking
+from .solvers.shooting import find_path_shooting
+from .solvers.steady_state import solve_stst
+from .parsing import parse, load
+import jax
+import logging
+import os
+# set number of cores for XLA
+os.environ["XLA_FLAGS"] = f"--xla_force_host_platform_device_count={os.cpu_count()}"
+
+
+__version__ = '0.4.4'
+
+jax.config.update("jax_enable_x64", True)
+
+
+class PizzaModel(dict):
+    def __init__(self, *args, **kwargs):
+        super(PizzaModel, self).__init__(*args, **kwargs)
+        self.__dict__ = self
+
+    def get_distributions(self, xst, shock=None):
+        """Get all disaggregated variables for a given trajectory of aggregate variables.
+
+        Parameters
+        ----------
+
+        self : PizzaModel
+            the model instance
+        xst : array
+            a _full_ trajectory of aggregate variables
+
+        Returns
+        -------
+        rdict : dict
+            a dictionary of the distributions
+        """
+
+        shocks = self.get("shocks") or ()
+        dist_names = list(self['distributions'].keys())
+        decisions_outputs = self['decisions']['outputs']
+        x = xst[1:-1].flatten()
+        x0 = xst[0]
+
+        # deal with shocks if any
+        shock_series = jnp.zeros((len(xst)-2, len(shocks)))
+        if shock is not None:
+            shock_series = shock_series.at[0,
+                                           shocks.index(shock[0])].set(shock[1])
+
+        # get functions and execute
+        backwards_sweep = self['context']['backwards_sweep']
+        forwards_sweep = self['context']['forwards_sweep']
+        decisions_output_storage = backwards_sweep(x, x0, shock_series.T)
+        dists_storage = forwards_sweep(decisions_output_storage)
+
+        # store this
+        rdict = {oput: decisions_output_storage[i]
+                 for i, oput in enumerate(decisions_outputs)}
+        rdict.update({oput: dists_storage[i]
+                     for i, oput in enumerate(dist_names)})
+
+        return rdict
+
+
+PizzaModel.solve_stst = solve_stst
+PizzaModel.solve_linear_state_space = solve_linear_state_space
+PizzaModel.find_path = find_path_stacking
+PizzaModel.find_path_stacking = find_path_stacking
+PizzaModel.find_path_linear = find_path_linear
+PizzaModel.find_path_linear_state_space = find_path_linear_state_space
+PizzaModel.find_path_shooting = find_path_shooting
+
+logging.basicConfig(level=logging.INFO)
+
+pth = os.path.dirname(__file__)
+
+example_nk = os.path.join(pth, "examples", "nk.yaml")
+example_bh = os.path.join(pth, "examples", "bh.yaml")
+example_dsge = os.path.join(pth, "examples", "med_scale_nk.yaml")
+example_hank = os.path.join(pth, "examples", "hank.yaml")
+example_hank_labor = os.path.join(pth, "examples", "hank_labor.yaml")
+example_hank2 = os.path.join(pth, "examples", "hank2.yaml")
---
+++ packages/econpizza/econpizza-0.4.4-py3-none-any.whl/econpizza/test_all.py
@@ -0,0 +1,177 @@
+#!/bin/python
+# -*- coding: utf-8 -*-
+
+import os
+from econpizza.__init__ import *
+import econpizza as ep
+
+filepath = os.path.dirname(__file__)
+
+
+def test_bh(create=False):
+
+    mod = load(example_bh, raise_errors=False)
+    _ = mod.solve_stst()
+
+    state = jnp.zeros(len(mod["variables"]))
+    state = state.at[:-1].set([0.1, 0.2, 0.0])
+
+    x, flag = find_path_shooting(
+        mod, state, T=50, max_horizon=500, tol=1e-8, verbose=2)
+
+    path = os.path.join(filepath, "test_storage", "bh.npy")
+
+    if create:
+        jnp.save(path, x)
+        print(f'Test file updated at {path}')
+    else:
+        test_x = jnp.load(path)
+
+        assert flag == 0
+        assert jnp.allclose(x, test_x)
+
+
+def test_nk(create=False):
+
+    mod = load(example_nk)
+    _ = mod.solve_stst()
+
+    state = mod["stst"].copy()
+    state["beta"] *= 1.02
+
+    x, flag = find_path_shooting(mod, state.values(), T=10,
+                                 max_horizon=10, verbose=2)
+
+    path = os.path.join(filepath, "test_storage", "nk.npy")
+
+    if create:
+        jnp.save(path, x)
+        print(f'Test file updated at {path}')
+    else:
+        test_x = jnp.load(path)
+
+        assert flag == 0
+        assert jnp.allclose(x, test_x)
+
+
+def test_stacked(create=False):
+
+    mod = load(example_nk)
+    _ = mod.solve_stst()
+
+    shk = ("e_beta", 0.02)
+
+    x, flag = find_path_stacking(mod, shock=shk, horizon=250)
+
+    path = os.path.join(filepath, "test_storage", "stacked.npy")
+
+    if create:
+        jnp.save(path, x)
+        print(f'Test file updated at {path}')
+    else:
+        test_x = jnp.load(path)
+
+        assert flag == 0
+        assert jnp.allclose(x, test_x)
+
+
+def test_hank(create=False):
+
+    mod_dict = ep.parse(example_hank)
+    mod = ep.load(mod_dict)
+    _ = mod.solve_stst(tol=1e-8)
+
+    x0 = mod['stst'].copy()
+    x0['beta'] *= 1.01  # setting a shock on the discount factor
+
+    x, flag = mod.find_path(x0=x0.values())
+    x_lin, _ = mod.find_path_linear(x0=x0.values())
+    het_vars = mod.get_distributions(x)
+    dist = het_vars['dist']
+
+    path_x = os.path.join(filepath, "test_storage", "hank.npy")
+    path_x_lin = os.path.join(filepath, "test_storage", "hank_lin.npy")
+    path_dist = os.path.join(filepath, "test_storage", "hank_dist.npy")
+
+    if create:
+        jnp.save(path_x, x)
+        jnp.save(path_x_lin, x_lin)
+        jnp.save(path_dist, dist)
+        print(f'Test file updated at {path_x},{path_x_lin} and {path_dist}')
+    else:
+        test_x = jnp.load(path_x)
+        test_x_lin = jnp.load(path_x_lin)
+        test_dist = jnp.load(path_dist)
+
+        assert flag == 0
+        assert jnp.allclose(x, test_x)
+        assert jnp.allclose(x_lin, test_x_lin)
+        assert jnp.allclose(dist, test_dist)
+
+
+def test_hank_labor(create=False):
+
+    mod_dict = ep.parse(example_hank_labor)
+    mod = ep.load(mod_dict)
+    _ = mod.solve_stst(tol=1e-8)
+
+    shocks = ('e_beta', .01)
+
+    x, flag = mod.find_path(shocks)
+
+    path = os.path.join(filepath, "test_storage", "hank_labor.npy")
+
+    if create:
+        jnp.save(path, x)
+        print(f'Test file updated at {path}')
+    else:
+        test_x = jnp.load(path)
+
+        assert flag == 0
+        assert jnp.allclose(x, test_x)
+
+
+def test_hank2(create=False):
+
+    mod_dict = ep.parse(example_hank2)
+    mod = ep.load(mod_dict)
+    _ = mod.solve_stst(tol=1e-6)
+
+    x0 = mod['stst'].copy()
+    x0['beta'] *= 1.01  # setting a shock on the discount factor
+
+    x, flag = mod.find_path(x0=x0.values())
+
+    path = os.path.join(filepath, "test_storage", "hank2.npy")
+
+    if create:
+        jnp.save(path, x)
+        print(f'Test file updated at {path}')
+    else:
+        test_x = jnp.load(path)
+
+        assert flag == 0
+        assert jnp.allclose(x, test_x)
+
+
+def test_solid(create=False):
+
+    mod_dict = ep.parse(example_hank)
+    mod = ep.load(mod_dict)
+    _ = mod.solve_stst(tol=1e-8)
+
+    shocks = ('e_beta', .01)
+
+    x, flag = mod.find_path(shocks, use_solid_solver=True,
+                            horizon=20, chunk_size=90)
+
+    path = os.path.join(filepath, "test_storage", "hank_solid.npy")
+
+    if create:
+        jnp.save(path, x)
+        print(f'Test file updated at {path}')
+    else:
+        test_x = jnp.load(path)
+
+        assert flag == 0
+        assert jnp.allclose(x, test_x)
