794b5e49aee0d0dd33ec5766a034749a18339b6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 02:02:21 +0000

    Add package opensearch-py-ml/1.0.0/opensearch_py_ml-1.0.0-py3-none-any.whl

---
+++ packages/opensearch-py-ml/opensearch_py_ml-1.0.0-py3-none-any.whl/opensearch_py_ml/ml_commons/ml_common_utils.py
@@ -0,0 +1,11 @@
+# SPDX-License-Identifier: Apache-2.0
+# The OpenSearch Contributors require contributions made to
+# this file be licensed under the Apache-2.0 license or a
+# compatible open source license.
+# Any modifications Copyright OpenSearch Contributors. See
+# GitHub history for details.
+
+ML_BASE_URI = "/_plugins/_ml"
+MODEL_UPLOAD_CHUNK_SIZE = 10_000_000
+MODEL_MAX_SIZE = 4_000_000_000
+BUF_SIZE = 65536  # lets read stuff in 64kb chunks!
