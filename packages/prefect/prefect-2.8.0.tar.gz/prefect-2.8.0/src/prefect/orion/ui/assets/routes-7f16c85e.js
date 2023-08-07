9b1562d19b8332068fc777e3d25ecdd03c122d15
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 02:02:50 +0000

    Add package prefect/2.8.0/prefect-2.8.0.tar.gz

---
+++ packages/prefect/prefect-2.8.0.tar.gz/prefect-2.8.0/src/prefect/orion/ui/assets/routes-7f16c85e.js
@@ -0,0 +1,1 @@
+import{q as t}from"./index-1ad4f081-98325998.js";const e={root:()=>({name:"root"}),404:()=>({name:"404"}),settings:()=>({name:"settings"}),...t()};export{e as r};
---
+++ packages/prefect/prefect-2.8.0.tar.gz/prefect-2.8.0/src/prefect/orion/ui/assets/useToast-3ce03e64.js
@@ -0,0 +1,1 @@
+import{aV as n,v as a}from"./index-1ad4f081-98325998.js";function u(){const t=[];return n(()=>t.forEach(s=>s.dismiss())),(...s)=>{const o=a(...s);return t.push(o),o}}export{u};
