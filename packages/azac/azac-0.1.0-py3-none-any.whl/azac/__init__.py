d332d964a4dd0448604980e8c3185a7aead48c96
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 02:01:29 +0000

    Add package azac/0.1.0/azac-0.1.0-py3-none-any.whl

---
+++ packages/azac/azac-0.1.0-py3-none-any.whl/azac/__init__.py
@@ -0,0 +1,2 @@
+from __future__ import print_function
+from azac.azac_core import AZaCCore
---
+++ packages/azac/azac-0.1.0-py3-none-any.whl/azac/azac_core.py
@@ -0,0 +1,6 @@
+class AZaCCore:
+    def __init__(self, value) -> None:
+        self._value = value
+
+    def add(self, num) -> int:
+        return self._value + num
