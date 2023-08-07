c2b8f667b53359ca08842d9a5c0102cd21d748f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 02:36:05 +0000

    Add package mpqml/0.0.7/mpqml-0.0.7.tar.gz

---
+++ packages/mpqml/mpqml-0.0.7.tar.gz/mpqml-0.0.7/setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages, setup
+setup(
+    name='mpqml',
+    version='0.0.7',
+    description='machine learning for MPQ_code',
+    author='MPQ',#作者
+    author_email='miaopeiqi@163.com',
+    url='https://github.com/miaopeiqi',
+    #packages=find_packages(),
+    packages=['mpqml'],  #这里是所有代码所在的文件夹名称
+    install_requires=[''],
+)
