fa76fb42029f0cbe83ae153cc202be7ad291d0d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 00:43:27 +0000

    Add package zonesmart-utils/0.0.3/zonesmart-utils-0.0.3.tar.gz

---
+++ packages/zonesmart-utils/zonesmart-utils-0.0.3.tar.gz/zonesmart-utils-0.0.3/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+
+setup(
+    name="zonesmart-utils",
+    version="0.0.3",
+    author="Zonesmart",
+    author_email="kamil@zonesmart.ru",
+    packages=[
+        "zs_utils",
+        "zs_utils.data",
+        "zs_utils.data.enums",
+        "tests",
+    ],
+)
---
+++ packages/zonesmart-utils/zonesmart-utils-0.0.3.tar.gz/zonesmart-utils-0.0.3/tests/settings.py
@@ -0,0 +1,7 @@
+INSTALLED_APPS = (
+    "zs_utils",
+    "tests",
+)
+
+USE_I18N = True
+USE_TZ = True
