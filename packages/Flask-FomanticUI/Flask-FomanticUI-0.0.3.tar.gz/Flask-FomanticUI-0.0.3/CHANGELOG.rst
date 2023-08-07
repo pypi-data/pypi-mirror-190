e3bddeecab4f2836220d7ab6a54aad6fb7ae4c8c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 02:30:56 +0000

    Add package Flask-FomanticUI/0.0.3/Flask-FomanticUI-0.0.3.tar.gz

---
+++ packages/Flask-FomanticUI/Flask-FomanticUI-0.0.3.tar.gz/Flask-FomanticUI-0.0.3/CHANGELOG.rst
@@ -0,0 +1,8 @@
+Changelog
+=========
+
+
+0.0.3
+-----
+
+- Bumped to FomanticUI 2.9.0 #2
---
+++ packages/Flask-FomanticUI/Flask-FomanticUI-0.0.3.tar.gz/Flask-FomanticUI-0.0.3/setup.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# This file is part of the
+#   Flask-FomanticUI Project (https://github.com/juniors90/Flask-FomanticUI/).
+# Copyright (c) 2022, Ferreira Juan David
+# License: MIT
+# Full Text: https://github.com/juniors90/Flask-FomanticUI/blob/master/LICENSE
+
+# =====================================================================
+# DOCS
+# =====================================================================
+
+"""This file is for distribute and install Flask-FomanticUI"""
+
+# ======================================================================
+# IMPORTS
+# ======================================================================
+
+import os
+import pathlib
+
+from setuptools import setup  # noqa
+
+# =============================================================================
+# CONSTANTS
+# =============================================================================
+
+PATH = pathlib.Path(os.path.abspath(os.path.dirname(__file__)))
+
+
+REQUIREMENTS = ["Flask>=2.0.2"]
+
+with open(PATH / "flask_fomanticui" / "__init__.py") as fp:
+    for line in fp.readlines():
+        if line.startswith("__version__ = "):
+            VERSION = line.split("=", 1)[-1].replace('"', "").strip()
+            break
+
+
+with open("README.md") as fp:
+    LONG_DESCRIPTION = fp.read()
+
+
+source = "https://github.com/juniors90/Flask-FomanticUI"
+tracker = "https://github.com/juniors90/Flask-FomanticUI/issues"
+donate = "https://www.paypal.com/donate?hosted_button_id=LFAQ7E7TJ4HSY"
+funding = "https://paypal.me/juniors90"
+
+
+# =============================================================================
+# FUNCTIONS
+# =============================================================================
+
+setup(
+    name="Flask-FomanticUI",
+    version=VERSION,
+    description="Flask extension to allow easy embedding of Fomantic-UI CSS Framework.",  # noqa: E501
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type="text/markdown",
+    author="Ferreira Juan David",
+    author_email="juandavid9a0@gmail.com",
+    url="https://github.com/juniors90/Flask-FomanticUI",
+    packages=["flask_fomanticui"],
+    include_package_data=True,
+    platforms="any",
+    license="The MIT License",
+    install_requires=REQUIREMENTS,
+    keywords=["Fomantic-UI", "Flask", "Framework CSS"],
+    project_urls={
+        "Source": source,
+        "Tracker": tracker,
+        "Donate": donate,
+        "Funding": funding,
+    },
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.8",
+        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+)
