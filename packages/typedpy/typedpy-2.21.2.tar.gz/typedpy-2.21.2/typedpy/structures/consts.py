e61f9ffd2eacd76a735d76930c19ace4d4c0ce07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 02:34:13 +0000

    Add package typedpy/2.21.2/typedpy-2.21.2.tar.gz

---
+++ packages/typedpy/typedpy-2.21.2.tar.gz/typedpy-2.21.2/typedpy/structures/consts.py
@@ -0,0 +1,26 @@
+REQUIRED_FIELDS = "_required"
+DEFAULTS = "_defaults"
+ADDITIONAL_PROPERTIES = "_additional_properties"
+OLD_ADDITIONAL_PROPERTIES = "_additionalProperties"
+IS_IMMUTABLE = "_immutable"
+OPTIONAL_FIELDS = "_optional"
+MUST_BE_UNIQUE = "_must_be_unique"
+IGNORE_NONE_VALUES = "_ignore_none"
+SERIALIZATION_MAPPER = "_serialization_mapper"
+DESERIALIZATION_MAPPER = "_deserialization_mapper"
+ENABLE_UNDEFINED = "_enable_undefined_value"
+DISABLE_PROTECTION = "_disable_protection"
+
+SPECIAL_ATTRIBUTES = {
+    REQUIRED_FIELDS,
+    ADDITIONAL_PROPERTIES,
+    IS_IMMUTABLE,
+    DEFAULTS,
+    OPTIONAL_FIELDS,
+    SERIALIZATION_MAPPER,
+    DESERIALIZATION_MAPPER,
+    IGNORE_NONE_VALUES,
+    ENABLE_UNDEFINED,
+}
+MAX_NUMBER_OF_INSTANCES_TO_VERIFY_UNIQUENESS = 100000
+INDEX = "    "
