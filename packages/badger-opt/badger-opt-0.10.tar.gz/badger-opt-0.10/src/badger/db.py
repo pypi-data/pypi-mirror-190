cdd30dfa150a6d78c388159ffddc514f5df877ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 00:40:03 +0000

    Add package badger-opt/0.10/badger-opt-0.10.tar.gz

---
+++ packages/badger-opt/badger-opt-0.10.tar.gz/badger-opt-0.10/src/badger/db.py
@@ -0,0 +1,236 @@
+import os
+from datetime import datetime
+import logging
+logger = logging.getLogger(__name__)
+import yaml
+import sqlite3
+from .settings import read_value
+
+
+# Check badger database root
+BADGER_DB_ROOT = read_value('BADGER_DB_ROOT')
+if BADGER_DB_ROOT is None:
+    raise Exception('Please set the BADGER_DB_ROOT env var!')
+elif not os.path.exists(BADGER_DB_ROOT):
+    os.makedirs(BADGER_DB_ROOT)
+    logger.info(
+        f'Badger database root {BADGER_DB_ROOT} created')
+
+
+def maybe_create_routines_db(func):
+
+    def func_safe(*args, **kwargs):
+        db_routine = os.path.join(BADGER_DB_ROOT, 'routines.db')
+
+        con = sqlite3.connect(db_routine)
+        cur = con.cursor()
+
+        cur.execute('create table if not exists routine (name not null primary key, config, savedAt timestamp)')
+
+        con.commit()
+        con.close()
+
+        return func(*args, **kwargs)
+
+    return func_safe
+
+
+def maybe_create_runs_db(func):
+
+    def func_safe(*args, **kwargs):
+        db_run = os.path.join(BADGER_DB_ROOT, 'runs.db')
+
+        con = sqlite3.connect(db_run)
+        cur = con.cursor()
+
+        cur.execute('create table if not exists run (id integer primary key, savedAt timestamp, finishedAt timestamp, routine, filename)')
+
+        con.commit()
+        con.close()
+
+        return func(*args, **kwargs)
+
+    return func_safe
+
+
+def filter_routines(records, tags):
+    records_filtered = []
+    for record in records:
+        try:
+            _tags = yaml.safe_load(record[1])['config']['tags']
+            if tags.items() <= _tags.items():
+                records_filtered.append(record)
+        except:
+            pass
+
+    return records_filtered
+
+
+@maybe_create_routines_db
+def save_routine(routine):
+    db_routine = os.path.join(BADGER_DB_ROOT, 'routines.db')
+
+    con = sqlite3.connect(db_routine)
+    cur = con.cursor()
+
+    # Insert a record
+    cur.execute('insert into routine values (?, ?, ?)',
+                (routine['name'], yaml.dump(routine, sort_keys=False), datetime.now()))
+
+    con.commit()
+    con.close()
+
+
+@maybe_create_routines_db
+@maybe_create_runs_db
+def remove_routine(name, remove_runs=False):
+    db_routine = os.path.join(BADGER_DB_ROOT, 'routines.db')
+
+    con = sqlite3.connect(db_routine)
+    cur = con.cursor()
+
+    cur.execute(f'delete from routine where name = "{name}"')
+
+    con.commit()
+    con.close()
+
+    if remove_runs:
+        # Remove all related run records
+        db_run = os.path.join(BADGER_DB_ROOT, 'runs.db')
+
+        con = sqlite3.connect(db_run)
+        cur = con.cursor()
+
+        cur.execute(f'delete from run where routine = "{name}"')
+
+        con.commit()
+        con.close()
+
+
+@maybe_create_routines_db
+def load_routine(name):
+    db_routine = os.path.join(BADGER_DB_ROOT, 'routines.db')
+    con = sqlite3.connect(db_routine)
+    cur = con.cursor()
+
+    cur.execute('select * from routine where name=:name', {'name': name})
+
+    records = cur.fetchall()
+    con.close()
+
+    if len(records) == 1:
+        return yaml.safe_load(records[0][1]), records[0][2]
+    elif len(records) == 0:
+        # logger.warning(f'Routine {name} not found in the database!')
+        return None, None
+    else:
+        raise Exception(
+            f'Multiple routines with name {name} found in the database!')
+
+
+@maybe_create_routines_db
+def list_routine(keyword='', tags={}):
+    db_routine = os.path.join(BADGER_DB_ROOT, 'routines.db')
+    con = sqlite3.connect(db_routine)
+    cur = con.cursor()
+
+    if not tags:
+        cur.execute(f'select name, savedAt from routine where name like "%{keyword}%" order by savedAt desc')
+
+        records = cur.fetchall()
+        names = [record[0] for record in records]
+        timestamps = [record[1] for record in records]
+        con.close()
+    else:
+        cur.execute(f'select name, config, savedAt from routine where name like "%{keyword}%" order by savedAt desc')
+
+        records = cur.fetchall()
+        records = filter_routines(records, tags)
+        names = [record[0] for record in records]
+        timestamps = [record[2] for record in records]
+        con.close()
+
+    return names, timestamps
+
+
+@maybe_create_runs_db
+def save_run(run):
+    db_run = os.path.join(BADGER_DB_ROOT, 'runs.db')
+
+    con = sqlite3.connect(db_run)
+    cur = con.cursor()
+
+    # Insert a record
+    routine_name = run['routine']['name']
+    run_filename = run['filename']
+    timestamps = run['data']['timestamp_raw']
+    time_start = datetime.fromtimestamp(timestamps[0])
+    time_finish = datetime.fromtimestamp(timestamps[-1])
+    cur.execute('insert into run values (?, ?, ?, ?, ?)',
+                (None, time_start, time_finish, routine_name, run_filename))
+
+    rid = cur.lastrowid
+
+    con.commit()
+    con.close()
+
+    return rid
+
+
+@maybe_create_runs_db
+def get_runs_by_routine(routine: str):
+    db_run = os.path.join(BADGER_DB_ROOT, 'runs.db')
+
+    con = sqlite3.connect(db_run)
+    cur = con.cursor()
+
+    cur.execute(f'select filename from run where routine = "{routine}" order by savedAt desc')
+    records = cur.fetchall()
+    con.close()
+
+    filenames = [record[0] for record in records]
+
+    return filenames
+
+
+@maybe_create_runs_db
+def get_runs():
+    db_run = os.path.join(BADGER_DB_ROOT, 'runs.db')
+
+    con = sqlite3.connect(db_run)
+    cur = con.cursor()
+
+    cur.execute(f'select filename from run order by savedAt desc')
+    records = cur.fetchall()
+
+    con.close()
+
+    filenames = [record[0] for record in records]
+
+    return filenames
+
+
+@maybe_create_runs_db
+def remove_run_by_filename(name):
+    db_run = os.path.join(BADGER_DB_ROOT, 'runs.db')
+
+    con = sqlite3.connect(db_run)
+    cur = con.cursor()
+
+    cur.execute(f'delete from run where filename = "{name}"')
+
+    con.commit()
+    con.close()
+
+
+@maybe_create_runs_db
+def remove_run_by_id(rid):
+    db_run = os.path.join(BADGER_DB_ROOT, 'runs.db')
+
+    con = sqlite3.connect(db_run)
+    cur = con.cursor()
+
+    cur.execute(f'delete from run where id = {rid}')
+
+    con.commit()
+    con.close()
---
+++ packages/badger-opt/badger-opt-0.10.tar.gz/badger-opt-0.10/src/badger/gui/default/components/routine_runner.py
@@ -0,0 +1,108 @@
+import logging
+logger = logging.getLogger(__name__)
+import time
+import pandas as pd
+from PyQt5.QtCore import pyqtSignal, QObject, QRunnable
+from ....utils import curr_ts, ts_to_str
+from ....core import run_routine
+
+
+class BadgerRoutineSignals(QObject):
+    env_ready = pyqtSignal(list)
+    finished = pyqtSignal()
+    progress = pyqtSignal(list, list, list, list, float)
+    error = pyqtSignal(Exception)
+    info = pyqtSignal(str)
+
+
+class BadgerRoutineRunner(QRunnable):
+
+    def __init__(self, routine, save, verbose=2, use_full_ts=False):
+        super().__init__()
+
+        # Signals should belong to instance rather than class
+        # Since there could be multiple runners runing in parallel
+        self.signals = BadgerRoutineSignals()
+
+        self.routine = routine
+        self.run_filename = None
+        self.var_names = var_names = [next(iter(d)) for d in routine['config']['variables']]
+        self.obj_names = obj_names = [next(iter(d)) for d in routine['config']['objectives']]
+        if routine['config']['constraints']:
+            self.con_names = con_names = [next(iter(d)) for d in routine['config']['constraints']]
+        else:
+            self.con_names = con_names = []
+        try:
+            self.sta_names = sta_names = routine['config']['states'] or []
+        except KeyError:  # this would happen when rerun an old version routine
+            self.sta_names = sta_names = []
+        self.data = pd.DataFrame(None, columns=['timestamp_raw', 'timestamp'] + obj_names + con_names + var_names + sta_names)
+        self.states = None  # system states to be saved at start of a run
+        self.save = save
+        self.verbose = verbose
+        self.use_full_ts = use_full_ts
+
+        self.is_paused = False
+        self.is_killed = False
+
+    def run(self):
+        error = None
+        try:
+            run_routine(self.routine, True, self.save, self.verbose,
+                        self.before_evaluate, self.after_evaluate,
+                        self.env_ready, self.pf_ready, self.states_ready)
+        except Exception as e:
+            if str(e) != 'Optimization run has been terminated!':
+                logger.exception(e)
+            error = e
+
+        self.signals.finished.emit()
+        if error:
+            if str(error) == 'Optimization run has been terminated!':
+                self.signals.info.emit(str(error))
+                return
+
+            self.signals.error.emit(error)
+
+    def before_evaluate(self, vars):
+        # vars: ndarray
+        while self.is_paused:
+            time.sleep(0)
+            if self.is_killed:
+                raise Exception('Optimization run has been terminated!')
+
+        if self.is_killed:
+            raise Exception('Optimization run has been terminated!')
+
+    def after_evaluate(self, vars, obses, cons, stas):
+        # vars: ndarray
+        # obses: ndarray
+        # cons: ndarray
+        # stas: list
+        ts = curr_ts()
+        self.signals.progress.emit(list(vars), list(obses), list(cons), list(stas), ts.timestamp())
+
+        # Append solution to data
+        fmt = 'lcls-log-full' if self.use_full_ts else 'lcls-log'
+        solution = [ts.timestamp(), ts_to_str(ts, fmt)] + list(obses) + list(cons) + list(vars) + list(stas)
+        self.data = self.data.append(pd.Series(solution, index=self.data.columns), ignore_index=True)
+
+        # take a break to let the outside signal to change the status
+        time.sleep(0.1)
+
+    def env_ready(self, env):
+        self.env = env
+        init_vars = env._get_vars(self.var_names)
+        self.signals.env_ready.emit(init_vars)
+
+    def pf_ready(self, pf):
+        self.pf = pf
+
+    def states_ready(self, states):
+        self.states = states
+
+    def ctrl_routine(self, pause):
+        self.is_paused = pause
+
+    def stop_routine(self):
+        self.is_killed = True
