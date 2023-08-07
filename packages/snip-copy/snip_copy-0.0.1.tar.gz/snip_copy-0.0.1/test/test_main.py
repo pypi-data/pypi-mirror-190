2633da59cb4b7725791bb24b9783296703b98fa1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 00:42:17 +0000

    Add package snip-copy/0.0.1/snip_copy-0.0.1.tar.gz

---
+++ packages/snip-copy/snip_copy-0.0.1.tar.gz/snip_copy-0.0.1/test/test_main.py
@@ -0,0 +1,27 @@
+import pathlib
+import shutil
+import tempfile
+
+import snip_copy
+from snip_copy.__main__ import main
+
+
+def test_main():
+    # build temp directory, load in an example file
+    tmp_dir = pathlib.Path(tempfile.TemporaryDirectory().name)
+    tmp_dir.mkdir()
+    file = pathlib.Path(snip_copy.__file__).parents[1] / 'test' / 'ex_hw' / \
+           'rubric.py'
+    file_copy = tmp_dir / file.name
+    shutil.copyfile(file, file_copy)
+
+    # call "CLI"
+    args = [str(file_copy), '--cmd', '# ?!', '--split', 'c']
+    main(args=args)
+
+    # check that new files are created via CLI
+    assert (tmp_dir / 'rubricsolution.py').exists()
+    assert (tmp_dir / 'rubricstudent.py').exists()
+
+    # cleanup
+    shutil.rmtree(tmp_dir)
