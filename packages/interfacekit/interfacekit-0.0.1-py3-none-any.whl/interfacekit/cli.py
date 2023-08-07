57af1784666515ff53ad851824a959fcd8ae85da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 02:02:11 +0000

    Add package interfacekit/0.0.1/interfacekit-0.0.1-py3-none-any.whl

---
+++ packages/interfacekit/interfacekit-0.0.1-py3-none-any.whl/interfacekit/cli.py
@@ -0,0 +1,12 @@
+# 
+#   InterfaceKit
+#   Copyright © 2023 NatML Inc. All Rights Reserved.
+#
+
+# Define entry
+def main ():
+    print("Hello from InterfaceKit!")
+
+# Run
+if __name__ ==  "__main__":
+    main()
