c37f61285192e5ecb8384abdda586737ebf1e007
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 02:02:08 +0000

    Add package pypebbletimeline/0.1.0/pypebbletimeline-0.1.0-py3-none-any.whl

---
+++ packages/pypebbletimeline/pypebbletimeline-0.1.0-py3-none-any.whl/pypebbletimeline/timeline.py
@@ -0,0 +1,24 @@
+import requests
+
+
+def pushPin(title, body, subtitle, time, token):
+	# valid times are:
+	# 1 - Instant
+	# 2 - In 30 min. (Timeline Subscription)
+	# 3 - In 60 min. 
+	# 4. - In 3 hours (Free)
+	pin = {
+	"time":f"{time}",
+	"layout":{
+		"type": "genericPin",
+		"title": f"{title}",
+		"body": f"{body}",
+		"subtitle": f"{subtitle}",
+		"tinyIcon": "system://images/NOTIFICATION_GENERIC"}
+		,"token": token
+	}
+	r = requests.post('https://willow.systems/pinproxy-ifttt/', data=pin)
+	if r.status_code == 200:
+		return 200
+	else:
+		return f"{r.status_code} {r.text}"
