40d94656e3b1c6911c2cbc3f5a750e550f9bebc9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 02:00:49 +0000

    Add package comm-ddp/0.0.1/comm_ddp-0.0.1.tar.gz

---
+++ packages/comm-ddp/comm_ddp-0.0.1.tar.gz/comm_ddp-0.0.1/setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+
+setuptools.setup(
+    name="comm_ddp",
+    version="0.0.1",
+    author="Chaoyi Zhang",
+    description="communication tools for pytorch-DDP",
+    packages=['comm_ddp']
+)
