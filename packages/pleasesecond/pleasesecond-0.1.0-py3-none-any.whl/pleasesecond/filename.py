9c0c4a32141dd57e1837832d3d87afe9398f6a23
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 00:39:58 +0000

    Add package pleasesecond/0.1.0/pleasesecond-0.1.0-py3-none-any.whl

---
+++ packages/pleasesecond/pleasesecond-0.1.0-py3-none-any.whl/pleasesecond/filename.py
@@ -0,0 +1,2 @@
+def func1(num):
+    print("You gave me " + str(num) + " but that number's stupid, here's " + str(num+1))
---
+++ packages/pleasesecond/pleasesecond-0.1.0-py3-none-any.whl/pleasesecond/subfolder/hidden.py
@@ -0,0 +1,2 @@
+def func2(num):
+    print("You have graciously provided the glorious number " + str(num) + " unfortunately I will have to make the following adjustments: " + str(num*2))
