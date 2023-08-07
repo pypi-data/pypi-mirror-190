697fe9367daa159444d2c72ba76aa5653f7a65b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 02:32:28 +0000

    Add package quadai/0.0.1/quadai-0.0.1.tar.gz

---
+++ packages/quadai/quadai-0.0.1.tar.gz/quadai-0.0.1/setup.py
@@ -0,0 +1,20 @@
+"""Setups the package for installation."""
+
+from setuptools import setup
+
+
+# def get_requirements():
+#     """Load requirements from file."""
+#     requirements_file = open("requirements.txt")
+#     return requirements_file.readlines()
+
+
+setup(
+    # install_requires=get_requirements(),
+    # setup_requires=["setuptools_scm"],
+    # include_package_data=True,
+    name = "quadai",
+    version = "0.0.1",
+    author = "Alexander Sajus",
+    author_email = "AlexanderSajus@gamil.com"
+)
