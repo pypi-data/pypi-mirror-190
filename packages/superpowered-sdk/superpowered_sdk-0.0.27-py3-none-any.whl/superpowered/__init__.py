40df230f098c43c971500e83b1f7dd1333346a30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 02:33:41 +0000

    Add package superpowered-sdk/0.0.27/superpowered_sdk-0.0.27-py3-none-any.whl

---
+++ packages/superpowered-sdk/superpowered_sdk-0.0.27-py3-none-any.whl/superpowered/__init__.py
@@ -0,0 +1,26 @@
+from superpowered.main import init, Incantation, Model, ModelInstance, KnowledgeBase
+from superpowered.main import create_incantation, get_incantation, update_incantation
+from superpowered.main import create_model, get_model, update_model
+from superpowered.main import create_knowledge_base, get_knowledge_base, add_file_to_kb
+from superpowered.main import list_incantations, list_models, list_instances, list_knowledge_bases
+
+__all__ = [
+    "init",
+    "Incantation",
+    "Model",
+    "ModelInstance",
+    "KnowledgeBase",
+    "create_incantation",
+    "get_incantation",
+    "update_incantation",
+    "create_model",
+    "get_model",
+    "update_model",
+    "create_knowledge_base",
+    "get_knowledge_base",
+    "add_file_to_kb",
+    "list_incantations",
+    "list_models",
+    "list_instances",
+    "list_knowledge_bases",
+]
