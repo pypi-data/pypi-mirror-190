3f4649a5e4c0685370b593fa90ea36ebed953c36
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 00:45:00 +0000

    Add package gamut/1.0.8/gamut-1.0.8.tar.gz

---
+++ packages/gamut/gamut-1.0.8.tar.gz/gamut-1.0.8/gamut/config.py
@@ -0,0 +1,33 @@
+from .sys import Console
+
+FILE_EXT = '.gamut'
+AUDIO_FORMATS = ['.wav', '.aif', '.aiff']
+MIME_TYPES = ['audio/x-wav', 'audio/x-aiff']
+CONSOLE = Console()
+ANALYSIS_TYPES = ['timbre', 'pitch']
+ENVELOPE_TYPES = [
+    'barthann',
+    'bartlett',
+    'blackman',
+    'blackmanharris',
+    'bohman',
+    'boxcar',
+    'chebwin',
+    'cosine',
+    'dpss',
+    'exponential',
+    'flattop',
+    'gaussian',
+    'general_cosine',
+    'general_gaussian'
+    'general_hamming'
+    'hamming'
+    'hann'
+    'kaiser_bessel_derived'
+    'lanczos'
+    'nuttall',
+    'parzen',
+    'taylor',
+    'triang',
+    'tukey',
+]
