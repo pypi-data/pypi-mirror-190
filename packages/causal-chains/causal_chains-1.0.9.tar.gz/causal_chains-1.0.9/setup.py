0ec4a44d98318736fa9498ca09b61bfb1f51fcf8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 00:43:56 +0000

    Add package causal-chains/1.0.9/causal_chains-1.0.9.tar.gz

---
+++ packages/causal-chains/causal_chains-1.0.9.tar.gz/causal_chains-1.0.9/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="causal_chains",
+    version="1.0.9",
+    author="Henry Leonardi",
+    author_email="leonardi.henry@gmail.com",
+    description="A package for extracting causal chains from text",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/helliun/causal-chains",
+    packages=setuptools.find_packages(),
+    install_requires = ["hatchling","transformers","sentence-transformers","pydot","tqdm"]
+,
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+)
