708925db7c975ee25f3708a16aa7ea2b5450d4c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 00:42:47 +0000

    Add package scrapyomama/0.0.4/scrapyomama-0.0.4.tar.gz

---
+++ packages/scrapyomama/scrapyomama-0.0.4.tar.gz/scrapyomama-0.0.4/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="scrapyomama",
+    version="0.0.4",
+    author="Thomas Garcia",
+    author_email="garciathomas@gmail.com",
+    description="Scrap Yo Mama",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/mrsoyer/symtest",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
