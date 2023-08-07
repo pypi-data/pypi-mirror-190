18a1cf22a38d132a4be09d9fa1d60b777fc83363
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 00:41:38 +0000

    Add package spotdl-romanized-lyrics/0.0.5/spotdl_romanized_lyrics-0.0.5.tar.gz

---
+++ packages/spotdl-romanized-lyrics/spotdl_romanized_lyrics-0.0.5.tar.gz/spotdl_romanized_lyrics-0.0.5/spotdl_romanized_lyrics/providers/lyrics/__init__.py
@@ -0,0 +1,8 @@
+"""
+Lyrics providers for spotdl_romanized_lyrics.
+"""
+
+from spotdl_romanized_lyrics.providers.lyrics.genius import Genius
+from spotdl_romanized_lyrics.providers.lyrics.musixmatch import MusixMatch
+from spotdl_romanized_lyrics.providers.lyrics.azlyrics import AzLyrics
+from spotdl_romanized_lyrics.providers.lyrics.lyricstranslate import Lyricstranslate
