d531b985941a37a004cddb888b2224aa63e5273b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 00:42:36 +0000

    Add package grgrlib/0.1.22/grgrlib-0.1.22-py3-none-any.whl

---
+++ packages/grgrlib/grgrlib-0.1.22-py3-none-any.whl/grgrlib/__init__.py
@@ -0,0 +1,24 @@
+#!/bin/python2
+# -*- coding: utf-8 -*-
+
+from .econtools import *
+from .generic import *
+from .plots import *
+
+__version__ = '0.1.22'
+
+try:
+    import chaospy
+except ModuleNotFoundError:
+    chaospy = None
+
+if chaospy is not None:
+    from .optimize import *
+
+try:
+    import pandas as pd
+except ModuleNotFoundError:
+    pd = None
+
+if pd is not None:
+    from .datatools import *
